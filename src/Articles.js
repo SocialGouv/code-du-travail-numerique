96250fe21a6a94a309017bfeb44354578b23102d
kemar <marc@marcarea.com>
2018-05-07 15:18:26 +0200

    Sort articles by a custom alphabetical order: L - R - D, then others (Annexe etc.)

--- src/Articles.js
+++ src/Articles.js
@@ -26,6 +26,29 @@ const PreviewArticle = ({ id, title, text }) => (
 );
 
 const PreviewArticles = ({ theme }) => {
+
+  // Sort articles by a custom alphabetical order: L - R - D, then others (Annexe etc.)
+  let articlesL = [];
+  let articlesR = [];
+  let articlesD = [];
+  let articlesOther = [];
+  theme.articles.forEach(function (article) {
+    switch (article[0]) {
+      case 'L':
+        articlesL.push(article);
+        break;
+      case 'R':
+        articlesR.push(article);
+        break;
+      case 'D':
+        articlesD.push(article);
+        break;
+      default:
+        articlesOther.push(article);
+    }
+  });
+  theme.articles = [].concat(articlesL.sort(), articlesR.sort(), articlesD.sort(), articlesOther.sort());
+
   if (theme.articles.length) {
     return <div>{enhanceArticles(theme.articles).map(PreviewArticle)}</div>;
   }
