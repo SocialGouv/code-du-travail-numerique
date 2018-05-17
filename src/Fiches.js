87d53944748aca4efd5408793c4f9162b6785f77
Julien Bouquillon <julien@bouquillon.com>
2018-05-17 12:26:32 +0200

    fix #1

--- src/Fiches.js
+++ src/Fiches.js
@@ -4,10 +4,15 @@ import { ExternalLink } from "react-feather";
 
 import fichesDgt from "./data/fiches";
 
-const isTheme = themeId => entry =>
-  entry.themes && entry.themes.indexOf(themeId) > -1;
+const hasCommonItem = (arr1, arr2) => arr1.some(r => arr2.includes(r));
 
-export const hasFiche = theme => fichesDgt.find(isTheme(theme.id));
+const isTheme = theme => entry =>
+  (entry.themes && entry.themes.indexOf(theme.id) > -1) ||
+  (entry.articles &&
+    theme.articles &&
+    hasCommonItem(entry.articles, theme.articles));
+
+export const hasFiche = theme => fichesDgt.find(isTheme(theme));
 
 const FicheDgt = ({ source, title, href }) => (
   <li key={href}>
