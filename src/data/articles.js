e16e196b56c68ad3ccba07e12d9ce1856d5a40a3
jrduscher <37538697+jrduscher@users.noreply.github.com>
2018-05-17 18:23:30 +0200

    Update articles.js

--- src/data/articles.js
+++ src/data/articles.js
@@ -8,10 +8,21 @@ export default {
     texte:
       "Les dispositions du présent livre sont applicables aux employeurs de droit privé ainsi qu'à leurs salariés.   Elles sont également applicables aux établissements publics à caractère industriel et commercial."
   },
-  "R3141-4": {
-    texte: "blablabla <b>coucou</b>"
+  "L1221-19": {
+    texte: "Le contrat de travail à durée indéterminée peut comporter une période d'essai dont la durée maximale est : 1° Pour les ouvriers et les employés, de deux mois ; 2° Pour les agents de maîtrise et les techniciens, de trois mois ; 3° Pour les cadres, de quatre mois."
   },
-  "L1411-1": {
-    texte: "bblablala <b>hello</b>"
+  "L1221-20": {
+    texte: "La période d'essai permet à l'employeur d'évaluer les compétences du salarié dans son travail, notamment au regard de son expérience, et au salarié d'apprécier si les fonctions occupées lui conviennent. "
+  },
+   "L1221-21": {
+    texte: "La période d'essai peut être renouvelée une fois si un accord de branche étendu le prévoit. Cet accord fixe les conditions et les durées de renouvellement. La durée de la période d'essai, renouvellement compris, ne peut pas dépasser : 1° Quatre mois pour les ouvriers et employés ; 2° Six mois pour les agents de maîtrise et techniciens ; 3° Huit mois pour les cadres."
+  },
+  "L1221-22": {
+    texte: "Les durées des périodes d'essai fixées par les articles L. 1221-19 et L. 1221-21 ont un caractère impératif, à l'exception :de durées plus longues fixées par les accords de branche conclus avant la date de publication de la loi n° 2008-596 du 25 juin 2008 portant modernisation du marché du travail ; de durées plus courtes fixées par des accords collectifs conclus après la date de publication de la loi n° 2008-596 du 25 juin 2008 précitée ;- de durées plus courtes fixées dans la lettre d'engagement ou le contrat de travail."
+  },
+  "L1221-23": {
+    texte: "La période d'essai et la possibilité de la renouveler ne se présument pas. Elles sont expressément stipulées dans la lettre d'engagement ou le contrat de travai"
   }
 };
+
+//
