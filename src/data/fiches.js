adf7fd1f5507e1843091abd190fcb4e50917053f
jrduscher <37538697+jrduscher@users.noreply.github.com>
2018-05-18 16:23:22 +0200

    Update fiches.js

--- src/data/fiches.js
+++ src/data/fiches.js
@@ -657,7 +657,8 @@ export default [
   {
     source: "Ministère du travail",
     href: "http://travail-emploi.gouv.fr/emploi/accompagnement-des-tpe-pme/",
-    title: "Groupement d’employeurs"
+    title: "Groupement d’employeurs",
+    themes:[14600,14700,14800,14500,14400,14900,13300,13400,13500,13600,13700,13800,13900,14000,14100,14200,14300]
   },
   {
     source: "Ministère du travail",
@@ -722,7 +723,8 @@ export default [
     source: "Ministère du travail",
     href:
       "http://travail-emploi.gouv.fr/droit-du-travail/litiges-individuels-et-conflits-collectifs/",
-    title: "Le conseil de prud’hommes"
+    title: "Le conseil de prud’hommes",
+    themes:[6600,6700,6800,6900,7000,7100,7200,7300,7400,7500,7600,7700,7800,7900,8000,8100,8200,8300,8400,8500,8600,8800,8900,9000,9100,9200,9300,9400,95009600,9700,9800,9900]
   },
   {
     source: "Ministère du travail",
@@ -751,14 +753,16 @@ export default [
   {
     source: "Ministère du travail",
     href:
-      "http://travail-emploi.gouv.fr/droit-du-travail/remuneration-et-participation-financiere/remuneration/",
-    title: 'Le chèque emploi-service universel (CESU) "déclaratif"'
+      "http://travail-emploi.gouv.fr/droit-du-travail/remuneration-et-participation-financiere/remuneration/article/le-cheque-emploi-service-universel-cesu-declaratif",
+    title: 'Le chèque emploi-service universel (CESU) "déclaratif"',
+    themes: [12400]
   },
   {
     source: "Ministère du travail",
     href:
-      "http://travail-emploi.gouv.fr/droit-du-travail/remuneration-et-participation-financiere/remuneration/",
-    title: 'Le chèque emploi-service universel (CESU) "préfinancé"'
+      "http://travail-emploi.gouv.fr/droit-du-travail/remuneration-et-participation-financiere/remuneration/article/le-cheque-emploi-service-universel-cesu-prefinance",
+    title: 'Le chèque emploi-service universel (CESU) "préfinancé"',
+    themes: [12400]
   },
   {
     source: "Ministère du travail",
@@ -946,38 +950,43 @@ export default [
     source: "Ministère du travail",
     href:
       "http://travail-emploi.gouv.fr/droit-du-travail/temps-de-travail-et-conges/temps-de-travail/",
-    title: "La journée de solidarité"
+    title: "La journée de solidarité",
+    themes:[]
   },
   {
     source: "Ministère du travail",
     href:
       "http://travail-emploi.gouv.fr/droit-du-travail/temps-de-travail-et-conges/temps-de-travail/",
-    title: "Les conventions de forfait"
+    title: "Les conventions de forfait",
+    themes:[20100]
   },
   {
     source: "Ministère du travail",
     href:
       "http://travail-emploi.gouv.fr/droit-du-travail/temps-de-travail-et-conges/temps-de-travail/article/le-travail-de-nuit",
     title: "Le travail de nuit",
-    themes: [216]
+    themes: [216OO]
   },
   {
     source: "Ministère du travail",
     href:
-      "http://travail-emploi.gouv.fr/droit-du-travail/temps-de-travail-et-conges/temps-de-travail/",
-    title: "Les heures supplémentaires : contreparties"
+      "http://travail-emploi.gouv.fr/droit-du-travail/temps-de-travail-et-conges/temps-de-travail/article/les-heures-supplementaires-contreparties",
+    title: "Les heures supplémentaires : contreparties",
+    themes:[20310, 20330, 20360, 20370]
   },
   {
     source: "Ministère du travail",
     href:
-      "http://travail-emploi.gouv.fr/droit-du-travail/temps-de-travail-et-conges/temps-de-travail/",
-    title: "Les heures supplémentaires : définition et limites"
+      "http://travail-emploi.gouv.fr/droit-du-travail/temps-de-travail-et-conges/temps-de-travail/article/les-heures-supplementaires-definition-et-limites",
+    title: "Les heures supplémentaires : définition et limites",
+    themes:[20310]
   },
   {
     source: "Ministère du travail",
     href:
-      "http://travail-emploi.gouv.fr/droit-du-travail/temps-de-travail-et-conges/temps-de-travail/",
-    title: "La durée légale du travail"
+      "http://travail-emploi.gouv.fr/droit-du-travail/temps-de-travail-et-conges/temps-de-travail/article/la-duree-legale-du-travail",
+    title: "La durée légale du travail",
+    themes:[20300]
   },
   {
     source: "Ministère du travail",
@@ -1025,8 +1034,9 @@ export default [
   {
     source: "Ministère du travail",
     href:
-      "http://travail-emploi.gouv.fr/droit-du-travail/contrats-et-carriere/",
-    title: "La période d’essai"
+      "http://travail-emploi.gouv.fr/droit-du-travail/contrats-et-carriere/article/la-periode-d-essai-100977",
+    title: "La période d’essai",
+    themes:[3700,3710,3720,3730,3740,3750,3760,3770,3780,49900,52100]
   },
   {
     source: "Ministère du travail",
@@ -1074,7 +1084,8 @@ export default [
     source: "Ministère du travail",
     href:
       "http://travail-emploi.gouv.fr/droit-du-travail/contrats-et-carriere/contrats-de-travail/",
-    title: "Le contrat à durée déterminée (CDD)"
+    title: "Le contrat à durée déterminée (CDD)",
+    themes:[49100,49200,49300,49400,49500,49600,49700,49800,49900,50000,50100,50200,50300,50400,50500,50600,50700,50800,50900]
   },
   {
     source: "Ministère du travail",
