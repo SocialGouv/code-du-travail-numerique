7347982713f78d12bf38a1b6b9f60e60931a7c06
jrduscher <37538697+jrduscher@users.noreply.github.com>
2018-05-23 16:03:10 +0200

    ajout d'articles pour la démo

--- src/data/articles.js
+++ src/data/articles.js
@@ -8,6 +8,7 @@ export default {
     texte:
       "Les dispositions du présent livre sont applicables aux employeurs de droit privé ainsi qu'à leurs salariés.   Elles sont également applicables aux établissements publics à caractère industriel et commercial."
   },
+  //période d'essai
   "L1221-19": {
     texte:
       "Le contrat de travail à durée indéterminée peut comporter une période d'essai dont la durée maximale est :<br><br> 1° Pour les ouvriers et les employés, de deux mois ; <br><br>2° Pour les agents de maîtrise et les techniciens, de trois mois ; <br><br>3° Pour les cadres, de quatre mois."
@@ -26,8 +27,36 @@ export default {
   },
   "L1221-23": {
     texte:
-      "La période d'essai et la possibilité de la renouveler ne se présument pas. Elles sont expressément stipulées dans la lettre d'engagement ou le contrat de travai"
-  }
+      "La période d'essai et la possibilité de la renouveler ne se présument pas. Elles sont expressément stipulées dans la lettre d'engagement ou le contrat de travail"
+
+  //durée max quotidienne
+  },
+  "L3121-18": {
+    texte:
+      "La durée quotidienne de travail effectif par salarié ne peut excéder dix heures, sauf :<p>1° En cas de dérogation accordée par l'inspecteur du travail dans des conditions déterminées par décret ;</p><p>2° En cas d'urgence, dans des conditions déterminées par décret ;</p><p>3° Dans les cas prévus à l'article L. 3121-19.</p>"
+  },
+   "L3121-19": {
+    texte:
+      "Une convention ou un accord d'entreprise ou d'établissement ou, à défaut, une convention ou un accord de branche peut prévoir le dépassement de la durée maximale quotidienne de travail effectif, en cas d'activité accrue ou pour des motifs liés à l'organisation de l'entreprise, à condition que ce dépassement n'ait pas pour effet de porter cette durée à plus de douze heures. "
+  
+  },
+   "D3121-4": {
+    texte:
+      "Le dépassement de la durée quotidienne maximale du travail effectif, prévue à l'article L. 3121-18, peut être autorisé dans les cas où un surcroît temporaire d'activité est imposé, notamment pour l'un des motifs suivants :<p>1° Travaux devant être exécutés dans un délai déterminé en raison de leur nature, des charges imposées à l'entreprise ou des engagements contractés par celle-ci ;</p><p>2° Travaux saisonniers ;</p><p>3° Travaux impliquant une activité accrue pendant certains jours de la semaine, du mois ou de l'année.</p>"
+      },
+   "D3121-5": {
+    texte:
+      "La demande de dépassement de la durée quotidienne maximale de travail, accompagnée des justifications utiles et de l'avis du comité social et économique, s'il existe, est adressée par l'employeur à l'inspecteur du travail.<p>L'inspecteur du travail fait connaître sa décision dans un délai de quinze jours à compter de la date de réception de la demande à l'employeur et aux représentants du personnel.</p>"
+      },
+   "D3121-6": {
+    texte:
+      "En cas d'urgence, l'employeur peut dépasser sous sa propre responsabilité, dans les hypothèses envisagées l'article D. 3121-4, à la durée quotidienne maximale du travail.<p>S'il n'a pas encore adressé de demande de dépassement, il présente immédiatement à l'inspecteur du travail une demande de régularisation accompagnée des justifications et avis mentionnés à l'article D. 3121-5 et de toutes explications nécessaires sur les causes ayant nécessité une prolongation de la durée quotidienne du travail sans autorisation préalable.</p><p>S'il se trouve dans l'attente d'une réponse à une demande de dépassement, il informe immédiatement l'inspecteur du travail de l'obligation où il s'est trouvé d'anticiper la décision attendue et en donne les raisons.</p><p>L'inspecteur du travail fait connaître sa décision dans un délai de quinze jours à compter de la date de réception de la demande à l'employeur et aux représentants du personnel.</p>
+"
+      },
+   "D3121-7": {
+    texte:
+      "Les recours hiérarchiques contre les décisions prévues aux articles D. 3121-5 et D. 3121-6 sont formés devant le directeur régional des entreprises, de la concurrence, de la consommation, du travail et de l'emploi dans le délai d'un mois suivant la date à laquelle les intéressés en ont reçu notification."
+
 };
 
 //
