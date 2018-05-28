66ece7557394520e397709eefa30f30c8db1f6f5
kemar <marc@marcarea.com>
2018-05-28 12:49:44 +0200

    Fix article.js syntax

--- src/data/articles.js
+++ src/data/articles.js
@@ -116,10 +116,8 @@ Ces dispositions ne sont pas applicables pendant la période d'essai.`
     texte: `Les dispositions du présent titre sont applicables lorsque le salarié est lié à plusieurs employeurs par des contrats de travail.`
   },
   "L1234-9": {
-    texte: `
+    texte: `Le salarié titulaire d'un contrat de travail à durée indéterminée, licencié alors qu'il compte 8 mois d'ancienneté ininterrompus au service du même employeur, a droit, sauf en cas de faute grave, à une indemnité de licenciement.
 
-Le salarié titulaire d'un contrat de travail à durée indéterminée, licencié alors qu'il compte 8 mois d'ancienneté ininterrompus au service du même employeur, a droit, sauf en cas de faute grave, à une indemnité de licenciement.
-
-Les modalités de calcul de cette indemnité sont fonction de la rémunération brute dont le salarié bénéficiait antérieurement à la rupture du contrat de travail. Ce taux et ces modalités sont déterminés par voie réglementaire.
-`
+Les modalités de calcul de cette indemnité sont fonction de la rémunération brute dont le salarié bénéficiait antérieurement à la rupture du contrat de travail. Ce taux et ces modalités sont déterminés par voie réglementaire.`
+  }
 };
