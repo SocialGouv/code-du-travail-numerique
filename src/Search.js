443f3ba5b2898f7ee1ce4b4566ce3ef558e624a8
kemar <marc@marcarea.com>
2018-06-27 16:59:35 +0200

    Add mode cors to Fecth

--- src/Search.js
+++ src/Search.js
@@ -43,7 +43,7 @@ class Search extends React.Component {
       return this.setState({ data: null });
     }
     this.setState({ pendingXHR: true, error: null }, () => {
-      fetch(API + query)
+      fetch(API + query, {mode: 'cors'})
         .then(response => {
           if (response.ok) {
             return response.json();
