3f71998b9042fa7cbd530778ec582e7bb053a6a7
kemar <marc@marcarea.com>
2018-08-02 17:29:59 +0200

    Use destructuring assignment (ter)

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -16,73 +16,69 @@ const NoResult = ({ data }) => (
 );
 
 const ResultsContainer = ({ data }) => (
-  <div>{data.map(result => <Result key={result["_id"]} data={result} />)}</div>
+  <div>{data.map(result => <Result key={result["_id"]} {...result} />)}</div>
 );
 
-class Result extends React.Component {
-  render() {
-    let data = this.props.data;
+const Result = ({ _id, _source, highlight }) => {
+  let excerpt = "";
+  if (highlight) {
+    let firstHighlightObjectKeyName = Object.keys(highlight)[0];
+    // Use the first `n` available highlights as excerpt.
+    const numExcerpts = 3;
+    excerpt = highlight[firstHighlightObjectKeyName]
+      .slice(0, numExcerpts)
+      .join(" … ");
+  }
 
-    let excerpt = "";
-    if (data.highlight) {
-      let firstHighlightObjectKeyName = Object.keys(data.highlight)[0];
-      // Use the first `n` available highlights as excerpt.
-      const numExcerpts = 3;
-      excerpt = data.highlight[firstHighlightObjectKeyName]
-        .slice(0, numExcerpts)
-        .join(" … ");
-    }
+  let source;
+  if (_source.source === "faq") {
+    source = "Source : FAQ";
+  } else if (_source.source === "code_du_travail") {
+    source = "Source : Legifrance";
+  } else if (_source.source === "fiches_service_public") {
+    source = "Source : Service Public";
+  } else if (_source.source === "fiches_ministere_travail") {
+    source = "Source : Ministère du Travail";
+  } else if (_source.source === "code_bfc") {
+    source = "Source : DIRECCTE Bourgogne-Franche-Comté (Juin 2017)";
+  } else if (_source.source === "conventions_collectives") {
+    source = "Source : Legifrance";
+  }
 
-    let source;
-    if (data._source.source === "faq") {
-      source = "Source : FAQ";
-    } else if (data._source.source === "code_du_travail") {
-      source = "Source : Legifrance";
-    } else if (data._source.source === "fiches_service_public") {
-      source = "Source : Service Public";
-    } else if (data._source.source === "fiches_ministere_travail") {
-      source = "Source : Ministère du Travail";
-    } else if (data._source.source === "code_bfc") {
-      source = "Source : DIRECCTE Bourgogne-Franche-Comté (Juin 2017)";
-    } else if (data._source.source === "conventions_collectives") {
-      source = "Source : Legifrance";
-    }
+  let body = (
+    <article key={_id} className={_source.source}>
+      <header>
+        <h1>{_source.title}</h1>
+      </header>
+      <blockquote
+        className="text-quote"
+        dangerouslySetInnerHTML={{ __html: excerpt }}
+      />
+      <footer>{source}</footer>
+    </article>
+  );
 
-    let body = (
-      <article key={data._id} className={data._source.source}>
-        <header>
-          <h1>{data._source.title}</h1>
-        </header>
-        <blockquote
-          className="text-quote"
-          dangerouslySetInnerHTML={{ __html: excerpt }}
-        />
-        <footer>{source}</footer>
-      </article>
-    );
-
-    // Internal link.
-    if (data._source.source === "faq" || data._source.source === "code_bfc") {
-      return (
-        <Link route="index" params={{ type: "questions", id: data._id }}>
-          <a className="search-results-link">{body}</a>
-        </Link>
-      );
-    }
-
-    // External link.
+  // Internal link.
+  if (_source.source === "faq" || _source.source === "code_bfc") {
     return (
-      <a
-        href={data._source.url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="search-results-link"
-      >
-        {body}
-      </a>
+      <Link route="index" params={{ type: "questions", id: _id }}>
+        <a className="search-results-link">{body}</a>
+      </Link>
     );
   }
-}
+
+  // External link.
+  return (
+    <a
+      href={_source.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="search-results-link"
+    >
+      {body}
+    </a>
+  );
+};
 
 const ResultsWrapper = styled.div`
   margin-top: 20px;
