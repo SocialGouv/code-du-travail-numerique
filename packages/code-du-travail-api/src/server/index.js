702318a95bad555cfe1f09cca3de45ff30118a7c
kemar <marc@marcarea.com>
2018-08-30 14:06:13 +0200

    Set up Elastic APM agent

--- packages/code-du-travail-api/src/server/index.js
+++ packages/code-du-travail-api/src/server/index.js
@@ -1,3 +1,10 @@
+// Set up Elastic APM agent: the agent must be started before any other modules.
+// https://www.elastic.co/guide/en/apm/agent/nodejs/current/koa.html
+require('elastic-apm-node').start({
+  serviceName: 'code-du-travail-api',
+  serverUrl: 'http://code-du-travail-apm-server:8200',
+})
+
 const bodyParser = require('koa-bodyparser')
 const cors = require('@koa/cors')
 const Koa = require('koa')
