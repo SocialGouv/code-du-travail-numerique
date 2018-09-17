c2bbb1e93b88728c99c96088d8bf76d9c3474c64
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-09-17 10:28:27 +0200

    api: search by source+slug and getDocsCount aggregation

--- packages/code-du-travail-api/src/server/data_sources/code_du_travail_numerique.js
+++ packages/code-du-travail-api/src/server/data_sources/code_du_travail_numerique.js
@@ -1,7 +1,7 @@
-const elasticsearchClient = require('../conf/elasticsearch.js')
+const elasticsearchClient = require("../conf/elasticsearch.js");
 
-const elasticsearchIndexName = 'code_du_travail_numerique'
-const elasticsearchTypeName = 'code_du_travail_numerique'
+const elasticsearchIndexName = "code_du_travail_numerique";
+const elasticsearchTypeName = "code_du_travail_numerique";
 
 /**
  * Return documents matching the given query from Elasticsearch.
@@ -10,8 +10,7 @@ const elasticsearchTypeName = 'code_du_travail_numerique'
  * @param {int} size The number of results to return.
  * @returns {Object} An elasticsearch response.
  */
-async function search (query, size) {
-
+async function search(query, size) {
   let elasticsearchQuery = {
     index: elasticsearchIndexName,
     body: {
@@ -19,137 +18,183 @@ async function search (query, size) {
       query: {
         bool: {
           must: [
-          // Fuziness is ignored with multi_match's cross_fields.
-          // https://github.com/elastic/elasticsearch/issues/6866
-          // Put multi_match clause in standby, use an inner bool with 2 should clauses instead.
-          // {
-          //   multi_match: {
-          //     query: query,
-          //     fields: [
-          //       'all_text.french_stemmed',
-          //       'all_text.french_exact',
-          //     ],
-          //     operator: 'and',
-          //     cutoff_frequency: 0.0007,
-          //     type: 'cross_fields',
-          //   },
+            // Fuziness is ignored with multi_match's cross_fields.
+            // https://github.com/elastic/elasticsearch/issues/6866
+            // Put multi_match clause in standby, use an inner bool with 2 should clauses instead.
+            // {
+            //   multi_match: {
+            //     query: query,
+            //     fields: [
+            //       'all_text.french_stemmed',
+            //       'all_text.french_exact',
+            //     ],
+            //     operator: 'and',
+            //     cutoff_frequency: 0.0007,
+            //     type: 'cross_fields',
+            //   },
             {
               bool: {
                 should: [
                   {
                     match: {
-                      'all_text.french_exact': {
+                      "all_text.french_exact": {
                         query: query,
-                        operator: 'and',
+                        operator: "and",
                         cutoff_frequency: 0.0007,
-                        fuzziness: 'AUTO',
-                      },
-                    },
+                        fuzziness: "AUTO"
+                      }
+                    }
                   },
                   {
                     match: {
-                      'all_text.french_stemmed': {
+                      "all_text.french_stemmed": {
                         query: query,
-                        operator: 'and',
+                        operator: "and",
                         cutoff_frequency: 0.0007,
-                        fuzziness: 'AUTO',
-                      },
-                    },
-                  },
-                ],
-              },
-            },
+                        fuzziness: "AUTO"
+                      }
+                    }
+                  }
+                ]
+              }
+            }
           ],
           should: [
             {
               multi_match: {
                 query: query,
-                fields: [
-                  'title.french_stemmed',
-                  'title.french_exact',
-                ],
-                type: 'most_fields',
-                boost: 2000,
-              },
+                fields: ["title.french_stemmed", "title.french_exact"],
+                type: "most_fields",
+                boost: 2000
+              }
             },
             {
               match: {
-                'all_text.shingle': {
+                "all_text.shingle": {
                   query: query,
-                  boost: 1500,
-                },
-              },
+                  boost: 1500
+                }
+              }
             },
             {
               multi_match: {
                 query: query,
-                fields: [
-                  'path.french_stemmed',
-                  'path.french_exact',
-                ],
-                type: 'most_fields',
-                boost: 500,
-              },
+                fields: ["path.french_stemmed", "path.french_exact"],
+                type: "most_fields",
+                boost: 500
+              }
             },
             // Temporarily put "fonction publique" and "agent public" results in a less prominent position.
             {
               query_string: {
-                query: '(title.shingle:"fonction publique") OR (title.shingle:"agent public")',
-                boost: -2000,
-              },
-            },
-          ],
-        },
+                query:
+                  '(title.shingle:"fonction publique") OR (title.shingle:"agent public")',
+                boost: -2000
+              }
+            }
+          ]
+        }
       },
       highlight: {
-        order: 'score',
-        pre_tags: ['<mark>'],
-        post_tags: ['</mark>'],
+        order: "score",
+        pre_tags: ["<mark>"],
+        post_tags: ["</mark>"],
         fragment_size: 40,
         fields: {
-          'title.french_stemmed': {},
-          'title.french_exact': {},
-          'all_text.french_stemmed': {},
-          'all_text.french_exact': {},
-          'all_text.shingle': {},
-          'path.french_stemmed': {},
-          'path.french_exact': {},
-        },
-      },
-    },
-  }
+          "title.french_stemmed": {},
+          "title.french_exact": {},
+          "all_text.french_stemmed": {},
+          "all_text.french_exact": {},
+          "all_text.shingle": {},
+          "path.french_stemmed": {},
+          "path.french_exact": {}
+        }
+      }
+    }
+  };
 
   try {
-    return await elasticsearchClient.search(elasticsearchQuery)
+    return await elasticsearchClient.search(elasticsearchQuery);
   } catch (error) {
-    console.trace(error.message)
+    console.trace(error.message);
   }
-
 }
 
 /**
- * Get a single JSON document from the index based on its id.
+ * Get a single JSON document from the index based on its id or source+slug
  *
  * @param {string} id The item ID to fetch.
+ * @param {source} id The item source to fetch.
+ * @param {slug} id The item slug to fetch.
  * @returns {Object} An elasticsearch response.
  */
-async function getSingleItem (id) {
-
-  let elasticsearchQuery = {
-    id: id,
-    index: elasticsearchIndexName,
-    type: elasticsearchTypeName,
-  }
-
+async function getSingleItem(params) {
   try {
-    return await elasticsearchClient.get(elasticsearchQuery)
-  } catch (error) {
-    console.trace(error.message)
-  }
+    const { id, source, slug } = params;
+    if (id) {
+      return await elasticsearchClient.get({
+        index: elasticsearchIndexName,
+        type: elasticsearchTypeName,
+        id
+      });
+    }
+    if (source && slug) {
+      return await elasticsearchClient
+        .search({
+          index: elasticsearchIndexName,
+          type: elasticsearchTypeName,
+          body: {
+            size: 1,
+            query: {
+              bool: {
+                must: {
+                  match: { source }
+                },
 
+                filter: { term: { slug } }
+              }
+            }
+          }
+        })
+        .then(res => (res.hits.total && res.hits.hits[0]) || null);
+    }
+  } catch (error) {
+    console.trace(error.message);
+  }
+}
+
+/**
+ * Return number of indexed documents by source
+ *
+ * @returns {Object} An elasticsearch response.
+ */
+async function getDocsCount() {
+  try {
+    return await elasticsearchClient
+      .search({
+        index: elasticsearchIndexName,
+        type: elasticsearchTypeName,
+        body: {
+          size: 0,
+          aggs: {
+            sources: {
+              terms: { field: "source" }
+            }
+          }
+        }
+      })
+      .then(res => {
+        console.log(res.aggregations);
+        return res.aggregations;
+      });
+    //(res.hits.total && res.hits.hits[0]) || null);
+  } catch (error) {
+    console.trace(error.message);
+  }
 }
 
 module.exports = {
   getSingleItem,
-  search,
-}
+  getDocsCount,
+  search
+};
