43d86c53cce04b6d6099c95c1bf1dd2166592dc4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-09-17 18:16:43 +0200

    feat(fiches-service-public): add server routing

--- routes.js
+++ routes.js
@@ -7,6 +7,11 @@ module.exports = routes()
   // - http://localhost:3000/questions/Zm5o72QB0wLMRXWgrAhM
 
   .add({ name: "question", page: "question", pattern: "/questions/:slug" })
+  .add({
+    name: "fiche-service-public",
+    page: "fiche-service-public",
+    pattern: "/fiche-service-public/:slug"
+  })
 
   .add({ name: "theme", page: "theme", pattern: "/themes/:slug+" }) // slug is an array of slugs
   .add({ name: "themes", page: "theme", pattern: "/themes" })
