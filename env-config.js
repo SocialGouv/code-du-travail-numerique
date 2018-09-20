46bcfed0ae6f35f4483d5d28d604ac1ff5fa8942
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-09-20 09:34:22 +0200

    chore: simpler use of process.env

--- env-config.js
+++ env-config.js
@@ -1,21 +1,4 @@
-const prod = process.env.NODE_ENV === "production";
-
-const env = {
+module.exports = {
   "process.env.NODE_ENV": process.env.NODE_ENV,
-  "process.env.API_URL":
-    process.env.API_URL ||
-    (prod
-      ? "https://cdtn-api.num.social.gouv.fr/api/v1"
-      : "http://localhost:1337/api/v1")
+  "process.env.API_URL": process.env.API_URL
 };
-
-console.log(`
-
-  Environment:
-
-    - process.env.NODE_ENV : ${process.env.NODE_ENV}
-    - process.env.API_URL : ${process.env.API_URL}
-
-`);
-
-module.exports = env;
