39a04496abae7b0601897b47f94fcbcbdd0aaa6a
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-09-24 11:06:15 +0200

    fix(fiche-sp): mise en forme for #96

--- pages/fiche-service-public.js
+++ pages/fiche-service-public.js
@@ -3,6 +3,8 @@ import { withRouter } from "next/router";
 import Head from "next/head";
 import fetch from "isomorphic-unfetch";
 import { Container, Alert, Article } from "@socialgouv/code-du-travail-ui";
+import styled from "styled-components";
+import { ExternalLink } from "react-feather";
 
 import SeeAlso from "../src/common/SeeAlso";
 import FeedbackForm from "../src/common/FeedbackForm";
@@ -13,6 +15,30 @@ const BigError = ({ children }) => (
   <Container style={{ fontSize: "2em", textAlign: "center", margin: "20%" }}>
     <Alert warning>{children}</Alert>
   </Container>
+);
+
+const ServicePublicCss = styled.div`
+  .sp__Titre {
+    font-size: 1.5rem;
+  }
+`;
+
+const Source = ({ name, url }) => (
+  <div
+    style={{
+      background: "var(--color-light-background)",
+      padding: 10,
+      marginTop: 50
+    }}
+  >
+    <a href={url} target="_blank">
+      Voir le contenu original sur : {name}{" "}
+      <ExternalLink
+        style={{ verticalAlign: "middle", margin: "0 5px" }}
+        size={16}
+      />
+    </a>
+  </div>
 );
 
 class Fiche extends React.Component {
@@ -42,9 +68,11 @@ class Fiche extends React.Component {
         {!data && <BigError>Cette fiche n'a pas été trouvée</BigError>}
         {data && (
           <Article title={data._source.title}>
-            <Html>{data._source.text}</Html>
+            <ServicePublicCss>
+              <Html>{data._source.html}</Html>
+            </ServicePublicCss>
+            <Source name="service-public.fr" url={data._source.url} />
           </Article>
-        )}
         )}
         <SeeAlso />
         <FeedbackForm query="" />
