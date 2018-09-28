610575b3fbbe4b190a0b2162feb87b61c18361b3
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-09-28 17:14:16 +0200

    fix: put disclaimer in FAQ+Fiches answers only fix #114

--- pages/question.js
+++ pages/question.js
@@ -53,6 +53,19 @@ class Question extends React.Component {
         {!data && <BigError>Cette question n'a pas été trouvée</BigError>}
         {data && (
           <Article title={data._source.title} footer="FAQ">
+            <Alert warning>
+              Important ! Ce site est en cours de construction : les données qui
+              s'y trouvent peuvent être erronées ou imprécises.
+              <br />
+              <br />
+              <a
+                target="_blank"
+                href="https://www.legifrance.gouv.fr/affichTexteArticle.do;jsessionid=AE9DCF75DDCF0465784CEE0E7D62729F.tplgfr37s_2?idArticle=JORFARTI000035607420&cidTexte=JORFTEXT000035607388&dateTexte=29990101&categorieLien=id"
+              >
+                L'ouverture officielle du site
+              </a>{" "}
+              est prévue pour 2020.
+            </Alert>
             <Html>{data._source.html}</Html>
             <Source name="Services de renseignements des DIRECCTE" />
           </Article>
