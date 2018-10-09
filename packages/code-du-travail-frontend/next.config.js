af37ea2764407828e8267e9f38f0ee3bbbef137b
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-10-09 10:13:18 +0200

    fix(build): dont embed static files

--- packages/code-du-travail-frontend/next.config.js
+++ packages/code-du-travail-frontend/next.config.js
@@ -7,6 +7,17 @@ module.exports = withImages(
   withCSS({
     // use routes.js
     useFileSystemPublicRoutes: false,
-    poweredByHeader: false
+    poweredByHeader: false,
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(png|woff|woff2|eot|ttf|svg|gif|jpg)$/,
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/",
+          outputPath: "static/"
+        }
+      });
+      return config;
+    }
   })
 );
