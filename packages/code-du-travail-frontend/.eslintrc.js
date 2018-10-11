72218d469596192e3bdee40773b08ea330ed24f5
Douglas Duteil <douglasduteil@gmail.com>
2018-10-11 12:42:17 +0200

    chore(frontend): add react version to remove eslint warning
    ```
    Warning: React version not specified in eslint-plugin-react settings. See https://github.com/yannickcr/eslint-plugin-react#configuration.
    ```

--- packages/code-du-travail-frontend/.eslintrc.js
+++ packages/code-du-travail-frontend/.eslintrc.js
@@ -39,5 +39,10 @@ module.exports = {
     "no-console": "warn",
     "no-unused-vars": "warn",
     "react/display-name": "off"
+  },
+  settings: {
+    react: {
+      version: "16"
+    }
   }
 };
