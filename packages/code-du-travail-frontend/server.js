ea229e55fabfbc71e49f94024e8a0ccff0e498c6
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-10-11 12:16:48 +0200

    fix(docker): restore docker setup

--- packages/code-du-travail-frontend/server.js
+++ packages/code-du-travail-frontend/server.js
@@ -13,6 +13,8 @@ const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 const API_URL = process.env.API_URL || "http://127.0.0.1:1337/api/v1";
 const SENTRY_PUBLIC_DSN = process.env.SENTRY_PUBLIC_DSN;
+const PIWIK_URL = process.env.PIWIK_URL;
+const PIWIK_SITE_ID = process.env.PIWIK_SITE_ID;
 
 app.prepare().then(() => {
   express()
@@ -28,6 +30,8 @@ app.prepare().then(() => {
     - process.env.NODE_ENV : ${NODE_ENV}
     - process.env.API_URL : ${API_URL}
     - process.env.SENTRY_PUBLIC_DSN : ${SENTRY_PUBLIC_DSN}
+    - process.env.PIWIK_URL : ${PIWIK_URL}
+    - process.env.PIWIK_SITE_ID : ${PIWIK_SITE_ID}
 
 `);
     });
