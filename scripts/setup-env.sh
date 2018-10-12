94351890e31c068e11d897dd4534443a4133bb00
Douglas Duteil <douglasduteil@gmail.com>
2018-10-12 15:47:58 +0200

    chore: setup env on post install (#165)

---
+++ scripts/setup-env.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -exu pipefail
+
+[[ -f .env ]] || {
+  # No `.env` file
+  # Create one from the `.env.sample` file
+  cp .env.sample .env
+}
+
+[[ -f docker-compose.override.yml ]] || {
+  # No `docker-compose.override.yml` file
+  # Create one from the `docker-compose.override.dev.yml` or `docker-compose.override.prod.yml` file
+  # Depending if the current NODE_ENV is "production" or else.
+  [[ ${NODE_ENV:-development} = production ]] && _ENV=prod || _ENV=dev
+  cp docker-compose.override.${_ENV}.yml docker-compose.override.yml
+  unset _ENV
+}
