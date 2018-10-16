b2cfb511eb79cd6c196d62ddfa1b46ced8f7d716
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-10-16 10:23:05 +0200

    fix(ui): backport ghost commits

--- packages/code-du-travail-ui/src/Article.js
+++ packages/code-du-travail-ui/src/Article.js
@@ -1,41 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
 
-import { ThumbsUp, ThumbsDown, MessageSquare } from "react-feather";
 import { Section, Tag } from ".";
 
-const RateContainer = styled.div`
-  flex: 0 0 130px;
-  border: 1px solid silver;
-  padding: 10px;
-  border-radius: 3px;
-  background: #eee;
-  text-align: center;
-  svg {
-    vertical-align: middle;
-    stroke: #aaa;
-    cursor: pointer;
-  }
-  svg:hover {
-    stroke: white;
-  }
-`;
+import { Question, Like, Unlike } from "./icons";
 
-const ArticleRate = ({ onValidate, onInvalidate }) => (
-  <RateContainer>
-    <ThumbsUp
-      onClick={onValidate}
-      size="36"
-      style={{ marginRight: 20 }}
-      title="Valider cette réponse"
-    />
-    <ThumbsDown
-      onClick={onInvalidate}
-      size="36"
-      title="Invalider cette réponse"
-    />
-  </RateContainer>
+const Tags = ({ tags, onTagClick }) => (
+  <React.Fragment>
+    {tags.map(tag => (
+      <Tag
+        key={tag}
+        style={{ cursor: "pointer" }}
+        onClick={() => onTagClick(tag)}
+      >
+        {tag}
+      </Tag>
+    ))}
+  </React.Fragment>
 );
 
 const Article = ({
@@ -48,39 +29,38 @@ const Article = ({
   onInvalidate,
   children
 }) => (
-  <Section light>
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        width: "calc(100% + 40px)",
-        alignItems: "flex-start",
-        marginTop: 20
-      }}
-    >
-      <div style={{ flex: "0 0 100px", textAlign: "center" }}>
-        <MessageSquare color="#aaa" size="45" style={{ marginTop: 10 }} />
+  <Section light className={className} style={style}>
+    <div className="article">
+      <div className="article__icon">
+        <Question fill="#aaa" />
       </div>
-      <div style={{ flex: "1 0 auto", flexShrink: 1, paddingRight: 20 }}>
-        <h2 style={{ fontSize: "2.5rem", lineHeight: "3rem" }}>{title}</h2>
-        <div style={{ marginLeft: -10 }}>
-          {tags.map(tag => (
-            <Tag
-              key={tag}
-              style={{ cursor: "pointer" }}
-              onClick={() => onTagClick(tag)}
-            >
-              {tag}
-            </Tag>
-          ))}
+      <div className="article__header">
+        <div className="article__title">{title}</div>
+        <div className="article__tags">
+          <Tags tags={tags} onTagClick={onTagClick} />
         </div>
       </div>
-      <ArticleRate onValidate={onValidate} onInvalidate={onInvalidate} />
+      <div className="article__widget_container" style={{}}>
+        <Like
+          onClick={onValidate}
+          style={{
+            marginRight: 10,
+            width: 30,
+            height: 30,
+            verticalAlign: "top"
+          }}
+          title="Valider cette réponse"
+          fill="#aaa"
+        />
+        <Unlike
+          onClick={onInvalidate}
+          style={{ marginTop: 10, width: 30, height: 30, verticalAlign: "top" }}
+          title="Invalider cette réponse"
+          fill="#aaa"
+        />
+      </div>
     </div>
-
-    <div style={{ margin: "20px 130px 20px 100px", textAlign: "justify" }}>
-      {children}
-    </div>
+    <div className="article__content">{children}</div>
   </Section>
 );
 
