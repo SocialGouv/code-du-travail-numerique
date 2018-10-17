371f447558a265c6e5f54ebb9be0ffc8345b67f8
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-10-17 12:52:39 +0200

    feat(search): ajout conventions collectives fix #99 :
    - ajout de la liste KALI et IDCC des conventions collectives dans ElasticSearch
    - creation de pages vides avec redirection pour les conventions

---
+++ pages/idcc.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { withRouter } from "next/router";
+import fetch from "isomorphic-unfetch";
+import { Button } from "@socialgouv/code-du-travail-ui";
+import { Download } from "react-feather";
+import Answer from "../src/search/Answer";
+
+// a FAQ answer
+
+const fetchIdcc = ({ slug }) =>
+  fetch(`${process.env.API_URL}/items/idcc/${slug}`)
+    .then(r => r.json())
+    .then(data => ({
+      data
+    }))
+    .catch(e => {
+      console.log("e", e);
+      throw e;
+    });
+
+class Idcc extends React.Component {
+  static async getInitialProps({ res, query }) {
+    return await fetchIdcc(query);
+  }
+  render() {
+    const { data } = this.props;
+    return (
+      <Answer
+        title={data._source.title}
+        emptyMessage="Cette convention collective n'a pas été trouvée"
+        footer="Informations fournies par la DILA"
+      >
+        <p>
+          Cliquez sur le lien ci dessous pour accéder à la convention collective
+          sur LegiFrance :
+        </p>
+        <a
+          target="_blank"
+          href={`https://www.legifrance.gouv.fr/rechConvColl.do?&champIDCC=${
+            data._source.id
+          }`}
+        >
+          <Button primary>
+            <Download style={{ verticalAlign: "middle", marginRight: 10 }} />
+            {data._source.title}
+          </Button>{" "}
+        </a>
+      </Answer>
+    );
+  }
+}
+
+export default withRouter(Idcc);
