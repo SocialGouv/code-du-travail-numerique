8dcbef592f466225fbb2e3cad7a03256747c0f7a
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-10-17 11:37:53 +0200

    chore: use @socialgouv/code-du-travail-css

--- packages/code-du-travail-frontend/next.config.js
+++ packages/code-du-travail-frontend/next.config.js
@@ -1,10 +1,11 @@
 const withCSS = require("@zeit/next-css");
-//const withImages = require("next-images");
+const withImages = require("next-images");
 
 require("dotenv").config();
 
-module.exports = withCSS({
-  // use routes.js
-  useFileSystemPublicRoutes: false,
-  poweredByHeader: false
-});
+module.exports = withImages(
+  withCSS({
+    // https://github.com/zeit/next.js/#disabling-file-system-routing
+    useFileSystemPublicRoutes: false
+  })
+);
