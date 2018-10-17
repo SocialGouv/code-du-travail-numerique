30d3d04e744dee14b0eba4067a7834243c43e8c7
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-10-17 09:55:14 +0200

    feat(piwik): add piwik tracker fix #113

---
+++ packages/code-du-travail-frontend/src/piwik.js
@@ -0,0 +1,24 @@
+import ReactPiwik from "react-piwik";
+import { Router } from "../routes";
+
+const onRouteChangeComplete = url => {
+  ReactPiwik.push(["trackPageView"]);
+};
+
+let piwik;
+
+if (
+  typeof window !== "undefined" &&
+  process.env.PIWIK_URL &&
+  process.env.SITE_ID
+) {
+  piwik = new ReactPiwik({
+    url: process.env.PIWIK_URL,
+    siteId: process.env.PIWIK_SITE_ID,
+    trackErrors: true
+  });
+
+  Router.events.on("routeChangeComplete", onRouteChangeComplete);
+}
+
+export default ReactPiwik;
