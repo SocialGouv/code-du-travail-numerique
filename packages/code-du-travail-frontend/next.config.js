653e7b89bf520aa48ba0e1605cd68d1e9fb1f798
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-10-17 11:37:53 +0200

    chore: use @socialgouv/code-du-travail-css and file-loader for images+fonts

--- packages/code-du-travail-frontend/next.config.js
+++ packages/code-du-travail-frontend/next.config.js
@@ -1,23 +1,19 @@
 const withCSS = require("@zeit/next-css");
-const withImages = require("next-images");
 
 require("dotenv").config();
 
-module.exports = withImages(
-  withCSS({
-    // use routes.js
-    useFileSystemPublicRoutes: false,
-    poweredByHeader: false,
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.(png|woff|woff2|eot|ttf|svg|gif|jpg)$/,
-        loader: "file-loader",
-        options: {
-          publicPath: "/_next/static/",
-          outputPath: "static/"
-        }
-      });
-      return config;
-    }
-  })
-);
+module.exports = withCSS({
+  // https://github.com/zeit/next.js/#disabling-file-system-routing
+  useFileSystemPublicRoutes: false,
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(png|woff|woff2|eot|ttf|svg|gif|jpg)$/,
+      loader: "file-loader",
+      options: {
+        publicPath: "/_next/static/",
+        outputPath: "static/"
+      }
+    });
+    return config;
+  }
+});
