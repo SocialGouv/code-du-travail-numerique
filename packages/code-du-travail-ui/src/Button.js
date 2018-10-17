abf4c7732957a117bd2eaa1112c3b4acfb74bca0
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-10-17 12:02:50 +0200

    fix(ui): backport ghost commits

--- packages/code-du-travail-ui/src/Button.js
+++ packages/code-du-travail-ui/src/Button.js
@@ -1,51 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const flavors = [
-  "primary",
-  "secondary",
-  "warning",
-  "success",
-  "info",
-  "danger"
-];
-
-// return the 1st truthy prop that is one of an Button versions
-const getFlavor = props => {
-  const versionProps = Object.keys(props).filter(
-    // ensure value is truthy
-    flavor => flavors.indexOf(flavor) > -1 && !!props[flavor]
-  );
-  return (versionProps.length && versionProps[0]) || "";
-};
+import {
+  getFlavor,
+  cleanProps,
+  propTypes as flavorsPropTypes,
+  defaultProps as flavorsDefaultProps
+} from "./flavors";
 
 const Button = props => (
-  <div className={`btn btn__${getFlavor(props)}`} {...props} />
+  <div className={`btn btn__${getFlavor(props)}`} {...cleanProps(props)} />
 );
 
 Button.propTypes = {
-  /** use .btn.btn__primary */
-  primary: PropTypes.bool,
-  /** use .btn.btn__secondary */
-  secondary: PropTypes.bool,
-  /** use .btn.btn__warning */
-  warning: PropTypes.bool,
-  /** use .btn.btn__success */
-  success: PropTypes.bool,
-  /** use .btn.btn__info */
-  info: PropTypes.bool,
-  /** use .btn.btn__danger */
-  danger: PropTypes.bool,
+  ...flavorsPropTypes,
   style: PropTypes.object
 };
 
 Button.defaultProps = {
-  primary: false,
-  secondary: false,
-  warning: false,
-  success: false,
-  info: false,
-  danger: false
+  ...flavorsDefaultProps
 };
 
 export default Button;
--- packages/code-du-travail-ui/src/Tag.js
+++ packages/code-du-travail-ui/src/Tag.js
@@ -1,57 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const flavors = [
-  "primary",
-  "secondary",
-  "warning",
-  "success",
-  "info",
-  "danger"
-];
-
-// return the 1st truthy prop that is one of a Tag versions
-const getFlavor = props => {
-  const versionProps = Object.keys(props).filter(
-    // ensure value is truthy
-    flavor => flavors.indexOf(flavor) > -1 && !!props[flavor]
-  );
-  return (versionProps.length && versionProps[0]) || "";
-};
+import {
+  getFlavor,
+  cleanProps,
+  propTypes as flavorsPropTypes,
+  defaultProps as flavorsDefaultProps
+} from "./flavors";
 
 const Tag = props => (
-  <div className={`tag tag__${getFlavor(props)}`} {...props} />
+  <div className={`tag tag__${getFlavor(props)}`} {...cleanProps(props)} />
 );
 
 Tag.propTypes = {
+  ...flavorsPropTypes,
   children: PropTypes.element.isRequired
 };
 
-Tag.defaultProps = {};
-
-Tag.propTypes = {
-  /** use .btn.btn__primary */
-  primary: PropTypes.bool,
-  /** use .btn.btn__secondary */
-  secondary: PropTypes.bool,
-  /** use .btn.btn__warning */
-  warning: PropTypes.bool,
-  /** use .btn.btn__success */
-  success: PropTypes.bool,
-  /** use .btn.btnrt__info */
-  info: PropTypes.bool,
-  /** use .btn.btn__danger */
-  danger: PropTypes.bool,
-  style: PropTypes.object
-};
-
 Tag.defaultProps = {
-  primary: false,
-  secondary: false,
-  warning: false,
-  success: false,
-  info: false,
-  danger: false
+  ...flavorsDefaultProps
 };
 
 export default Tag;
