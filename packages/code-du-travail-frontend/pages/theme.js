bc936f33b02441fbe09cecc0eceaacb9111c1402
LionelB <lionel@lumographe.fr>
2018-11-13 11:28:27 +0100

    feat: add search faceting

--- packages/code-du-travail-frontend/pages/theme.js
+++ packages/code-du-travail-frontend/pages/theme.js
@@ -122,7 +122,9 @@ class Theme extends React.Component {
             )) ||
             null}
           {theme &&
-            theme.type !== "root" && <SearchQuery query={theme.title} excludeSources={["themes"]} />}
+            theme.type !== "root" && (
+              <SearchQuery query={theme.title} excludeSources="themes" />
+            )}
         </Container>
       </React.Fragment>
     );
