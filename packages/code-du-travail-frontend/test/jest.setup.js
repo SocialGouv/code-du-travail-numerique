292d3a58c2d1c95aa120ed8e2a902f23a7e32360
LionelB <lionel@lumographe.fr>
2018-11-28 18:49:35 +0100

    test(frontend): add global mock for next/config

--- packages/code-du-travail-frontend/test/jest.setup.js
+++ packages/code-du-travail-frontend/test/jest.setup.js
@@ -3,6 +3,12 @@ import "react-testing-library/cleanup-after-each";
 
 global.fetch = jest.fn();
 
+jest.mock("next/config", () => () => ({
+  publicRuntimeConfig: {
+    API_URL: "api.url"
+  }
+}));
+
 // trick to prevent @reach-modal warning if styles are not imported
 // jsdom doesn"t support it for now @see https://github.com/jsdom/jsdom/issues/1895
 document.documentElement.style.setProperty("--reach-modal", "1");
