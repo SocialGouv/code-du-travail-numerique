d37f744ee1d3dd2de1d04bfdb0dc36871e946544
LionelB <lionel@lumographe.fr>
2018-12-04 12:20:40 +0100

    add searchAnnuaireMethod

--- packages/code-du-travail-frontend/src/annuaire/adresse.service.js
+++ packages/code-du-travail-frontend/src/annuaire/adresse.service.js
@@ -1,9 +1,14 @@
+import getConfig from "next/config";
 import memoizee from "memoizee";
+
+const {
+  publicRuntimeConfig: { API_URL }
+} = getConfig();
 
 function search(query) {
   const url = `https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(
     query
-  )}&type=housenumber`;
+  )}&type=housenumber&limit=5`;
   return fetch(url).then(response => {
     if (response.ok) {
       return response.json().then(results => results.features);
@@ -12,6 +17,39 @@ function search(query) {
   });
 }
 
+function searchAnnuaireByQuery(query) {
+  const url = `${API_URL}/annuaire/search?q=${encodeURIComponent(query)}`;
+  return fetch(url).then(response => {
+    if (response.ok) {
+      return response.json().then(results => results.hits.hits);
+    }
+    throw new Error(
+      "Un problème est survenu lors de la requete des fiches annuaires"
+    );
+  });
+}
+
+function searchAnnuaireByCoord(coord) {
+  const { lat, lon } = coord;
+  const url = `${API_URL}/annuaire/search?coord=${lon}:${lat}`;
+  return fetch(url).then(response => {
+    if (response.ok) {
+      return response.json().then(results => results.hits.hits);
+    }
+    throw new Error(
+      "Un problème est survenu lors de la requete des fiches annuaires"
+    );
+  });
+}
+
+function searchAnnuaire() {}
+
 const searchAddressMemo = memoizee(query => search(query), { promise: true });
 
-export { searchAddressMemo as searchAddress, search as _search };
+export {
+  searchAddressMemo as searchAddress,
+  search as _search,
+  searchAnnuaireByQuery,
+  searchAnnuaireByCoord,
+  searchAnnuaire
+};
