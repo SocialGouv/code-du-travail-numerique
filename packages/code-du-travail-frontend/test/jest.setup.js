208ae8d09743647f2c50d05a52f12492ded7bfbb
LionelB <lionel@lumographe.fr>
2018-12-05 17:32:08 +0100

    add API_ADDRESS to mock

--- packages/code-du-travail-frontend/test/jest.setup.js
+++ packages/code-du-travail-frontend/test/jest.setup.js
@@ -1,16 +1,18 @@
 // this is basically: afterEach(cleanup)
 import "react-testing-library/cleanup-after-each";
 
-global.fetch = jest.fn();
-
 jest.mock("next/config", () => () => ({
   publicRuntimeConfig: {
-    API_URL: "api.url"
+    API_URL: "api.url",
+    API_ADDRESS: "addresse-api.data"
   }
 }));
 jest.mock("react-piwik", () => ({
   push: jest.fn()
 }));
+
+global.fetch = jest.fn();
+
 // trick to prevent @reach-modal warning if styles are not imported
 // jsdom doesn"t support it for now @see https://github.com/jsdom/jsdom/issues/1895
 document.documentElement.style.setProperty("--reach-modal", "1");
