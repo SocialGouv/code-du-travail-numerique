d464516acea152067ec094450fd02fce37a4b8ea
LionelB <lionel@lumographe.fr>
2018-12-05 18:06:24 +0100

    fix lint

--- packages/code-du-travail-api/src/server/index.js
+++ packages/code-du-travail-api/src/server/index.js
@@ -32,7 +32,6 @@ app.use(async (ctx, next) => {
   try {
     await next();
   } catch (err) {
-    console.error(err.status);
     ctx.status = err.status || 500;
     ctx.body = err.message;
     ctx.app.emit("error", err, ctx);
