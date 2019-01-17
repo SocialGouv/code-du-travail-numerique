ee3dd63b2ec6c204973b91513a06c37e0647e47a
LionelB <lionel@lumographe.fr>
2019-01-17 11:53:56 +0100

    refactor: add a new elastic text analyzer

--- packages/code-du-travail-data/search/indexing/analysis.py
+++ packages/code-du-travail-data/search/indexing/analysis.py
@@ -4,12 +4,14 @@ from search.extraction.synonyms.data import SYNONYMS
 filters = {
     # Normalize acronyms so that no matter the format, the resulting token will be the same.
     # E.g.: SmiC => S.M.I.C. => SMIC => smic.
-    'acronyms': {
+    'french_acronyms': {
         'type': 'word_delimiter',
         'catenate_all': True,
         'generate_word_parts': False,
         'generate_number_parts': False,
     },
+    # Remove elision (l'avion => avion)
+    # ne prend pas en compte la casse (L'avion = l'avion = avion)
     'french_elision': {
         'type': 'elision',
         'articles_case': True,
@@ -34,6 +36,15 @@ filters = {
             'quelqu',
         ],
     },
+    # liste de termes et leurs synonymes
+    'french_synonyms': {
+        'type': 'synonym',
+        'ignore_case': True,
+        'expand': True,
+        'synonyms': SYNONYMS,
+    },
+    # Il existe 3 stemmer pour le francais french, light_french, minimal_french
+    # light french et le median
     'french_stemmer': {
         'type': 'stemmer',
         'language': 'light_french',
@@ -49,13 +60,32 @@ filters = {
         'output_unigrams': False,
         'filler_token': '',
     },
-    'synonyms': {
-        'type': 'synonym',
-        'synonyms': SYNONYMS,
-    },
 }
 
 analyzers = {
+    'idcc_ape': {
+      'tokenizer': 'whitespace',
+      'filters': [
+        'french_acronyms'
+      ]
+    },
+    'french_heavy': {
+      'tokenizer': 'icu_tokenizer',
+      'filters': [
+        'french_elision',
+        'icu_folding',
+        'french_stemmer',
+        'french_synonyms'
+      ]
+    },
+    'french_light': {
+      'tokenizer': 'icu_tokenizer',
+      'filters': [
+        'french_elision',
+        'icu_folding',
+        'french_synonym'
+      ]
+    },
     'french_stemmed': {
         'type': 'custom',
         'char_filter': ['html_strip'],
@@ -64,8 +94,8 @@ analyzers = {
             'french_elision',
             'icu_folding',
             'lowercase',
-            'acronyms',
-            'synonyms',
+            'french_acronyms',
+            'french_synonyms',
             'french_stop',
             'french_stemmer',
         ],
@@ -78,8 +108,8 @@ analyzers = {
             'french_elision',
             'icu_folding',
             'lowercase',
-            'acronyms',
-            'synonyms',
+            'french_acronyms',
+            'french_synonyms',
             'french_stop',
         ],
     },
@@ -91,8 +121,8 @@ analyzers = {
             'french_elision',
             'icu_folding',
             'lowercase',
-            'acronyms',
-            'synonyms',
+            'french_acronyms',
+            'french_synonyms',
             'french_stop',
             'shingle',
         ],
