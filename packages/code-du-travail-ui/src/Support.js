1eb0583cbe870a3b46926ef685605aaeb1606d35
LionelB <lionel@lumographe.fr>
2019-02-05 11:11:34 +0100

    fix(ui): update support component to work with default props

--- packages/code-du-travail-ui/src/Support.js
+++ packages/code-du-travail-ui/src/Support.js
@@ -2,43 +2,60 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Search } from "./icons";
 
-const Support = ({ onSubmit, children }) => (
-  <section className="section-dark">
-    <div className="container center">
-      <div className="wrapper-narrow">
-        <header>
-          <h2>Besoin d’un accompagnement personnel ?</h2>
-          <p>
-            Services de renseignement, conseil aux salariés, défenseurs des
-            droits…
-            <br /> Indiquez l&apos;adresse de votre lieu de travail pour obtenir
-            une liste de vos interlocuteurs.
-          </p>
-        </header>
-        <form className="support__form" onSubmit={onSubmit}>
-          {/*eslint-disable-next-line jsx-a11y/label-has-for */}
-          <label className="support__label">
-            {children}
-            <Search className="support__icon" title="Rechercher par adresse" />
-          </label>
-        </form>
-      </div>
-    </div>
-  </section>
-);
+class Support extends React.Component {
+  static propTypes = {
+    children: PropTypes.element,
+    onSubmit: PropTypes.func.isRequired
+  };
 
-Support.propTypes = {
-  children: PropTypes.element,
-  onSubmit: PropTypes.func.isRequired
-};
+  inputRef = React.createRef();
 
-Support.defaultProps = {
-  children: (
-    <input
-      type="search"
-      className="support__input"
-      placeholder="ex: 12 avenue du Palais, Metz"
-    />
-  )
-};
+  submit = event => {
+    event.preventDefault();
+    if (this.inputRef.current) {
+      this.props.onSubmit({ target: this.inputRef.current });
+    } else {
+      this.props.onSubmit({ target: event.target });
+    }
+  };
+  render() {
+    const { children } = this.props;
+    return (
+      <section className="section-dark">
+        <div className="container center">
+          <div className="wrapper-narrow">
+            <header>
+              <h2>Besoin d’un accompagnement personnel ?</h2>
+              <p>
+                Services de renseignement, conseil aux salariés, défenseurs des
+                droits…
+                <br /> Indiquez l&apos;adresse de votre lieu de travail pour
+                obtenir une liste de vos interlocuteurs.
+              </p>
+            </header>
+            <form className="support__form" onSubmit={this.submit}>
+              {/*eslint-disable-next-line jsx-a11y/label-has-for */}
+              <label className="support__label">
+                {children ? (
+                  children
+                ) : (
+                  <input
+                    ref={this.inputRef}
+                    type="search"
+                    className="support__input"
+                    placeholder="ex: 12 avenue du Palais, Metz"
+                  />
+                )}
+                <Search
+                  className="support__icon"
+                  title="Rechercher par adresse"
+                />
+              </label>
+            </form>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 export default Support;
