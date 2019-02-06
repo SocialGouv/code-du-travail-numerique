3f0aafa810d048bffa603aa05bb263e6fb8b7d70
LionelB <lionel@lumographe.fr>
2019-02-06 14:45:02 +0100

    fix(data): fix pylint

--- packages/code-du-travail-data/search/indexing/document-slug-checker.py
+++ packages/code-du-travail-data/search/indexing/document-slug-checker.py
@@ -14,7 +14,7 @@ DUPLICATE_SLUGS = []
 def documents_slug_checker():
     slugs = list(map(lambda item: f"{item.get('source')}/{item.get('slug')}", CDTN_DOCUMENTS))
     slugsCount = list(map(lambda item: (item, slugs.count(item)), slugs))
-    DUPLICATE_SLUGS = set(filter(lambda item: item[1] > 1, slugsCount))
+    global DUPLICATE_SLUGS = set(filter(lambda item: item[1] > 1, slugsCount))
     logger.debug("slug | count")
     logger.debug("-----|----")
     for slug, count in DUPLICATE_SLUGS:
@@ -32,7 +32,7 @@ if __name__ == '__main__':
 
 documents_slug_checker()
 
-if len(DUPLICATE_SLUGS) > 0:
+if DUPLICATE_SLUGS:
     sys.exit()
 else:
     sys.exit("documents slug are not uniq ")
