bf64acdeab699595ee8a087e020251f8fd3b225a
LionelB <lionel@lumographe.fr>
2019-02-25 17:03:19 +0100

    feat(frontend): add a ErrorBoundary component

---
+++ packages/code-du-travail-frontend/src/common/ErrorBoundary.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <i>widget non disponible</i>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export { ErrorBoundary };
---
+++ packages/code-du-travail-frontend/src/common/__tests__/ErrorBoundary.test.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { render } from "react-testing-library";
+import { ErrorBoundary } from "../ErrorBoundary";
+
+describe("Error Boundary", () => {
+  // Hack(lionelb): remove noisy error logging catch by ErrorBoundary
+  // @see https://github.com/facebook/react/issues/11098#issuecomment-412682721
+  const topLevelErrors = [];
+  function handleTopLevelError(event) {
+    // calling preventDefault() will prevent error logging
+    event.preventDefault();
+    topLevelErrors.push(event.error);
+  }
+
+  beforeEach(() => {
+    window.addEventListener("error", handleTopLevelError);
+  });
+  afterEach(() => {
+    window.removeEventListener("error", handleTopLevelError);
+  });
+
+  it("render children", () => {
+    const Child = () => <div>component</div>;
+
+    const { container } = render(
+      <ErrorBoundary>
+        <Child />
+      </ErrorBoundary>
+    );
+    expect(container).toMatchInlineSnapshot(`
+<div>
+  <div>
+    component
+  </div>
+</div>
+`);
+  });
+
+  it("render an error message if child component throw", () => {
+    const Child = () => {
+      throw new Error("oups");
+    };
+
+    const { container } = render(
+      <ErrorBoundary>
+        <Child />
+      </ErrorBoundary>
+    );
+    expect(container).toMatchInlineSnapshot(
+      `
+<div>
+  <i>
+    widget non disponible
+  </i>
+</div>
+`
+    );
+    expect(topLevelErrors.length).toBe(1);
+  });
+});
