e1162f332814eaf166b1f085c5d9ce456cb06339
Lionel <lionel@lumographe.fr>
2019-02-26 14:38:49 +0100

    fix(frontend): change lib  mathjax (#525)
    * feat(frontend): add a ErrorBoundary component
    
    * test(frontend): update snapshot after react bump
    
    * fix(frontend): update mathjax library and use ErrorBoundary component
    
    * fix: review

---
+++ packages/code-du-travail-frontend/src/common/ErrorBoundary.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static propTypes = {
+    message: PropTypes.string
+  };
+
+  static defaultProps = {
+    message: "widget non disponible"
+  };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <i>{this.props.message}</i>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export { ErrorBoundary };
