7f194fd2c4923a5de2c89553688a41e932d3f839
LionelB <lionel@lumographe.fr>
2019-02-27 17:19:18 +0100

    fix(data): fix data review

--- packages/code-du-travail-data/dataset/fiches_service_public/ficheFilter.js
+++ packages/code-du-travail-data/dataset/fiches_service_public/ficheFilter.js
@@ -1,8 +1,14 @@
 const select = require("xpath.js");
 
 module.exports = function ficheFilter(xml) {
-  if (excludeDossierId.some(id => matchFilDAriane(xml, id))) {
+  if (excludeFicheId.some(id => matchFilDAriane(xml, id))) {
     return false;
+  }
+
+  if (excludeDossierId.some(id => matchFilDAriane(xml, id))) {
+    // Si la fiche provient d'un dossier qu'on souhaite ecarter
+
+    return includeFicheId.some(id => matchFilDAriane(xml, id));
   }
   const includeList = includeThemeId.concat(includeDossierId, includeFicheId);
   if (includeList.some(id => matchFilDAriane(xml, id))) {
@@ -32,6 +38,46 @@ const excludeDossierId = [
   "N379"
 ];
 
+const excludeFicheId = [
+  "F10027",
+  "F13375",
+  "F20314",
+  "F20678",
+  "F22290",
+  "F22295",
+  "F22354",
+  "F22356",
+  "F22359",
+  "F22424",
+  "F22726",
+  "F23369",
+  "F23459",
+  "F23460",
+  "F23507",
+  "F23756",
+  "F23992",
+  "F23994",
+  "F23997",
+  "F24005",
+  "F24013",
+  "F31233",
+  "F31409",
+  "F31422",
+  "F31427",
+  "F31479",
+  "F31670",
+  "F31712",
+  "F31713",
+  "F31837",
+  "F31926",
+  "F32095",
+  "F32258",
+  "F32307",
+  "F32308",
+  "F32581",
+  "F32703",
+  "F33843"
+];
 const includeDossierId = [
   "N20286",
   "N31477",
@@ -64,5 +110,21 @@ const includeFicheId = [
   "F33050",
   "F32329",
   "F2607",
-  "F153"
+  "F153",
+  "F10041",
+  "F1190",
+  "F1234",
+  "F12382",
+  "F14809",
+  "F14868",
+  "F15132",
+  "F15813",
+  "F174",
+  "F2140",
+  "F2142",
+  "F24610",
+  "F2642",
+  "F32709",
+  "F34030",
+  "F92"
 ];
