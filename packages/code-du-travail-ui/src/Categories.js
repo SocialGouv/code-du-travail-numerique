d872a411c1095c25ecca2b637131329e49ba8ba6
LionelB <lionel@lumographe.fr>
2019-03-13 15:50:03 +0100

    refactor(frontend): move categories to home folder

--- packages/code-du-travail-ui/src/Categories.js
+++ packages/code-du-travail-ui/src/Categories.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Categories = props => (
+const Categories = ({ className = "", ...props }) => (
   <div className="categories">
-    <ul className="categories__list" {...props} />
+    <ul className={`categories__list ${className}`} {...props} />
   </div>
 );
+
+Categories.propTypes = {
+  className: PropTypes.string
+};
 
 export default Categories;
