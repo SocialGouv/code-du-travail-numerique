a9dfa8ac78edbf094a01d787b75a334e0b9991fa
Nicolas Bovorasmy <nbovorasmy@octo.com>
2019-03-20 00:47:51 +0100

    ci: add stage to delete useless pods

---
+++ k8s/scripts/delete-pods.py
@@ -0,0 +1,30 @@
+from subprocess import check_output
+
+def get_merged_branches():
+  merged_branches_list = []
+  raw_merged_branches = check_output('git branch -r --merged master', shell=True)
+  for  merged_branch in raw_merged_branches.decode('utf-8').split('\n'):
+    if merged_branch and merged_branch.strip() != 'origin/master' and merged_branch.strip() != 'origin/HEAD -> origin/master':
+      merged_branches_list.append(merged_branch.replace('origin/','').replace('/','-').strip())
+  return merged_branches_list
+
+def get_pod_to_delete(label):
+  pod_to_delete_list = []
+  check_pod = ('kubectl get po --selector app='+label)
+  print(check_pod)
+  active_pod = check_output(check_pod , shell=True)
+  if active_pod:
+    pod_to_delete_list.append(label)
+  else:
+    print('No pod with label:',label)
+  return pod_to_delete_list
+
+def delete_pod(label):
+  command_to_delete_pods = ('kubectl delete po --selector app='+label)
+  print(command_to_delete_pods)
+  check_output(command_to_delete_pods, shell=True)
+
+if __name__ == '__main__':
+  for merged_branch in get_merged_branches():
+    for pod_to_delete in get_pod_to_delete(merged_branch):
+      delete_pod(pod_to_delete)
