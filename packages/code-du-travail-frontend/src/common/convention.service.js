70ca9e4c57cb2ecab3a1d32fc60df7cd6c836ec0
Adrien Di Pasquale <adrien@dipasquale.fr>
2019-03-20 16:01:11 +0100

    Add new companies calls to the idcc2siret API in the convention service #49

--- packages/code-du-travail-frontend/src/common/convention.service.js
+++ packages/code-du-travail-frontend/src/common/convention.service.js
@@ -2,7 +2,7 @@ import getConfig from "next/config";
 import memoizee from "memoizee";
 
 const {
-  publicRuntimeConfig: { API_URL }
+  publicRuntimeConfig: { API_URL, API_SIRET2IDCC_URL }
 } = getConfig();
 
 function searchIdcc(query) {
@@ -16,7 +16,42 @@ function searchIdcc(query) {
   });
 }
 
+function searchCompanies(siret) {
+  const url = `${API_SIRET2IDCC_URL}/api/v1/companies?siret=${encodeURIComponent(
+    siret
+  )}`;
+
+  return fetch(url).then(response => {
+    if (response.ok) {
+      return response.json().then(result => result.companies);
+    }
+    throw new Error("Un problème est survenu.");
+  });
+}
+
+function getCompany(siret) {
+  const url = `${API_SIRET2IDCC_URL}/api/v1/company/${siret}`;
+
+  return fetch(url).then(response => {
+    if (response.ok) {
+      return response.json().then(result => result.company);
+    }
+    throw new Error("Un problème est survenu.");
+  });
+}
+
 // memoize search results
 const searchIdccMemo = memoizee(query => searchIdcc(query), { promise: true });
+const searchCompaniesMemo = memoizee(siret => searchCompanies(siret), {
+  promise: true
+});
+const getCompanyMemo = memoizee(siret => getCompany(siret), { promise: true });
 
-export { searchIdccMemo as searchIdcc, searchIdcc as _searchIdcc };
+export {
+  searchIdccMemo as searchIdcc,
+  searchIdcc as _searchIdcc,
+  searchCompaniesMemo as searchCompanies,
+  searchCompanies as _searchCompanies,
+  getCompanyMemo as getCompany,
+  getCompany as _getCompany
+};
