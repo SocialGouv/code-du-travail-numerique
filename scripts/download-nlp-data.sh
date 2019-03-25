7f22556c223d8f2fb1cb34b9a159ca6ab85f2f5c
LionelB <lionel@lumographe.fr>
2019-03-25 18:02:21 +0100

    fix(data): handle gist with multiple data files

--- scripts/download-nlp-data.sh
+++ scripts/download-nlp-data.sh
@@ -5,3 +5,4 @@ export $(grep -v '^#' .env | xargs)
 set -exu pipefail
 
 curl -L $SUGGEST_DATA_URL | tar zx -C packages/code-du-travail-nlp/data --strip-components=1
+cat packages/code-du-travail-nlp/data/data-*.txt > packages/code-du-travail-nlp/data.txt
