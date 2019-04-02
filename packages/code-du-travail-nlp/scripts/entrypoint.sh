b03a4b2051d1dc1e06ffbd33d87dbf2f9c8aa876
LionelB <lionel@lumographe.fr>
2019-04-02 15:45:37 +0200

    fix(nlp): update docker image +  deployement

--- packages/code-du-travail-nlp/scripts/entrypoint.sh
+++ packages/code-du-travail-nlp/scripts/entrypoint.sh
@@ -5,7 +5,7 @@ count=0
 for file in $(curl -Ls $SUGGEST_DATA_URL); do
   curl -L $file > data/data-$count.zip
   unzip -j -o -d data data/data-$count.zip
-  ((count++))
+ count=$((count+1))
 done;
 
 cat data/data-*.txt > data/data.txt
