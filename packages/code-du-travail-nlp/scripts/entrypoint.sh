9bd808395beffa486bdb313edf6fefb20f7a2a18
Lionel <lionel@lumographe.fr>
2019-04-02 19:33:57 +0200

    ci(nlp): update nlp container (#656)
    * ci(nlp): update nlp container
    
    * ci(nlp): add nlp to k8s (#657)
    
    * fix(nlp): update docker image +  deployement
    
    * ci(nlp): add nlp api to k8s cluster
    
    * fix SUGGEST_DATA_URL
    
    fix #648

---
+++ packages/code-du-travail-nlp/scripts/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -exu pipefail
+count=0
+for file in $(curl -Ls $SUGGEST_DATA_URL); do
+  curl -L $file > data/data-$count.zip
+  unzip -j -o -d data data/data-$count.zip
+ count=$((count+1))
+done;
+
+cat data/data-*.txt > data/data.txt
+rm data/data-*
+
+# start the serveur
+gunicorn -t 200 -w 4 -b :${NLP_PORT} "api:create_app()"
