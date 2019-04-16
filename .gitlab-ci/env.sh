77163a6ee6a088dcfae5a18ae5057cafb329653c
Douglas Duteil <douglasduteil@gmail.com>
2019-04-16 21:15:22 +0200

    ci(gitlab): no slack in hostname k8s said

--- .gitlab-ci/env.sh
+++ .gitlab-ci/env.sh
@@ -15,17 +15,17 @@ case ${BRANCH_NAME} in
   master)
     export ENVIRONMENT=ops;
     #
-    export API_HOST=api.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr/;
-    export FRONTEND_HOST=code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr/;
-    export NLP_HOST=nlp.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr/;
+    export API_HOST=api.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
+    export FRONTEND_HOST=code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
+    export NLP_HOST=nlp.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
     export ELASTICSEARCH_HOST=elasticsearch:${ES_PORT};
     ;;
   *)
     export ENVIRONMENT=dev;
     #
-    export API_HOST=api.${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr/;
-    export FRONTEND_HOST=${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr/;
-    export NLP_HOST=nlp.${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr/;
+    export API_HOST=api.${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
+    export FRONTEND_HOST=${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
+    export NLP_HOST=nlp.${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
     export ELASTICSEARCH_HOST=${BRANCH_HASH}.elasticsearch:${ES_PORT};
     ;;
 esac
