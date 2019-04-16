10cb05a65a4e0660e560377a65b0c1566ee2cfd6
Douglas Duteil <douglasduteil@gmail.com>
2019-04-16 19:35:54 +0200

    ci(gitlab): fix sent url during deployment (#736)

--- k8s/scripts/get-deploy-id.sh
+++ k8s/scripts/get-deploy-id.sh
@@ -13,7 +13,7 @@ curl -0 -sS \
 {
   "auto_merge": false,
   "description": "Deplying ${CI_PROJECT_PATH}@${CI_COMMIT_SHORT_SHA}",
-  "environment": "${CI_ENVIRONMENT_NAME}",
+  "environment": "${ENVIRONMENT}",
   "ref": "${CI_COMMIT_REF_NAME}",
   "required_contexts": [],
   "transient_environment": true
