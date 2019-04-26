f0b23cf7380a6484673f9e21cf6a26c2c00cd387
LionelB <lionel@lumographe.fr>
2019-04-26 16:09:43 +0200

    feat(data): add missing fiche ministere-travail

---
+++ packages/code-du-travail-data/dataset/fiches_ministere_travail/updateTheme.js
@@ -0,0 +1,22 @@
+const getThemeMapping = require("./cdtn-theme-mt.js");
+
+async function updateTheme(fiches) {
+  const themeMapping = await getThemeMapping()
+  fiches.forEach(fiche => {
+    const [_, slug] = fiche.url.match(/\/([\w-]+)\/?$/);
+    console.error(slug)
+    fiche.themeCdtn = themeMapping[slug];
+  });
+}
+
+async function main() {
+  const fiches = require('./fiches-min-travail.json');
+  await updateTheme(fiches);
+  console.log(JSON.stringify(fiches, null, 2))
+}
+
+if (module === require.main) {
+  main()
+}
+
+module.exports = updateTheme
