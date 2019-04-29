e73fbd52faf50d678a6bff4ac2bb9902285efcaa
Manu <colasante.manuel@gmail.com>
2019-04-29 09:59:09 +0200

    refactor(frontend): remove deprecated source (#785)

--- packages/code-du-travail-frontend/src/sources.js
+++ packages/code-du-travail-frontend/src/sources.js
@@ -1,7 +1,6 @@
 // mapping elastic search source type -> route name
 const routeBySource = {
   faq: "question",
-  code_bfc: "fiche-code-bfc",
   fiches_service_public: "fiche-service-public",
   fiches_ministere_travail: "fiche-ministere-travail",
   code_du_travail: "code-du-travail",
@@ -16,7 +15,6 @@ const routeBySource = {
 // mapping elastic search source type -> source label
 const labelBySource = {
   faq: "Réponses",
-  code_bfc: "DIRECCTE Bourgogne-Franche-Comté",
   fiches_service_public: "Service Public",
   fiches_ministere_travail: "Ministère du Travail",
   code_du_travail: "Code du travail",
