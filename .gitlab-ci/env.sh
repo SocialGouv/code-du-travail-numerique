6fca54bf2443cbf06886f425b90aff3f8adb03d1
Nicolas Bovorasmy <nbovorasmy@octo.com>
2019-04-29 12:17:21 +0200

    ci: change deployment environment variable

--- .gitlab-ci/env.sh
+++ .gitlab-ci/env.sh
@@ -13,19 +13,19 @@ export BRANCH_HASH=${BRANCH_HASH:=$BRANCH_NAME_HASHED}
 
 case ${BRANCH_NAME} in
   master)
-    export ENVIRONMENT=ops;
+    export ENVIRONMENT=incubateur;
     #
-    export API_HOST=api.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
-    export FRONTEND_HOST=code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
-    export NLP_HOST=nlp.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
+    export API_HOST=api.code-du-travail-numerique.${ENVIRONMENT}.social.gouv.fr;
+    export FRONTEND_HOST=code-du-travail-numerique.${ENVIRONMENT}.social.gouv.fr;
+    export NLP_HOST=nlp.code-du-travail-numerique.${ENVIRONMENT}.social.gouv.fr;
     export ELASTICSEARCH_HOST=elasticsearch:${ES_PORT};
     ;;
   *)
-    export ENVIRONMENT=dev;
+    export ENVIRONMENT=dev.factory;
     #
-    export API_HOST=api.${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
-    export FRONTEND_HOST=${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
-    export NLP_HOST=nlp.${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
+    export API_HOST=api.${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.social.gouv.fr;
+    export FRONTEND_HOST=${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.social.gouv.fr;
+    export NLP_HOST=nlp.${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.social.gouv.fr;
     export ELASTICSEARCH_HOST=elasticsearch-${BRANCH_HASH}:${ES_PORT};
     ;;
 esac
