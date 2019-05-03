756c7dd4ed41b5aa97706db0ba0dbd96027b88b0
LionelB <lionel@lumographe.fr>
2019-05-03 16:30:33 +0200

    fix(frontend): fix js for ie and firefox
    update the polyfill.io bundle to add missing Array#includes
    fix #764

--- packages/code-du-travail-frontend/pages/_document.js
+++ packages/code-du-travail-frontend/pages/_document.js
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
           />
           <script
             crossOrigin="anonymous"
-            src="https://polyfill.io/v3/polyfill.min.js?flags=gated&features=default%2CArray.prototype.find%2CArray.prototype.findIndex%2CObject.setPrototypeOf%2CNumber.isFinite%2Cfetch%2CWeakMap%2CSymbol%2CSymbol.hasInstance%2CSymbol.isConcatSpreadable%2CSymbol.iterator%2CSymbol.unscopables%2CSymbol.toStringTag%2CSymbol.toPrimitive%2CSymbol.split%2CSymbol.search%2CSymbol.species%2CSymbol.replace%2CSymbol.match%2CSet%2CMap"
+            src="https://polyfill.io/v3/polyfill.min.js?features=default%2CArray.prototype.includes%2CArray.prototype.find%2CArray.prototype.findIndex%2CObject.setPrototypeOf%2CNumber.isFinite%2Cfetch%2CSymbol%2CSymbol.hasInstance%2CSymbol.isConcatSpreadable%2CSymbol.iterator%2CSymbol.unscopables%2CSymbol.toStringTag%2CSymbol.toPrimitive%2CSymbol.split%2CSymbol.search%2CSymbol.species%2CSymbol.replace%2CSymbol.match%2CSet%2CMap%2CWeakMap"
           />
           <script
             dangerouslySetInnerHTML={{
