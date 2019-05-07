178b6438ae85571f7af3b78a008b1eab30efee5c
Manu <colasante.manuel@gmail.com>
2019-05-07 11:14:33 +0200

    feat(ui): add new toast component (#801)
    * refactor(ui): add hooks to toast
    
    * feat(front): add disclaimer (#805)

---
+++ packages/code-du-travail-ui/src/Toast/test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { fireEvent, render } from "react-testing-library";
+import Toast from "../Toast";
+
+describe("<Toast />", () => {
+  it("renders", () => {
+    const { container } = render(<Toast kind="warning">Some content</Toast>);
+    expect(container).toMatchSnapshot();
+  });
+  it("Should call onRemove when clicking on remove button if an onRemove prop is provided", () => {
+    const onRemove = jest.fn();
+    const { container } = render(
+      <Toast onRemove={onRemove}>I am an annoying toast, remove me.</Toast>
+    );
+    const onRemoveButton = container.querySelector("button");
+    fireEvent.click(onRemoveButton);
+    expect(onRemove.mock.calls.length).toBe(1);
+  });
+});
