9735ddd5e744d2f75d9641bc08a85185b01b9692
Manu <colasante.manuel@gmail.com>
2019-05-17 12:50:16 +0200

    refactor(data): split-intro-from-html (#827)

--- packages/code-du-travail-data/search/indexing/cdtn_documents.py
+++ packages/code-du-travail-data/search/indexing/cdtn_documents.py
@@ -105,6 +105,7 @@ def populate_cdtn_documents():
             'text': val['text'],
             'description': val['description'],
             'anchor': val['anchor'],
+            'intro': val['intro'],
             'html': val["html"],
             'title': val['title'],
             'url': val['url'],
