cb46573a48308d15220ca70ae83bd63b6531afb3
Manu <colasante.manuel@gmail.com>
2019-06-11 09:36:04 +0200

    feature(data): add-fiches-mt (#909)

--- packages/code-du-travail-data/dataset/fiches_ministere_travail/updateTheme.js
+++ packages/code-du-travail-data/dataset/fiches_ministere_travail/updateTheme.js
@@ -1,22 +1,22 @@
 const getThemeMapping = require("./cdtn-theme-mt.js");
 
 async function updateTheme(fiches) {
-  const themeMapping = await getThemeMapping()
+  const themeMapping = await getThemeMapping();
   fiches.forEach(fiche => {
     const [_, slug] = fiche.url.match(/\/([\w-]+)\/?$/);
-    console.error(slug)
+    console.error(slug);
     fiche.themeCdtn = themeMapping[slug];
   });
 }
 
 async function main() {
-  const fiches = require('./fiches-min-travail.json');
+  const fiches = require("./fiches-min-travail.json");
   await updateTheme(fiches);
-  console.log(JSON.stringify(fiches, null, 2))
+  console.log(JSON.stringify(fiches, null, 2));
 }
 
 if (module === require.main) {
-  main()
+  main();
 }
 
-module.exports = updateTheme
+module.exports = updateTheme;
