cdab5d0af34f93188274f0ebd42822433f27aa65
Manu <colasante.manuel@gmail.com>
2019-06-13 10:34:11 +0200

    feat(ui): make tabs mobile almost friendly (#892)

--- packages/code-du-travail-ui/src/Tabs/index.js
+++ packages/code-du-travail-ui/src/Tabs/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Tab, Tabs as RootTabs, TabList, TabPanel } from "react-tabs";
-import { box, colors, fonts, spacing } from "../theme";
+import { box, breakpoints, colors, fonts, spacing } from "../theme";
 
 class Tabs extends React.PureComponent {
   render() {
     const { data, defaultIndex, onSelect, selectedIndex } = this.props;
-    const tabs = data.map((item, index) => <Tab key={index}>{item.tab}</Tab>);
-    const tabContents = data.map(item => (
-      <TabPanel key={item.key}>{item.panel}</TabPanel>
+    const tabs = data.map((item, index) => (
+      <StyledTab key={index}>{item.tab}</StyledTab>
+    ));
+    const tabContents = data.map((item, index) => (
+      <StyledTabPanel key={index}>{item.panel}</StyledTabPanel>
     ));
 
     const props = {
@@ -22,7 +24,7 @@ class Tabs extends React.PureComponent {
 
     return (
       <StyledTabs {...props}>
-        <TabList>{tabs}</TabList>
+        <StyledTabList>{tabs}</StyledTabList>
         {tabContents}
       </StyledTabs>
     );
@@ -35,7 +37,6 @@ Tabs.propTypes = {
   defaultIndex: PropTypes.number,
   data: PropTypes.arrayOf(
     PropTypes.shape({
-      key: PropTypes.string.isRequired,
       tab: PropTypes.node.isRequired,
       panel: PropTypes.node.isRequired
     })
@@ -51,41 +52,55 @@ export default Tabs;
 
 const StyledTabs = styled(RootTabs)`
   margin-bottom: ${spacing.large};
+`;
 
-  .react-tabs__tab-list {
-    position: relative;
-    top: 1px;
-    display: flex;
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
+const StyledTabList = styled(TabList)`
+  display: flex;
+  flex-wrap: nowrap;
+  position: relative;
+  top: 1px;
+  margin: 0;
+  padding: 0;
+  max-width: 100%;
+  list-style-type: none;
+  overflow-x: auto;
+  @media (max-width: ${breakpoints.tablet}) {
+    flex-wrap: wrap;
   }
+`;
 
-  .react-tabs__tab {
-    margin-right: ${spacing.tiny};
-    padding: ${spacing.small} ${spacing.base};
-    font-size: ${fonts.sizeH6};
-    background-color: ${colors.darkBackground};
-    border: 1px solid ${colors.elementBorder};
-    border-bottom: 1px solid ${colors.primaryBackground};
-    border-top-left-radius: ${box.lightBorderRadius};
-    border-top-right-radius: ${box.lightBorderRadius};
-    cursor: pointer;
-    &:hover {
-      text-decoration: underline;
-    }
+const StyledTab = styled(Tab)`
+  margin-right: ${spacing.tiny};
+  padding: ${spacing.small} ${spacing.base};
+  font-size: ${fonts.sizeH6};
+  background-color: ${colors.darkBackground};
+  border: 1px solid ${colors.elementBorder};
+  border-bottom: 1px solid ${colors.primaryBackground};
+  border-radius: ${box.lightBorderRadius} ${box.lightBorderRadius} 0 0;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
   }
-
-  .react-tabs__tab--selected {
+  &[aria-selected="true"] {
     color: ${colors.primaryText};
     background-color: ${colors.primaryBackground};
   }
+  @media (max-width: ${breakpoints.tablet}) {
+    flex: 1 1 auto;
+    margin: ${spacing.tiny};
+    border-bottom: 1px solid ${colors.elementBorder};
+    border-radius: ${box.lightBorderRadius};
+  }
+`;
 
-  .react-tabs__tab-panel--selected {
+const StyledTabPanel = styled(TabPanel)`
+  &.react-tabs__tab-panel--selected {
     padding: ${spacing.base};
     border: 1px solid ${colors.primaryBackground};
-    border-top-right-radius: ${box.borderRadius};
-    border-bottom-right-radius: ${box.borderRadius};
-    border-bottom-left-radius: ${box.borderRadius};
+    border-radius: 0 ${box.borderRadius} ${box.borderRadius} ${box.borderRadius};
+    @media (max-width: ${breakpoints.tablet}) {
+      margin-top: ${spacing.tiny};
+      border-radius: ${box.borderRadius};
+    }
   }
 `;
