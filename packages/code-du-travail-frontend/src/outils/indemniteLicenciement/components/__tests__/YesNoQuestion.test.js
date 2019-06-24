a068e6958c7ab60ca90e3fed5c980f0334114ce2
LionelB <lionel@lumographe.fr>
2019-06-24 09:22:30 +0200

    fix(frontend): display error message in ccn infos
    This bug appears only on safari. When selecting cdd or faute-grave on
    the first screen of the idl.
    Error messages are now trigger using the dirty flag
    
    fix #960

--- packages/code-du-travail-frontend/src/outils/indemniteLicenciement/components/__tests__/YesNoQuestion.test.js
+++ packages/code-du-travail-frontend/src/outils/indemniteLicenciement/components/__tests__/YesNoQuestion.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-testing-library";
+import { render, fireEvent } from "react-testing-library";
 import { YesNoQuestion } from "../YesNoQuestion";
 import { Form } from "react-final-form";
 
@@ -37,7 +37,7 @@ describe("<YesNoQuestion />", () => {
     expect(onChange).toHaveBeenCalledWith(true);
   });
 
-  it("should render error", () => {
+  it("should render error when form validate", () => {
     const onSubmit = jest.fn();
     const { getByText } = render(
       <Form onSubmit={onSubmit}>
@@ -49,8 +49,30 @@ describe("<YesNoQuestion />", () => {
         )}
       </Form>
     );
-    const bt = getByText(/validate/i);
-    bt.click();
+    const non = getByText(/validate/i);
+    fireEvent.click(non);
     expect(getByText(/ce champ est requis/i)).toBeDefined();
+  });
+
+  it("should render error when radio change", () => {
+    const onSubmit = jest.fn();
+    const { getByLabelText, getByText } = render(
+      <Form
+        onSubmit={onSubmit}
+        validate={values =>
+          values.test === "oui" ? {} : { test: "mauvaise réponse" }
+        }
+      >
+        {({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <YesNoQuestion name="test" label="lorem ipsum ?" />
+            <button>validate</button>
+          </form>
+        )}
+      </Form>
+    );
+    const non = getByLabelText(/non/i);
+    fireEvent.click(non);
+    expect(getByText(/mauvaise réponse/i)).toBeDefined();
   });
 });
