905c3e00bde105f07d4b07e8b363dc166affeae3
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-07-01 17:21:35 +0200

    fix(idl): fix regression #986 + add snapshot

---
+++ packages/code-du-travail-frontend/src/outils/indemniteLicenciement/__tests__/SansIndemniteLicenciement.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { render } from "react-testing-library";
+import SansIndemniteLicenciement from "../ccn/SansIndemniteLicenciement";
+
+describe("<SansIndemniteLicenciement />", () => {
+  it("should render", () => {
+    const form = {
+      getState() {
+        return {
+          values: {
+            branche: "1518",
+            contrat: "cdi",
+            fauteGrave: false,
+            inaptitude: false,
+            dateEntree: "2013-07-02",
+            anciennete: 6,
+            dateNotification: "2019-07-02",
+            dateSortie: "2019-07-31",
+            hasAbsenceProlonge: false,
+            absencePeriods: [],
+            hasTempsPartiel: false,
+            salairePeriods: [],
+            hasSameSalaire: true,
+            salaires: [],
+            salaire: "2390"
+          }
+        };
+      }
+    };
+    const { container } = render(<SansIndemniteLicenciement form={form} />);
+    expect(container).toMatchSnapshot();
+  });
+});
