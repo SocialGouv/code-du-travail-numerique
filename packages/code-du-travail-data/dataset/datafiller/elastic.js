5f5a17497b90d2096fbac83de70da901b81c3f43
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-07-03 13:38:16 +0200

    fix(datafiller): extract ES search logic

--- packages/code-du-travail-data/dataset/datafiller/elastic.js
+++ packages/code-du-travail-data/dataset/datafiller/elastic.js
@@ -3,16 +3,16 @@ const fetch = require("node-fetch");
 // fetch datafiller results then check against ES index using a slug match
 const ES_URL = process.env.ELASTICSEARCH_URL || "http://127.0.0.1:9200";
 
-// remove prefix (source)
+// strip prefix (source)
 const getSlug = url => url.replace(/^(\/[^/]+\/)(.*)/, "$2");
 
 // try to get the local ES reference for a given "result" (= a reference slug in datafiller)
-const getReference = (result, ref) => {
+const getReference = (title, ref) => {
   const slug = getSlug(ref.url);
 
   if (slug.match(/^https?:\/\//)) {
-    console.log(`EXTERNAL | ${result.title} | ${slug} | external link`);
-    return false;
+    console.log(`EXTERNAL | ${title} | ${slug} | external link`);
+    return;
   }
 
   // find the good slug
@@ -44,38 +44,47 @@ const getReference = (result, ref) => {
       if (res.hits.hits.length) {
         if (res.hits.hits.length > 1) {
           console.log(
-            `ERROR | ${result.title} | ${slug} | plusieurs résultats trouvés`
+            `ERROR | ${title} | ${slug} | plusieurs résultats trouvés`
           );
           return;
         }
         if (res.hits.hits[0]._source.slug !== slug) {
           console.log(
-            `FIXED | ${result.title} | ${slug} | ${
-              res.hits.hits[0]._source.slug
-            }`
+            `FOUND | ${title} | ${slug} | ${res.hits.hits[0]._source.slug}`
           );
         }
-        return {
-          ...res.hits.hits[0],
-          relevance: ref.relevance
-        };
+        return res.hits.hits[0];
       }
-      console.log(`ERROR | ${result.title} | ${slug} | not found in ES`);
+      console.log(`ERROR | ${title} | ${slug} | not found in ES`);
     })
     .catch(e => {
-      if (slug.match(/^http/)) {
-        // silent for external links
-        return false;
-      }
       console.log("ERROR", e);
-      return false;
+      return;
     });
+};
+
+const replaceReference = async (title, ref) => {
+  const hit = await getReference(title, ref);
+  if (hit) {
+    return {
+      ...hit,
+      relevance: ref.relevance
+    };
+  }
+  return {
+    _source: {
+      title,
+      url: ref.url
+    }
+  };
 };
 
 // check refs against ES
 const getReferences = result =>
   Promise.all(
-    result.refs.filter(ref => ref.url).map(ref => getReference(result, ref))
+    result.refs
+      .filter(ref => ref.url)
+      .map(ref => replaceReference(result.title, ref))
   ).then(arr => arr.filter(Boolean));
 
-module.exports = { getReferences };
+module.exports = { getReference, getReferences };
