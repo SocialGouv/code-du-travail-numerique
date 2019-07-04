74b164eec1e478846ff32bc3d5ff49d46a7c1d75
ManuC <colasante.manuel@gmail.com>
2019-07-04 11:06:19 +0200

    refactor(frontend): wording changes

--- packages/code-du-travail-frontend/src/outils/indemniteLicenciement/steps/Introduction.js
+++ packages/code-du-travail-frontend/src/outils/indemniteLicenciement/steps/Introduction.js
@@ -5,27 +5,27 @@ const StepIntro = () => (
   <Container>
     <Wrapper>
       <p>
-        Cet outil permet de calculer l’indemnité minimale de licenciement d’un
-        salarié.
+        Ce simulateur est un outil qui permet d’estimer le montant de
+        l’indemnité minimale de licenciement d’un salarié.
       </p>
       <p>
-        Vous pourrez calculer simplement le montant de l’indemnité légale
-        (définie par le Code du travail) et de l’indemnité conventionnelle (si
-        votre branche d’activité prévoit un montant supérieur au Code du
-        travail).
+        Vous pourrez simuler simplement le montant de l’indemnité légale définie
+        par le Code du travail et le montant de l’indemnité conventionnelle de
+        licenciement si votre branche d’activité prévoit un montant supérieur au
+        Code du travail.
       </p>
       <p>
-        Cette simulation nécessite entre 5 et 10 minutes pour être complétée.
-        Afin de pouvoir remplir les renseignements demandés, munissez-vous des
-        informations relatives à votre licenciement (dates d’entrée et de sortie
-        de l’entreprise, courrier de licenciement, derniers bulletins de
-        salaire, etc.).
+        Cette simulation nécessite entre 5 et 10 minutes. Afin de pouvoir
+        remplir les renseignements demandés, munissez-vous des informations
+        relatives à votre licenciement (dates d’entrée et de sortie de
+        l’entreprise, courrier de licenciement, derniers bulletins de salaire,
+        etc.).
       </p>
       <p>
         Pour en savoir plus sur l’indemnité de licenciement et son mode de
-        calcul, consultez cet article (lien vers la fiche SP sur l’IDL). Pour
-        commencer la simulation cliquez sur &quot;Suivant&quot;.
+        calcul, consultez cet article (lien vers la fiche SP sur l’IDL).
       </p>
+      <p>Pour commencer la simulation cliquez sur &quot;Suivant&quot;.</p>
     </Wrapper>
   </Container>
 );
