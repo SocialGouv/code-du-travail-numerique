68c9c51b5e35ab081f3626628f2bda58ff4b7469
Lionel <lionel@lumographe.fr>
2019-07-17 15:37:29 +0200

    style(frontend): use typographical quote
    replace html entity `&apos;`   with its typographical quote equivalent

--- packages/code-du-travail-frontend/src/conventions/ConventionExplainer.js
+++ packages/code-du-travail-frontend/src/conventions/ConventionExplainer.js
@@ -34,30 +34,30 @@ class ConventionExplainer extends React.Component {
         {!hidden && (
           <React.Fragment>
             <SubTitle>
-              Qu&apos;est ce qu&apos;une convention collective ?
+              Qu’est ce qu’une convention collective ?
             </SubTitle>
             <p>
               Une convention collective est un accord négocié entre des
               organisations syndicales de salariés et des organisations
-              d&apos;employeurs.
+              d’employeurs.
               <br />
-              Elle permet d&apos;aménager les règles issues du code du travail
-              concernant les conditions d&apos;emploi, la formation
+              Elle permet d’aménager les règles issues du code du travail
+              concernant les conditions d’emploi, la formation
               professionnelle, le travail des salariés et les garanties
-              sociales aux spécificités des secteurs d&apos;activité et
-              géographiques concernés. Elle peut également prévoir d&apos;autres
+              sociales aux spécificités des secteurs d’activité et
+              géographiques concernés. Elle peut également prévoir d’autres
               mesures qui ne sont pas prévues par le code du travail.
             </p>
             <SubTitle>
-              Une convention collective de branche s&apos;applique-t-elle à ma
+              Une convention collective de branche s’applique-t-elle à ma
               situation ?
             </SubTitle>
             <p>
-              Pour pouvoir s&apos;appliquer à vous, la convention collective de
+              Pour pouvoir s’appliquer à vous, la convention collective de
               branche doit être applicable à votre entreprise.
               <br />
-              L&apos;employeur a l&apos;obligation d’appliquer la convention collective
-              lorsque l&apos;entreprise entre dans le champ d&apos;application professionnel
+              L’employeur a l’obligation d’appliquer la convention collective
+              lorsque l’entreprise entre dans le champ d’application professionnel
               et géographie défini par la convention et que :
             </p>
             <ul>
@@ -65,7 +65,7 @@ class ConventionExplainer extends React.Component {
                 La convention collective de branche a été étendue par le ministère du travail.
               </li>
               <li>
-                L&apos;entreprise est adhérente à une organisation patronale signataire
+                L’entreprise est adhérente à une organisation patronale signataire
                 de la convention collective.
               </li>
             </ul>
