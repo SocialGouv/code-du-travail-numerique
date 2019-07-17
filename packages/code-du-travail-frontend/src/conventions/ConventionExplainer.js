a3e3cab19213f37f75335ae2aff4ddb70baab251
Clément Chapalain <clement.chapalain@beta.gouv.fr>
2019-07-17 17:31:59 +0200

    fix(frontend): improve CCN explanation text (#1034)
    * Update wording CCNs
    
    * change test to match new subtitle text
    
    * change snapshot to match wording fix
    
    * update snapshot to match wording fix bis
    
    * change capitalisation
    
    * new changes by Nadia - fichier de base
    
    * new changes by Nadia - snapshot 1
    
    * new changes by Nadia - snapshot 2
    
    * style(frontend): use typographical quote
    
    replace html entity `&apos;`   with its typographical quote equivalent
    
    * style(frontend): use typographic quote
    
    fix missing last htmlentity
    
    * test(frontend): update snapshots
    
    * test(frontend): update snapshots
    
    * style(frontend): fix lint
    
    * test(frontend): update snapshot
    
    fix #1011

--- packages/code-du-travail-frontend/src/conventions/ConventionExplainer.js
+++ packages/code-du-travail-frontend/src/conventions/ConventionExplainer.js
@@ -28,49 +28,44 @@ class ConventionExplainer extends React.Component {
             sessionStorage.setItem(HIDDEN_KEY, (!hidden).toString());
           }}
         >
-          Explications sur les conventions collectives nationales
+          Plus d’informations sur les conventions collectives
           <VerticalArrow />
         </Title>
         {!hidden && (
           <React.Fragment>
-            <SubTitle>
-              Qu&apos;est ce qu&apos;une convention collective ?
-            </SubTitle>
+            <SubTitle>Qu’est ce qu’une convention collective ?</SubTitle>
             <p>
               Une convention collective est un accord négocié entre des
               organisations syndicales de salariés et des organisations
-              d&apos;employeurs.
+              d’employeurs.
               <br />
-              Elle permet d&apos;aménager les règles issues du code du travail
-              concernant les conditions d&apos;emploi, la formation
-              professionnelle, le travail des salariés et de leurs garanties
-              sociales aux spécificités du secteurs d&apos;activité et
-              géographique concernés. Elle peut également prévoir d&apos;autres
-              mesures qui ne sont pas prévues par le code du travail.
+              Elle permet d’aménager les règles issues du code du travail
+              concernant les conditions d’emploi, la formation professionnelle,
+              le travail des salariés et les garanties sociales aux spécificités
+              des secteurs d’activité et géographiques concernés. Elle peut
+              également prévoir d’autres mesures qui ne sont pas prévues par le
+              code du travail.
             </p>
             <SubTitle>
-              Une convention collective de branche s&apos;applique-t-elle à ma
+              Une convention collective de branche s’applique-t-elle à ma
               situation ?
             </SubTitle>
             <p>
-              Pour pouvoir s&apos;appliquer à vous, la convention collective de
+              Pour pouvoir s’appliquer à vous, la convention collective de
               branche doit être applicable à votre entreprise.
               <br />
-              Lorsque votre entreprise entre dans le champ professionnel et
-              géographique d&apos;une convention collective de branche, il
-              existe deux situations dans lesquelles votre entreprise a
-              l&apos;obligation de l&apos;appliquer :
+              L’employeur a l’obligation d’appliquer la convention collective
+              lorsque l’entreprise entre dans le champ d’application
+              professionnel et géographie défini par la convention et que :
             </p>
             <ul>
               <li>
-                soit une convention collective de branche a été étendue par le
-                Ministère du travail rendant la convention collective
-                obligatoire pour toutes les entreprises qui entrent dans son
-                champ d&apos;application professionnel et géographique.
+                La convention collective de branche a été étendue par le
+                ministère du travail.
               </li>
               <li>
-                soit votre entreprise est signataire ou adhérente à une
-                organisation patronale signataire de la convention collective.
+                L’entreprise est adhérente à une organisation patronale
+                signataire de la convention collective.
               </li>
             </ul>
           </React.Fragment>
