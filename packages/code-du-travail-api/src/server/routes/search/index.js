28cc53c62314d5a9289a844de016c693bffd46d5
Armand Giraud <armand.giraud.ag@gmail.com>
2019-08-05 11:57:59 +0200

    modify search prep

--- packages/code-du-travail-api/src/server/routes/search/index.js
+++ packages/code-du-travail-api/src/server/routes/search/index.js
@@ -18,6 +18,7 @@ const router = new Router({ prefix: API_BASE_URL });
 router.get("/search", async ctx => {
   const query = ctx.request.query.q;
   const response = await fetch(
+    
     `${process.env.NLP_URL}:5000/api/search?q=${query}`
   );
   const results = await response.json();
