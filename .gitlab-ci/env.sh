b1cba482df24098ba5e017f82b89cdebc086c896
Lionel <lionel@lumographe.fr>
2019-08-09 17:58:49 +0200

    ci: add a go prod stage to deploy tag on gitlab (#1055)

--- .gitlab-ci/env.sh
+++ .gitlab-ci/env.sh
@@ -2,30 +2,61 @@
 
 #
 
-export BRANCH_NAME=${BRANCH_NAME:=$CI_COMMIT_REF_NAME}
+export BRANCH_NAME=${BRANCH_NAME:=$CI_COMMIT_REF_SLUG}
+export COMMIT_TAG=${COMMIT_TAG:=$CI_COMMIT_TAG}
 export COMMIT=${COMMIT:=$CI_COMMIT_SHA}
+export ENVIRONMENT=${ENVIRONMENT:="dev.factory"};
+export HASH_SIZE=${HASH_SIZE:=7}
 export JOB_ID=${JOB_ID:=$CI_JOB_ID}
-export HASH_SIZE=${HASH_SIZE}
-BRANCH_NAME_HASHED=$(printf "${BRANCH_NAME}" | sha1sum | cut -c1-${HASH_SIZE} )
+export PROJECT_PATH=${PROJECT_PATH:=$CI_PROJECT_PATH}
+
+BRANCH_NAME_HASHED=$( printf "${BRANCH_NAME}" | sha1sum | cut -c1-${HASH_SIZE} )
 export BRANCH_HASH=${BRANCH_HASH:=$BRANCH_NAME_HASHED}
+
+export DOMAIN="code-du-travail-numerique.${ENVIRONMENT}.social.gouv.fr";
 
 #
 
-case ${BRANCH_NAME} in
-  master)
-    export ENVIRONMENT=incubateur;
-    #
-    export API_HOST=api.code-du-travail-numerique.${ENVIRONMENT}.social.gouv.fr;
-    export FRONTEND_HOST=code-du-travail-numerique.${ENVIRONMENT}.social.gouv.fr;
-    export NLP_HOST=nlp.code-du-travail-numerique.${ENVIRONMENT}.social.gouv.fr;
-    export ELASTICSEARCH_HOST=elasticsearch:${ES_PORT};
-    ;;
-  *)
-    export ENVIRONMENT=dev;
-    #
-    export API_HOST=api.${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
-    export FRONTEND_HOST=${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
-    export NLP_HOST=nlp.${BRANCH_HASH}.code-du-travail-numerique.${ENVIRONMENT}.factory.social.gouv.fr;
-    export ELASTICSEARCH_HOST=elasticsearch-${BRANCH_HASH}:${ES_PORT};
-    ;;
-esac
+#
+# For master branch we keep branch name as branch hash
+if [[ "${BRANCH_NAME}" = "master" ]]; then
+  export BRANCH_HASH=master;
+fi
+
+if [[ -n "${COMMIT_TAG}" ]]; then
+  # For versions we replace the version number v2.3.1 to v2-3-1
+  export BRANCH_HASH=$( printf "${COMMIT_TAG}" | sed "s/\./-/g" );
+fi
+
+if [[ -n "${PRODUCTION+x}" ]]; then
+  export BRANCH_HASH=prod;
+else
+  export DOMAIN="${BRANCH_HASH}.${DOMAIN}";
+fi
+
+#
+
+export API_HOST="api.${DOMAIN}";
+export ELASTICSEARCH_HOST="${K8S_NAMESPACE}-elasticsearch-${BRANCH_HASH}";
+export ELASTICSEARCH_URL="http://${ELASTICSEARCH_HOST}:${ES_PORT}";
+export FRONTEND_HOST="${DOMAIN}";
+export NLP_HOST="nlp.${DOMAIN}";
+
+#
+
+if [[ -n "${PRODUCTION+x}" ]]; then
+  export API_URL="https://${API_HOST}"
+  export FRONTEND_URL="https://${FRONTEND_HOST}"
+  export NLP_URL="https://${NLP_HOST}"
+else
+  export API_URL="http://${API_HOST}"
+  export FRONTEND_URL="http://${FRONTEND_HOST}"
+  export NLP_URL="http://${NLP_HOST}"
+fi
+
+printenv | grep -E \
+  "BRANCH_HASH|BRANCH_NAME|BRANCH_HASH_DOT|COMMIT|COMMIT_TAG|ENVIRONMENT|CLUSTER_NAME|HASH_SIZE|JOB_ID" \
+  | sort
+printenv | grep -E \
+  "API_HOST|API_URL|ELASTICSEARCH_HOST|FRONTEND_HOST|FRONTEND_URL|NLP_HOST|NLP_URL" \
+  | sort
