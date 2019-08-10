bc617473968eadba2e430195bd1b7827faf063d7
Douglas Duteil <douglasduteil@gmail.com>
2019-08-10 19:36:53 +0200

    build(k8s): update data config

--- .k8s/data/create-index.sh
+++ .k8s/data/create-index.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-CDTN_POD_STATUS=$(kubectl get pod cdtn-data"$1")
+CDTN_POD_STATUS=$(kubectl get pod "$1")
 
 # Check if cdtn-data pod exists
-if [ ! "$CDTN_POD_STATUS" ]
-then
-    kubectl apply -f k8s/code-du-travail-data/deployment-cdtn-data.yml
-else
-    kubectl delete po cdtn-data"$1"
-    kubectl apply -f k8s/code-du-travail-data/deployment-cdtn-data.yml
+if [ "$CDTN_POD_STATUS" ]; then
+  kubectl delete po "$1"
 fi
+
+kubectl apply -f ./deployment.yml
