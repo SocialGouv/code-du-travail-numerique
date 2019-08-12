d78186124da7f6dadf346cf1d549e4d8f7b0e51c
ManuC <colasante.manuel@gmail.com>
2019-08-12 13:44:32 +0200

    refactor(data): merge ccn into ES

---
+++ packages/code-du-travail-data/dataset/conventions_collectives/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const slugify = require("slugify");
+
+const conventions = require("@socialgouv/kali-data/data/index.json");
+
+const slugifiedConventions = conventions.map(convention => ({
+  ...convention,
+  slug: slugify(`${convention.num}-${convention.titre}`.substring(0, 80), {
+    lower: true
+  }),
+  url: `https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=${
+    convention.id
+  }`
+}));
+
+fs.writeFileSync(
+  "./ccn-list.json",
+  JSON.stringify(slugifiedConventions, null, 2)
+);
