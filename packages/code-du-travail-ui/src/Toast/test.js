5300e5fa8461382fe762185bdb7266d9e1a67345
Douglas Duteil <douglasduteil@gmail.com>
2019-08-26 23:19:46 +0200

    test(ui): update toast tests

--- packages/code-du-travail-ui/src/Toast/test.js
+++ packages/code-du-travail-ui/src/Toast/test.js
@@ -3,10 +3,18 @@ import { fireEvent, render } from "react-testing-library";
 import Toast from "../Toast";
 
 describe("<Toast />", () => {
-  it("renders", () => {
-    const { container } = render(<Toast kind="warning">Some content</Toast>);
+  it("render default info toast", () => {
+    const { container } = render(<Toast>Default info content</Toast>);
     expect(container).toMatchSnapshot();
   });
+
+  it.each([["warning"], ["success"]])("render %s toast", variant => {
+    const { container } = render(
+      <Toast variant={variant}>{variant} content</Toast>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
   it("Should call onRemove when clicking on remove button if an onRemove prop is provided", () => {
     const onRemove = jest.fn();
     const { container } = render(
@@ -16,4 +24,37 @@ describe("<Toast />", () => {
     fireEvent.click(onRemoveButton);
     expect(onRemove.mock.calls.length).toBe(1);
   });
+
+  it("Should call onRemove after 1 second", () => {
+    jest.useFakeTimers();
+    const onRemove = jest.fn();
+    render(
+      <Toast onRemove={onRemove} timeout={1000}>
+        I disappear in 1 sec.
+      </Toast>
+    );
+
+    jest.advanceTimersByTime(1000);
+
+    expect(onRemove.mock.calls.length).toBe(1);
+  });
+
+  it("Should render alternative wide toast", () => {
+    const { container } = render(
+      <Toast shadow wide>
+        I big and beautiful
+      </Toast>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it.each([["from-top"], ["from-right"], ["from-bottom"], ["from-left"]])(
+    "render with %s animated toast",
+    animation => {
+      const { container } = render(
+        <Toast animate={animation}>{animation} content</Toast>
+      );
+      expect(container).toMatchSnapshot();
+    }
+  );
 });
