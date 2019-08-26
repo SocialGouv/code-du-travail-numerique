2e9ec2e582981ca3eb9f8035c1db112a14f57836
Douglas Duteil <douglasduteil@gmail.com>
2019-08-26 21:29:08 +0200

    fix(frontend): @sentry/browser does not contain a default export (#1188)

--- packages/code-du-travail-frontend/src/__tests__/sentry.test.js
+++ packages/code-du-travail-frontend/src/__tests__/sentry.test.js
@@ -1,4 +1,4 @@
-import Sentry from "@sentry/browser";
+import * as Sentry from "@sentry/browser";
 import { initializeSentry, notifySentry } from "../sentry";
 
 jest.mock("@sentry/browser", () => ({
