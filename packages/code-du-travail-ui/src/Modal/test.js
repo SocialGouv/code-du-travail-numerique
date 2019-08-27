e7e17631635ad26c35feca397d21257524c9f5fa
Douglas Duteil <douglasduteil@gmail.com>
2019-08-27 11:43:18 +0200

    test(ui): move @reach-modal warning prevent to modal test file (#1196)

--- packages/code-du-travail-ui/src/Modal/test.js
+++ packages/code-du-travail-ui/src/Modal/test.js
@@ -3,6 +3,19 @@ import styled from "styled-components";
 import { render, cleanup } from "@testing-library/react";
 import { Modal } from ".";
 
+// HACK(lionelB): trick to prevent @reach-modal warning if styles are not imported
+// jsdom doesn"t support it for now @see https://github.com/jsdom/jsdom/issues/1895
+// We will be able to use :
+// document.documentElement.style.setProperty("--reach-modal", "1");
+// when jsdom support it :)
+//
+// Meanwhile...
+//
+// HACK(douglasduteil): mock the check style function from `@reach/utils`
+// As `@reach/*` packages are using the "checkStyles" function from `@reach/utils`
+// to warn us about missing stylesheet, we silent it with a mock 💩
+require("@reach/utils").checkStyles = jest.fn();
+
 // force cleanup dom since we use portal
 afterEach(cleanup);
 
