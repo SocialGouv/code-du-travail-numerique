0678a0070ac88f7a7e17c8ccbd9b3a1f8160414d
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-08-30 14:43:00 +0200

    fix typo utils

--- packages/code-du-travail-data/dataset/datafiller/utils.js
+++ packages/code-du-travail-data/dataset/datafiller/utils.js
@@ -1,15 +1,14 @@
-
-
 const sortByKey = key => (a, b) => a[key].localeCompare(b[key]);
 
 const getVariants = row => {
   const others =
-    (row.variants && row.variants.split("\n").map(variant => variant.trim())) ||[];
+    (row.variants && row.variants.split("\n").map(variant => variant.trim())) ||
+    [];
   const variants = [row.title.replace("-", " ")].concat(others);
   return [...new Set(variants)];
 };
 
 module.exports = {
-  sortByKey
+  sortByKey,
   getVariants
-}
+};
