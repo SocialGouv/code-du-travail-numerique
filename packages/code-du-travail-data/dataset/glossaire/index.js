3056136cc18f4922c3a642d67653b4c6d2d20f33
LionelB <lionel@lumographe.fr>
2019-09-03 09:39:33 +0200

    fix(data): use datafiller as glossary's datasource

--- packages/code-du-travail-data/dataset/glossaire/index.js
+++ packages/code-du-travail-data/dataset/glossaire/index.js
@@ -1,38 +1,20 @@
-const GoogleSpreadsheets = require("google-spreadsheets");
-const { promisify } = require("util");
-const getCells = promisify(GoogleSpreadsheets.cells);
+const fetch = require("node-fetch");
 
-const SPREADSHEET_KEY = "1WrmotMiu4kBxRTKW47Q3CzNjxc0GycfAilSHcXH_hfA";
+const DATAFILLER_URL =
+  process.env.DATAFILLER_URL || "https://datafiller.num.social.gouv.fr";
 
-const columns = {
-  term: 1,
-  abbrev: 2,
-  synonym: 3,
-  definition: 4,
-  reference: 5
-};
-
-function transformRow(row) {
-  return Object.entries(columns).reduce(
-    (state, [key, colIndex]) => ({
-      ...state,
-      [key]: Object.hasOwnProperty.call(row, colIndex)
-        ? row[colIndex].value
-        : undefined
-    }),
-    {}
-  );
-}
+const RECORDS_URL = `${DATAFILLER_URL}/kinto/v1/buckets/datasets/collections/glossaire/records`;
 
 async function getGlossaire() {
-  const { cells } = await getCells({
-    key: SPREADSHEET_KEY,
-    worksheet: 2
-  });
-  return Object.values(cells)
-    .slice(1)
-    .filter(row => Object.hasOwnProperty.call(row, "1"))
-    .map(transformRow);
+  const response = await fetch(RECORDS_URL);
+  const items = await response.json();
+  return items.data.map(({ title, abbrs, variants, definition, refs }) => ({
+    title,
+    abbrs,
+    variants,
+    definition,
+    refs
+  }));
 }
 
 module.exports = getGlossaire;
