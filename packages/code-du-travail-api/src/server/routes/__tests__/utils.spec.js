0d676f5dc7e473dbd3ca310122df7c96a16764e2
Armand Giraud <armand.giraud.ag@gmail.com>
2019-09-04 19:22:37 +0200

    feat: test utils

--- packages/code-du-travail-api/src/server/routes/__tests__/utils.spec.js
+++ packages/code-du-travail-api/src/server/routes/__tests__/utils.spec.js
@@ -32,8 +32,83 @@ const tests = [
   }
 ];
 
+
+
+const test_key = [
+  {
+    input: [
+      {
+        _source: {
+          slug: "whateverslug",
+          source: "fiches"
+        }
+      }
+    ],
+    expected: [{
+      _source: {
+        slug: "whateverslug",
+        source: "fiches"
+      },
+      key: "ficheswhateverslug"
+    }]
+  },
+  {
+    input: [
+      {
+        _source: {
+          slug: "anotherkey",
+          source: "fiches"
+        }
+      }
+    ],
+
+    expected:[{
+      _source: {
+        slug: "anotherkey",
+        source: "fiches"
+      }, key: "fichesanotherkey"
+    }]
+  }
+];
+
+
+
+const test_dupl = [
+  {
+    input: [
+      {
+        whatever: "whatever",
+        key: "hey"
+      },
+      {
+        otherever: "whatvvero",
+        key: "hey"
+      }
+    ],
+    expected: [{
+      whatever: "whatever",
+      key: "hey"
+    }]
+  }
+];
+
 test("test merge two", () => {
   tests.forEach(t => {
     expect(utils.merge(t.input[0], t.input[1], 10)).toEqual(t.expected);
   });
 });
+
+//console.log(addKey(test_key.input[0]))
+test("test addKey", () => {
+  test_key.forEach(t => {
+    expect(utils.addKey(t.input)).toEqual(t.expected);
+  });
+});
+
+test("test remove duplicates", () => {
+  test_dupl.forEach(t => {
+    expect(utils.removeDuplicate(t.input)).toEqual(t.expected);
+  });
+});
+
+
