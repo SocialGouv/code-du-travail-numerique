777e21fded5a0c580ab2df7cbc8eac35eeed9bea
Armand Giraud <armand.giraud.ag@gmail.com>
2019-09-04 17:16:30 +0200

    fix: test

--- packages/code-du-travail-api/src/server/routes/__tests__/utils.spec.js
+++ packages/code-du-travail-api/src/server/routes/__tests__/utils.spec.js
@@ -32,10 +32,6 @@ const tests = [
   }
 ];
 
-const test_one_missing = [
-  
-];
-
 test("test merge two", () => {
   tests.forEach(t => {
     expect(utils.merge(t.input[0], t.input[1], 10)).toEqual(t.expected);
