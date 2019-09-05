06d8d5750227f271c59d3fff3dbdde73490871c1
LionelB <lionel@lumographe.fr>
2019-09-05 10:23:34 +0200

    fix review

--- packages/code-du-travail-frontend/src/conventions/Search/SearchCC.js
+++ packages/code-du-travail-frontend/src/conventions/Search/SearchCC.js
@@ -8,25 +8,24 @@ const SearchCC = ({ query, render }) => {
   const [results, setResults] = useState();
   const [status, setStatus] = useState("idle");
 
-  async function load(query) {
-    if (query) {
-      setStatus("loading");
-      setResults([]);
-      const results = await loadResults(query);
-      setResults(results);
-      if (results && results.length) {
-        setStatus("success");
-      } else {
-        setStatus("error");
-      }
-    } else {
-      setResults([]);
-      setStatus("idle");
-    }
-  }
-
   // load results when query change
   useEffect(() => {
+    async function load(query) {
+      if (query) {
+        setStatus("loading");
+        setResults([]);
+        const results = await loadResults(query);
+        setResults(results);
+        if (results && results.length) {
+          setStatus("success");
+        } else {
+          setStatus("error");
+        }
+      } else {
+        setResults([]);
+        setStatus("idle");
+      }
+    }
     load(query);
   }, [query]);
 
