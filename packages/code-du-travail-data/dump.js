ddfbec9f990d2b7a42f1b449bd306c91a42a3fe1
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-09-10 17:07:16 +0200

    fix dump

--- packages/code-du-travail-data/dump.js
+++ packages/code-du-travail-data/dump.js
@@ -1,8 +1,6 @@
 import { cdtnDocumentsGen } from "./indexing/populate";
 import { logger } from "./indexing/logger";
 
-const fs = require("fs");
-//
 logger.silent = true;
 
 let slugs = [];
@@ -16,10 +14,8 @@ for (const documents of cdtnDocumentsGen()) {
     }))
   );
 }
-//eslint-disable-next-line no-console
+
 const json = JSON.stringify(slugs, 0, 2);
 
-fs.writeFile("dump.json", json, "utf8", function(err) {
-  if (err) throw err;
-  console.log("Dump created..");
-});
+//eslint-disable-next-line no-console
+console.log(json);
