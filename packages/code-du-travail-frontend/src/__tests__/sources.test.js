58fe8b1814436fe6cd6ae4dbf1d073ca9cc416f3
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-09-16 17:08:12 +0200

    fix: remove old test

--- packages/code-du-travail-frontend/src/__tests__/sources.test.js
+++
@@ -1,9 +0,0 @@
-import { getExcludeSources } from "../sources";
-
-test("getExcludeSources should return empty array if value is empty", () => {
-  expect(getExcludeSources("")).toMatchSnapshot();
-});
-
-test("getExcludeSources should return all sources execpt one that match value", () => {
-  expect(getExcludeSources("faq")).toMatchSnapshot();
-});
