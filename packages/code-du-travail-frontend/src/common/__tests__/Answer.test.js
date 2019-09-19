dc4580fa5d5030ea77e10a1254a70bda44340dd1
LionelB <lionel@lumographe.fr>
2019-09-19 14:35:21 +0200

    test(glossary): add tooltip tests

--- packages/code-du-travail-frontend/src/common/__tests__/Answer.test.js
+++ packages/code-du-travail-frontend/src/common/__tests__/Answer.test.js
@@ -41,4 +41,20 @@ describe("<Answer />", () => {
     );
     expect(container).toMatchSnapshot();
   });
+  it("should render tooltip", () => {
+    const { container } = render(
+      <Answer
+        title="Article du code"
+        intro="intro de l'article"
+        html="<p class='test-content'>Contenu au format <strong>html</strong> contenant des tooltip sur certains mot comme rescrit</p>"
+        footer="pied de page"
+        date="03/11/1979"
+        sourceType="social groove"
+        icon={icons.Question}
+      >
+        <div>Contenu supplémentaire</div>
+      </Answer>
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
