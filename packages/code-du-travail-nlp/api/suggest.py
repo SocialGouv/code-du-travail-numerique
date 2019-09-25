9665d6983b1d8b17f20c534c6b192ef8645f24a3
Armand Giraud <armand.giraud.ag@gmail.com>
2019-09-25 19:04:18 +0200

    fix print and add_ready method

--- packages/code-du-travail-nlp/api/suggest.py
+++ packages/code-du-travail-nlp/api/suggest.py
@@ -15,8 +15,6 @@ def load_in_background(is_ready, queries_path, stops_path):
   is_ready.data['suggester'] = suggester
   is_ready.ready['suggester'] = True
 
-
-
 def add_suggest(app, is_ready, queries_path, stops_path):
 
   thread = threading.Thread(target=load_in_background, args=(is_ready, queries_path, stops_path))
@@ -27,6 +25,7 @@ def add_suggest(app, is_ready, queries_path, stops_path):
   def suggest():
     is_ready.check_status('suggester')
     suggester = is_ready.get('suggester')
+    print("suggester is ready...")
     input = request.args.get('q')
     results =  suggester.auto_suggest_fast(input, nb_next_words=12)
     results = [r[0] for r in results]
