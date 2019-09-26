9f3db94691a5d629cb82b614e237b06412feb09b
LionelB <lionel@lumographe.fr>
2019-09-26 14:13:28 +0200

    fix(nlp): rename add_search

--- packages/code-du-travail-nlp/api/ready.py
+++ packages/code-du-travail-nlp/api/ready.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from flask_cors import CORS, cross_origin
 
 
-def add_search(app, nlp):
+def add_ready(app, nlp):
     @app.route('/api/ready', methods=['GET'])
     @cross_origin()
     def ready():
