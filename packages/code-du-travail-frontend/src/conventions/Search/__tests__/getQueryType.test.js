d82c93dc51d4cb8eea0988f3372def6417d7cc11
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-10-03 09:47:26 +0200

    lint

--- packages/code-du-travail-frontend/src/conventions/Search/__tests__/getQueryType.test.js
+++ packages/code-du-travail-frontend/src/conventions/Search/__tests__/getQueryType.test.js
@@ -1,12 +1,12 @@
 import getQueryType from "../getQueryType";
 
 test.each`
-  query               | expected
-  ${`carrefour paris`}| ${`text`}
-  ${`airbus`}         | ${`text`}
-  ${`148`}            | ${`idcc`}
-  ${`1486`}           | ${`idcc`}
-  ${`14861486977779`} | ${`siret`}
+  query                | expected
+  ${`carrefour paris`} | ${`text`}
+  ${`airbus`}          | ${`text`}
+  ${`148`}             | ${`idcc`}
+  ${`1486`}            | ${`idcc`}
+  ${`14861486977779`}  | ${`siret`}
 `("returns $expected when $query", ({ query, expected }) => {
   expect(getQueryType(query)).toBe(expected);
 });
