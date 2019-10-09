26f4f46b40701d3b811c3086ef7b4a5f992dbde0
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-10-09 14:56:22 +0200

    test transpile modules

--- packages/code-du-travail-frontend/next.config.js
+++ packages/code-du-travail-frontend/next.config.js
@@ -1,26 +1,32 @@
 const withSourceMaps = require("@zeit/next-source-maps");
+const withTM = require("next-transpile-modules");
 
-module.exports = withSourceMaps({
-  webpack: (config, { dev }) => {
-    config.module.rules.push({
-      test: /\.test.js$/,
-      loader: "ignore-loader"
-    });
-    return config;
-  },
-  // https://github.com/zeit/next.js/#disabling-file-system-routing
-  useFileSystemPublicRoutes: true,
-  publicRuntimeConfig: {
-    API_ADDRESS: "https://api-adresse.data.gouv.fr/search",
-    API_SIRET2IDCC_URL:
-      process.env.API_SIRET2IDCC_URL || "https://siret2idcc.num.social.gouv.fr",
-    API_URL: process.env.API_URL || "http://127.0.0.1:1337/api/v1",
-    FRONTEND_PORT: process.env.FRONTEND_PORT,
-    NODE_ENV: process.env.NODE_ENV,
-    PACKAGE_VERSION: process.env.VERSION || require("./package.json").version,
-    PIWIK_SITE_ID: process.env.PIWIK_SITE_ID,
-    PIWIK_URL: process.env.PIWIK_URL,
-    SENTRY_PUBLIC_DSN: process.env.SENTRY_PUBLIC_DSN,
-    SUGGEST_URL: process.env.SUGGEST_URL || "http://127.0.0.1:5000/api/suggest"
-  }
-});
+module.exports = withSourceMaps(
+  withTM({
+    webpack: config => {
+      config.module.rules.push({
+        test: /\.test.js$/,
+        loader: "ignore-loader"
+      });
+      return config;
+    },
+    // https://github.com/zeit/next.js/#disabling-file-system-routing
+    useFileSystemPublicRoutes: true,
+    publicRuntimeConfig: {
+      API_ADDRESS: "https://api-adresse.data.gouv.fr/search",
+      API_SIRET2IDCC_URL:
+        process.env.API_SIRET2IDCC_URL ||
+        "https://siret2idcc.num.social.gouv.fr",
+      API_URL: process.env.API_URL || "http://127.0.0.1:1337/api/v1",
+      FRONTEND_PORT: process.env.FRONTEND_PORT,
+      NODE_ENV: process.env.NODE_ENV,
+      PACKAGE_VERSION: process.env.VERSION || require("./package.json").version,
+      PIWIK_SITE_ID: process.env.PIWIK_SITE_ID,
+      PIWIK_URL: process.env.PIWIK_URL,
+      SENTRY_PUBLIC_DSN: process.env.SENTRY_PUBLIC_DSN,
+      SUGGEST_URL:
+        process.env.SUGGEST_URL || "http://127.0.0.1:5000/api/suggest"
+    },
+    transpileModules: ["@cdt/sources", "@cdt/data"]
+  })
+);
