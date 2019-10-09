38f180f20eb93b2b71bff1fc8305391ccb16bf8e
ManuC <colasante.manuel@gmail.com>
2019-10-09 16:59:28 +0200

    fix(frontend): dumb linter

--- packages/code-du-travail-frontend/test/utils.js
+++ packages/code-du-travail-frontend/test/utils.js
@@ -20,7 +20,7 @@ const customRender = (ui, options) =>
   render(ui, { wrapper: AllTheProviders, ...options });
 
 // re-export everything
-export * from "@testing-library/react";
+export { fireEvent, getByTitle, wait, waitForElement } from "@testing-library/react";
 
 // override render method
 export { customRender as render };
