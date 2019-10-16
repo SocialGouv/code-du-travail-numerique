f93df9eadb8fd2d840dfbbd70d380ea80992eb00
LionelB <lionel@lumographe.fr>
2019-10-16 18:28:38 +0200

    test(ui): update Sectiontitle snapshot name

--- packages/react-ui/src/SectionTitle/test.js
+++ packages/react-ui/src/SectionTitle/test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { SectionTitle } from ".";
 
-describe("<List />", () => {
+describe("<SectionTitle />", () => {
   test("should render title with description", () => {
     const { container } = render(
       <SectionTitle desc="Hello">Lorem Ipsum</SectionTitle>
