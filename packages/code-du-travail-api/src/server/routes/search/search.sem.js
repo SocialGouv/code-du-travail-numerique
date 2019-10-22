924e327bcef8f55da264714544e0e70e8354fc55
ManuC <colasante.manuel@gmail.com>
2019-10-22 15:38:56 +0200

    fix(api): case where description was not returned

--- packages/code-du-travail-api/src/server/routes/search/search.sem.js
+++ packages/code-du-travail-api/src/server/routes/search/search.sem.js
@@ -4,7 +4,15 @@ function getSemQuery({ query_vector, size, sources = [] }) {
   }
   return {
     size: size,
-    _source: ["title", "source", "slug", "anchor", "url", "breadcrumbs"],
+    _source: [
+      "title",
+      "source",
+      "slug",
+      "description",
+      "anchor",
+      "url",
+      "breadcrumbs"
+    ],
     query: {
       script_score: {
         query: {
