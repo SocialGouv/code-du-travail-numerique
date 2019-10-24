d681f7db69538dc8042385ee1d0237a0b768e6cb
Nicolas KREMER <nico.kremer@gmail.com>
2019-10-24 13:56:35 +0200

    wip(frontend):Added message when cc is not yet processed

--- packages/code-du-travail-frontend/src/outils/IndemnitePrecarite/steps/InfosCCn.js
+++ packages/code-du-travail-frontend/src/outils/IndemnitePrecarite/steps/InfosCCn.js
@@ -1,14 +1,11 @@
-import { StepInfoCCnMandatory } from "../../common/InfosCCn";
-import data from "@cdt/data...preavis-demission/data.json";
+import data from "@cdt/data...prime-precarite/precarite.data.json";
+import { StepInfoCCnOptionnal } from "../../common/InfosCCn";
 
 import { isNotYetProcessed } from "../../common/situations.utils";
 
-StepInfoCCnMandatory.validate = values => {
+StepInfoCCnOptionnal.validate = values => {
   const errors = {};
   const { ccn } = values;
-  if (Object.keys(values).length === 0) {
-    errors.ccn = "Veuillez renseigner votre convention collective ?";
-  }
   if (ccn && isNotYetProcessed(data, ccn.num)) {
     errors.ccn =
       "Nous n’avons pas encore traité votre convention collective mais nous vous invitons à poursuivre la simulation afin d’obtenir le montant défini par le Code du travail.";
@@ -16,4 +13,4 @@ StepInfoCCnMandatory.validate = values => {
   return errors;
 };
 
-export const StepInfoCCn = StepInfoCCnMandatory;
+export const StepInfoCCn = StepInfoCCnOptionnal;
