8cc35167b79425139578d949c383a2f7e1c61105
Armand Giraud <armand.giraud.ag@gmail.com>
2019-10-24 14:40:10 +0200

    fix(nlp) remove unused code and change tf image

--- packages/code-du-travail-nlp/api/sem_search.py
+++ packages/code-du-travail-nlp/api/sem_search.py
@@ -3,17 +3,14 @@ import logging
 import json
 import tensorflow as tf
 import tensorflow_hub as hub
-import numpy as np
 import tf_sentencepiece
 import unicodedata
-from typing import List
 import time
 
 stops_path = "../data/stops.txt"
 
 cache_path = "~/Downloads"
 
-stringvec = List[str]
 logger = logging.getLogger("gunicorn.errror")
 
 
@@ -63,12 +60,6 @@ class SemSearch():
         session.run(init_op)
         self.session = session
 
-    def build_responses(self):
-        responses = [(self.remove_stops(self.strip_accents(t)))
-                     for t in self.titles]
-        self.response_results = self.session.run(self.response_embeddings, {self.r_placeholder: responses,
-                                                                            self.c_placeholder: self.context})
-
     def compute_vector(self, string, context):
         cleanStr = self.remove_stops(self.strip_accents(string))
         out = self.session.run(self.response_embeddings, {self.r_placeholder: [
@@ -80,49 +71,6 @@ class SemSearch():
         out = self.session.run(self.response_embeddings, {
                                self.r_placeholder: cleanStr, self.c_placeholder: contexts})
         return out["outputs"].tolist()
-
-    def predict_slugs(self, query: str):
-        query = self.remove_stops(self.strip_accents(query))
-        questions = [query]
-
-        self.question_results = self.session.run(
-            self.question_embeddings, {self.q_placeholder: questions})
-        return self.question_results["outputs"].squeeze().tolist()
-
-    def filter_sources(self, slug_list, exclude_sources):
-        if exclude_sources:
-            return [s for s in slug_list if s["source"] not in exclude_sources.split(',')]
-        else:
-            return slug_list
-
-    def slug2source(self, slug):
-        source, _ = slug.split("/")[1:]
-        return source
-
-    def _return_hit(self, slug: stringvec, title: stringvec, score: float):
-        """simple utility to return a dict from slugs, titles and score"""
-        source, slug_short = slug.split("/")[1:]
-        return {
-            "_source": {
-                # need clean source in format fiches_service_public...
-                "source": source,
-                "slug": slug_short,
-                "title": title,
-                # float necessary to make numpy float json serializable
-                "_score_sem": float(score)
-            },
-            "_id": slug.replace("/", "")
-
-        }
-
-    def compute_scores(self, content_path):
-        with open(content_path, "r") as f:
-            data = json.load(f)
-            data = list(map(add_slash, data))
-
-            return [
-                dict(item, vector_title=self.response_results["outputs"][self.slugs.index(item['slug'])].tolist()) for item in tqdm.tqdm(data) if item["slug"] in self.slugs
-            ]
 
     def strip_accents(self, s: str):
         return unicodedata.normalize('NFD', s).encode('ascii', 'ignore').decode('utf-8')
