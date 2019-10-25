c72f37ff587193a2a5aab6bf50f3a2a47ccc6cf8
Lionel <lionel@lumographe.fr>
2019-10-25 15:51:37 +0200

    Fix/1511 ui regression searchcc (#1566)
    * fix(ccn): update section title in ccn modal
    
    fix #1511
    
    * feat(ccn): trim space when  detect query type
    
    fix #1563

--- packages/code-du-travail-frontend/src/conventions/Search/__tests__/getQueryType.test.js
+++ packages/code-du-travail-frontend/src/conventions/Search/__tests__/getQueryType.test.js
@@ -1,12 +1,13 @@
 import getQueryType from "../getQueryType";
 
 test.each`
-  query                | expected
-  ${`carrefour paris`} | ${`text`}
-  ${`airbus`}          | ${`text`}
-  ${`148`}             | ${`idcc`}
-  ${`1486`}            | ${`idcc`}
-  ${`14861486977779`}  | ${`siret`}
+  query                   | expected
+  ${`carrefour paris`}    | ${`text`}
+  ${`airbus`}             | ${`text`}
+  ${`148`}                | ${`idcc`}
+  ${`1486`}               | ${`idcc`}
+  ${`14861486977779`}     | ${`siret`}
+  ${` 821 980 877 00020`} | ${`siret`}
 `("returns $expected when $query", ({ query, expected }) => {
   expect(getQueryType(query)).toBe(expected);
 });
