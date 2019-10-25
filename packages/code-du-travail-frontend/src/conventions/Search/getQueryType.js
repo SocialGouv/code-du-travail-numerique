ad52b8cfa0b4a0d996f0268b71ba3b3530d379ab
LionelB <lionel@lumographe.fr>
2019-10-25 11:43:14 +0200

    feat(ccn): trim space when  detect query type
    fix #1563

--- packages/code-du-travail-frontend/src/conventions/Search/getQueryType.js
+++ packages/code-du-travail-frontend/src/conventions/Search/getQueryType.js
@@ -1,12 +1,13 @@
 // detect query type (IA level++)
 const getQueryType = str => {
   if (str.match(/^[\d .-]+$/) && str.length >= 2) {
-    if (str.length <= 4) {
+    const strClean = str.replace(/[\s .-]/g, "");
+    if (strClean.length <= 4) {
       return "idcc";
       // todo: SIREN not implemented yet
       // } else if (str.length === 9) {
       //   return "siren";
-    } else if (str.length === 14) {
+    } else if (strClean.length === 14) {
       return "siret";
     }
     return;
