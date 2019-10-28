4bcbdbd42c8174b460244cc44d7b6576889c0bee
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-10-29 00:53:25 +0100

    feat(server): force 301 to main hostname in production

--- packages/code-du-travail-frontend/server/server.js
+++ packages/code-du-travail-frontend/server/server.js
@@ -11,6 +11,7 @@ const LIVEPROD = process.env.ENVIRONMENT === "production";
 const PORT = process.env.FRONTEND_PORT || 3000;
 const FRONTEND_HOST = process.env.FRONTEND_HOST || `http://localhost:${PORT}`;
 const dev = process.env.NODE_ENV !== "production";
+const PROD_HOSTNAME = process.env.PROD_HOSTNAME || "code.travail.gouv.fr";
 
 const app = next({ dev });
 const handler = app.getRequestHandler();
@@ -24,27 +25,40 @@ const disallowRobots = (_, res, next) => {
   next();
 };
 
-const robotTxtHandler = (req, res) => {
-  const robotsProd = [
-    "User-agent: *",
-    "Disallow: /assets/",
-    "Disallow: /images/",
-    "",
-    `Sitemap: ${FRONTEND_HOST}/sitemap.xml`
-  ].join("\n");
-  const robotsDev = ["User-agent: *", "Disallow: /"].join("\n");
+const robotsProd = [
+  "User-agent: *",
+  "Disallow: /assets/",
+  "Disallow: /images/",
+  "",
+  `Sitemap: ${FRONTEND_HOST}/sitemap.xml`
+].join("\n");
 
+const robotsDev = ["User-agent: *", "Disallow: /"].join("\n");
+
+const robotTxtHandler = (req, res) => {
   res
     .status(200)
     .set("Content-Type", "text/plain")
     .send(LIVEPROD ? robotsProd : robotsDev);
 };
 
+const redirectHostname = (req, res, next) => {
+  const isProdUrl = req.get("Host") === PROD_HOSTNAME;
+  if (!isProdUrl) {
+    return res.redirect(301, `https://${PROD_HOSTNAME}${req.originalUrl}`);
+  }
+  return next();
+};
+
 app.prepare().then(() => {
-  const server = express().get("/robots.txt", robotTxtHandler);
+  const server = express();
+
+  server.get("/robots.txt", robotTxtHandler);
   server.use(expressSitemap(getUrls, FRONTEND_HOST));
 
-  if (!LIVEPROD) {
+  if (LIVEPROD) {
+    server.use(redirectHostname);
+  } else {
     server.use(disallowRobots);
   }
   server.use(handler);
