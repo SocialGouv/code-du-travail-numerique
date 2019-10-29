047d709e2d80676bb42ca44e9f06714c77989d05
Nicolas KREMER <nico.kremer@gmail.com>
2019-10-29 12:51:59 +0100

    feat(frontend):Added cookie consent toast to all pages

---
+++ packages/code-du-travail-frontend/src/common/CookieConsent.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect, useCallback } from "react";
+import styled from "styled-components";
+import cookie from "js-cookie";
+import { Toast, theme } from "@socialgouv/react-ui";
+
+const CookieConsent = () => {
+  const [isCookieConsentHidden, setCookieConsentHidden] = useState(true);
+  useEffect(() => {
+    setCookieConsentHidden(cookie.get("cookieConsent"));
+  }, []);
+  const onToastRemove = useCallback(() => {
+    setCookieConsentHidden(true);
+    cookie.set("cookieConsent", true, { expires: 365 });
+  }, []);
+
+  if (isCookieConsentHidden) return null;
+
+  return (
+    <Wrapper>
+      <Toast
+        variant="info"
+        wide
+        shadow
+        animate="from-bottom"
+        onRemove={onToastRemove}
+      >
+        En poursuivant votre navigation sur ce site, vous acceptez l’utilisation
+        de cookies pour établir des mesures de fréquentation et d’utilisation du
+        site.
+        <A
+          title="Mentions legales"
+          rel="noopener noreferrer"
+          href="http://master.code-du-travail-numerique.dev.factory.social.gouv.fr/mentions-legales"
+        >
+          En savoir plus
+        </A>
+      </Toast>
+    </Wrapper>
+  );
+};
+
+export default CookieConsent;
+
+const { spacing } = theme;
+
+const Wrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  padding: ${spacing.base};
+  width: 100%;
+  z-index: 1000;
+`;
+
+const A = styled.a`
+  padding: 0 ${spacing.tiny};
+`;
