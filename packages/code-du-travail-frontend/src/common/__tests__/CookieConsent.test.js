159807eecc64d9e449b7e6e3e9bf7284ab9cc3c0
Nicolas KREMER <nico.kremer@gmail.com>
2019-10-29 16:52:38 +0100

    test(frontend):Updated cookie exist test

--- packages/code-du-travail-frontend/src/common/__tests__/CookieConsent.test.js
+++ packages/code-du-travail-frontend/src/common/__tests__/CookieConsent.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import cookie from "js-cookie";
 import CookieConsent from "../CookieConsent";
 
 describe("<CookieConsent />", () => {
   it("should render", () => {
+    const { container } = render(<CookieConsent />);
+    expect(container).toMatchSnapshot();
+  });
+  it("should not render if cookie exist", () => {
+    cookie.get = jest.fn().mockImplementation(() => true);
     const { container } = render(<CookieConsent />);
     expect(container).toMatchSnapshot();
   });
