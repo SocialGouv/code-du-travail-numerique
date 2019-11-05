04657f9dd77381592e4c5db4d74b6818619fcb05
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-11-05 09:26:50 +0100

    fix(ci): add --org to snyk scan (#1629)
    prevent limited scans

--- scripts/snyk-docker-scan.sh
+++ scripts/snyk-docker-scan.sh
@@ -12,6 +12,6 @@ declare -a images=("data" "api" "frontend" "nlp")
 for image in "${images[@]}"
 do
   docker pull "$REGISTRY"/code-du-travail-numerique/"$image":"$CI_COMMIT_SHA"
-  snyk test --docker "$REGISTRY"/code-du-travail-numerique/"$image":"$CI_COMMIT_SHA" --file=./packages/code-du-travail-"$image"/Dockerfile
+  snyk test --org=SocialGouv --docker "$REGISTRY"/code-du-travail-numerique/"$image":"$CI_COMMIT_SHA" --file=./packages/code-du-travail-"$image"/Dockerfile
 done
 
