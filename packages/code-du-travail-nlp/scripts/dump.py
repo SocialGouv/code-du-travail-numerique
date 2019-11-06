4a42de14b97a1a2615f2cbfece21049c470ae03b
RemiM <rmelisson@gmail.com>
2019-11-06 10:30:12 +0100

    remove python suggester : scissors are too big

---
+++ packages/code-du-travail-nlp/scripts/dump.py
@@ -0,0 +1,36 @@
+import os
+import json
+import time
+from api.sem_search import SemSearch
+import logging
+
+logger = logging.getLogger("nlp")
+logger.setLevel(logging.INFO)
+
+data_path = os.path.join(
+    os.path.dirname(os.path.abspath(__name__)),
+    "data"
+)
+
+stops_path = os.path.join(data_path, 'stops.txt')
+dump_path = os.getenv("DATA_DUMP", os.path.join(data_path, 'dump.json'))
+
+
+with open(dump_path, "r") as dump:
+    documents = json.load(dump)
+    start = time.time()
+    logger.info("Init nlp dump 🦄")
+    ss = SemSearch(stops_path)
+    endSem = time.time()
+    logger.info("SemSearch ready in {:.2f}sec⚡️".format(endSem - start))
+    documents = [dict(d, text="") if d.get("source") == "themes" else d
+                 for d in documents]
+    for document in documents:
+        if document.get("source") != "code_du_travail":
+            document["title_vector"] = ss.compute_vector(
+                document.get("title"), document.get("text"))
+
+    with open(dump_path.replace(".json", ".tf.json"), 'w') as fp:
+        json.dump(documents, fp, ensure_ascii=False)
+    end = time.time()
+    logger.info("Dump with vectors done in {:.2f}sec 🤖".format(end-start))
---
+++ packages/code-du-travail-nlp/scripts/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+set -exu pipefail
+
+
+
+# start the serveur
+gunicorn -t 3000 --threads 4 -b :${NLP_PORT} "api:create_app()"
+
+
