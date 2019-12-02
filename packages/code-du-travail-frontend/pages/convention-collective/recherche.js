29bb333483488619f599f015db016fafa8f550e6
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-12-02 18:07:29 +0100

    feat: add /convention-collective/recherche

---
+++ packages/code-du-travail-frontend/pages/convention-collective/recherche.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container, PageTitle, Section, Wrapper } from "@socialgouv/react-ui";
+import { Layout } from "../../src/layout/Layout";
+import Metas from "../../src/common/Metas";
+import ConventionForm from "../../src/conventions/Search/Form";
+
+const SearchConvention = ({ ogImage, pageUrl }) => (
+  <Layout currentPage="about">
+    <Metas
+      url={pageUrl}
+      title="Recherche de convention collective"
+      description="Recherchez une convention collective par Entreprise, SIRET, Nom ou numéro IDCC."
+      image={ogImage}
+    />
+    <Section>
+      <Container narrow>
+        <Wrapper variant="light">
+          <PageTitle>Recherchez votre convention collective</PageTitle>
+          <ConventionForm />
+        </Wrapper>
+      </Container>
+    </Section>
+  </Layout>
+);
+
+export default SearchConvention;
