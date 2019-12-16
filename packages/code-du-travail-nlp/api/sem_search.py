1d9d41cb43fb8f6090115e058805f0cd45fa9e16
RemiM <rmelisson@gmail.com>
2019-12-16 09:57:31 +0100

    TF2 : first attempt

--- packages/code-du-travail-nlp/api/sem_search.py
+++ packages/code-du-travail-nlp/api/sem_search.py
@@ -1,11 +1,10 @@
-
 import logging
 import json
-import tensorflow as tf
-import tensorflow_hub as hub
-import tf_sentencepiece
 import unicodedata
 import time
+import tensorflow.compat.v2 as tf
+import tensorflow_hub as hub
+from tensorflow_text import SentencepieceTokenizer
 
 stops_path = "../data/stops.txt"
 
@@ -38,47 +37,65 @@ class SemSearch():
         logger.info("SemSearch ready in {} secondes ✅".format(end-start))
 
     def load_graph(self):
-        g = tf.Graph()
-        with g.as_default():
-            self.q_placeholder = tf.placeholder(tf.string, shape=[None])
-            self.r_placeholder = tf.placeholder(tf.string, shape=[None])
-            self.c_placeholder = tf.placeholder(tf.string, shape=[None])
-            module = hub.Module(
-                "https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/1")
-            self.question_embeddings = module(
-                dict(input=self.q_placeholder),
-                signature="question_encoder", as_dict=True)
+        #g = tf.Graph()
+        #with g.as_default():
+        #self.q_variable = tf.Variable("init_q", "q")
+        #self.r_variable = tf.placeholder("init_r", "r")
+        #self.c_variable = tf.placeholder("init_c", "c")
+        #module_url = "https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3" #@param ["https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3", "https://tfhub.dev/google/universal-sentence-encoder-qa/3"]
+        module = hub.load('https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3')
+        self.model = module
 
-            self.response_embeddings = module(
-                dict(input=self.r_placeholder,
-                     context=self.c_placeholder),
-                signature="response_encoder", as_dict=True)
-            init_op = tf.group(
-                [tf.global_variables_initializer(), tf.tables_initializer()])
-        g.finalize()
-        session = tf.Session(graph=g)
-        session.run(init_op)
-        self.session = session
+        # self.model = hub.load(module_url)
+        #self.question_embeddings = module.signatures['question_encoder'](self.q_variable)
+
+        # module(
+        # dict(input=self.q_placeholder),
+        # signature="question_encoder", as_dict=True)
+
+        #self.response_embeddings = module.signatures['response_encoder'](input=self.r_variable, context=self.c_variable)
+        # self.response_embeddings = module(
+        # dict(input=self.r_placeholder,
+        #  context=self.c_placeholder),
+        # signature="response_encoder", as_dict=True)
+        #init_op = tf.group(
+        #    [tf.global_variables_initializer(), tf.tables_initializer()])
+        #g.finalize()
+        #session = tf.Session(graph=g)
+        #session.run(init_op)
+        #self.session = session
 
     def predict_query_vector(self, query: str):
         query = self.remove_stops(self.strip_accents(query))
-        questions = [query]
+        tf_query = tf.constant([query])
 
-        self.question_results = self.session.run(
-            self.question_embeddings, {self.q_placeholder: questions})
+        result = self.model.signatures['question_encoder'](tf_query)['outputs'][0].numpy().tolist()
 
-        return self.question_results["outputs"].squeeze().tolist()
+        return(result)
 
+        # self.question_results = self.session.run(
+        # self.question_embeddings, {self.q_placeholder: questions})
+
+        # return self.question_results["outputs"].squeeze().tolist()
+    
+    ''' not used :
     def compute_vector(self, string, context):
         cleanStr = self.remove_stops(self.strip_accents(string))
         out = self.session.run(self.response_embeddings, {self.r_placeholder: [
                                cleanStr], self.c_placeholder: [context]})
         return out["outputs"].squeeze().tolist()
+    '''
 
     def compute_batch_vectors(self, strings, contexts):
         cleanStr = [self.remove_stops(self.strip_accents(s)) for s in strings]
-        out = self.session.run(self.response_embeddings, {
-                               self.r_placeholder: cleanStr, self.c_placeholder: contexts})
+            
+        tf_title = tf.constant(cleanStr)
+        tf_context = tf.constant(contexts)
+
+        out = self.model.signatures['response_encoder'](input=tf_title, context=tf_context)
+
+        # out = self.session.run(self.response_embeddings, {
+                            #    self.r_placeholder: cleanStr, self.c_placeholder: contexts})
         return out["outputs"].tolist()
 
     def strip_accents(self, s: str):
