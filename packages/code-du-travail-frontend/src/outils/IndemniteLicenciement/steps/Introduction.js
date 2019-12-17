13a51368d87f531924876d63298fd365bcb84f16
Lionel <lionel@lumographe.fr>
2019-12-17 10:00:00 +0100

    fix(idl): hide ccn paragraph (#1990)

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Introduction.js
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Introduction.js
@@ -7,12 +7,12 @@ const StepIntro = () => (
       Ce simulateur est un outil qui permet d’estimer le montant de l’indemnité
       minimale de licenciement d’un salarié.
     </p>
-    <p>
+    {/* <p>
       Vous pourrez simuler simplement le montant de l’indemnité légale définie
       par le Code du travail et le montant de l’indemnité conventionnelle de
       licenciement si la branche d’activité prévoit un montant supérieur au Code
       du travail.
-    </p>
+    </p> */}
     <p>
       Cette simulation nécessite entre 5 et 10 minutes. Afin de pouvoir remplir
       les renseignements demandés, munissez-vous des informations relatives au
