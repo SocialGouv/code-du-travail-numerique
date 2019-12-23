9d6d5d5034cb660f4a954c8dab733f32d82112c6
LionelB <lionel@lumographe.fr>
2019-12-23 18:08:49 +0100

    fix(contrib): add name to search ccn input

--- packages/code-du-travail-frontend/src/conventions/Search/Form.js
+++ packages/code-du-travail-frontend/src/conventions/Search/Form.js
@@ -111,7 +111,7 @@ const Search = ({
         placeholder="Nom d'entreprise, SIRET, nom de convention collective"
         value={query}
         type="search"
-        // name="q"
+        name="q"
         onChange={onInputChange}
       />
       <SearchCC
