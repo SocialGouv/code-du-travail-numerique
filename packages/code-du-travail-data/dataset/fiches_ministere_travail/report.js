63366b2f44ac3068a75686631545e9c3d735c4dc
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-01-09 10:23:17 +0100

    Revert "feat(CI): automate fiches-MT fetch (#2171)" (#2206)
    This reverts commit 8371c29415ebf5e2496b441af9646f5f5e7dcbb1.

--- packages/code-du-travail-data/dataset/fiches_ministere_travail/report.js
+++ packages/code-du-travail-data/dataset/fiches_ministere_travail/report.js
@@ -2,13 +2,9 @@ const fiches = require("./fiches-mt.json");
 
 const hasNoTheme = fiche =>
   !fiche.breadcrumbs || fiche.breadcrumbs.length === 0;
+const printFicheLink = fiche => console.log(`- [${fiche.title}](${fiche.url})`);
 
 const themeLessFiches = fiches.filter(hasNoTheme);
-console.log(`
-### Fiches travail.gouv.fr sans thème : ${themeLessFiches.length}/${
-  fiches.length
-}
+console.log(`### ${themeLessFiches.length}/${fiches.length} fiches sans theme`);
 
-${themeLessFiches.map(fiche => ` - [${fiche.title}](${fiche.url})`).join("\n")}
-
-`);
+themeLessFiches.forEach(printFicheLink);
