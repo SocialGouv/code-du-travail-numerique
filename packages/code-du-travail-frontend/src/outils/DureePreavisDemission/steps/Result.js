f19a5bf2ff95275c90fdb72e41257ba2e73ed613
LionelB <lionel@lumographe.fr>
2020-01-09 12:15:14 +0100

    fix(tools): update results wording

--- packages/code-du-travail-frontend/src/outils/DureePreavisDemission/steps/Result.js
+++ packages/code-du-travail-frontend/src/outils/DureePreavisDemission/steps/Result.js
@@ -25,66 +25,72 @@ function StepResult({ form }) {
     refUrl:
       "https://www.legifrance.gouv.fr/affichCodeArticle.do?cidTexte=LEGITEXT000006072050&idArticle=LEGIARTI000006901174"
   };
-  // No ccn selected
-  if (idcc === 0) {
+
+  // No ccn selected or UunhandledCC
+  if (idcc === 0 || possibleSituations.length === 0) {
+    let result =
+      "Aucun résultat : la convention collective n'a pas encore été traitée par nos services";
+    if (idcc === 0) {
+      result =
+        "Aucun résultat : la convention collective n'a pas été renseignée.";
+    }
     return (
       <>
-        <SectionTitle>Pas de CC</SectionTitle>
+        <SectionTitle>Durée du préavis</SectionTitle>
+        <Highlight>{result}</Highlight>
+        <p>
+          Le code du travail ne prévoit pas de durée de préavis de démission
+          sauf, cas particuliers. Nous vous invitons donc à consulter le contenu
+          de la convention collective.
+        </p>
+        <Toast>
+          <p>
+            Un accord collectif d’entreprise peut également prévoir une durée de
+            préavis de démission différente de celle de la convention
+            collective, qu’elle soit inférieure ou supérieure. Dans ce cas, la
+            durée de préavis de démission prévu par l’accord d’entreprise doit
+            s’appliquer.
+          </p>
+          <p>
+            Dans tous les cas, le contrat de travail peut toujours prévoir une
+            durée de préavis de démission plus courte que la convention
+            collective, l’accord collectif ou un usage. Il faudra alors
+            l’appliquer en priorité.
+          </p>
+        </Toast>
+        <SectionTitle>Source</SectionTitle>
         {getRef([refLegal])}
       </>
     );
   }
   // CCn Selected
-  switch (possibleSituations.length) {
-    case 1: {
-      const [situation] = possibleSituations;
-      const { idcc } = situation;
-      const { title: ccLabel } = ccn.convention;
+  const [situation] = possibleSituations;
+  const { title: ccLabel } = ccn.convention;
 
-      return (
-        <>
-          <SectionTitle>Durée du préavis</SectionTitle>
-          <p>
-            À partir des éléments que vous avez saisis, la durée du préavis de
-            démission est estimée à&nbsp;:
-          </p>
-          <p>
-            <Highlight>{situation.answer}</Highlight>.
-          </p>
-          <Toast>
-            Une durée de préavis de démission plus favorable au salarié peut
-            aussi être prévue dans une convention ou un accord au niveau de
-            l’entreprise
-          </Toast>
-          <SectionTitle>Détails</SectionTitle>
-          <p>Éléments saisis&nbsp;:</p>
-          {recapSituation({
-            "Convention collective": `${ccLabel} (${idcc})`,
-            ...situation.criteria
-          })}
-          <SectionTitle>Source</SectionTitle>
-          {getRef([refLegal, situation])}
-        </>
-      );
-    }
-    default:
-      return (
-        <>
-          <SectionTitle>CC non traitée</SectionTitle>
-          <p>
-            Le code du travail ne prévoyant pas de durée précise du préavis de
-            démission, nous vous invitons à consulter le contenu de la
-            convention collective. Par ailleurs, un accord collectif
-            d’entreprise peut prévoir une durée de préavis de démission
-            différente, qu’elle soit inférieure ou supérieure. Dans ce cas la
-            durée de préavis de démission prévu par l’accord au niveau de
-            l’entreprise s’applique. Référence :
-          </p>
-
-          {getRef([refLegal])}
-        </>
-      );
-  }
+  return (
+    <>
+      <SectionTitle>Durée du préavis</SectionTitle>
+      <p>
+        À partir des éléments que vous avez saisis, la durée du préavis de
+        démission est estimée à&nbsp;:
+      </p>
+      <p>
+        <Highlight>{situation.answer}</Highlight>.
+      </p>
+      <Toast>
+        Une durée de préavis de démission plus favorable au salarié peut aussi
+        être prévue dans une convention ou un accord au niveau de l’entreprise
+      </Toast>
+      <SectionTitle>Détails</SectionTitle>
+      <p>Éléments saisis&nbsp;:</p>
+      {recapSituation({
+        "Convention collective": `${ccLabel} (${idcc})`,
+        ...situation.criteria
+      })}
+      <SectionTitle>Source</SectionTitle>
+      {getRef([refLegal, situation])}
+    </>
+  );
 }
 
 StepResult.propTypes = {
