77f2298278d30f347091283b8fefff9ef8c259b9
Lionel <lionel@lumographe.fr>
2020-01-16 12:16:59 +0100

    chore(modele): update tableau-heure-supp (#2255)
    * chore(modele): update tableau-heure-supp
    
    * fix(modeles): update docx
    
    * fix(tools): update wording for indemnité licenciement

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Indemnite.js
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Indemnite.js
@@ -14,9 +14,10 @@ function StepIndemnite({ form }) {
         infoCalcul={infoCalculLegal}
       />
       <Toast>
-        Un accord d’entreprise, le contrat de travail ou un usage peuvent
-        prévoir un montant plus favorable pour le salarié. Dans ce cas, le
-        montant dû est le montant le plus favorable pour le salarié.
+        Une convention collective, un accord d’entreprise, le contrat de travail
+        ou un usage peuvent prévoir un montant plus favorable pour le salarié.
+        Dans ce cas, le montant dû est le montant le plus favorable pour le
+        salarié.
       </Toast>
     </>
   );
