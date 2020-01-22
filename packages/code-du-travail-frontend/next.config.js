ea5a1737e4a132e7770b2039a356990d6879898c
ManuC <colasante.manuel@gmail.com>
2020-01-22 18:06:18 +0100

    text(frontend): update snaps

--- packages/code-du-travail-frontend/next.config.js
+++ packages/code-du-travail-frontend/next.config.js
@@ -9,7 +9,7 @@ const withTM = function(config) {
   if (process.env.NODE_ENV === "production") {
     return withTranspileModule(config);
   }
-  return withTranspileModule(config);
+  return config;
 };
 
 const compose = (...fns) => args => fns.reduceRight((arg, fn) => fn(arg), args);
