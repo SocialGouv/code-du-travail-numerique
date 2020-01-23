5b21016e1556b537abd1f02664a62f8b2dfc5866
LionelB <lionel@lumographe.fr>
2020-01-23 17:40:02 +0100

    feat(ui): add text component

---
+++ packages/react-ui/src/Text/text.story.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Section } from "../layout/Section";
+import { Text } from ".";
+
+export default {
+  component: Text,
+  title: "Components|Text"
+};
+
+export const base = () => (
+  <>
+    <Section>
+      <p>
+        <Text>This is a basic text</Text>
+      </p>
+      <p>
+        <Text fontSize="small">This is a small text</Text>
+      </p>
+      <p>
+        <Text variant="primary">This is a primary text</Text>
+      </p>
+      <p>
+        <Text variant="secondary">This is a secondary text</Text>
+      </p>
+      <p>
+        <Text variant="secondary" fontSize="hsmall">
+          This is a heading small text
+        </Text>
+      </p>
+      <p>
+        <Text variant="primary" fontSize="hlarge" fontWeight="700">
+          This is a heading large text with (fontWeight 700)
+        </Text>
+      </p>
+    </Section>
+  </>
+);
