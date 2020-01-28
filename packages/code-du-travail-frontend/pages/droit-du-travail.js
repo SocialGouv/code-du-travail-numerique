182adaada286099afb071e630fc04f3981a3ce7c
ManuC <colasante.manuel@gmail.com>
2020-01-28 12:37:52 +0100

    feat(frontend): new page droit du travail

--- packages/code-du-travail-frontend/pages/droit-du-travail.js
+++ packages/code-du-travail-frontend/pages/droit-du-travail.js
@@ -2,9 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import {
   Container,
+  IconStripe,
+  icons,
+  InsertTitle,
   PageTitle,
   Section,
   theme,
+  Title,
   Wrapper
 } from "@socialgouv/react-ui";
 
@@ -23,90 +27,71 @@ const DroitDuTravail = ({ ogImage, pageUrl }) => (
     />
     <Section>
       <Container>
-        <PageTitle>Le droit du travail, c’est quoi&nbsp;?</PageTitle>
-        <FlexContainer>
-          <BigBlock>
-            <Wrapper>
-              <strong>
-                Le droit du travail est l’ensemble des règles juridiques
-                applicables aux relations entre employeurs privés et salariés, à
-                l’occasion du travail.
-              </strong>
-              <p>
-                Le droit du travail organise les relations professionnelles de
-                travail entre l’employeur et le salarié individuellement et la
-                collectivité des salariés. Il encadre de nombreux domaines tels
-                que le contrat de travail, la rémunération, la durée du travail,
-                les congés, la discipline, le licenciement, l’emploi, la
-                formation, la sécurité et la santé au travail, la négociation
-                collective, la grève et la représentation du personnel.
-              </p>
-              <p>
-                Le droit du travail est un droit en constante évolution car il
-                comprend des enjeux politiques, économiques et sociaux forts.
-              </p>
+        <PageTitle
+          subtitle={
+            <span>
+              Retrouvez la définition du droit du travail, les textes qui en
+              sont à l’origine
+              <DesktopOnlyLineBreak />
+              ainsi que leur articulation.
+            </span>
+          }
+        >
+          Le droit du travail
+        </PageTitle>
+        <Container narrow noPadding>
+          <Wrapper variant="main">
+            <Title shift={spacings.larger}>
+              Qu’est-ce que le droit du travail&nbsp;?
+            </Title>
+            <strong>
+              Le droit du travail est l’ensemble des règles juridiques
+              applicables aux relations entre employeurs privés et salariés, à
+              l’occasion du travail.
+            </strong>
+            <p>
+              Le droit du travail organise les relations professionnelles de
+              travail entre l’employeur et le salarié individuellement et la
+              collectivité des salariés. Il encadre de nombreux domaines tels
+              que le contrat de travail, la rémunération, la durée du travail,
+              les congés, la discipline, le licenciement, l’emploi, la
+              formation, la sécurité et la santé au travail, la négociation
+              collective, la grève et la représentation du personnel.
+            </p>
+            <p>
+              Le droit du travail est un droit en constante évolution car il
+              comprend des enjeux sociaux, économiques et politiques forts.
+            </p>
+            <Wrapper variant="dark">
+              <IconStripe icon={icons.Warning}>
+                <InsertTitle>Le droit du travail, ce n’est pas…</InsertTitle>
+                <StyledP>
+                  Le droit du travail ne concerne pas les travailleurs qui sont
+                  soumis au droit public (par exemple, les fonctionnaires), les
+                  travailleurs indépendants (artisan, commerçant, professions
+                  libérales…), les bénévoles et les dirigeants d’entreprise.
+                </StyledP>
+              </IconStripe>
             </Wrapper>
-          </BigBlock>
-          <SmallBlock>
-            <Wrapper>
-              <h2>Ce n’est pas…</h2>
-              <p>
-                Le droit du travail ne concerne pas les travailleurs qui sont
-                soumis au droit public (par exemple, les fonctionnaires), les
-                travailleurs indépendants (artisan, commerçant, professions
-                libérales...), les bénévoles et les dirigeants d’entreprise.
-              </p>
-            </Wrapper>
-          </SmallBlock>
-        </FlexContainer>
-        <Origins />
-        <Hierarchy />
+          </Wrapper>
+        </Container>
       </Container>
+      <Origins />
+      <Hierarchy />
     </Section>
   </Layout>
 );
 
 export default DroitDuTravail;
 
-const { box, breakpoints, spacings } = theme;
+const { breakpoints, fonts, spacings } = theme;
 
-const FlexContainer = styled.div`
-  display: flex;
-  align-items: flex-end;
-  margin-bottom: ${spacings.large};
-  @media (max-width: ${breakpoints.tablet}) {
-    flex-wrap: wrap;
-    margin-bottom: 0;
+const DesktopOnlyLineBreak = styled.br`
+  @media (max-width: ${breakpoints.desktop}) {
+    display: none;
   }
 `;
 
-const BigBlock = styled.div`
-  position: relative;
-  left: 4%;
-  flex-basis: 80%;
-  padding-right: 8%;
-  background-color: ${({ theme }) => theme.bgSecondary};
-  border-radius: ${box.borderRadius};
-  @media (max-width: ${breakpoints.tablet}) {
-    left: 0;
-    flex-basis: 100%;
-    margin-bottom: ${spacings.medium};
-    padding: 0;
-  }
-`;
-
-const SmallBlock = styled.div`
-  position: relative;
-  right: 4%;
-  bottom: ${spacings.medium};
-  flex-basis: 410px;
-  flex-shrink: 0;
-  background-color: ${({ theme }) => theme.bgTertiary};
-  border-radius: ${box.borderRadius};
-  @media (max-width: ${breakpoints.tablet}) {
-    right: 0;
-    bottom: 0;
-    flex-basis: 100%;
-    padding: 0;
-  }
+const StyledP = styled.p`
+  margin: 0;
 `;
