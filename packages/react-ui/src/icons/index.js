03d81d3631328d1d81785158571a437457a0334e
ManuC <colasante.manuel@gmail.com>
2020-01-28 12:25:55 +0100

    feat(ui): new icons

--- packages/react-ui/src/icons/index.js
+++ packages/react-ui/src/icons/index.js
@@ -1,13 +1,18 @@
 // Bicolor icons
+export { default as Agreement } from "./components/bicolor/Agreement";
 export { default as Calculator } from "./components/bicolor/Calculator";
+export { default as Constitution } from "./components/bicolor/Constitution";
 export { default as Contract } from "./components/bicolor/Contract";
 export { default as Custom } from "./components/bicolor/Custom";
 export { default as Depart } from "./components/bicolor/Depart";
 export { default as Document } from "./components/bicolor/Document";
+export { default as Eurotext } from "./components/bicolor/Eurotext";
 export { default as Formation } from "./components/bicolor/Formation";
 export { default as Health } from "./components/bicolor/Health";
 export { default as Holidays } from "./components/bicolor/Holidays";
 export { default as Indemnity } from "./components/bicolor/Indemnity";
+export { default as International } from "./components/bicolor/International";
+export { default as Laws } from "./components/bicolor/Laws";
 export { default as Logo } from "./components/bicolor/Logo";
 export { default as Nego } from "./components/bicolor/Nego";
 export { default as Precarity } from "./components/bicolor/Precarity";
@@ -17,6 +22,8 @@ export { default as Rules } from "./components/bicolor/Resignation";
 export { default as Salary } from "./components/bicolor/Salary";
 export { default as Termination } from "./components/bicolor/Termination";
 export { default as Time } from "./components/bicolor/Time";
+export { default as Uses } from "./components/bicolor/Uses";
+export { default as Warning } from "./components/bicolor/Warning";
 export { default as WorkersHero } from "./components/bicolor/WorkersHero";
 export { default as WorkersTheme } from "./components/bicolor/WorkersTheme";
 
