14701fc956911718ecb6039b362b8f2cc814f941
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-01-29 15:03:40 +0100

    fix(outils): wording for #1602

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/components/AbsencePeriods.js
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/components/AbsencePeriods.js
@@ -28,10 +28,10 @@ function AbsencePeriods({ name, visible = true, onChange }) {
                 congé de présence parental ,l&apos;arrêt de travail lié à un
                 accident du travail ou une maladie professionnelle, le congé
                 individuel de formation (CIF), le congé de solidarité
-                internationale, le congé de solidarité familiale et le stage de
-                fin d&apos;étude de plus de 2 mois sont déjà prises en compte
-                dans l&apos;ancienneté et ne sont pas des périodes à renseigner
-                ci-après :
+                internationale, le congé de solidarité familiale, le congé de
+                proche aidant et le stage de fin d&apos;étude de plus de 2 mois
+                sont déjà prises en compte dans l&apos;ancienneté et ne sont pas
+                des périodes à renseigner ci-après :
               </p>
               <Question as="p">
                 Quels sont le motif et la durée de ces absences
@@ -149,6 +149,8 @@ export const motifs = [
   { label: "Congés sans solde", value: 1.0 },
   { label: "Grève", value: 1.0 },
   { label: "Mise à pied", value: 1.0 },
+  { label: "Maladie d'origine non professionnelle", value: 1.0 },
+  { label: "Congé de paternité", value: 1.0 },
   { label: "Maladie d'origine non professionnelle", value: 1.0 },
   { label: "Congé de paternité", value: 1.0 }
 ];
