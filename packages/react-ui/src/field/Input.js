e90139ae233fe878e31861dc7d7aa6a89b9286b2
ManuC <colasante.manuel@gmail.com>
2020-02-07 12:25:24 +0100

    fix(ui): lint issue

--- packages/react-ui/src/field/Input.js
+++ packages/react-ui/src/field/Input.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { box, breakpoints, fonts, spacings } from "../theme";
 
 export const Input = ({ icon: Icon, className, ...props }) => {
