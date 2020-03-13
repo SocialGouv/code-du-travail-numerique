ea90868bedfe4ec342d202e2ce47935893c48d3f
ManuC <colasante.manuel@gmail.com>
2020-03-13 11:40:59 +0100

    feat(tools): move link from intro to result

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Indemnite.js
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Indemnite.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Link from "next/link";
 import { Toast } from "@socialgouv/react-ui";
 
 import { getIndemniteFromFinalForm } from "../indemnite";
@@ -17,6 +18,17 @@ function StepIndemnite({ form }) {
         Une convention collective, un accord d’entreprise, le contrat de travail
         ou un usage peuvent prévoir un montant plus favorable pour le salarié.
       </Toast>
+      <p>
+        Pour en savoir plus sur l’indemnité de licenciement et son mode de
+        calcul, consultez{" "}
+        <Link
+          href="/fiche-service-public/[slug]"
+          as={`/fiche-service-public/indemnite-de-licenciement-du-salarie-en-cdi`}
+        >
+          <a>cet article</a>
+        </Link>
+        .
+      </p>
     </>
   );
 }
