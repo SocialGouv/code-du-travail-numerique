36cc641b911aadf386fa6c1d732a555e0be6b801
ManuC <colasante.manuel@gmail.com>
2020-03-27 16:37:40 +0100

    feat(data): update-data and hide simulateur

--- packages/code-du-travail-data/dataset/tools/index.js
+++ packages/code-du-travail-data/dataset/tools/index.js
@@ -2,30 +2,32 @@ const { SOURCES, getRouteBySource } = require("@cdt/sources");
 const allThemes = require("@cdt/data...datafiller/themes.data.json");
 const tools = require("./internals.json");
 
-const themes = allThemes.filter(theme =>
-  theme.refs.some(ref =>
-    ref.url.startsWith(`/${getRouteBySource(SOURCES.LETTERS)}`),
-  ),
+const themes = allThemes.filter((theme) =>
+  theme.refs.some((ref) =>
+    ref.url.startsWith(`/${getRouteBySource(SOURCES.LETTERS)}`)
+  )
 );
 
-const toolsWithBreadCrumbs = tools.map(tool => {
-  const theme = themes.find(theme =>
-    theme.refs.some(ref => ref.url.match(new RegExp(tool.slug))),
-  );
-  let breadcrumbs = [];
-  if (theme) {
-    breadcrumbs = (theme.breadcrumbs || []).concat([
-      {
-        label: theme.title,
-        slug: `/${getRouteBySource(SOURCES.THEMES)}/${theme.slug}`,
-      },
-    ]);
-  }
+const toolsWithBreadCrumbs = tools
+  .filter((tool) => tool.slug !== "simulateur-embauche")
+  .map((tool) => {
+    const theme = themes.find((theme) =>
+      theme.refs.some((ref) => ref.url.match(new RegExp(tool.slug)))
+    );
+    let breadcrumbs = [];
+    if (theme) {
+      breadcrumbs = (theme.breadcrumbs || []).concat([
+        {
+          label: theme.title,
+          slug: `/${getRouteBySource(SOURCES.THEMES)}/${theme.slug}`,
+        },
+      ]);
+    }
 
-  return {
-    ...tool,
-    breadcrumbs,
-  };
-});
+    return {
+      ...tool,
+      breadcrumbs,
+    };
+  });
 
 module.exports = toolsWithBreadCrumbs;
