9443894f05928d3693c7acbf5cfe1ac5100e01a3
LionelB <lionel@lumographe.fr>
2020-03-31 11:10:38 +0200

    fix rebase

--- packages/code-du-travail-frontend/__tests__/glossaire.test.js
+++ packages/code-du-travail-frontend/__tests__/glossaire.test.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Glossaire from "../pages/glossaire";
-
+const glossary = [
+  {
+    title: "Accord d'entreprise",
+    slug: "accord-dentreprise",
+    abbrs: [],
+    variants: [
+      "accord collectif d'entreprise",
+      "accords d'entreprise",
+      "accords collectifs d'entreprise",
+    ],
+    definition:
+      "<p>Accord collectif de travail conclu au niveau d'une entreprise.</p>",
+    refs: [],
+  },
+];
 describe("<Glossaire />", () => {
   it("should render", () => {
-    const { container } = render(<Glossaire />);
+    const { container } = render(<Glossaire glossary={glossary} />);
     expect(container).toMatchSnapshot();
   });
 });
