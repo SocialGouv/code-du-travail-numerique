e253aef3ef0baad3c7552ba5024e6428a941232c
ManuC <colasante.manuel@gmail.com>
2020-04-03 14:23:55 +0200

    feat(front): add wrapper around contact modal warning

--- packages/code-du-travail-frontend/src/common/ContactModal.js
+++ packages/code-du-travail-frontend/src/common/ContactModal.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { Button, Heading, Modal, Text } from "@socialgouv/react-ui";
+import { Button, Heading, Modal, Wrapper } from "@socialgouv/react-ui";
 
 import { ServiceRenseignement } from "./ServiceRenseignement";
 
@@ -28,29 +28,28 @@ export const ContactModal = ({ children: renderProp }) => {
       <Modal isOpen={isModalVisible} onDismiss={closeModal} title="Contact">
         {(isServiceRenseignement && <ServiceRenseignement />) || (
           <>
-            <Heading as="h4">Contact </Heading>
-            <p>
-              <Text>
-                <strong>Attention :</strong> aucune réponse en droit du travail
-                ne pourra vous être apportée via l&apos;adresse ci-dessous.
-                <br />
-                <br />
+            <StyledHeading as="h4">Contact</StyledHeading>
+            <Wrapper variant="dark">
+              <p>
                 Si vous souhaitez nous interroger sur vos droits ou sur des
-                dispositions en droit du travail, nous vous invitons à contacter
+                dispositions en droit du travail, nous vous invitons à contacter{" "}
                 <Button variant="link" onClick={showServiceRenseignement}>
-                  les services du ministre du travail.
+                  les services du ministre du travail
                 </Button>
-              </Text>
-            </p>
+              </p>
+              <p>
+                <strong>Attention</strong> : Aucune réponse en droit du travail
+                ne pourra vous être apportée via l’adresse ci-dessous.
+              </p>
+            </Wrapper>
             <p>
-              <Text>
-                Si vous avez une suggestion sur l&apos;utilisation de ce site ou
-                si vous avez rencontré un problème technique, vous pouvez nous
-                contacter à cette adresse&nbsp;:&nbsp;
-                <StyledLink href="mailto:codedutravailnumerique@travail.gouv.fr">
-                  codedutravailnumerique@travail.gouv.fr
-                </StyledLink>
-              </Text>
+              Si vous avez une <strong>suggestion</strong> sur l’utilisation de
+              ce site ou si vous avez rencontré un{" "}
+              <strong>problème technique</strong>, vous pouvez nous contacter à
+              cette adresse&nbsp;:{" "}
+              <StyledLink href="mailto:codedutravailnumerique@travail.gouv.fr">
+                codedutravailnumerique@travail.gouv.fr
+              </StyledLink>
             </p>
           </>
         )}
@@ -64,6 +63,10 @@ export default ContactModal;
 ContactModal.propTypes = {
   children: PropTypes.func.isRequired,
 };
+
+const StyledHeading = styled(Heading)`
+  margin-top: 0;
+`;
 
 const StyledLink = styled.a`
   font-weight: bold;
