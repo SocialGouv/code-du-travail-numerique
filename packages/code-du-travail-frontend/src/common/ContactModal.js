cc1b9c55326f0cd3bc20a1b6744898799c40ba69
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-04-03 15:20:36 +0200

    fix: contact modal wording fix #2533 (#2553)
    * fix: fix contact modal fix #2533
    
    * test: update snaps
    
    * feat(front): add wrapper around contact modal warning
    
    Co-authored-by: ManuC <colasante.manuel@gmail.com>

--- packages/code-du-travail-frontend/src/common/ContactModal.js
+++ packages/code-du-travail-frontend/src/common/ContactModal.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { Button, Heading, Modal, Text } from "@socialgouv/react-ui";
+import { Button, Heading, Modal, Wrapper } from "@socialgouv/react-ui";
 
 import { ServiceRenseignement } from "./ServiceRenseignement";
 
@@ -28,27 +28,28 @@ export const ContactModal = ({ children: renderProp }) => {
       <Modal isOpen={isModalVisible} onDismiss={closeModal} title="Contact">
         {(isServiceRenseignement && <ServiceRenseignement />) || (
           <>
-            <Heading as="h4">Contact </Heading>
-            <p>
-              <Text>
-                Vous pouvez joindre l’équipe en charge de la conception et du
-                développement du site à l’adresse suivante&nbsp;:&nbsp;
-                <StyledLink href="mailto:codedutravailnumerique@travail.gouv.fr">
-                  codedutravailnumerique@travail.gouv.fr
-                </StyledLink>
-              </Text>
-            </p>
-            <p>
-              <Text>
-                Attention : l’équipe n’est pas habilitée à répondre aux
-                questions concernant le droit du travail. Si vous avez besoin de
-                renseignements sur votre situation ou celle d’un proche, nous
-                vous invitons à contacter&nbsp;
+            <StyledHeading as="h4">Contact</StyledHeading>
+            <Wrapper variant="dark">
+              <p>
+                Si vous souhaitez nous interroger sur vos droits ou sur des
+                dispositions en droit du travail, nous vous invitons à contacter{" "}
                 <Button variant="link" onClick={showServiceRenseignement}>
-                  les services dédiés
+                  les services du ministre du travail
                 </Button>
-                .
-              </Text>
+              </p>
+              <p>
+                <strong>Attention</strong> : Aucune réponse en droit du travail
+                ne pourra vous être apportée via l’adresse ci-dessous.
+              </p>
+            </Wrapper>
+            <p>
+              Si vous avez une <strong>suggestion</strong> sur l’utilisation de
+              ce site ou si vous avez rencontré un{" "}
+              <strong>problème technique</strong>, vous pouvez nous contacter à
+              cette adresse&nbsp;:{" "}
+              <StyledLink href="mailto:codedutravailnumerique@travail.gouv.fr">
+                codedutravailnumerique@travail.gouv.fr
+              </StyledLink>
             </p>
           </>
         )}
@@ -62,6 +63,10 @@ export default ContactModal;
 ContactModal.propTypes = {
   children: PropTypes.func.isRequired,
 };
+
+const StyledHeading = styled(Heading)`
+  margin-top: 0;
+`;
 
 const StyledLink = styled.a`
   font-weight: bold;
