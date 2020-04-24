c1af42734fa92b37454935237ee8666b09b2096d
ManuC <colasante.manuel@gmail.com>
2020-04-24 10:26:46 +0200

    feat(front): add prime to ordonnances

--- packages/code-du-travail-frontend/pages/dossiers/ministere-du-travail-notre-dossier-sur-le-coronavirus/ce-que-changent-les-ordonnances-en-droit-du-travail.js
+++ packages/code-du-travail-frontend/pages/dossiers/ministere-du-travail-notre-dossier-sur-le-coronavirus/ce-que-changent-les-ordonnances-en-droit-du-travail.js
@@ -538,6 +538,85 @@ const Fiche = ({ pageUrl, ogImage }) => {
       title: " Intéressement et participation : report des versements",
     },
     {
+      anchor: "prime-exceptionnelle",
+      content: (
+        <>
+          <p>
+            Les conditions de la prime exceptionnelle de pouvoir d’achat
+            (également appelée « prime Macron ») ont été assouplies par
+            l’ordonnance du 1er avril 2020.
+          </p>
+          <p>
+            Ainsi, depuis le 2 avril 2020, toutes les entreprises peuvent verser
+            la prime de pouvoir d’achat. Elles n’ont plus l’obligation de
+            disposer d’un accord d’intéressement.
+          </p>
+          <p>
+            En outre, la date limite de versement de la prime a été reportée au
+            31 août 2020. (Auparavant, cette date était fixée au 30 juin 2020.)
+          </p>
+          <p>
+            La prime de pouvoir d’achat est exonérée de cotisations et
+            contributions sociales et d’impôt sur le revenu jusqu’à :
+          </p>
+          <ul>
+            <Li>
+              <strong>1000€</strong>, en l’absence d’accord d’intéressement
+              applicable dans l’entreprise.
+            </Li>
+            <Li>
+              <strong>2000€</strong>, si l’entreprise applique un accord
+              d’intéressement. Dans ce cas, la possibilité de conclure un accord
+              d’intéressement est repoussée jusqu’au 31 août 2020. L’accord peut
+              avoir une durée comprise entre 1 et 3 ans.
+            </Li>
+            <Li>
+              <strong>2000€</strong> pour les associations et fondations (avec
+              ou sans accord d’intéressement).
+            </Li>
+          </ul>
+          <p>
+            Enfin, il est toujours possible de moduler le montant de la prime
+            entre les salariés selon les critères suivants : la rémunération, le
+            niveau de classification, la durée de présence effective pendant
+            l’année écoulée et la durée de travail prévue au contrat de travail.
+            L’ordonnance du 1er avril 2020 a ajouté un critère supplémentaire :
+            les conditions de travail liées à l’épidémie de covid-19.
+          </p>
+          <ReferencesJuridiques
+            accordionDisplay={1}
+            references={[
+              {
+                id: "prime-exceptionnelle-1",
+                title:
+                  "Ordonnance n° 2020-385 du 1er avril 2020 modifiant la date limite et les conditions de versement de la prime exceptionnelle de pouvoir d’achat",
+                type: TYPE_REFERENCE.external,
+                url:
+                  "https://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000041776879",
+              },
+              {
+                id: "prime-exceptionnelle-2",
+                title:
+                  "Rapport au Président de la République relatif à l’ordonnance n° 2020-385 du 1er avril 2020 modifiant la date limite et les conditions de versement de la prime exceptionnelle de pouvoir d’achat",
+                type: TYPE_REFERENCE.external,
+                url:
+                  "https://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000041776873",
+              },
+              {
+                id: "prime-exceptionnelle-2",
+                title:
+                  "Ordonnance n° 2020-460 du 22 avril 2020 portant diverses mesures prises pour faire face à l’épidémie de covid-19, article 19",
+                type: TYPE_REFERENCE.external,
+                url:
+                  "https://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000041814597",
+              },
+            ]}
+          />
+        </>
+      ),
+      title: "Prime exceptionnelle de pouvoir d’achat",
+    },
+    {
       anchor: "assistant-maternel",
       content: (
         <>
