3b66c3fe5baef484014c127dacc599ec69a0808d
Lionel <lionel@lumographe.fr>
2020-04-30 19:28:02 +0200

    fix(sitemap): remove duplicate content (#2676)
    * fix(sitemap): remove duplicate content
    
    * fix(sitemap): update default dump path

--- packages/code-du-travail-frontend/scripts/generate-sitemap.js
+++ packages/code-du-travail-frontend/scripts/generate-sitemap.js
@@ -5,18 +5,28 @@ import glossary from "@socialgouv/datafiller-data/data/glossary.json";
 import { SOURCES, getRouteBySource } from "@cdt/sources";
 import slugify from "@cdt/data/slugify";
 
-const DOCUMENT_PATH = process.env.DUMP_PATH || "../data/dump.data.json";
+const DOCUMENT_PATH =
+  process.env.DUMP_PATH || "../../code-du-travail-data/dist/dump.data.json";
 const PROD_HOSTNAME = process.env.PROD_HOSTNAME || "code.travail.gouv.fr";
 
 const writeFile = promisify(fs.writeFile);
 
-const glossaryPages = glossary.map(
-  ({ title }) => `${getRouteBySource(SOURCES.GLOSSARY)}/${slugify(title)}`
+const glossaryPages = [getRouteBySource(SOURCES.GLOSSARY)].concat(
+  glossary.map(
+    ({ title }) => `${getRouteBySource(SOURCES.GLOSSARY)}/${slugify(title)}`
+  )
 );
 const staticPages = ["a-propos", "droit-du-travail", "mentions-legales"];
 const documentPages = require(DOCUMENT_PATH)
-  .filter(({ slug }) => Boolean(slug))
-  .map(({ source, slug }) => `${getRouteBySource(source)}/${slug}`);
+  .filter(
+    ({ slug, source }) =>
+      Boolean(slug) && ![SOURCES.CCN_PAGE, SOURCES.SHEET_MT].includes(source)
+  )
+  .map(({ source, slug }) =>
+    source === SOURCES.SHEET_MT_PAGE
+      ? `${getRouteBySource(SOURCES.SHEET_MT)}/${slug}`
+      : `${getRouteBySource(source)}/${slug}`
+  );
 
 const documents = staticPages.concat(glossaryPages, documentPages);
 
