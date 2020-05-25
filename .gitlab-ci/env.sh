50fe46e4811a9f1f5a1085cf45faa3e5423f965f
Lionel <lionel@lumographe.fr>
2020-05-25 18:33:04 +0200

    ci: remove unused artifact from e2e (#2751)
    * ci: remove unused artifact from e2e
    
    * ci: update use_master artifact lifespan
    
    * fix: fix use master path
    
    * fix: fix
    
    * fix: udpate artifact path
    
    * ci: fix use_master path in register stage

--- .gitlab-ci/env.sh
+++ .gitlab-ci/env.sh
@@ -56,7 +56,7 @@ export API_HOST="api-${DOMAIN}";
 export FRONTEND_HOST="${DOMAIN}";
 
 export NLP_HOST="nlp-python";
-if [[ -f packages/code-du-travail-nlp/USE_MASTER ]] && [[ "${BRANCH_NAME}" != "master" ]]; then
+if [[ -f packages/code-du-travail-nlp/${CI_COMMIT_REF_SLUG}_USE_MASTER ]] && [[ "${BRANCH_NAME}" != "master" ]]; then
   export NLP_HOST="${NLP_HOST}.cdtn-master.svc.cluster.local";
 fi
 
