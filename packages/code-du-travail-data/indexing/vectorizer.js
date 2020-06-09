9d6cb3afcc81ee312313addfe28dd393baed7d4c
RemiM <rmelisson@gmail.com>
2020-06-09 11:38:52 +0200

    JS vectorizer ok

--- packages/code-du-travail-data/indexing/vectorizer.js
+++ packages/code-du-travail-data/indexing/vectorizer.js
@@ -1,34 +1,68 @@
-// URL the TF serve deployment
+import * as fetch from "node-fetch";
+import semantic_stopwords from "../dataset/stop_words";
+
+// URL of the TF serve deployment
 const NLP_HOST = process.env.NLP_HOST || "http://localhost:8501/";
 const tfServeURL = NLP_HOST + "v1/models/sentqam:predict";
-const fetch = require("node-fetch");
+
+function stripAccents(text) {
+    // strip accents
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+const stopWords = semantic_stopwords.map(stripAccents);
+
+function preprocess(text) {
+    // remove stop words
+    const stripped = stripAccents(text);
+
+    // 09/06/20 : cheap tokenizer, we should probably use something more solid
+    // keep it like this for now to ensure embedding stability despite refactoring
+    const split = stripped.split(" ");
+
+    const noSW = split.filter((t) => !stopWords.includes(t.toLowerCase()));
+
+    return noSW.join(" ");
+}
 
 async function callTFServe(body) {
     const response = await fetch(tfServeURL, { method: "POST", body });
     if (response.ok) {
         const json = await response.json();
-        return json;
+        return json["outputs"];
     } else {
         throw new Error(response.statusText);
     }
 }
 
 async function vectorizeDocument(title, content) {
+    if (title == undefined || title == "") {
+        throw new Error("Cannot vectorize document with empty title.");
+    }
+
+    const input = [preprocess(title)];
+    const context = content != "" ? [preprocess(content)] : "";
+
     const body = JSON.stringify({
         signature_name: "response_encoder",
-        inputs: { input: [title], context: [content] },
+        inputs: { input, context },
     });
     const vectors = await callTFServe(body);
     return vectors[0];
 }
 
 async function vectorizeQuery(query) {
+    if (query == undefined || query == "") {
+        throw new Error("Cannot vectorize empty query.");
+    }
+
+    const inputs = [preprocess(query)];
     const body = JSON.stringify({
         signature_name: "question_encoder",
-        inputs: [query],
+        inputs,
     });
     const vectors = await callTFServe(body);
     return vectors[0];
 }
 
-export { vectorizeDocument, vectorizeQuery };
+export { vectorizeDocument, vectorizeQuery, preprocess };
