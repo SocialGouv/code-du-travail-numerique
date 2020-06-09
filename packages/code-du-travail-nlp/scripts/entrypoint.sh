83f01e90da37f7ff7ffd989ff44fa22920a59a92
RemiM <rmelisson@gmail.com>
2020-06-09 17:27:39 +0200

    remove nlp package

--- packages/code-du-travail-nlp/scripts/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -exu pipefail
-
-
-
-# start the serveur
-gunicorn -t 60 --keep-alive 20 --threads=4 --worker-class=gthread -b :${NLP_PORT:=5000} --env NLP_HOST=${NLP_HOST} "api:app"
-
