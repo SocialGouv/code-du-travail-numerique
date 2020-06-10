6c7c6ac89aa8fee0f2e91801fc9560bd1430800b
RemiM <rmelisson@gmail.com>
2020-06-10 17:27:26 +0200

    ci wip

--- scripts/snyk-docker-scan.sh
+++ scripts/snyk-docker-scan.sh
@@ -7,7 +7,7 @@
 
 export REGISTRY=registry.gitlab.factory.social.gouv.fr/socialgouv
 
-declare -a images=("data" "api" "frontend" "nlp")
+declare -a images=("data" "api" "frontend")
 
 for image in "${images[@]}"
 do
