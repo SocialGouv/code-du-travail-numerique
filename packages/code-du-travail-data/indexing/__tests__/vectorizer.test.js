3fef2d8708c426defe96d0bd2add14522bdd2c45
RemiM <rmelisson@gmail.com>
2020-06-10 11:22:20 +0200

    fix linter

--- packages/code-du-travail-data/indexing/__tests__/vectorizer.test.js
+++ packages/code-du-travail-data/indexing/__tests__/vectorizer.test.js
@@ -1,37 +1,37 @@
 const {
-    vectorizeDocument,
-    vectorizeQuery,
-    preprocess,
+  vectorizeDocument,
+  vectorizeQuery,
+  preprocess,
 } = require("../vectorizer");
 
 test("Should vectorize document", async () => {
-    const vector1 = await vectorizeDocument("titre", "contenu");
-    expect(vector1).toBeDefined();
-    expect(vector1).toMatchSnapshot();
+  const vector1 = await vectorizeDocument("titre", "contenu");
+  expect(vector1).toBeDefined();
+  expect(vector1).toMatchSnapshot();
 
-    // preprocessing should make those embeddings equal
-    const vector2 = await vectorizeDocument("le titre", "et le contènu");
-    expect(vector2).toEqual(vector1);
+  // preprocessing should make those embeddings equal
+  const vector2 = await vectorizeDocument("le titre", "et le contènu");
+  expect(vector2).toEqual(vector1);
 });
 
 test("Should vectorize query", async () => {
-    const vector1 = await vectorizeQuery("requete");
-    expect(vector1).toMatchSnapshot();
-    const vector2 = await vectorizeQuery("la requête");
-    expect(vector2).toEqual(vector1);
+  const vector1 = await vectorizeQuery("requete");
+  expect(vector1).toMatchSnapshot();
+  const vector2 = await vectorizeQuery("la requête");
+  expect(vector2).toEqual(vector1);
 });
 
 test("Should fail when no content passed", async () => {
-    await vectorizeQuery().catch((e) =>
-        expect(e).toEqual(new Error("Cannot vectorize empty query."))
-    );
+  await vectorizeQuery().catch((e) =>
+    expect(e).toEqual(new Error("Cannot vectorize empty query."))
+  );
 });
 
 test("Should preprocess text", async () => {
-    expect(preprocess("à la nôtre")).toEqual("");
-    expect(preprocess("çode du tràvail")).toEqual("code travail");
-    // this one should be "aime code travail" when using better tokenization
-    expect(preprocess("j'aime le code du travail")).toEqual(
-        "j'aime code travail"
-    );
+  expect(preprocess("à la nôtre")).toEqual("");
+  expect(preprocess("çode du tràvail")).toEqual("code travail");
+  // this one should be "aime code travail" when using better tokenization
+  expect(preprocess("j'aime le code du travail")).toEqual(
+    "j'aime code travail"
+  );
 });
