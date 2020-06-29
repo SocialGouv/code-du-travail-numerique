ac9a5c65456ac7168611781804b22177debc2b27
LionelB <lionel@lumographe.fr>
2020-06-29 17:37:59 +0200

    update to sh

--- scripts/changes.sh
+++ scripts/changes.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
+## cli tools to detect changes between commit branch and master
+# Usage :
+#   sh scripts/changes.sh packages/code-du-travail-data packages/sources
 
 git fetch origin --quiet
 files=$(git diff --name-only master ${CI_COMMIT_SHA})
-
 for pattern in "$@"
 do
   for file in $files
   do
     if [[ file == pattern ]]; then
       echo "$file trigger data update !";
+      export "skip_data_update"
       exit 0
     fi
   done
