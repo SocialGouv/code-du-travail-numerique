abe9f650a0b8f11725e2462a65eedbba5428af6e
LionelB <lionel@lumographe.fr>
2020-06-30 12:02:16 +0200

    debug

--- scripts/changes.sh
+++ scripts/changes.sh
@@ -3,16 +3,15 @@
 # Usage :
 #   sh scripts/changes.sh code-du-travail-data packages/sources
 
-git fetch origin --quiet
-git checkout master
-git checkout -
-
+# git fetch origin --quiet
+# git checkout master
+# git checkout -
+echo "detect changes between master and ${CI_COMMIT_SHA}"
 files=$(git diff --name-only master ${CI_COMMIT_SHA})
 for pattern in "$@"
 do
   for file in $files
   do
-    echo $file
     if [[ $file == *$pattern* ]]; then
       echo "$file trigger data update !";
       touch $(basename ${pattern}_SKIP | tr '[:lower:]' '[:upper:]' | tr '-' '_' )
@@ -21,3 +20,9 @@ do
   done
   echo "no update detected for $pattern"
 done
+
+# pattern=packages/code-du-travail-data
+# file=packages/code-du-travail-data/dataset/courrier-type/CHANGELOG.md
+# if [[ $file == *$pattern* ]]; then
+#   echo "$file trigger data update !";
+# fi
