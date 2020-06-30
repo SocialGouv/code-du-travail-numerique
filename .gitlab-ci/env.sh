fb7c2df95f1212bff5bce52e9877387d2fa40cbb
RemiM <rmelisson@gmail.com>
2020-06-30 11:22:44 +0200

    add NLP_URL for prod

--- .gitlab-ci/env.sh
+++ .gitlab-ci/env.sh
@@ -14,6 +14,8 @@ export VERSION=${VERSION:=$CI_COMMIT_REF_NAME}
 BRANCH_NAME_HASHED=$( printf "${BRANCH_NAME}" | sha1sum | cut -c1-${HASH_SIZE} )
 export BRANCH_HASH=${BRANCH_HASH:=$BRANCH_NAME_HASHED}
 
+export NLP_URL="https://preprod-serving-ml.dev2.fabrique.social.gouv.fr/";
+
 #
 
 export K8S_NAMESPACE="${PROJECT}-feature-${BRANCH_HASH}"
@@ -45,6 +47,7 @@ if [[ -n "${PRODUCTION+x}" ]]; then
   export ES_INDEX_PREFIX="cdtn-prod"
   export K8S_NAMESPACE="cdtn"
   export DOMAIN="code.travail.fabrique.social.gouv.fr";
+  export NLP_URL="https://serving-ml.fabrique.social.gouv.fr/";
 else
   export DOMAIN="${BRANCH_HASH}-code-travail.${KUBE_INGRESS_BASE_DOMAIN}";
 fi
@@ -55,7 +58,6 @@ fi
 export API_HOST="api-${DOMAIN}";
 export FRONTEND_HOST="${DOMAIN}";
 
-export NLP_URL="https://preprod-serving-ml.dev2.fabrique.social.gouv.fr/";
 #
 
 if [[ -n "${PRODUCTION+x}" ]]; then
