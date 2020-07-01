8cb9ed1d7b0b38f51c01ccb629bb97bf21d41157
LionelB <lionel@lumographe.fr>
2020-07-01 13:03:19 +0200

    make script work on ash / sh

--- scripts/changes.sh
+++ scripts/changes.sh
@@ -6,20 +6,26 @@
 # git fetch origin --quiet
 # git checkout master
 # git checkout -
-echo "detect changes between master and ${CI_COMMIT_SHA}"
 files=$(git diff --name-only master ${CI_COMMIT_SHA})
+echo "detect changes between master and ${CI_COMMIT_SHA}"
 for pattern in "$@"
 do
   for file in $files
   do
-    if [[ $file == *$pattern* ]]; then
+   ## Poor man str contain
+    case $file in *$pattern*)
       echo "$file trigger data update !";
       touch $(basename ${pattern}_SKIP | tr '[:lower:]' '[:upper:]' | tr '-' '_' )
       exit 0
-    fi
+    esac
   done
   echo "no update detected for $pattern"
 done
+
+NEEDLE="ert"
+HAYSTACK="quwertyuiop"
+T="${HAYSTACK/${NEEDLE}/}"
+echo $T
 
 # pattern=packages/code-du-travail-data
 # file=packages/code-du-travail-data/dataset/courrier-type/CHANGELOG.md
