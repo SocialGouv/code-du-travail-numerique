483af8d2b8f32106cd5fe158388f57285cc8b080
LionelB <lionel@lumographe.fr>
2020-07-01 14:06:49 +0200

    udapte script

--- scripts/changes.sh
+++ scripts/changes.sh
@@ -4,14 +4,15 @@
 #   sh scripts/changes.sh code-du-travail-data packages/sources
 
 # git fetch origin --quiet
-# git checkout master
-# git checkout -
+git checkout master
+git checkout -
 files=$(git diff --name-only master ${CI_COMMIT_SHA})
 echo "detect changes between master and ${CI_COMMIT_SHA}"
 for pattern in "$@"
 do
   for file in $files
   do
+    echo $file
    ## Poor man str contain
     case $file in *$pattern*)
       echo "$file trigger data update !";
@@ -21,14 +22,3 @@ do
   done
   echo "no update detected for $pattern"
 done
-
-NEEDLE="ert"
-HAYSTACK="quwertyuiop"
-T="${HAYSTACK/${NEEDLE}/}"
-echo $T
-
-# pattern=packages/code-du-travail-data
-# file=packages/code-du-travail-data/dataset/courrier-type/CHANGELOG.md
-# if [[ $file == *$pattern* ]]; then
-#   echo "$file trigger data update !";
-# fi
