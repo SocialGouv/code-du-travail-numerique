be6ce7f9b587a4eb842fa995be8c899568bbefc1
LionelB <lionel@lumographe.fr>
2020-07-01 14:14:32 +0200

    add git fetch :skull:

--- scripts/changes.sh
+++ scripts/changes.sh
@@ -3,17 +3,16 @@
 # Usage :
 #   sh scripts/changes.sh code-du-travail-data packages/sources
 
-# git fetch origin --quiet
+git fetch origin --quiet
 git checkout master
 git checkout -
+
 files=$(git diff --name-only master ${CI_COMMIT_SHA})
 echo "detect changes between master and ${CI_COMMIT_SHA}"
-echo $files
 for pattern in "$@"
 do
   for file in $files
   do
-    echo $file
     ## Poor man str contain
     case $file in *$pattern*)
       echo "$file trigger data update !";
