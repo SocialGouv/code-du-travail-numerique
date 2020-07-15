72631eeaea07fe140ec2852847eb919a5f720ed1
LionelB <lionel@lumographe.fr>
2020-07-15 11:39:32 +0200

    chore(types): add typings definition to published modules

---
+++ packages/sources/index.d.ts
@@ -0,0 +1,7 @@
+export as namespace cdtnSources
+
+export function getLabelBySourceFn(src:string): string
+
+export function getRouteBySourceFn(src: string): string
+
+export function getSourceByRouteFn(slug: string): string
