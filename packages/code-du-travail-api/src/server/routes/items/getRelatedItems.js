11c77a74fa9d1860254e7f37cd6fba08b7bc060e
RemiM <rmelisson@gmail.com>
2020-07-15 09:54:03 +0200

    add A/B testing

--- packages/code-du-travail-api/src/server/routes/items/getRelatedItems.js
+++ packages/code-du-travail-api/src/server/routes/items/getRelatedItems.js
@@ -14,6 +14,10 @@ const MAX_RESULTS = 5;
 const ES_INDEX_PREFIX = process.env.ES_INDEX_PREFIX || "cdtn";
 const index = `${ES_INDEX_PREFIX}_${DOCUMENTS}`;
 
+// ratio for A/B testing between covisit and search based related items (50%)
+const abRatio = 0.5;
+
+// select certain fields and add recommandation source (covisits or search)
 function mapSource(reco, source) {
   return (({
     action,
@@ -28,18 +32,21 @@ function mapSource(reco, source) {
     action,
     description,
     icon,
+    reco,
     slug,
     source,
     subtitle,
     title,
     url,
-    reco,
   }))(source);
 }
 
 async function getRelatedItems({ title, settings, slug, covisits }) {
   // console.log(covisits);
-  if (covisits) {
+
+  const useCovisit = Math.random() > abRatio;
+
+  if (covisits & useCovisit) {
     // covisits as related items
     // console.log(covisits);
 
@@ -79,7 +86,6 @@ async function getRelatedItems({ title, settings, slug, covisits }) {
       // we filter fields and add some info about recommandation type for evaluation purpose
       .map(({ _source }) => mapSource("covisits", _source));
 
-    // console.log(covisitedItems);
     return covisitedItems;
   } else {
     // standard related items :
@@ -124,15 +130,13 @@ async function getRelatedItems({ title, settings, slug, covisits }) {
     const { hits: { hits: fullTextHits } = { hits: [] } } = esResponse;
     const [rootSlug] = slug.split("#");
 
-    // eslint-disable-next-line no-unused-vars
-    const res = utils
-      .mergePipe(fullTextHits, semanticHits, MAX_RESULTS)
-      .filter(({ _source }) => !_source.slug.startsWith(rootSlug))
-      // we filter fields and add some info about recommandation type for evaluation purpose
-      .map(({ _source }) => mapSource("search", _source));
-
-    // we only return covisites for testing purpose
-    return [];
+    return (
+      utils
+        .mergePipe(fullTextHits, semanticHits, MAX_RESULTS)
+        .filter(({ _source }) => !_source.slug.startsWith(rootSlug))
+        // we filter fields and add some info about recommandation type for evaluation purpose
+        .map(({ _source }) => mapSource("search", _source))
+    );
   }
 }
 
