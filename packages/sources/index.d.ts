90dd8924e863d080591c9567ac7852e40498c92b
Lionel <lionel.breduillieard@beta.gouv.fr>
2020-10-13 13:27:01 +0200

    fix(sources): update typings (#3044)

--- packages/sources/index.d.ts
+++ packages/sources/index.d.ts
@@ -1,16 +1,16 @@
 export as namespace cdtnSources
 
-export function getLabelBySourceFn(src: SourceLabel): string
+export function getLabelBySource(src: SourceLabel): string
 
-export function getRouteBySourceFn(src: SourceRoute): string
+export function getRouteBySource(src: SourceRoute): RouteValues
 
-export function getSourceByRouteFn(slug: string): SourceValues | null
+export function getSourceByRoute(slug: string): SourceValues | null
 
 export const SOURCES:SOURCES
 
 type SourceLabel = Exclude<SourceValues, "highlights" | "page_convention_collective" | "glossary" | "prequalified" | "page_fiche_ministere_travail"| "versions" >
 type SourceRoute = Exclude<SourceValues, "highlights" |  "prequalified" | "versions" >
-  
+
 type SOURCES = {
   CCN: "conventions_collectives",
   CCN_PAGE: "page_convention_collective",
@@ -33,5 +33,25 @@ type SOURCES = {
   VERSIONS: "versions",
 }
 
+type Routes = {
+  [SOURCES.CCN]: "convention-collective",
+  [SOURCES.CCN_PAGE]: "page-convention-collective",
+  [SOURCES.CDT]: "code-du-travail",
+  [SOURCES.CONTRIBUTIONS]: "contribution",
+  [SOURCES.EDITORIAL_CONTENT]: "information",
+  [SOURCES.EXTERNALS]: "external",
+  [SOURCES.GLOSSARY]: "glossaire",
+  [SOURCES.IDCC]: "idcc",
+  [SOURCES.LABOUR_LAW]: "droit-du-travail",
+  [SOURCES.LETTERS]: "modeles-de-courriers",
+  [SOURCES.SHEET_MT]: "fiche-ministere-travail",
+  [SOURCES.SHEET_MT_PAGE]: "fiche-ministere-travail",
+  [SOURCES.SHEET_SP]: "fiche-service-public",
+  [SOURCES.THEMATIC_FILES]: "dossiers",
+  [SOURCES.THEMES]: "themes",
+  [SOURCES.TOOLS]: "outils",
+}
+
 type SourceKeys = keyof SOURCES;
 type SourceValues = SOURCES[SourceKeys];
+type RouteValues = Routes[SourceRoute];
