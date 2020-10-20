bb48c8d35393662a6e709282c7d7673488466818
LionelB <lionel@lumographe.fr>
2020-10-20 19:01:04 +0200

    fix: handle multiple ref

--- packages/code-du-travail-frontend/src/lib/replaceArticlesRefs.js
+++ packages/code-du-travail-frontend/src/lib/replaceArticlesRefs.js
@@ -2,11 +2,11 @@
  * prefix relative link with a given domain
  */
 
-const RE_ARTICLE_NUM = /<a(.*)href=["']((?<!https:\/\/)[^'"]+)["']([^>]+)>/gim;
+const RE_ARTICLE_NUM = /href=["']((?<!https:\/\/)[^'"]+)["']/g;
 
 export const replaceArticlesRefs = (domain, html = "") =>
-  html.replace(RE_ARTICLE_NUM, (_, param1, url, param2) => {
-    return `<a${param1}href="${domain}${
-      url.startsWith("/") ? "" : "/"
-    }${url}"${param2} rel="nofollow noopener" target="_blank">`;
+  html.replace(RE_ARTICLE_NUM, (_, href) => {
+    return `href='${domain}${
+      href.startsWith("/") ? "" : "/"
+    }${href}' rel='nofollow noopener' target='_blank'`;
   });
