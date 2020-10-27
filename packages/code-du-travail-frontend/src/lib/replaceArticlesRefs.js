e167fc24b447c66415e766dca8ef1148f4925969
LionelB <lionel@lumographe.fr>
2020-10-27 16:55:47 +0100

    fix(frontend): remove unsuported IE regexp features

--- packages/code-du-travail-frontend/src/lib/replaceArticlesRefs.js
+++ packages/code-du-travail-frontend/src/lib/replaceArticlesRefs.js
@@ -2,11 +2,9 @@
  * prefix relative link with a given domain
  */
 
-const RE_ARTICLE_NUM = /href=["']((?<!https:\/\/)[^'"]+)["']/g;
+const RE_ARTICLE_NUM = /href=["'](\/[^'"]+)["']/g;
 
 export const replaceArticlesRefs = (domain, html = "") =>
   html.replace(RE_ARTICLE_NUM, (_, href) => {
-    return `href='${domain}${
-      href.startsWith("/") ? "" : "/"
-    }${href}' rel='nofollow noopener' target='_blank'`;
+    return `href='${domain}${href}' rel='nofollow noopener' target='_blank'`;
   });
