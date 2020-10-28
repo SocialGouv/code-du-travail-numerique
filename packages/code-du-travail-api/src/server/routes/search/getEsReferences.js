d9abc39e73be0e71885803335d3b25e57198d4f6
Manu <colasante.manuel@gmail.com>
2020-10-28 14:18:53 +0100

    fix(api): issue where same slug causes duplicate content (#3072)
    * fix(api): issue where same slug causes duplicate content
    
    * remove console.log

--- packages/code-du-travail-api/src/server/routes/search/getEsReferences.js
+++ packages/code-du-travail-api/src/server/routes/search/getEsReferences.js
@@ -1,3 +1,4 @@
+const { getSourceByRoute } = require("@socialgouv/cdtn-sources");
 const { DOCUMENTS } = require("@cdt/data/indexing/esIndexName");
 
 const elasticsearchClient = require("../../conf/elasticsearch.js");
@@ -10,12 +11,12 @@ const index = `${ES_INDEX_PREFIX}_${DOCUMENTS}`;
 
 const makeHit = (data) => ({
   _index: index,
-  _type: "_doc",
   _source: {
     source: "external",
     title: "",
     ...data,
   },
+  _type: "_doc",
 });
 
 const indexQuery = { index };
@@ -46,12 +47,14 @@ const getEsReferences = async (refs = []) => {
   // mix with non ES-results (ex: external, or no match)
   const hits = refs.map((ref) => {
     if (isInternalUrl(ref.url)) {
-      const [, , slug] = ref.url.split("/");
+      const [, source, slug] = ref.url.split("/");
       const esReference = responses.find(
-        (response) => response._source.slug === slug
+        (response) =>
+          response._source.slug === slug &&
+          response._source.source === getSourceByRoute(source)
       );
       if (esReference) {
-        return { ...esReference };
+        return esReference;
       }
     }
     return makeHit(ref);
