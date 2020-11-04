60a40daf765900497aff5752434adfac5f6a4c25
RemiM <rmelisson@gmail.com>
2020-11-04 15:20:12 +0100

    remove tiles from related items

--- packages/code-du-travail-frontend/src/common/RelatedItems.js
+++ packages/code-du-travail-frontend/src/common/RelatedItems.js
@@ -1,14 +1,9 @@
-import {
-  getLabelBySource,
-  getRouteBySource,
-  SOURCES,
-} from "@socialgouv/cdtn-sources";
+import { getRouteBySource, SOURCES } from "@socialgouv/cdtn-sources";
 import {
   ArrowLink,
   Container,
   FlatList,
   Heading,
-  icons,
   theme,
 } from "@socialgouv/cdtn-ui";
 import Link from "next/link";
@@ -16,162 +11,83 @@ import React from "react";
 import styled from "styled-components";
 
 import { matopush } from "../piwik";
-import { CallToActionTile } from "./tiles/CallToAction";
+
+const matoSelectRelated = (reco, selection) => {
+  matopush([
+    "trackEvent",
+    "selectRelated",
+    JSON.stringify({
+      reco,
+      selection,
+    }),
+  ]);
+};
 
 export const RelatedItems = ({ items = [] }) => {
-  const tileSources = [SOURCES.EXTERNALS, SOURCES.LETTERS, SOURCES.TOOLS];
+  const nonArticleSources = [SOURCES.EXTERNALS, SOURCES.LETTERS, SOURCES.TOOLS];
 
-  const relatedTilesItems = items
-    .filter(({ source }) => tileSources.includes(source))
+  const relatedOtherItems = items
+    .filter(({ source }) => nonArticleSources.includes(source))
     .slice(0, 2);
-  const relatedLinkItems = items
-    .filter(({ source }) => !tileSources.includes(source))
+  const relatedArticleItems = items
+    .filter(({ source }) => !nonArticleSources.includes(source))
     .slice(0, 6);
 
-  const matoSelectRelated = (reco, selection) => {
-    matopush([
-      "trackEvent",
-      "selectRelated",
-      JSON.stringify({
-        reco,
-        selection,
-      }),
-    ]);
-  };
+  const relatedGroups = [
+    { items: relatedOtherItems, title: "Modèles et outils liés" },
+    { items: relatedArticleItems, title: "Articles liés" },
+  ];
 
   return (
     <Container>
-      <StyledFlatList>
-        {relatedTilesItems.map(
-          ({
-            action,
-            description,
-            icon,
-            slug,
-            source,
-            subtitle,
-            title,
-            url,
-            reco,
-          }) => (
-            <StyledTileItem key={slug || url}>
-              {source !== SOURCES.EXTERNALS ? (
-                <Link
-                  as={`/${getRouteBySource(source)}/${slug}`}
-                  href={`/${getRouteBySource(source)}/[slug]`}
-                  passHref
-                >
-                  <CallToActionTile
-                    rel="nofollow"
-                    action={action || "Consulter"}
-                    custom
-                    icon={
-                      source === SOURCES.LETTERS ? icons.Document : icons[icon]
-                    }
-                    title={title}
-                    subtitle={getLabelBySource(source)}
-                    onClick={() =>
-                      matoSelectRelated(
-                        reco,
-                        `/${getRouteBySource(source)}/${slug}`
-                      )
-                    }
-                  />
-                </Link>
-              ) : (
-                <CallToActionTile
-                  action={action || "Consulter"}
-                  custom={false}
-                  href={url}
-                  icon={icons[icon]}
-                  rel="noopener noreferrer nofollow"
-                  subtitle={subtitle}
-                  target="_blank"
-                  title={title}
-                  onClick={() => matoSelectRelated(reco, url)}
-                >
-                  {description}
-                </CallToActionTile>
-              )}
-            </StyledTileItem>
-          )
-        )}
-      </StyledFlatList>
-      {relatedLinkItems.length > 0 && (
-        <>
-          <Heading as="div">
-            Les articles pouvant vous intéresser&nbsp;:
-          </Heading>
-          <FlatList>
-            {relatedLinkItems.map(({ slug, source, title, reco }) => {
-              let rootSlug = slug;
-              let hash;
-              if (slug.includes("#")) {
-                [rootSlug, hash] = slug.split("#");
-              }
-              hash = hash ? `#${hash}` : "";
-              rootSlug = rootSlug ? `/${rootSlug}` : "";
-              const route = getRouteBySource(source);
+      {relatedGroups.map(
+        ({ title, items }) =>
+          items.length > 0 && (
+            <>
+              <Heading as="div">{title}&nbsp;:</Heading>
+              <FlatList>
+                {items.map(({ slug, source, title, reco }) => {
+                  let rootSlug = slug;
+                  let hash;
+                  if (slug.includes("#")) {
+                    [rootSlug, hash] = slug.split("#");
+                  }
+                  hash = hash ? `#${hash}` : "";
+                  rootSlug = rootSlug ? `/${rootSlug}` : "";
+                  const route = getRouteBySource(source);
 
-              return (
-                <StyledLinkItem key={slug}>
-                  <Link
-                    href={`/${route}${rootSlug ? "/[slug]" : ""}`}
-                    as={`/${route}${rootSlug}${hash}`}
-                    passHref
-                  >
-                    <ArrowLink
-                      rel="nofollow"
-                      arrowPosition="left"
-                      onClick={() =>
-                        matoSelectRelated(reco, `${route}${rootSlug}${hash}`)
-                      }
-                    >
-                      {title}
-                    </ArrowLink>
-                  </Link>
-                </StyledLinkItem>
-              );
-            })}
-          </FlatList>
-        </>
+                  return (
+                    <StyledLinkItem key={slug}>
+                      <Link
+                        href={`/${route}${rootSlug ? "/[slug]" : ""}`}
+                        as={`/${route}${rootSlug}${hash}`}
+                        passHref
+                      >
+                        <ArrowLink
+                          rel="nofollow"
+                          arrowPosition="left"
+                          onClick={() =>
+                            matoSelectRelated(
+                              reco,
+                              `${route}${rootSlug}${hash}`
+                            )
+                          }
+                        >
+                          {title}
+                        </ArrowLink>
+                      </Link>
+                    </StyledLinkItem>
+                  );
+                })}
+              </FlatList>
+            </>
+          )
       )}
     </Container>
   );
 };
 
-const { breakpoints, spacings } = theme;
-
-const StyledFlatList = styled(FlatList)`
-  @media (max-width: ${breakpoints.tablet}) {
-    display: flex;
-  }
-  @media (max-width: ${breakpoints.mobile}) {
-    flex-direction: column;
-  }
-`;
-
-const StyledTileItem = styled.li`
-  display: flex;
-  margin: 0 0 ${spacings.base} 0;
-  padding: 0;
-  @media (max-width: ${breakpoints.tablet}) {
-    display: flex;
-    flex: 1 0 auto;
-    justify-content: stretch;
-    width: calc((100% - ${spacings.base}) / 2);
-    & + & {
-      margin-left: ${spacings.base};
-    }
-  }
-  @media (max-width: ${breakpoints.mobile}) {
-    flex: 1 0 auto;
-    width: 100%;
-    & + & {
-      margin-left: 0;
-    }
-  }
-`;
+const { spacings } = theme;
 
 const StyledLinkItem = styled.li`
   margin: 0 0 ${spacings.base} 0;
