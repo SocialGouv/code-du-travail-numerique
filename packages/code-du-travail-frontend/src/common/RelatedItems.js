539669e18784154000f83b9d15267473e34055f2
ManuC <colasante.manuel@gmail.com>
2020-11-04 19:34:17 +0100

    feat(frontend): add survey modal

--- packages/code-du-travail-frontend/src/common/RelatedItems.js
+++ packages/code-du-travail-frontend/src/common/RelatedItems.js
@@ -1,16 +1,19 @@
 import { getRouteBySource, SOURCES } from "@socialgouv/cdtn-sources";
 import {
   ArrowLink,
+  Button,
   Container,
   FlatList,
   Heading,
   theme,
+  Toast,
 } from "@socialgouv/cdtn-ui";
 import Link from "next/link";
 import React from "react";
 import styled from "styled-components";
 
 import { matopush } from "../piwik";
+import { SurveyModal } from "./SurveyModal";
 
 const matoSelectRelated = (reco, selection) => {
   matopush([
@@ -41,6 +44,48 @@ export const RelatedItems = ({ items = [] }) => {
 
   return (
     <Container>
+      <SurveyModal>
+        {({
+          setModalVisible,
+          isPromptVisible,
+          isSurveyDisabled,
+          setPromptVisible,
+          setSurveyDisabled,
+        }) =>
+          !isSurveyDisabled &&
+          isPromptVisible && (
+            <PromptWrapper>
+              <Toast animate="from-right" wide shadow>
+                <CloseButton
+                  variant="naked"
+                  small
+                  narrow
+                  title="ne pas répondre aux questions"
+                  onClick={() => {
+                    setPromptVisible(false);
+                    setSurveyDisabled(true);
+                  }}
+                >
+                  <ScreenReaderOnly>fermer la modale</ScreenReaderOnly>
+                  <CloseIcon aria-hidden />
+                </CloseButton>
+                <PromptContainer>
+                  <Subtitle>Questionnaire</Subtitle>
+                  <PromptLabel>Aidez-nous à améliorer le site</PromptLabel>
+                  <Button
+                    small
+                    onClick={() => {
+                      setModalVisible(true);
+                    }}
+                  >
+                    Commencer
+                  </Button>
+                </PromptContainer>
+              </Toast>
+            </PromptWrapper>
+          )
+        }
+      </SurveyModal>
       {relatedGroups.map(
         ({ title, items }) =>
           items.length > 0 && (
@@ -87,4 +132,52 @@ const { spacings } = theme;
 const StyledLinkItem = styled.li`
   margin: 0 0 ${spacings.base} 0;
   padding: 0;
+`;
+
+// Survey stuff
+
+const PromptWrapper = styled.div`
+  margin-bottom: ${spacings.larger};
+  @media (max-width: ${breakpoints.tablet}) {
+    position: fixed;
+    right: 27%;
+    bottom: ${spacings.small};
+    left: 27%;
+    z-index: 1;
+    margin-bottom: 0;
+  }
+  @media (max-width: ${breakpoints.mobile}) {
+    right: ${spacings.small};
+    left: ${spacings.small};
+  }
+`;
+
+const PromptContainer = styled.div`
+  padding: ${spacings.base} 0;
+  text-align: center;
+  @media (max-width: ${breakpoints.mobile}) {
+    padding: ${spacings.xsmall} 0;
+  }
+`;
+
+const CloseButton = styled(Button)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 2;
+  color: ${({ theme }) => theme.secondary};
+`;
+
+const CloseIcon = styled(icons.Close)`
+  width: 2rem;
+  height: 2rem;
+`;
+
+const PromptLabel = styled.div`
+  margin-bottom: ${spacings.base};
+  font-weight: bold;
+  font-size: 1.7rem;
+  @media (max-width: ${breakpoints.mobile}) {
+    padding-right: ${spacings.xmedium};
+  }
 `;
