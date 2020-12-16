fbe7a6a3aa18eda7242e4ebcdd6933db0b4c1853
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-12-16 14:25:25 +0100

    clean

--- .k8s/__tests__/kosko generate --env prod restore.ts
+++ .k8s/__tests__/kosko generate --env prod restore.ts
@@ -9,9 +9,6 @@ test("kosko generate --prod restore", async () => {
   process.env.SOURCE_SERVER = "prod";
   process.env.DESTINATION_CONTAINER = "destination-container";
   process.env.DESTINATION_SERVER = "dev";
-  process.env.BACKUP_DB_OWNER = "some-owner";
-  process.env.BACKUP_DB_NAME = "some-database";
-  process.env.BACKUP_DB_FILE = "some-backup.sql.gz";
   expect(
     await getEnvManifests("prod", "restore", {
       ...project("code-du-travail-numerique").prod,
