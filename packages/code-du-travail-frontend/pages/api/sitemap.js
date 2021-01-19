4971ecb04b80e98dd8c61a2e3e1a174f19cdea62
LionelB <lionel@lumographe.fr>
2021-01-19 13:53:07 +0100

    fix(frontend): handle branch sitemap

--- packages/code-du-travail-frontend/pages/api/sitemap.js
+++ packages/code-du-travail-frontend/pages/api/sitemap.js
@@ -4,13 +4,33 @@ import https from "https";
 const {
   AZURE_BASE_URL = "https://cdtnadminprod.blob.core.windows.net",
   SITEMAP_FILE = "sitemap.xml",
+  FRONTEND_HOST,
 } = process.env;
 
+let sitemapFile = SITEMAP_FILE;
+
 export default async function Sitemap(req, res) {
-  const promise = new Promise((resolve, reject) => {
+  if (/dev2/.test(FRONTEND_HOST)) {
+    try {
+      const response = await fetch(
+        `${AZURE_BASE_URL}/sitemap/${SITEMAP_FILE}`,
+        {
+          method: "HEAD",
+        }
+      );
+      if (response.ok) {
+        const [hash] = FRONTEND_HOST.match(/^\w+/);
+        sitemapFile = `sitemap-${hash}.xml`;
+      }
+    } catch (error) {
+      sitemapFile = "sitemap.xml";
+    }
+  }
+
+  const promise = new Promise(function SitemapStream(resolve, reject) {
     const get = AZURE_BASE_URL.startsWith("https") ? https.get : http.get;
     const sitempaReq = get(
-      `${AZURE_BASE_URL}/sitemap/${SITEMAP_FILE}`,
+      `${AZURE_BASE_URL}/sitemap/${sitemapFile}`,
       (response) => {
         response.pipe(res);
         response.on("end", () => {
