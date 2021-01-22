210bccbf227e66d7efb3736c71edb51a8eb07afc
ManuC <colasante.manuel@gmail.com>
2021-01-22 14:56:06 +0100

    refactor(data): autolint

--- packages/code-du-travail-data/dataset/synonyms/extract-TESS.js
+++ packages/code-du-travail-data/dataset/synonyms/extract-TESS.js
@@ -4,29 +4,22 @@
 
 const thesaurus = require("./extract.json");
 
-const rmBreaks = row =>
-  row
-    .replace(/\n/g, " ")
-    .replace(/(\s+)/g, " ")
-    .trim();
+const rmBreaks = (row) => row.replace(/\n/g, " ").replace(/(\s+)/g, " ").trim();
 
-const cleanTerm = row =>
-  row
-    .replace(/\\n/g, " ")
-    .replace(/(\s+)/g, " ")
-    .trim();
+const cleanTerm = (row) =>
+  row.replace(/\\n/g, " ").replace(/(\s+)/g, " ").trim();
 
-const cleanMulti = row =>
+const cleanMulti = (row) =>
   row
     .split("\n")
-    .map(x => x.trim())
+    .map((x) => x.trim())
     .filter(Boolean);
 
 console.log(
   JSON.stringify(
     thesaurus.pageTables
-      .filter(page => page.page > 1)
-      .map(page => {
+      .filter((page) => page.page > 1)
+      .map((page) => {
         if (page.page === 2) {
           return page.tables.slice(3);
         }
@@ -34,16 +27,16 @@ console.log(
       })
       .reduce((terms, c) => {
         terms.push(
-          ...c.map(x => ({
-            term: cleanTerm(x[0]),
-            generic: rmBreaks(x[1]),
-            specific: cleanMulti(x[2]),
-            related: rmBreaks(x[3].trim()),
-            notes: x[4].trim(),
+          ...c.map((x) => ({
             equivalent: rmBreaks(x[5].trim()),
-          })),
+            generic: rmBreaks(x[1]),
+            notes: x[4].trim(),
+            related: rmBreaks(x[3].trim()),
+            specific: cleanMulti(x[2]),
+            term: cleanTerm(x[0]),
+          }))
         );
         return terms;
-      }, []),
-  ),
+      }, [])
+  )
 );
