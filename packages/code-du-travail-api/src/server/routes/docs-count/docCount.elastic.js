a8357003a0c9bb2a57de6a353465f5d11d5fa365
ManuC <colasante.manuel@gmail.com>
2021-02-02 18:14:28 +0100

    refactor(api): make doc-counts display all buckets

--- packages/code-du-travail-api/src/server/routes/docs-count/docCount.elastic.js
+++ packages/code-du-travail-api/src/server/routes/docs-count/docCount.elastic.js
@@ -1,8 +1,15 @@
 module.exports = {
-  size: 0,
   aggs: {
     sources: {
-      terms: { field: "source" },
+      terms: {
+        exclude: ["fiches_ministere_travail", "versions"],
+        field: "source",
+        order: {
+          _key: "asc",
+        },
+        size: 17,
+      },
     },
   },
+  size: 0,
 };
