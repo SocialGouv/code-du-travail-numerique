cc6951e2d8e32c0ee726b0a11e1774d977e50b15
RemiM <rmelisson@gmail.com>
2021-02-17 09:58:35 +0100

    fix import

--- packages/code-du-travail-frontend/src/conventions/Convention/__tests__/index.test.js
+++ packages/code-du-travail-frontend/src/conventions/Convention/__tests__/index.test.js
@@ -1,8 +1,8 @@
 import { render } from "@testing-library/react";
 import React from "react";
 
-import { agreement } from "../../Search/api/__tests__/conventions.mock";
 import Convention from "..";
+import { agreement } from "../../Search/api/__tests__/conventions.mock";
 
 describe("<Convention />", () => {
   it("renders", () => {
