9691bfb695f0efe9edcf4fa739e40b5c7fe5352f
LionelB <lionel@lumographe.fr>
2021-02-17 13:57:27 +0100

    feat(frontend): add canonical link on service-public and travail-emploi document

--- packages/code-du-travail-frontend/pages/fiche-service-public/[slug].js
+++ packages/code-du-travail-frontend/pages/fiche-service-public/[slug].js
@@ -2,6 +2,7 @@ import { Container, Section } from "@socialgouv/cdtn-ui";
 import { FicheServicePublic } from "@socialgouv/react-fiche-service-public";
 import fetch from "isomorphic-unfetch";
 import getConfig from "next/config";
+import Head from "next/head";
 import { withRouter } from "next/router";
 import React from "react";
 
@@ -48,6 +49,9 @@ class Fiche extends React.Component {
     return (
       <Layout>
         <Metas title={title} description={description} />
+        <Head>
+          <link rel="canonical" href={url} />
+        </Head>
         <Answer
           title={title}
           relatedItems={relatedItems}
