fefa7b421c4b172e0a2c244f21615f3f27b9efba
RemiM <rmelisson@gmail.com>
2021-02-23 19:21:47 +0100

    update linter & fix

--- packages/code-du-travail-frontend/src/conventions/Convention/__tests__/index.test.js
+++ packages/code-du-travail-frontend/src/conventions/Convention/__tests__/index.test.js
@@ -1,13 +1,12 @@
 import { render } from "@testing-library/react";
 import React from "react";
 
-import Convention from "..";
 import { agreement } from "../../Search/api/__tests__/conventions.mock";
+import Convention from "..";
 
 describe("<Convention />", () => {
   it("renders", () => {
     const { container } = render(<Convention convention={agreement} />);
-
     expect(container).toMatchSnapshot();
   });
 });
