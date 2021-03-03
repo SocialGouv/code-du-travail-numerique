6047c91b5e7084536ecfbbbb5323efba8fd44d29
Douglas DUTEIL <douglasduteil@gmail.com>
2021-03-03 13:45:27 +0100

    fix(data): do not insert packages with dots

--- packages/code-du-travail-data/indexing/versions.js
+++ packages/code-du-travail-data/indexing/versions.js
@@ -2,9 +2,14 @@ const dependencies = require("../package.json").dependencies;
 
 export function getVersions(pattern = "^@socialgouv/") {
   const packageRule = new RegExp(pattern);
-  return Object.entries(dependencies)
-    .filter(([name]) => {
-      return packageRule.test(name);
-    })
-    .reduce((state, [name, version]) => ({ ...state, [name]: version }), {});
+  return (
+    Object.entries(dependencies)
+      // NOTE(douglasduteil): do not list packages with dots.
+      // As most @socialgouv/cdtn...* are internals we do not need to list them versions
+      .filter(([name]) => name.includes("."))
+      .filter(([name]) => {
+        return packageRule.test(name);
+      })
+      .reduce((state, [name, version]) => ({ ...state, [name]: version }), {})
+  );
 }
