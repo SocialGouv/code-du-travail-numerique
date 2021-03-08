a6a14bec4d31ef8745cf0e400267d325a00154b8
Douglas DUTEIL <douglasduteil@gmail.com>
2021-03-08 16:10:37 +0100

    feat(logger): allow process.env.LOG_LEVEL changes

--- infra/logger/src/index.ts
+++ infra/logger/src/index.ts
@@ -4,9 +4,10 @@ import { createLogger, format, transports } from "winston";
 
 const { Console } = transports;
 
+export const LOG_LEVEL = process.env.LOG_LEVEL ?? "info";
 export const logger = createLogger({
   format: format.json(),
-  level: "info",
+  level: LOG_LEVEL,
   transports: [
     new Console({
       format: format.prettyPrint(),
