5f526364ebdc1596863bd5b6ba110c7ad0a7442a
Douglas DUTEIL <douglasduteil@gmail.com>
2021-03-19 22:10:14 +0100

    build(k8s): add dummy ES_INDEX_PREFIX logic

--- .k8s/components/api.ts
+++ .k8s/components/api.ts
@@ -2,10 +2,26 @@ import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/app";
 import { IIoK8sApiCoreV1HTTPGetAction } from "kubernetes-models/v1";
 
+let ES_INDEX_PREFIX =
+  process.env.ES_INDEX_PREFIX ?? `cdtn-feature-${process.env.CI_ENVIRONMENT_SLUG}`;
+
+if (process.env.CI_COMMIT_REF_SLUG === "master") {
+  ES_INDEX_PREFIX = "cdtn-master"
+}
+
+if (process.env.CI_COMMIT_TAG) {
+  ES_INDEX_PREFIX = "cdtn-preprod"
+}
+
+if (process.env.PRODUCTION) {
+  ES_INDEX_PREFIX = "cdtn-prod"
+}
+
 const httpGet: IIoK8sApiCoreV1HTTPGetAction = {
   path: "/api/v1/version",
   port: "http",
 };
+
 const manifests = create("api", {
   env,
   config: {
@@ -36,7 +52,13 @@ const manifests = create("api", {
           cpu: "1000m",
           memory: "2Gi",
         },
-      }
+      },
+      env: [
+        {
+          name: "ES_INDEX_PREFIX",
+          value: ES_INDEX_PREFIX
+        }
+      ]
     },
   },
 });
