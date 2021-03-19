d15dcfdfd05eb9745a688ce0be80d3f5b000a533
Douglas DUTEIL <douglasduteil@gmail.com>
2021-03-19 22:46:26 +0100

    build(k8s): add dummy ES_INDEX_PREFIX logic (5)

--- .k8s/components/api.ts
+++ .k8s/components/api.ts
@@ -1,9 +1,5 @@
 import { create } from "@socialgouv/kosko-charts/components/app";
-import { addInitContainer } from "@socialgouv/kosko-charts/utils/addInitContainer";
-import { waitForHttp } from "@socialgouv/kosko-charts/utils/waitForHttp";
-import { ok } from "assert";
 import env from "@kosko/env";
-import type { Deployment } from "kubernetes-models/apps/v1/Deployment";
 import type { IIoK8sApiCoreV1HTTPGetAction } from "kubernetes-models/v1";
 
 import { ES_INDEX_PREFIX } from "../utils/ES_INDEX_PREFIX";
@@ -53,24 +49,6 @@ const manifests = create("api", {
     },
   },
 });
-
-//
-
-const deployment = manifests.find(
-  (manifest: { kind: string }): manifest is Deployment =>
-    manifest.kind === "Deployment"
-);
-ok(deployment);
-
-const waitForElasticsearchIndexContainer = waitForHttp({
-  name: "elasticsearch-index",
-  url: "${ELASTICSEARCH_URL}/_cat/health?h=status",
-});
-waitForElasticsearchIndexContainer.image = "registry.gitlab.factory.social.gouv.fr/socialgouv/docker/wait-for-http:5.0.1"
-waitForElasticsearchIndexContainer.envFrom = [
-  { secretRef: { name: "api-secret" } },
-];
-addInitContainer(deployment, waitForElasticsearchIndexContainer);
 
 //
 
