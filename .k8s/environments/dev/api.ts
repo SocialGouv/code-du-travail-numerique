e1c09ea65ed9c6ef7ab5f41707ab2f54540cbcc9
Douglas DUTEIL <douglasduteil@gmail.com>
2021-03-22 13:31:33 +0100

    build(k8s): deploy with kosko

---
+++ .k8s/environments/dev/api.ts
@@ -0,0 +1,17 @@
+import { AppConfig } from "@socialgouv/kosko-charts/components/app";
+
+export default {
+  container: {
+    resources: {
+      requests: {
+        cpu: "100m",
+        memory: "256Mi",
+      },
+      limits: {
+        cpu: "200m",
+        memory: "320Mi",
+      },
+    },
+  },
+  subDomainPrefix: "api-",
+} as Partial<AppConfig>;
