71f7503cdc230751c152f317723ed750edf41a7f
Martial Maillot <martial.maillot@gmail.com>
2021-04-06 14:52:17 +0200

    feat(simulator): init folder for publicode models (#3416)
    * feat(simulator): init the sociale modèle package
    
    * feat(simulator): add a first model with unit test
    
    * feat(simulator): add a readme
    
    * feat(simulator): add a missing reference about retired notice
    
    * doc(simulator): avoid multiple top level headings
    
    * feat(simulator): generate concat models in the lib folder

---
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.spec.ts
@@ -0,0 +1,52 @@
+import Engine from "publicodes";
+import { mergeModels } from "../utils/merger";
+
+const engine = new Engine(mergeModels());
+
+test.each`
+  seniority | expectedNotice
+  ${3}      | ${0}
+  ${6}      | ${1}
+  ${11}     | ${1}
+  ${12}     | ${1}
+  ${23}     | ${1}
+  ${24}     | ${2}
+  ${25}     | ${2}
+`(
+  "Pour un employé possédant $seniority mois d'ancienneté, son préavis de mise à la retraite devrait être $expectedNotice mois",
+  ({ seniority, expectedNotice }) => {
+    const result = engine
+      .setSituation({
+        "contrat salarié . ancienneté": seniority,
+        "contrat salarié . mise à la retraite": "oui",
+      })
+      .evaluate("contrat salarié . préavis de retraite");
+
+    expect(result.nodeValue).toEqual(expectedNotice);
+    expect(result.unit?.numerators).toEqual(["mois"]);
+  }
+);
+
+test.each`
+  seniority | expectedNotice
+  ${3}      | ${0}
+  ${6}      | ${1}
+  ${11}     | ${1}
+  ${12}     | ${1}
+  ${23}     | ${1}
+  ${24}     | ${2}
+  ${25}     | ${2}
+`(
+  "Pour un employé possédant $seniority mois d'ancienneté, son préavis de départ à la retraite devrait être $expectedNotice mois",
+  ({ seniority, expectedNotice }) => {
+    const result = engine
+      .setSituation({
+        "contrat salarié . ancienneté": seniority,
+        "contrat salarié . mise à la retraite": "non",
+      })
+      .evaluate("contrat salarié . préavis de retraite");
+
+    expect(result.nodeValue).toEqual(expectedNotice);
+    expect(result.unit?.numerators).toEqual(["mois"]);
+  }
+);
---
+++ packages/code-du-travail-modeles/src/utils/merger.ts
@@ -0,0 +1,25 @@
+import path from "path";
+import fs from "fs";
+import yaml from "yaml";
+
+const publicodesDir = path.resolve(__dirname, "../../src/modeles");
+
+export function mergeModels(): any {
+  return yaml.parse(concatenateFilesInDir(publicodesDir));
+}
+
+function concatenateFilesInDir(dirPath: string): string {
+  return fs
+    .readdirSync(dirPath)
+    .map((filename) => {
+      const fullpath = path.join(dirPath, filename);
+      if (fs.statSync(fullpath).isDirectory()) {
+        return concatenateFilesInDir(fullpath);
+      } else {
+        return filename.endsWith(".yaml")
+          ? fs.readFileSync(fullpath).toString()
+          : "";
+      }
+    })
+    .join("\n");
+}
