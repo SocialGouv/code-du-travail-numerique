c33f1c41c3eda96ec01a6e9c08cecabfcf2bcffe
LionelB <lionel.breduillieard@beta.gouv.fr>
2021-04-27 17:56:57 +0200

    fix missing defined icons

--- packages/react-ui/src/icons/index.js
+++ packages/react-ui/src/icons/index.js
@@ -1,19 +1,24 @@
 // Bicolor icons
 export { default as Agreement } from "./components/bicolor/Agreement";
+export { default as Apprenticeship } from "./components/bicolor/Apprenticeship";
 export { default as Calculator } from "./components/bicolor/Calculator";
 export { default as Constitution } from "./components/bicolor/Constitution";
 export { default as Contract } from "./components/bicolor/Contract";
+export { default as CrisisCompagny } from "./components/bicolor/CrisisCompagny";
 export { default as Custom } from "./components/bicolor/Custom";
 export { default as Depart } from "./components/bicolor/Depart";
 export { default as Document } from "./components/bicolor/Document";
 export { default as Eurotext } from "./components/bicolor/Eurotext";
 export { default as Focus } from "./components/bicolor/Focus";
 export { default as Formation } from "./components/bicolor/Formation";
+export { default as France } from "./components/bicolor/France";
+export { default as Handicap } from "./components/bicolor/Handicap";
 export { default as Health } from "./components/bicolor/Health";
 export { default as Holidays } from "./components/bicolor/Holidays";
 export { default as Indemnity } from "./components/bicolor/Indemnity";
 export { default as International } from "./components/bicolor/International";
 export { default as Laws } from "./components/bicolor/Laws";
+export { default as Lens } from "./components/bicolor/Lens";
 export { default as Logo } from "./components/bicolor/Logo";
 export { default as Nego } from "./components/bicolor/Nego";
 export { default as Oups } from "./components/bicolor/Oups";
@@ -26,6 +31,7 @@ export { default as Resources } from "./components/bicolor/Resources";
 export { default as Rules } from "./components/bicolor/Rules";
 export { default as Salary } from "./components/bicolor/Salary";
 export { default as Security } from "./components/bicolor/Security";
+export { default as SpecificJob } from "./components/bicolor/SpecificJob";
 export { default as Termination } from "./components/bicolor/Termination";
 export { default as Time } from "./components/bicolor/Time";
 export { default as Unemployment } from "./components/bicolor/Unemployment";
@@ -33,6 +39,7 @@ export { default as Uses } from "./components/bicolor/Uses";
 export { default as Warning } from "./components/bicolor/Warning";
 export { default as WorkersHero } from "./components/bicolor/WorkersHero";
 export { default as WorkersTheme } from "./components/bicolor/WorkersTheme";
+export { default as Youth } from "./components/bicolor/Youth";
 
 // Monochrome icons
 export { default as ArrowDown } from "./components/monochrome/ArrowDown";
