eb91a34ac9840e4c613d72e112520900f3d01edc
LionelB <lionel.breduillieard@beta.gouv.fr>
2021-05-07 16:32:29 +0200

    feat(tracking): add tracking context with unified id

--- packages/code-du-travail-frontend/src/outils/ConventionCollective/agreement/SearchAgreement.tsx
+++ packages/code-du-travail-frontend/src/outils/ConventionCollective/agreement/SearchAgreement.tsx
@@ -1,15 +1,12 @@
 import { Section } from "@socialgouv/cdtn-ui";
-import { useRouter } from "next/router";
-import pDebounce from "p-debounce";
 import React, { useEffect, useState } from "react";
-import { v4 as generateUUID } from "uuid";
 
 import {
   Agreement,
   searchConvention,
 } from "../../../conventions/Search/api/convention.service";
-import { matopush } from "../../../piwik";
 import { createSuggesterHook, FetchReducerState } from "../common/Suggester";
+import { useTrackingContext } from "../common/TrackingContext";
 import { SearchAgreementInput } from "./SearchAgreementInput";
 
 type Props = {
@@ -19,32 +16,19 @@ type Props = {
   ) => JSX.Element;
 };
 
-const trackInput = pDebounce(
-  (query: string, path: string, trackingUID: string) => {
-    if (query.length > 1) {
-      matopush(["trackEvent", "cc_search", path, `${trackingUID} : ${query}`]);
-    }
-  },
-  2000
-);
-
 const useAgreementSuggester = createSuggesterHook(searchConvention);
 
 export function SearchAgreement({ renderResults }: Props): JSX.Element {
   const [query, setQuery] = useState("");
-  const [trackingUID, setTrackingUID] = useState("");
-  const router = useRouter();
   const state = useAgreementSuggester(query);
+  const { trackEvent, title, uuid } = useTrackingContext();
 
   useEffect(() => {
-    // we want to connect events that are
-    // related so we only generate an uuid on mount
-    setTrackingUID(generateUUID());
-  }, []);
+    trackEvent("cc_search", title, query, uuid);
+  }, [query, trackEvent, title, uuid]);
 
   const searchInputHandler = (keyEvent) => {
     const value = keyEvent.target.value;
-    trackInput(value, router.asPath, trackingUID);
     setQuery(value);
   };
 
