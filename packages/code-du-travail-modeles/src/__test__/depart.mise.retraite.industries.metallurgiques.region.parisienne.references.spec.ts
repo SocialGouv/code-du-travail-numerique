a96daad7ba7f990f77f63966ebcdd91899355e16
Martial Maillot <martial.maillot@gmail.com>
2021-05-21 11:05:07 +0200

    feat(simulator): retirement notice, add references on CC 54

---
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.industries.metallurgiques.region.parisienne.references.spec.ts
@@ -0,0 +1,56 @@
+import Engine from "publicodes";
+import { mergeModels } from "../internal/merger";
+import { getNotifications } from "../utils/GetNotifications";
+import {
+  DepartRetraiteReferences,
+  MiseRetraiteReferences,
+} from "./common/LegalReferences";
+import { getReferences } from "../utils/GetReferences";
+
+const engine = new Engine(mergeModels());
+
+const MiseRetraiteCCReferences = [
+  {
+    article: "Article 34 bis de l'avenant mensuel",
+    url:
+      "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000036772209/?idConteneur=KALICONT000005635149",
+  },
+  {
+    article: "Article 11 bis de l'accord national du 10 juillet 1970",
+    url:
+      "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000023173509?idConteneur=KALICONT000005635602",
+  },
+];
+
+const DepartRetraiteCCReferences = [
+  {
+    article: "Article 34 de l'avenant mensuel",
+    url:
+      "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000032062291/?idConteneur=KALICONT000005635149",
+  },
+  {
+    article: "Article 11 de l'accord national du 10 juillet 1970",
+    url:
+      "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000023173503",
+  },
+];
+
+test.each`
+  retirement  | expectedReferences
+  ${"mise"}   | ${MiseRetraiteReferences.concat(MiseRetraiteCCReferences)}
+  ${"depart"} | ${DepartRetraiteReferences.concat(DepartRetraiteCCReferences)}
+`(
+  "Vérification des références juridiques pour un employé en $retirement à la retraite",
+  ({ retirement, expectedReferences }) => {
+    const result = getReferences(
+      engine.setSituation({
+        "contrat salarié . convention collective": "'IDCC0054'",
+        "contrat salarié . mise à la retraite":
+          retirement === "mise" ? "oui" : "non",
+        "contrat salarié . ancienneté": 5,
+      })
+    );
+
+    expect(result).toEqual(expect.arrayContaining(expectedReferences));
+  }
+);
