e6371f4055f411cba5a4139d27626c0fba61292a
Martial Maillot <martial.maillot@gmail.com>
2021-06-11 13:36:06 +0200

    fix(simulator): new implementation for CC 675

--- packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.habillement.commerce.succursales.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.habillement.commerce.succursales.spec.ts
@@ -4,19 +4,20 @@ import { mergeModels } from "../internal/merger";
 const engine = new Engine(mergeModels());
 
 test.each`
-  seniority | category                | expectedNotice
-  ${5}      | ${"Cadres"}             | ${3}
-  ${6}      | ${"Cadres"}             | ${1}
-  ${24}     | ${"Cadres"}             | ${2}
-  ${5}      | ${"Employés"}           | ${0}
-  ${6}      | ${"Employés"}           | ${1}
-  ${24}     | ${"Employés"}           | ${2}
-  ${5}      | ${"Agents de maîtrise"} | ${0}
-  ${6}      | ${"Agents de maîtrise"} | ${1}
-  ${24}     | ${"Agents de maîtrise"} | ${2}
+  seniority | category                | expectedNotice | expectedUnit
+  ${5}      | ${"Cadres"}             | ${3}           | ${"mois"}
+  ${6}      | ${"Cadres"}             | ${1}           | ${"mois"}
+  ${24}     | ${"Cadres"}             | ${2}           | ${"mois"}
+  ${0}      | ${"Employés"}           | ${0}           | ${"mois"}
+  ${1}      | ${"Employés"}           | ${15}          | ${"jours"}
+  ${6}      | ${"Employés"}           | ${1}           | ${"mois"}
+  ${24}     | ${"Employés"}           | ${1}           | ${"mois"}
+  ${5}      | ${"Agents de maîtrise"} | ${2}           | ${"mois"}
+  ${6}      | ${"Agents de maîtrise"} | ${1}           | ${"mois"}
+  ${24}     | ${"Agents de maîtrise"} | ${2}           | ${"mois"}
 `(
-  "Pour un $category possédant $seniority mois d'ancienneté, son préavis de départ à la retraite devrait être $expectedNotice mois",
-  ({ seniority, category, expectedNotice }) => {
+  "Pour un $category possédant $seniority mois d'ancienneté, son préavis de départ à la retraite devrait être $expectedNotice $expectedUnit",
+  ({ seniority, category, expectedNotice, expectedUnit }) => {
     const result = engine
       .setSituation({
         "contrat salarié . convention collective": "'IDCC0675'",
@@ -27,7 +28,7 @@ test.each`
       .evaluate("contrat salarié . préavis de retraite");
 
     expect(result.nodeValue).toEqual(expectedNotice);
-    expect(result.unit?.numerators).toEqual(["mois"]);
+    expect(result.unit?.numerators).toEqual([expectedUnit]);
     expect(result.missingVariables).toEqual({});
   }
 );
