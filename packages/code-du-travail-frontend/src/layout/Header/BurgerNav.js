02ce90a0c6537ff5326d39310c7ffe3db6015d5e
maxgfr <maxgolfier@gmail.com>
2021-09-01 19:16:26 +0200

    fix(frontend): update div instead of li in main page

--- packages/code-du-travail-frontend/src/layout/Header/BurgerNav.js
+++ packages/code-du-travail-frontend/src/layout/Header/BurgerNav.js
@@ -3,6 +3,7 @@ import {
   BurgerNavButton as NavButton,
   BurgerNavCurrent as NavCurrent,
   BurgerNavLink as NavAnchor,
+  theme,
 } from "@socialgouv/cdtn-ui";
 import Link from "next/link";
 import React from "react";
@@ -12,12 +13,10 @@ import { AccessibilityModal } from "../../common/AccessibilityModal";
 
 export const BurgerNav = ({ currentPage }) => (
   <StyledBurgerNav role="navigation" id="navigation">
-    <>
-      <AccessibilityModal>
-        {(openModal) => (
-          <NavButton onClick={openModal}>Accessibilité</NavButton>
-        )}
-      </AccessibilityModal>
+    <AccessibilityModal>
+      {(openModal) => <NavButton onClick={openModal}>Accessibilité</NavButton>}
+    </AccessibilityModal>
+    <StyledUl>
       {currentPage !== "tools" ? (
         <Link href="/outils" passHref>
           <NavAnchor>Boîte&nbsp;à&nbsp;outils</NavAnchor>
@@ -25,17 +24,32 @@ export const BurgerNav = ({ currentPage }) => (
       ) : (
         <NavCurrent>Boîte&nbsp;à&nbsp;outils</NavCurrent>
       )}
-      {currentPage !== "themes" ? (
-        <Link href="/themes" passHref>
-          <NavAnchor>Thèmes</NavAnchor>
-        </Link>
-      ) : (
-        <NavCurrent>Thèmes</NavCurrent>
-      )}
-    </>
+      <StyledLi>
+        {currentPage !== "themes" ? (
+          <Link href="/themes" passHref>
+            <NavAnchor>Thèmes</NavAnchor>
+          </Link>
+        ) : (
+          <NavCurrent>Thèmes</NavCurrent>
+        )}
+      </StyledLi>
+    </StyledUl>
   </StyledBurgerNav>
 );
 
 const StyledBurgerNav = styled(RootBurgerNav)`
   order: 2;
 `;
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  @media (max-width: ${theme.breakpoints.tablet}) {
+    flex-direction: column;
+  }
+  margin: 0;
+  padding: 0;
+`;
+
+const StyledLi = styled.li``;
