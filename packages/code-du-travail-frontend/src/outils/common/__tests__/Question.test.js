4c5fac09f562e6ae573696ad141ed6eefb009b59
Martial Maillot <martial.maillot@gmail.com>
2021-09-07 14:16:21 +0200

    feat(simulators): small UI issues

--- packages/code-du-travail-frontend/src/outils/common/__tests__/Question.test.js
+++ packages/code-du-travail-frontend/src/outils/common/__tests__/Question.test.js
@@ -8,71 +8,85 @@ describe("<Question />", () => {
   it("should render question", () => {
     const { container } = render(<Question>ma question</Question>);
     expect(container).toMatchInlineSnapshot(`
-      .c0 {
-        display: block;
-        margin-top: 2rem;
-        margin-bottom: 1rem;
-        font-weight: 600;
-        font-size: 1.8rem;
-        cursor: pointer;
-      }
+.c0 {
+  display: block;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  font-size: 1.8rem;
+  cursor: pointer;
+}
 
-      @media (max-width:600px) {
-        .c0 {
-          font-size: 1.6rem;
-        }
-      }
+.c1 {
+  font-weight: 600;
+}
 
-      <div>
-        <label
-          class="c0"
-        >
-          ma question
-        </label>
-      </div>
-    `);
+@media (max-width:600px) {
+  .c0 {
+    font-size: 1.6rem;
+  }
+}
+
+<div>
+  <label
+    class="c0"
+  >
+    <span
+      class="c1"
+    >
+      ma question
+    </span>
+  </label>
+</div>
+`);
   });
   it("should render mandatory question", () => {
     const { container } = render(
       <Question required>ma question obligatoire</Question>
     );
     expect(container).toMatchInlineSnapshot(`
-      .c1 {
-        color: #3e486e;
-        line-height: 1.25;
-        font-size: 1.6rem;
-        font-weight: 400;
-      }
+.c2 {
+  color: #3e486e;
+  line-height: 1.25;
+  font-size: 1.6rem;
+  font-weight: 400;
+}
 
-      .c0 {
-        display: block;
-        margin-top: 2rem;
-        margin-bottom: 1rem;
-        font-weight: 600;
-        font-size: 1.8rem;
-        cursor: pointer;
-      }
+.c0 {
+  display: block;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  font-size: 1.8rem;
+  cursor: pointer;
+}
 
-      @media (max-width:600px) {
-        .c0 {
-          font-size: 1.6rem;
-        }
-      }
+.c1 {
+  font-weight: 600;
+}
 
-      <div>
-        <label
-          class="c0"
-        >
-          ma question obligatoire
-          <span
-            class="c1"
-            font-size="default"
-            font-weight="400"
-          >
-             (obligatoire)
-          </span>
-        </label>
-      </div>
-    `);
+@media (max-width:600px) {
+  .c0 {
+    font-size: 1.6rem;
+  }
+}
+
+<div>
+  <label
+    class="c0"
+  >
+    <span
+      class="c1"
+    >
+      ma question obligatoire
+    </span>
+    <span
+      class="c2"
+      font-size="default"
+      font-weight="400"
+    >
+       (obligatoire)
+    </span>
+  </label>
+</div>
+`);
   });
 });
