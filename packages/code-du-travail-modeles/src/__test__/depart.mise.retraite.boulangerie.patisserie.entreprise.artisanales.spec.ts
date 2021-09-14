084a1d6a741b02167f367059cff5347f50fdc91b
Martial Maillot <martial.maillot@gmail.com>
2021-09-14 09:36:21 +0200

    feat(simulators): implement the agreement boulangerie patisserie

---
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.boulangerie.patisserie.entreprise.artisanales.spec.ts
@@ -0,0 +1,27 @@
+import Engine from "publicodes";
+import { mergeModels } from "../internal/merger";
+
+const engine = new Engine(mergeModels());
+
+test.each`
+  seniority | expectedNotice | expectedUnit
+  ${3}      | ${1}           | ${"semaine"}
+  ${6}      | ${1}           | ${"mois"}
+  ${24}     | ${6}           | ${"mois"}
+`(
+  "Pour un employé dans la convention collective Boulangerie Patisserie Entreprises Artisanales possédant $seniority mois d'ancienneté, son préavis de mise à la retraite devrait être $expectedNotice $expectedUnit",
+  ({ seniority, expectedNotice, expectedUnit }) => {
+    const result = engine
+      .setSituation({
+        "contrat salarié . convention collective": "'IDCC0843'",
+        "contrat salarié . ancienneté": seniority,
+        "contrat salarié . mise à la retraite": "oui",
+        "contrat salarié . travailleur handicapé": "non",
+      })
+      .evaluate("contrat salarié . préavis de retraite");
+
+    expect(result.nodeValue).toEqual(expectedNotice);
+    expect(result.unit?.numerators).toEqual([expectedUnit]);
+    expect(result.missingVariables).toEqual({});
+  }
+);
