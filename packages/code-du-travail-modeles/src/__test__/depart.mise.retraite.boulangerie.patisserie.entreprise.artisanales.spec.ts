158f193b7969bc4af709f63e0f4777b6ccba0019
maxgfr <maxgolfier@gmail.com>
2021-09-14 11:03:45 +0200

    feat(modeles): add idcc0843

--- packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.boulangerie.patisserie.entreprise.artisanales.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.boulangerie.patisserie.entreprise.artisanales.spec.ts
@@ -25,3 +25,26 @@ test.each`
     expect(result.missingVariables).toEqual({});
   }
 );
+
+test.each`
+  seniority | expectedNotice | expectedUnit
+  ${2}      | ${1}           | ${"semaine"}
+  ${6}      | ${1}           | ${"mois"}
+  ${24}     | ${2}           | ${"mois"}
+`(
+  "Pour un employé dans la convention collective Boulangerie Patisserie Entreprises Artisanales possédant $seniority mois d'ancienneté, son préavis de départ à la retraite devrait être $expectedNotice $expectedUnit",
+  ({ seniority, expectedNotice, expectedUnit }) => {
+    const result = engine
+      .setSituation({
+        "contrat salarié . convention collective": "'IDCC0843'",
+        "contrat salarié . ancienneté": seniority,
+        "contrat salarié . mise à la retraite": "non",
+        "contrat salarié . travailleur handicapé": "non",
+      })
+      .evaluate("contrat salarié . préavis de retraite");
+
+    expect(result.nodeValue).toEqual(expectedNotice);
+    expect(result.unit?.numerators).toEqual([expectedUnit]);
+    expect(result.missingVariables).toEqual({});
+  }
+);
