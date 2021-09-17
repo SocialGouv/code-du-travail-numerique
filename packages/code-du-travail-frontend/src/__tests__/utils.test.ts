33b74e8f926a32ab7af43a726c6dfa674351202e
Maxime Golfier <maxgolfier@gmail.com>
2021-09-17 14:59:16 +0200

    feat(frontend): add canonical link to contributions (#3735)
    * feat(frontend): add canonical link to contributions
    
    * fix(frontend): change host
    
    * fix(frontend): add test and transform url in canonical
    
    * fix(frontend): prettier hmm
    
    * fix(config): update linter to set it on typescript
    
    * fix(test): update unit test
    
    * fix(linter): add ts
    
    * fix(frontend): add new test
    
    * fix(frontend): fix regex

---
+++ packages/code-du-travail-frontend/src/__tests__/utils.test.ts
@@ -0,0 +1,29 @@
+import { getCanonicalUrl } from "../utils";
+
+test("should transform a url to a canonical url", () => {
+  const parsedUrl = getCanonicalUrl(
+    "/contribution/44-les-conges-pour-evenements-familiaux"
+  );
+  expect(parsedUrl).toBe("/contribution/les-conges-pour-evenements-familiaux");
+
+  const parsedUrlWithOrigin = getCanonicalUrl(
+    "https://code.travail.gouv.fr/contribution/12-les-conges-pour-evenements-familiaux"
+  );
+  expect(parsedUrlWithOrigin).toBe(
+    "https://code.travail.gouv.fr/contribution/les-conges-pour-evenements-familiaux"
+  );
+
+  const parsedUrlWithOriginNumber = getCanonicalUrl(
+    "https://code.travail.gouv.fr/contribution/les-conges-10-pour-evenements-familiaux"
+  );
+  expect(parsedUrlWithOriginNumber).toBe(
+    "https://code.travail.gouv.fr/contribution/les-conges-10-pour-evenements-familiaux"
+  );
+
+  const parsedUrlWithOriginNumberAtBeginning = getCanonicalUrl(
+    "https://code.travail.gouv.fr/contribution/24-les-conges-10-pour-evenements-familiaux"
+  );
+  expect(parsedUrlWithOriginNumberAtBeginning).toBe(
+    "https://code.travail.gouv.fr/contribution/les-conges-10-pour-evenements-familiaux"
+  );
+});
