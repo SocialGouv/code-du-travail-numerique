fe5849d88c54c93338321ae2f8ab1026953c2050
maxgfr <maxgolfier@gmail.com>
2021-09-17 12:14:37 +0200

    fix(frontend): add new test

--- packages/code-du-travail-frontend/src/__tests__/utils.test.ts
+++ packages/code-du-travail-frontend/src/__tests__/utils.test.ts
@@ -5,10 +5,18 @@ test("should transform a url to a canonical url", () => {
     "/contribution/44-les-conges-pour-evenements-familiaux"
   );
   expect(parsedUrl).toBe("/contribution/les-conges-pour-evenements-familiaux");
+
   const parsedUrlWithOrigin = getCanonicalUrl(
     "https://code.travail.gouv.fr/contribution/12-les-conges-pour-evenements-familiaux"
   );
   expect(parsedUrlWithOrigin).toBe(
+    "https://code.travail.gouv.fr/contribution/les-conges-pour-evenements-familiaux"
+  );
+
+  const parsedUrlWithOriginNumber = getCanonicalUrl(
+    "https://code.travail.gouv.fr/contribution/les-conges-10-pour-evenements-familiaux"
+  );
+  expect(parsedUrlWithOriginNumber).toBe(
     "https://code.travail.gouv.fr/contribution/les-conges-pour-evenements-familiaux"
   );
 });
