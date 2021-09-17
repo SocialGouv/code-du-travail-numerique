43c65aa63a236137f86f4242391c01b1393318cc
maxgfr <maxgolfier@gmail.com>
2021-09-17 14:53:52 +0200

    fix(test): fix

---
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.commerces.detail.alimentation.references.spec.ts
@@ -0,0 +1,73 @@
+import Engine from "publicodes";
+import { mergeModels } from "../internal/merger";
+import {
+  DepartRetraiteReferences,
+  MiseRetraiteReferences,
+} from "./common/LegalReferences";
+import { getReferences } from "../utils/GetReferences";
+
+const engine = new Engine(mergeModels());
+
+const DepartRetraiteCadresReferences = [
+  ...DepartRetraiteReferences,
+  {
+    article: "Article 9 de l'annexe III",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000041517573/?idConteneur=KALICONT000005635085",
+  },
+];
+ 
+
+const MiseRetraiteOuvriersReferences = [
+  ...MiseRetraiteReferences,
+  {
+    article: "Article 3.12",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000023307000/?idConteneur=KALICONT000005635085",
+  },
+  {
+    article: "Article 3 de l'annexe I",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000041517472/?idConteneur=KALICONT000005635085",
+  },
+];
+
+const MiseRetraiteTechniciensReferences = [
+  ...MiseRetraiteReferences,
+  {
+    article: "Article 5.1 de l'annexe II",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000041517514/?idConteneur=KALICONT000005635085",
+  },
+];
+
+const MiseRetraiteCadresReferences = [
+  ...MiseRetraiteReferences,
+  {
+    article: "Article 9 de l'annexe III",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000041517573/?idConteneur=KALICONT000005635085",
+  },
+];
+ 
+
+test.each`
+  retirement  | category                              | expectedReferences
+  ${"départ"} | ${"Employés et ouvriers"}             | ${DepartRetraiteReferences}
+  ${"départ"} | ${"Techniciens & Agents de maîtrise"} | ${DepartRetraiteReferences}
+  ${"départ"} | ${"Cadres"}                           | ${DepartRetraiteCadresReferences}
+  ${"mise"}   | ${"Employés et ouvriers"}             | ${MiseRetraiteOuvriersReferences}
+  ${"mise"}   | ${"Techniciens & Agents de maîtrise"} | ${MiseRetraiteTechniciensReferences}
+  ${"mise"}   | ${"Cadres"}                           | ${MiseRetraiteCadresReferences}
+`(
+  "Vérification des références juridiques pour un $category en $retirement à la retraite",
+  ({ retirement, category, expectedReferences }) => {
+    const result = getReferences(
+      engine.setSituation({
+        "contrat salarié . convention collective": "'IDCC2216'",
+        "contrat salarié . mise à la retraite":
+          retirement === "mise" ? "oui" : "non",
+        "contrat salarié . convention collective . commerce gros et detail alimentation . catégorie professionnelle": `'${category}'`,
+        "contrat salarié . travailleur handicapé": "non",
+      })
+    );
+
+    expect(result).toHaveLength(expectedReferences.length);
+    expect(result).toEqual(expect.arrayContaining(expectedReferences));
+  }
+);
---
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.commerces.detail.alimentation.spec.ts
@@ -0,0 +1,64 @@
+import Engine from "publicodes";
+import { mergeModels } from "../internal/merger";
+
+const engine = new Engine(mergeModels());
+
+test.each`
+  seniority | category                              | expectedNotice
+  ${1}      | ${"Employés et ouvriers"}             | ${0}
+  ${6}      | ${"Employés et ouvriers"}             | ${1}
+  ${24}     | ${"Employés et ouvriers"}             | ${2}
+  ${1}      | ${"Techniciens & Agents de maîtrise"} | ${0}
+  ${6}      | ${"Techniciens & Agents de maîtrise"} | ${1}
+  ${24}     | ${"Techniciens & Agents de maîtrise"} | ${2}
+  ${1}      | ${"Cadres"}                           | ${6}
+  ${6}      | ${"Cadres"}                           | ${1}
+  ${24}     | ${"Cadres"}                           | ${2}
+`(
+  "Pour un $category possédant $seniority mois d'ancienneté, son préavis de départ à la retraite devrait être $expectedNotice $expectedNoticeUnit",
+  ({ seniority, category, expectedNotice }) => {
+    const result = engine
+      .setSituation({
+        "contrat salarié . convention collective": "'IDCC2216'",
+        "contrat salarié . ancienneté": seniority,
+        "contrat salarié . mise à la retraite": "non",
+        "contrat salarié . travailleur handicapé": "non",
+        "contrat salarié . convention collective . commerce gros et detail alimentation . catégorie professionnelle": `'${category}'`,
+      })
+      .evaluate("contrat salarié . préavis de retraite");
+
+    expect(result.nodeValue).toEqual(expectedNotice);
+    expect(result.unit?.numerators).toEqual(["mois"]);
+    expect(result.missingVariables).toEqual({});
+  }
+);
+
+test.each`
+  seniority | category                              | expectedNotice
+  ${1}      | ${"Employés et ouvriers"}             | ${1}
+  ${6}      | ${"Employés et ouvriers"}             | ${1}
+  ${24}     | ${"Employés et ouvriers"}             | ${2}
+  ${1}      | ${"Techniciens & Agents de maîtrise"} | ${2}
+  ${6}      | ${"Techniciens & Agents de maîtrise"} | ${2}
+  ${24}     | ${"Techniciens & Agents de maîtrise"} | ${2}
+  ${1}      | ${"Cadres"}                           | ${6}
+  ${6}      | ${"Cadres"}                           | ${6}
+  ${24}     | ${"Cadres"}                           | ${6}
+`(
+  "Pour un $category possédant $seniority mois d'ancienneté, son préavis de mise à la retraite devrait être $expectedNotice $expectedNoticeUnit",
+  ({ seniority, category, expectedNotice }) => {
+    const result = engine
+      .setSituation({
+        "contrat salarié . convention collective": "'IDCC2216'",
+        "contrat salarié . ancienneté": seniority,
+        "contrat salarié . mise à la retraite": "oui",
+        "contrat salarié . travailleur handicapé": "non",
+        "contrat salarié . convention collective . commerce gros et detail alimentation . catégorie professionnelle": `'${category}'`,
+      })
+      .evaluate("contrat salarié . préavis de retraite");
+
+    expect(result.nodeValue).toEqual(expectedNotice);
+    expect(result.unit?.numerators).toEqual(["mois"]);
+    expect(result.missingVariables).toEqual({});
+  }
+);
