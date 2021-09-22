29e5b56123fa8dad073d6a6919b460ee5c3f35ea
caroline <caroline.bourdeu@gmail.com>
2021-09-22 13:11:32 +0200

    feat(simulator):
    Add CC 0787

---
+++ packages/code-du-travail-modeles/src/__test__/depart.retraite.comptables.spec.ts
@@ -0,0 +1,56 @@
+import Engine from "publicodes";
+import { mergeModels } from "../internal/merger";
+
+const engine = new Engine(mergeModels());
+
+test.each`
+  seniority | category      | expectedNotice
+  ${4}      | ${"Employés"} | ${1}
+  ${24}     | ${"Employés"} | ${2}
+  ${5}      | ${"Cadres"}   | ${3}
+  ${24}     | ${"Cadres"}   | ${3}
+`(
+  "Pour un $category possédant $seniority mois d'ancienneté, son préavis de mise à la retraite devrait être $expectedNotice mois",
+  ({ seniority, category, expectedNotice }) => {
+    const result = engine
+      .setSituation({
+        "contrat salarié . convention collective": "'IDCC0787'",
+        "contrat salarié . ancienneté": seniority,
+        "contrat salarié . mise à la retraite": "oui",
+        "contrat salarié . travailleur handicapé": "non",
+        "contrat salarié . convention collective . comptables . mise à la retraite . catégorie professionnelle": `'${category}'`,
+      })
+      .evaluate("contrat salarié . préavis de retraite");
+
+    expect(result.nodeValue).toEqual(expectedNotice);
+    expect(result.unit?.numerators).toEqual(["mois"]);
+    expect(result.missingVariables).toEqual({});
+  }
+);
+
+test.each`
+  seniority | category        | expectedNotice
+  ${4}      | ${"Assistants"} | ${0}
+  ${10}     | ${"Assistants"} | ${1}
+  ${24}     | ${"Assistants"} | ${2}
+  ${5}      | ${"Autres"}     | ${0}
+  ${10}     | ${"Autres"}     | ${1}
+  ${24}     | ${"Autres"}     | ${2}
+`(
+  "Pour un $category possédant $seniority mois d'ancienneté, son préavis de départ à la retraite devrait être $expectedNotice mois",
+  ({ seniority, category, expectedNotice }) => {
+    const result = engine
+      .setSituation({
+        "contrat salarié . convention collective": "'IDCC0787'",
+        "contrat salarié . ancienneté": seniority,
+        "contrat salarié . mise à la retraite": "non",
+        "contrat salarié . travailleur handicapé": "non",
+        "contrat salarié . convention collective . comptables . départ à la retraite . catégorie professionnelle": `'${category}'`,
+      })
+      .evaluate("contrat salarié . préavis de retraite");
+
+    expect(result.nodeValue).toEqual(expectedNotice);
+    expect(result.unit?.numerators).toEqual(["mois"]);
+    expect(result.missingVariables).toEqual({});
+  }
+);
