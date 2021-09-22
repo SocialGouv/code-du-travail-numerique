7da9e1f1fe1da1061156dc1a8b6f0fb5da1ca29f
maxgfr <maxgolfier@gmail.com>
2021-09-22 20:17:24 +0200

    feat(cc): add 1043 / is bugging

---
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.concierge.references.spec.ts
@@ -0,0 +1,107 @@
+// import Engine from "publicodes";
+// import { mergeModels } from "../internal/merger";
+// import {
+//   DepartRetraiteReferences,
+//   MiseRetraiteReferences,
+// } from "./common/LegalReferences";
+// import { getReferences } from "../utils/GetReferences";
+
+// const engine = new Engine(mergeModels());
+
+// const DépartRetraiteNonLogéInférieur602References = [
+//   ...DepartRetraiteReferences,
+//   {
+//     article: "Article 14",
+//     url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000034978399/?idConteneur=KALICONT000005635953",
+//   },
+//   {
+//     article: "Article 17",
+//     url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000034978485/?idConteneur=KALICONT000005635953",
+//   },
+// ];
+// const DépartRetraiteNonLogéSupérieur602References = [
+//   ...DepartRetraiteReferences,
+//   {
+//     article: "Article 14",
+//     url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000034978399/?idConteneur=KALICONT000005635954",
+//   },
+//   {
+//     article: "Article 17",
+//     url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000034978485/?idConteneur=KALICONT000005635953",
+//   },
+// ];
+// const DépartRetraiteLogéReferences = [
+//   ...DepartRetraiteReferences,
+//   {
+//     article: "Article 14",
+//     url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000034978399/?idConteneur=KALICONT000005635955",
+//   },
+//   {
+//     article: "Article 17",
+//     url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000034978485/?idConteneur=KALICONT000005635953",
+//   },
+// ];
+
+// const MiseRetraiteCatAReferences = [
+//   ...MiseRetraiteReferences,
+//   {
+//     article: "Article 14",
+//     url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000034978399/?idConteneur=KALICONT000005635953",
+//   },
+//   {
+//     article: "Article 17",
+//     url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000034978485/?idConteneur=KALICONT000005635953",
+//   },
+// ];
+
+// const MiseRetraiteCatBReferences = [
+//   ...MiseRetraiteReferences,
+//   {
+//     article: "Article 17",
+//     url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000034978485/?idConteneur=KALICONT000005635953",
+//   },
+// ];
+
+// test.each`
+//   retirement  | category         | accommodation | coefficient | expectedReferences
+//   ${"départ"} | ${"Catégorie A"} | ${"Oui"}      | ${602}      | ${DépartRetraiteLogéReferences}
+//   ${"départ"} | ${"Catégorie B"} | ${"Oui"}      | ${602}      | ${DépartRetraiteLogéReferences}
+//   ${"départ"} | ${"Catégorie A"} | ${"Non"}      | ${602}      | ${DépartRetraiteNonLogéInférieur602References}
+//   ${"départ"} | ${"Catégorie B"} | ${"Non"}      | ${602}      | ${DépartRetraiteNonLogéInférieur602References}
+//   ${"départ"} | ${"Catégorie A"} | ${"Non"}      | ${603}      | ${DépartRetraiteNonLogéSupérieur602References}
+//   ${"départ"} | ${"Catégorie B"} | ${"Non"}      | ${603}      | ${DépartRetraiteNonLogéSupérieur602References}
+//   ${"départ"} | ${"Catégorie A"} | ${"Oui"}      | ${603}      | ${DépartRetraiteLogéReferences}
+//   ${"départ"} | ${"Catégorie B"} | ${"Oui"}      | ${603}      | ${DépartRetraiteLogéReferences}
+//   ${"mise"}   | ${"Catégorie A"} | ${"Oui"}      | ${602}      | ${MiseRetraiteCatAReferences}
+//   ${"mise"}   | ${"Catégorie B"} | ${"Oui"}      | ${602}      | ${MiseRetraiteCatBReferences}
+//   ${"mise"}   | ${"Catégorie A"} | ${"Non"}      | ${602}      | ${MiseRetraiteCatAReferences}
+//   ${"mise"}   | ${"Catégorie B"} | ${"Non"}      | ${602}      | ${MiseRetraiteCatBReferences}
+//   ${"mise"}   | ${"Catégorie A"} | ${"Non"}      | ${603}      | ${MiseRetraiteCatAReferences}
+//   ${"mise"}   | ${"Catégorie B"} | ${"Non"}      | ${603}      | ${MiseRetraiteCatBReferences}
+//   ${"mise"}   | ${"Catégorie A"} | ${"Oui"}      | ${603}      | ${MiseRetraiteCatAReferences}
+//   ${"mise"}   | ${"Catégorie B"} | ${"Oui"}      | ${603}      | ${MiseRetraiteCatBReferences}
+// `(
+//   "Vérification des références juridiques pour un salarié $category en $retirement à la retraite ayant un logement : $accommodation et avec ce coefficient $coefficient",
+//   ({
+//     retirement,
+//     category,
+//     expectedReferences,
+//     accommodation,
+//     coefficient,
+//   }) => {
+//     const result = getReferences(
+//       engine.setSituation({
+//         "contrat salarié . convention collective": "'IDCC1043'",
+//         "contrat salarié . mise à la retraite":
+//           retirement === "mise" ? "oui" : "non",
+//         "contrat salarié . convention collective . gardien concierge . catégorie professionnelle": `'${category}'`,
+//         "contrat salarié . convention collective . gardien concierge . logement": `'${accommodation}'`,
+//         "contrat salarié . convention collective . gardien concierge . coefficient": `'${coefficient}'`,
+//         "contrat salarié . travailleur handicapé": "non",
+//       })
+//     );
+
+//     expect(result).toHaveLength(expectedReferences.length);
+//     expect(result).toEqual(expect.arrayContaining(expectedReferences));
+//   }
+// );
---
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.concierge.spec.ts
@@ -0,0 +1,54 @@
+import Engine from "publicodes";
+import { mergeModels } from "../internal/merger";
+
+const engine = new Engine(mergeModels());
+
+// test.each`
+//   accommodation | coefficient | expectedResult | expectedPeriod
+//   ${"Non"}      | ${602}      | ${8}           | ${"jour"}
+//   ${"Non"}      | ${603}      | ${1}           | ${"mois"}
+//   ${"Oui"}      | ${602}      | ${1}           | ${"mois"}
+//   ${"Oui"}      | ${603}      | ${1}           | ${"mois"}
+// `(
+//   "Pour un salarié disposant de ce coefficient $coefficient et ayant un logement : $accommodation, son préavis de départ à la retraite devrait être $expectedResult $expectedPeriod",
+//   ({ accommodation, coefficient, expectedResult, expectedPeriod }) => {
+//     const result = engine
+//       .setSituation({
+//         "contrat salarié . convention collective": "'IDCC1043'",
+//         "contrat salarié . mise à la retraite": "non",
+//         "contrat salarié . travailleur handicapé": "non",
+//         "contrat salarié . convention collective . gardien concierge . logement": `'${accommodation}'`,
+//         "contrat salarié . convention collective . gardien concierge . coefficient": `'${coefficient}'`,
+//       })
+//       .evaluate("contrat salarié . préavis de retraite");
+
+//     expect(result.nodeValue).toEqual(expectedResult);
+//     expect(result.unit?.numerators).toEqual([expectedPeriod]);
+//     expect(result.missingVariables).toEqual({});
+//   }
+// );
+
+test.each`
+  category         | seniority | expectedResult
+  ${"Catégorie A"} | ${12}     | ${1}
+  ${"Catégorie B"} | ${12}     | ${6}
+  ${"Catégorie A"} | ${24}     | ${2}
+  ${"Catégorie B"} | ${24}     | ${6}
+`(
+  "Pour un salarié de tel $category possédant $seniority mois d'ancienneté, son préavis de mise à la retraite devrait être $expectedResult mois",
+  ({ seniority, category, expectedResult }) => {
+    const result = engine
+      .setSituation({
+        "contrat salarié . convention collective": "'IDCC1043'",
+        "contrat salarié . ancienneté": seniority,
+        "contrat salarié . mise à la retraite": "oui",
+        "contrat salarié . travailleur handicapé": "non",
+        "contrat salarié . convention collective . gardien concierge . catégorie professionnelle": `'${category}'`,
+      })
+      .evaluate("contrat salarié . préavis de retraite");
+
+    expect(result.nodeValue).toEqual(expectedResult);
+    expect(result.unit?.numerators).toEqual(["mois"]);
+    expect(result.missingVariables).toEqual({});
+  }
+);
