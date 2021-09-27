b822322d412072eaf814dd3293bfd75a43afe289
caroline <caroline.bourdeu@gmail.com>
2021-09-27 15:14:33 +0200

    :recycle: extract disclaimer component

---
+++ packages/code-du-travail-frontend/src/outils/IndemnitePrecarite/components/Disclaimer.js
@@ -0,0 +1,62 @@
+import { icons, IconStripe } from "@socialgouv/cdtn-ui";
+import React from "react";
+
+import {
+  Warning,
+  WarningTitle,
+} from "../../DureePreavisRetraite/steps/component/WarningResult";
+
+function Disclaimer({ situation, idcc }) {
+  if (idcc > 0 && situation.idcc > 0) {
+    return (
+      <Warning>
+        <IconStripe centered icon={icons.Warning}>
+          <WarningTitle>
+            Attention il peut exister un montant plus favorable
+          </WarningTitle>
+        </IconStripe>
+        <p>
+          Un accord d’entreprise peut prévoir un montant différent qu’il soit
+          plus élevé ou plus faible. Dans ce cas, s’applique le montant prévu
+          par l’accord d’entreprise, sauf si le contrat de travail prévoit un
+          montant plus favorable pour le salarié.
+        </p>
+        {!situation.hasConventionalProvision && (
+          <p>
+            Attention, dans le cas où l’accord d’entreprise prévoit un taux
+            inférieur à 10% dans la limite de 6%, il doit y avoir des
+            contreparties offertes au salarié, notamment sous la forme d’un
+            accès privilégié à la formation professionnelle (action de
+            formation, bilan de compétences).
+          </p>
+        )}
+      </Warning>
+    );
+  }
+  // case for no ccn provided or unhandled ccn
+  return (
+    <Warning>
+      <IconStripe centered icon={icons.Warning}>
+        <WarningTitle>
+          Attention il peut exister un montant plus favorable
+        </WarningTitle>
+      </IconStripe>
+      <p>
+        Une convention collective de branche étendue ou un accord d’entreprise
+        peut prévoir un montant différent qu’il soit plus élevé ou plus faible
+        que celui prévu par le code du travail.
+      </p>
+      <p>
+        Attention, dans le cas où la convention ou l’accord collectif prévoit un
+        taux inférieur à 10% dans la limite de 6%, il doit y avoir des
+        contreparties offertes au salarié, notamment sous la forme d’un accès
+        privilégié à la formation professionnelle (action de formation, bilan de
+        compétences). Dans tous les cas, le contrat de travail peut prévoir un
+        montant plus favorable pour le salarié. Il faut alors appliquer ce
+        montant.
+      </p>
+    </Warning>
+  );
+}
+
+export { Disclaimer };
--- packages/code-du-travail-frontend/src/outils/IndemnitePrecarite/steps/Indemnite.js
+++ packages/code-du-travail-frontend/src/outils/IndemnitePrecarite/steps/Indemnite.js
@@ -1,5 +1,5 @@
 import data from "@cdt/data...prime-precarite/precarite.data.json";
-import { Accordion, icons, IconStripe } from "@socialgouv/cdtn-ui";
+import { Accordion } from "@socialgouv/cdtn-ui";
 import Link from "next/link";
 import React from "react";
 
@@ -12,65 +12,9 @@ import {
   getSituationsFor,
 } from "../../common/situations.utils";
 import { HighlightResult, SectionTitle } from "../../common/stepStyles";
-import {
-  Warning,
-  WarningTitle,
-} from "../../DureePreavisRetraite/steps/component/WarningResult";
+import { Disclaimer } from "../components/Disclaimer";
 import { CONTRACT_TYPE } from "../components/TypeContrat";
 import { getIndemnitePrecarite } from "../indemnite";
-
-function Disclaimer({ situation, idcc }) {
-  if (idcc > 0 && situation.idcc > 0) {
-    return (
-      <Warning>
-        <IconStripe centered icon={icons.Warning}>
-          <WarningTitle>
-            Attention il peut exister un montant plus favorable
-          </WarningTitle>
-        </IconStripe>
-        <p>
-          Un accord d’entreprise peut prévoir un montant différent qu’il soit
-          plus élevé ou plus faible. Dans ce cas, s’applique le montant prévu
-          par l’accord d’entreprise, sauf si le contrat de travail prévoit un
-          montant plus favorable pour le salarié.
-        </p>
-        {!situation.hasConventionalProvision && (
-          <p>
-            Attention, dans le cas où l’accord d’entreprise prévoit un taux
-            inférieur à 10% dans la limite de 6%, il doit y avoir des
-            contreparties offertes au salarié, notamment sous la forme d’un
-            accès privilégié à la formation professionnelle (action de
-            formation, bilan de compétences).
-          </p>
-        )}
-      </Warning>
-    );
-  }
-  // case for no ccn provided or unhandled ccn
-  return (
-    <Warning>
-      <IconStripe centered icon={icons.Warning}>
-        <WarningTitle>
-          Attention il peut exister un montant plus favorable
-        </WarningTitle>
-      </IconStripe>
-      <p>
-        Une convention collective de branche étendue ou un accord d’entreprise
-        peut prévoir un montant différent qu’il soit plus élevé ou plus faible
-        que celui prévu par le code du travail.
-      </p>
-      <p>
-        Attention, dans le cas où la convention ou l’accord collectif prévoit un
-        taux inférieur à 10% dans la limite de 6%, il doit y avoir des
-        contreparties offertes au salarié, notamment sous la forme d’un accès
-        privilégié à la formation professionnelle (action de formation, bilan de
-        compétences). Dans tous les cas, le contrat de travail peut prévoir un
-        montant plus favorable pour le salarié. Il faut alors appliquer ce
-        montant.
-      </p>
-    </Warning>
-  );
-}
 
 function extractRefs(refs = []) {
   //some ref are duplicated so we need to dedup them
