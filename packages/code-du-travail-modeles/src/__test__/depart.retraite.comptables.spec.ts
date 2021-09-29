d6dd6b32134056876e9bf0d995d96b508ada122b
caroline <caroline.bourdeu@gmail.com>
2021-09-29 11:12:33 +0200

    More readable

--- packages/code-du-travail-modeles/src/__test__/depart.retraite.comptables.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/depart.retraite.comptables.spec.ts
@@ -7,10 +7,10 @@ test.each`
   seniority | category      | expectedNotice
   ${4}      | ${"Employés"} | ${1}
   ${10}     | ${"Employés"} | ${1}
-  ${24}     | ${"Employés"} | ${2}
+  ${25}     | ${"Employés"} | ${2}
   ${4}      | ${"Cadres"}   | ${3}
   ${10}     | ${"Cadres"}   | ${3}
-  ${24}     | ${"Cadres"}   | ${3}
+  ${25}     | ${"Cadres"}   | ${3}
 `(
   "Pour un $category possédant $seniority mois d'ancienneté, son préavis de mise à la retraite devrait être $expectedNotice mois",
   ({ seniority, category, expectedNotice }) => {
@@ -34,10 +34,10 @@ test.each`
   seniority | category        | expectedNotice
   ${4}      | ${"Assistants"} | ${1}
   ${10}     | ${"Assistants"} | ${1}
-  ${24}     | ${"Assistants"} | ${2}
+  ${25}     | ${"Assistants"} | ${2}
   ${4}      | ${"Autres"}     | ${2}
   ${10}     | ${"Autres"}     | ${1}
-  ${24}     | ${"Autres"}     | ${2}
+  ${25}     | ${"Autres"}     | ${2}
 `(
   "Pour un $category possédant $seniority mois d'ancienneté, son préavis de départ à la retraite devrait être $expectedNotice mois",
   ({ seniority, category, expectedNotice }) => {
