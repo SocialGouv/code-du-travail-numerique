4bae7ea5c33279a30fba2fed444fe4e725c3151b
maxgfr <maxgolfier@gmail.com>
2021-09-29 19:19:20 +0200

    fix(e2e): add codeceptjs in packages

--- packages/code-du-travail-frontend-e2e/codecept.conf.js
+++ packages/code-du-travail-frontend-e2e/codecept.conf.js
@@ -1,11 +1,14 @@
 exports.config = {
-  output: "./output",
-  rerun: {
-    // run 3 times until 1st success
-    minSuccess: 1,
-    maxReruns: 3,
+  bootstrap: null,
+  gherkin: {
+    features: "./features/**/*.feature",
+    steps: ["./step_definitions/global.js"],
   },
   helpers: {
+    FileSystem: {},
+    FocusHelper: {
+      require: "./helpers/focus.js",
+    },
     Puppeteer: {
       chrome: {
         args: (process.env.CI
@@ -13,8 +16,8 @@ exports.config = {
           : ["--window-size=1024,1024"]
         ).concat(["--lang=fr_FR"]),
         defaultViewport: {
-          width: 1024,
           height: 1024,
+          width: 1024,
         },
         executablePath: process.env.CI && "/usr/bin/chromium-browser",
         headless: process.env.CODECEPT_HEADED ? false : true,
@@ -22,28 +25,25 @@ exports.config = {
       restart: false,
       url: process.env.CODECEPT_BASEURL || "http://localhost:3000",
     },
-    FileSystem: {},
-    FocusHelper: {
-      require: "./helpers/focus.js",
-    },
   },
+  hooks: [],
   include: {
     I: "./steps_file.js",
   },
   mocha: {},
-  bootstrap: null,
-  teardown: null,
-  hooks: [],
-  gherkin: {
-    features: "../../features/**/*.feature",
-    steps: ["./step_definitions/global.js"],
-  },
+  name: "code-du-travail-numerique",
+  output: "./output",
   plugins: {
     pauseOnFail: {},
     screenshotOnFail: {
       enabled: true,
     },
   },
+  rerun: {
+    maxReruns: 3,
+    // run 3 times until 1st success
+    minSuccess: 1,
+  },
+  teardown: null,
   tests: "./specs/*_test.js",
-  name: "code-du-travail-numerique",
 };
