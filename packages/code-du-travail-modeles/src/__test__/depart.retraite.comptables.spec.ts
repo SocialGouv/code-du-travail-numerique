adf45e82de9a5533637c641e9d7ea56b98c65564
caroline <caroline.bourdeu@gmail.com>
2021-09-29 11:11:30 +0200

    Add test coverage

--- packages/code-du-travail-modeles/src/__test__/depart.retraite.comptables.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/depart.retraite.comptables.spec.ts
@@ -6,8 +6,10 @@ const engine = new Engine(mergeModels());
 test.each`
   seniority | category      | expectedNotice
   ${4}      | ${"Employés"} | ${1}
+  ${10}     | ${"Employés"} | ${1}
   ${24}     | ${"Employés"} | ${2}
-  ${5}      | ${"Cadres"}   | ${3}
+  ${4}      | ${"Cadres"}   | ${3}
+  ${10}     | ${"Cadres"}   | ${3}
   ${24}     | ${"Cadres"}   | ${3}
 `(
   "Pour un $category possédant $seniority mois d'ancienneté, son préavis de mise à la retraite devrait être $expectedNotice mois",
@@ -33,7 +35,7 @@ test.each`
   ${4}      | ${"Assistants"} | ${1}
   ${10}     | ${"Assistants"} | ${1}
   ${24}     | ${"Assistants"} | ${2}
-  ${5}      | ${"Autres"}     | ${2}
+  ${4}      | ${"Autres"}     | ${2}
   ${10}     | ${"Autres"}     | ${1}
   ${24}     | ${"Autres"}     | ${2}
 `(
