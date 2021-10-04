80c17bc432d897de11bdf2043aa444fab444ce9f
caroline <caroline.bourdeu@gmail.com>
2021-10-04 16:18:17 +0200

    Remove notification (which is now un-necessary)

--- packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.batiment.etam.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.batiment.etam.spec.ts
@@ -49,32 +49,3 @@ test.each`
     expect(result.missingVariables).toEqual({});
   }
 );
-
-test("Pour un départ à la retraite dans la convention collective Bâtiment Etam on attend une notification", () => {
-  const notifications = getNotifications(
-    engine.setSituation({
-      "contrat salarié . convention collective": "'IDCC2609'",
-      "contrat salarié . ancienneté": 3,
-      "contrat salarié . mise à la retraite": "non",
-      "contrat salarié . travailleur handicapé": "non",
-    })
-  );
-
-  expect(notifications).toHaveLength(1);
-  expect(notifications[0].description).toEqual(
-    "Le salarié doit notifier à l’employeur son départ en retraite par une lettre recommandée avec accusé de réception. La date de première présentation de cette lettre fixe le point de départ du délai de préavis.  Exemple: Si le 6 mars 2020 l'employeur a reçu la lettre recommandée notifiant le départ en retraite, le préavis à effectuer débutera à cette même date."
-  );
-});
-
-test("Pour une mise à la retraite dans la convention collective Bâtiment Etam on attend une notification", () => {
-  const notifications = getNotifications(
-    engine.setSituation({
-      "contrat salarié . convention collective": "'IDCC2609'",
-      "contrat salarié . ancienneté": 3,
-      "contrat salarié . mise à la retraite": "oui",
-      "contrat salarié . travailleur handicapé": "non",
-    })
-  );
-
-  expect(notifications).toHaveLength(0);
-});
