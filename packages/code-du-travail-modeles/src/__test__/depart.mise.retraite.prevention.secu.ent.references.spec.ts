44b34104542ad1a1c9d43465a4d26a2e19a0e360
Maxime Golfier <maxgolfier@gmail.com>
2021-10-05 11:44:25 +0200

    feat(modeles): add cc 1351 retirement (#3778)
    * feat(cc): add 1351
    
    * fix(notification): test references doesnt work
    
    * fix references (with @maxgfr)
    
    * fix(cc): remove notif
    
    Co-authored-by: caroline <caroline.bourdeu@gmail.com>

---
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.prevention.secu.ent.references.spec.ts
@@ -0,0 +1,109 @@
+import Engine from "publicodes";
+import { mergeModels } from "../internal/merger";
+import {
+  DepartRetraiteReferences,
+  MiseRetraiteReferences,
+} from "./common/LegalReferences";
+import { getReferences } from "../utils/GetReferences";
+
+const engine = new Engine(mergeModels());
+
+const DepartRetraiteExploitationReferences = [
+  ...DepartRetraiteReferences,
+  {
+    article: "Article 6.14",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000021994236/?idConteneur=KALICONT000005635405",
+  },
+  {
+    article:
+      "Annexe IV : Agents d'exploitation, employés administratifs et techniciens, article 10",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005853822/?idConteneur=KALICONT000005635405",
+  },
+];
+
+const DepartRetraiteMaitriseReferences = [
+  ...DepartRetraiteReferences,
+  {
+    article: "Article 6.14",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000021994236/?idConteneur=KALICONT000005635405",
+  },
+  {
+    article: "Annexe V : Agents de maîtrise, article 9",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005853833/?idConteneur=KALICONT000005635405",
+  },
+];
+
+const DepartRetraiteCadresReferences = [
+  ...DepartRetraiteReferences,
+  {
+    article: "Article 6.14",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000021994236/?idConteneur=KALICONT000005635405",
+  },
+  {
+    article: "Annexe VI : Cadres, article 10",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005853845/?idConteneur=KALICONT000005635405",
+  },
+];
+
+const MiseRetraiteExploitationsReferences = [
+  ...MiseRetraiteReferences,
+  {
+    article: "Article 6.13",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000021994236/?idConteneur=KALICONT000005635405",
+  },
+  {
+    article:
+      "Annexe IV : Agents d'exploitation, employés administratifs et techniciens, article 10",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005853822/?idConteneur=KALICONT000005635405",
+  },
+];
+
+const MiseRetraiteMaitriseReferences = [
+  ...MiseRetraiteReferences,
+  {
+    article: "Article 6.13",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000021994236/?idConteneur=KALICONT000005635405",
+  },
+  {
+    article: "Annexe V : Agents de maîtrise, article 9",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005853833/?idConteneur=KALICONT000005635405",
+  },
+];
+
+const MiseRetraiteCadresReferences = [
+  ...MiseRetraiteReferences,
+  {
+    article: "Article 6.13",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000021994236/?idConteneur=KALICONT000005635405",
+  },
+  {
+    article: "Annexe VI : Cadres, article 10",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005853845/?idConteneur=KALICONT000005635405",
+  },
+];
+
+test.each`
+  retirement  | category                                                           | expectedReferences
+  ${"départ"} | ${"Agents d'exploitation, employés administratifs et techniciens"} | ${DepartRetraiteExploitationReferences}
+  ${"départ"} | ${"Agents de maîtrise"}                                            | ${DepartRetraiteMaitriseReferences}
+  ${"départ"} | ${"Cadres"}                                                        | ${DepartRetraiteCadresReferences}
+  ${"mise"}   | ${"Agents d'exploitation, employés administratifs et techniciens"} | ${MiseRetraiteExploitationsReferences}
+  ${"mise"}   | ${"Agents de maîtrise"}                                            | ${MiseRetraiteMaitriseReferences}
+  ${"mise"}   | ${"Cadres"}                                                        | ${MiseRetraiteCadresReferences}
+`(
+  "Vérification des références juridiques pour un $category en $retirement à la retraite",
+  ({ retirement, category, expectedReferences }) => {
+    const result = getReferences(
+      engine.setSituation({
+        "contrat salarié . convention collective": "'IDCC1351'",
+        "contrat salarié . mise à la retraite":
+          retirement === "mise" ? "oui" : "non",
+        "contrat salarié . travailleur handicapé": "non",
+        "contrat salarié . convention collective . prevention sécurité entreprise . catégorie professionnelle": `'${category}'`,
+      })
+    );
+
+    expect(result).toHaveLength(expectedReferences.length);
+    expect(result).toEqual(expect.arrayContaining(expectedReferences));
+  }
+);
