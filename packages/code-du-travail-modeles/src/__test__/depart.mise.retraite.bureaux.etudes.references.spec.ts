233c22fb36b6fb60bb5aa162ff5eb2fc73169a88
maxgfr <maxgolfier@gmail.com>
2021-10-06 15:55:52 +0200

    feat(cc): add 1486

---
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.bureaux.etudes.references.spec.ts
@@ -0,0 +1,74 @@
+import Engine from "publicodes";
+import { mergeModels } from "../internal/merger";
+import {
+  DepartRetraiteReferences,
+  MiseRetraiteReferences,
+} from "./common/LegalReferences";
+import { getReferences } from "../utils/GetReferences";
+
+const engine = new Engine(mergeModels());
+
+const DepartRetraiteCcReferences = [
+  ...DepartRetraiteReferences,
+  {
+    article: "Article 20",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005851451/?idConteneur=KALICONT000005635173",
+  },
+  {
+    article: "Annexe « Enquêteurs », article 26",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005851324/?idConteneur=KALICONT000005635173",
+  },
+];
+
+const MiseRetraiteCCReferencesAutre = [
+  ...MiseRetraiteReferences,
+  {
+    article: "Article 20",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005851451/?idConteneur=KALICONT000005635173",
+  },
+];
+
+const MiseRetraiteCCReferencesCharge = [
+  ...MiseRetraiteReferences,
+  {
+    article: "Annexe « Enquêteurs », article 26",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005851324/?idConteneur=KALICONT000005635173",
+  },
+];
+
+describe("Vérification des références juridiques pour le départ et la mise retraite de la CC 1486", () => {
+  test("Vérification des références juridiques pour un départ à la retraite", () => {
+    const result = getReferences(
+      engine.setSituation({
+        "contrat salarié . convention collective": "'IDCC1486'",
+        "contrat salarié . ancienneté": 6,
+        "contrat salarié . mise à la retraite": "non",
+        "contrat salarié . travailleur handicapé": "non",
+      })
+    );
+    expect(result).toHaveLength(DepartRetraiteCcReferences.length);
+    expect(result).toEqual(expect.arrayContaining(DepartRetraiteCcReferences));
+  });
+
+  test.each`
+    category                             | expectedReferences
+    ${"Autres salariés"}                 | ${MiseRetraiteCCReferencesAutre}
+    ${"Chargés d'enquête intermittents"} | ${MiseRetraiteCCReferencesCharge}
+  `(
+    "Vérification des références juridiques pour une mise à la retraite",
+    ({ category, expectedReferences }) => {
+      const result = getReferences(
+        engine.setSituation({
+          "contrat salarié . convention collective": "'IDCC1486'",
+          "contrat salarié . ancienneté": 6,
+          "contrat salarié . mise à la retraite": "oui",
+          "contrat salarié . travailleur handicapé": "non",
+          "contrat salarié . convention collective . bureaux études techniques . catégorie professionnelle": `'${category}'`,
+        })
+      );
+
+      expect(result).toHaveLength(expectedReferences.length);
+      expect(result).toEqual(expect.arrayContaining(expectedReferences));
+    }
+  );
+});
