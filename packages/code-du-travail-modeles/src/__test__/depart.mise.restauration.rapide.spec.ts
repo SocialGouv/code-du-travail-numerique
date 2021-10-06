3869f3e0576fb23d37a6549d4815d80c8ef78352
caroline <caroline.bourdeu@gmail.com>
2021-10-06 17:00:58 +0200

    add test coverage

--- packages/code-du-travail-modeles/src/__test__/depart.mise.restauration.rapide.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.restauration.rapide.spec.ts
@@ -27,16 +27,19 @@ describe("restauration rapide", () => {
       seniority | category                | expectedNotice | expectedUnit | expectedReferences
       ${4}      | ${"Ouvriers"}           | ${8}           | ${"jour"}    | ${DepartRetraiteRefs}
       ${8}      | ${"Ouvriers"}           | ${1}           | ${"mois"}    | ${DepartRetraiteRefs}
+      ${24}     | ${"Ouvriers"}           | ${1}           | ${"mois"}    | ${DepartRetraiteRefs}
       ${25}     | ${"Ouvriers"}           | ${1}           | ${"mois"}    | ${DepartRetraiteRefs}
       ${4}      | ${"Employés"}           | ${8}           | ${"jour"}    | ${DepartRetraiteRefs}
       ${8}      | ${"Employés"}           | ${15}          | ${"jour"}    | ${DepartRetraiteRefs}
+      ${24}     | ${"Employés"}           | ${15}          | ${"jour"}    | ${DepartRetraiteRefs}
       ${25}     | ${"Employés"}           | ${1}           | ${"mois"}    | ${DepartRetraiteRefs}
       ${4}      | ${"Agents de maîtrise"} | ${1}           | ${"mois"}    | ${DepartRetraiteRefs}
       ${8}      | ${"Agents de maîtrise"} | ${1}           | ${"mois"}    | ${DepartRetraiteRefs}
+      ${24}     | ${"Agents de maîtrise"} | ${1}           | ${"mois"}    | ${DepartRetraiteRefs}
       ${25}     | ${"Agents de maîtrise"} | ${2}           | ${"mois"}    | ${DepartRetraiteRefs}
       ${4}      | ${"Cadres"}             | ${3}           | ${"mois"}    | ${DepartRetraiteRefs}
       ${8}      | ${"Cadres"}             | ${1}           | ${"mois"}    | ${DepartRetraiteRefs}
-      ${25}     | ${"Cadres"}             | ${2}           | ${"mois"}    | ${DepartRetraiteRefs}
+      ${24}     | ${"Cadres"}             | ${2}           | ${"mois"}    | ${DepartRetraiteRefs}
     `(
       "Pour un $category possédant $seniority mois d'ancienneté, son préavis est $expectedNotice $expectedUnit",
       ({
@@ -71,15 +74,19 @@ describe("restauration rapide", () => {
       seniority | category                | expectedNotice | expectedUnit | expectedReferences
       ${4}      | ${"Ouvriers"}           | ${8}           | ${"jour"}    | ${MiseRetraiteRefs}
       ${8}      | ${"Ouvriers"}           | ${1}           | ${"mois"}    | ${MiseRetraiteRefs}
+      ${24}     | ${"Ouvriers"}           | ${2}           | ${"mois"}    | ${MiseRetraiteRefs}
       ${25}     | ${"Ouvriers"}           | ${2}           | ${"mois"}    | ${MiseRetraiteRefs}
       ${4}      | ${"Employés"}           | ${8}           | ${"jour"}    | ${MiseRetraiteRefs}
       ${8}      | ${"Employés"}           | ${1}           | ${"mois"}    | ${MiseRetraiteRefs}
+      ${24}     | ${"Employés"}           | ${2}           | ${"mois"}    | ${MiseRetraiteRefs}
       ${25}     | ${"Employés"}           | ${2}           | ${"mois"}    | ${MiseRetraiteRefs}
       ${4}      | ${"Agents de maîtrise"} | ${1}           | ${"mois"}    | ${MiseRetraiteRefs}
       ${8}      | ${"Agents de maîtrise"} | ${1}           | ${"mois"}    | ${MiseRetraiteRefs}
+      ${24}     | ${"Agents de maîtrise"} | ${2}           | ${"mois"}    | ${MiseRetraiteRefs}
       ${25}     | ${"Agents de maîtrise"} | ${2}           | ${"mois"}    | ${MiseRetraiteRefs}
       ${4}      | ${"Cadres"}             | ${3}           | ${"mois"}    | ${MiseRetraiteRefs}
       ${8}      | ${"Cadres"}             | ${3}           | ${"mois"}    | ${MiseRetraiteRefs}
+      ${24}     | ${"Cadres"}             | ${3}           | ${"mois"}    | ${MiseRetraiteRefs}
       ${25}     | ${"Cadres"}             | ${3}           | ${"mois"}    | ${MiseRetraiteRefs}
     `(
       "Pour un $category possédant $seniority mois d'ancienneté, son préavis est $expectedNotice $expectedUnit",
