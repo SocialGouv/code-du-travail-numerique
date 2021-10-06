0e8f6cc49d81eb4136a7191f7e912b1da9086142
maxgfr <maxgolfier@gmail.com>
2021-10-06 10:58:50 +0200

    remove babel-dist

--- packages/code-du-travail-api/babel-dist/routes/__tests__/v1.prefix.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var _v = require("../v1.prefix");
-
-test("prefix", async () => {
-  jest.mock("../../../package.json", () => ({
-    dependencies: { "@socialgouv/cdtn-elasticsearch": "^1.2.3" },
-  }));
-  expect(_v.CDTN_ADMIN_VERSION).toBe("v1");
-});
--- packages/code-du-travail-api/babel-dist/routes/version/versions.elastic.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const { SOURCES } = require("@socialgouv/cdtn-sources");
-
-function getVersionsBody() {
-  return {
-    query: { bool: { filter: { term: { source: SOURCES.VERSIONS } } } },
-  };
-}
-module.exports = getVersionsBody;
