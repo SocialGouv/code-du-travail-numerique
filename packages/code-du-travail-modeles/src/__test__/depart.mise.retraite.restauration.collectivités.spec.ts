8901b084709f777dc9c63d6b8331db3c0e4422cf
Caroline <4971715+carolineBda@users.noreply.github.com>
2021-10-07 15:21:37 +0200

    feat(simulator): add CC 1266 (#3775)

---
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.restauration.collectivités.spec.ts
@@ -0,0 +1,66 @@
+import Engine from "publicodes";
+import { mergeModels } from "../internal/merger";
+
+const engine = new Engine(mergeModels());
+describe("Mise à la retraite", () => {
+  test.each`
+    seniority | category       | expectedNotice | expectedUnit
+    ${5}      | ${"Employés"}  | ${8}           | ${"jour"}
+    ${12}     | ${"Employés"}  | ${1}           | ${"mois"}
+    ${25}     | ${"Employés"}  | ${2}           | ${"mois"}
+    ${5}      | ${"Maîtrises"} | ${1}           | ${"mois"}
+    ${12}     | ${"Maîtrises"} | ${1}           | ${"mois"}
+    ${25}     | ${"Maîtrises"} | ${2}           | ${"mois"}
+    ${5}      | ${"Cadres"}    | ${3}           | ${"mois"}
+    ${12}     | ${"Cadres"}    | ${3}           | ${"mois"}
+    ${25}     | ${"Cadres"}    | ${3}           | ${"mois"}
+  `(
+    "Pour un $category possédant $seniority mois d'ancienneté, son préavis devrait être $expectedNotice $expectedUnit",
+    ({ seniority, category, expectedNotice, expectedUnit }) => {
+      const result = engine
+        .setSituation({
+          "contrat salarié . convention collective": "'IDCC1266'",
+          "contrat salarié . ancienneté": seniority,
+          "contrat salarié . mise à la retraite": "oui",
+          "contrat salarié . travailleur handicapé": "non",
+          "contrat salarié . convention collective . restauration collectivités . catégorie professionnelle": `'${category}'`,
+        })
+        .evaluate("contrat salarié . préavis de retraite");
+
+      expect(result.nodeValue).toEqual(expectedNotice);
+      expect(result.unit?.numerators).toEqual([expectedUnit]);
+      expect(result.missingVariables).toEqual({});
+    }
+  );
+});
+describe("Départ à la retraite", () => {
+  test.each`
+    seniority | category       | expectedNotice | expectedUnit
+    ${5}      | ${"Employés"}  | ${8}           | ${"jour"}
+    ${12}     | ${"Employés"}  | ${1}           | ${"mois"}
+    ${25}     | ${"Employés"}  | ${1}           | ${"mois"}
+    ${5}      | ${"Maîtrises"} | ${1}           | ${"mois"}
+    ${12}     | ${"Maîtrises"} | ${1}           | ${"mois"}
+    ${25}     | ${"Maîtrises"} | ${2}           | ${"mois"}
+    ${5}      | ${"Cadres"}    | ${3}           | ${"mois"}
+    ${12}     | ${"Cadres"}    | ${1}           | ${"mois"}
+    ${25}     | ${"Cadres"}    | ${2}           | ${"mois"}
+  `(
+    "Pour un $category possédant $seniority mois d'ancienneté, son préavis devrait être $expectedNotice $expectedUnit",
+    ({ seniority, category, expectedNotice, expectedUnit }) => {
+      const result = engine
+        .setSituation({
+          "contrat salarié . convention collective": "'IDCC1266'",
+          "contrat salarié . ancienneté": seniority,
+          "contrat salarié . mise à la retraite": "non",
+          "contrat salarié . travailleur handicapé": "non",
+          "contrat salarié . convention collective . restauration collectivités . catégorie professionnelle": `'${category}'`,
+        })
+        .evaluate("contrat salarié . préavis de retraite");
+
+      expect(result.nodeValue).toEqual(expectedNotice);
+      expect(result.unit?.numerators).toEqual([expectedUnit]);
+      expect(result.missingVariables).toEqual({});
+    }
+  );
+});
