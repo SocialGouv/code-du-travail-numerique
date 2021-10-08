a4d766c6a9b3a9c8f37fdb2bbd0e94c78e44c719
maxgfr <maxgolfier@gmail.com>
2021-10-08 10:36:33 +0200

    fix(notification): add cadre

--- packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.commerce.detail.fruits.legumes.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/depart.mise.retraite.commerce.detail.fruits.legumes.spec.ts
@@ -5,6 +5,7 @@ import {
   MiseRetraiteReferences,
 } from "./common/LegalReferences";
 import { getReferences } from "../utils/GetReferences";
+import { getNotifications } from "..";
 
 const ArticleCc = {
   article: "Article 3.10.3",
@@ -91,6 +92,45 @@ describe("CC 1505", () => {
         expect(result.unit?.numerators).toEqual(["mois"]);
         expect(references).toHaveLength(expectedReferences.length);
         expect(references).toEqual(expect.arrayContaining(expectedReferences));
+      }
+    );
+  });
+
+  describe("Notifications", () => {
+    const notification =
+      "Attention: L'article de la convention collective ou la convention collective saisie n’a pas été étendue au niveau national. Par conséquent, pour que ce résultat soit applicable à votre situation, il faut que l’employeur ait adhéré à l’organisation patronale signataire de cette convention. Sans cette adhésion, l'employeur n'a pas l'obligation d'appliquer les règles de la convention mais il applique le préavis prévu par le code du travail.";
+    test.each`
+      seniority | category                                   | expectedNotificationLength | expectedNotification
+      ${5}      | ${"Ouvriers, Employés (Niveaux N1 à N4)"}  | ${0}                       | ${null}
+      ${6}      | ${"Ouvriers, Employés (Niveaux N1 à N4)"}  | ${0}                       | ${null}
+      ${24}     | ${"Ouvriers, Employés (Niveaux N1 à N4)"}  | ${0}                       | ${null}
+      ${5}      | ${"Agents de maîtrise (Niveaux N5 et N6)"} | ${0}                       | ${null}
+      ${6}      | ${"Agents de maîtrise (Niveaux N5 et N6)"} | ${0}                       | ${null}
+      ${24}     | ${"Agents de maîtrise (Niveaux N5 et N6)"} | ${0}                       | ${null}
+      ${5}      | ${"Cadres (Niveaux N7 et N8)"}             | ${1}                       | ${notification}
+      ${6}      | ${"Cadres (Niveaux N7 et N8)"}             | ${1}                       | ${notification}
+      ${24}     | ${"Cadres (Niveaux N7 et N8)"}             | ${1}                       | ${notification}
+    `(
+      "Pour un $category possédant $seniority mois d'ancienneté en départ à la retraite, le nombre de notification a affiché est de $expectedNotificationLength",
+      ({
+        seniority,
+        category,
+        expectedNotificationLength,
+        expectedNotification,
+      }) => {
+        const result = getNotifications(
+          engine.setSituation({
+            "contrat salarié . convention collective": "'IDCC1505'",
+            "contrat salarié . ancienneté": seniority,
+            "contrat salarié . départ à la retraite": "oui",
+            "contrat salarié . travailleur handicapé": "non",
+            "contrat salarié . convention collective . commerces de détail fruits et légumes . catégorie professionnelle": `'${category}'`,
+          })
+        );
+
+        expect(result).toHaveLength(expectedNotificationLength);
+        if (result.length > 0)
+          expect(result[0].description).toBe(expectedNotification);
       }
     );
   });
