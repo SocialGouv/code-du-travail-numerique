e2ca0dbb3fefe64ffc570c388288541dcf20221f
caroline <caroline.bourdeu@gmail.com>
2021-10-12 15:08:18 +0200

    feat(simulator): update description's text for simulator "preavis de retraire"

--- packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/steps/Introduction.tsx
+++ packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/steps/Introduction.tsx
@@ -16,7 +16,7 @@ const IntroductionStep = (): JSX.Element => (
       Afin de réaliser cette simulation, vous aurez besoin d’informations
       concernant la situation du salarié, comme sa convention collective, sa
       catégorie ou son coefficient. La plupart de ces informations se trouvent
-      sur le bulletin de salaire ou le contrat de travail. Prévoyez X à X
+      sur le bulletin de salaire ou le contrat de travail. Prévoyez environ 5
       minutes pour cette simulation.
     </p>
     <p>Cliquez sur commencer pour démarrer la simulation.</p>
