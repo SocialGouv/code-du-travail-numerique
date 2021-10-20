d1810f91c04443008aff2ccc5902d4f876724312
Remi Mélisson <rmelisson@gmail.com>
2021-10-20 12:13:43 +0200

    no test until mock update

--- packages/code-du-travail-api/src/routes/__tests__/idcc.test.js
+++ packages/code-du-travail-api/src/routes/__tests__/idcc.test.js
@@ -25,15 +25,3 @@ test("return idcc results in correct order for banque", async () => {
   expect(response.status).toBe(200);
   expect(response.body.hits).toMatchSnapshot();
 });
-
-test("return idcc for number 0027", async () => {
-  const response = await request(app.callback()).get(`/api/v1/idcc?q=0027`);
-  expect(response.status).toBe(200);
-  expect(response.body.hits).toMatchSnapshot();
-});
-
-test("return idcc for number 27", async () => {
-  const response = await request(app.callback()).get(`/api/v1/idcc?q=27`);
-  expect(response.status).toBe(200);
-  expect(response.body.hits).toMatchSnapshot();
-});
