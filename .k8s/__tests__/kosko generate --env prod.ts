a78821ea962ac1b21d7281749dd6cfd2999b1803
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-10-22 13:54:07 +0200

    feat: add env variable

--- .k8s/__tests__/kosko generate --env prod.ts
+++ .k8s/__tests__/kosko generate --env prod.ts
@@ -1,5 +1,5 @@
 import { getEnvManifests } from "@socialgouv/kosko-charts/testing";
-import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
+import { project } from "@socialgouv/kosko-charts/testing/fake/github-actions.env";
 
 jest.setTimeout(1000 * 60);
 test("kosko generate --prod", async () => {
