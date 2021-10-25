f8c1683f1efc187901109c966fa5ff03846891a1
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-10-25 19:22:44 +0200

    fix: tarte au citron

--- packages/code-du-travail-frontend/public/static/tarteaucitron/initTarteaucitron.js
+++ packages/code-du-travail-frontend/public/static/tarteaucitron/initTarteaucitron.js
@@ -4,25 +4,18 @@ window.tarteaucitronForceLanguage = currentLanguage;
 
 tarteaucitron.init({
   privacyUrl: "/politique-confidentialite#cookie" /* Privacy policy url */,
-
   hashtag: "#tarteaucitron" /* Open the panel with this hashtag */,
   cookieName: "tarteaucitron" /* Cookie name */,
-
   orientation: "bottom" /* Banner position (top - bottom) */,
   showAlertSmall: false /* Show the small banner on bottom right */,
   cookieslist: false /* Show the cookie list */,
-
   adblocker: false /* Show a Warning if an adblocker is detected */,
   AcceptAllCta: true /* Show the accept all button when highPrivacy on */,
   highPrivacy: true /* Disable auto consent */,
   handleBrowserDNTRequest: false /* If Do Not Track == 1, disallow all */,
-
   removeCredit: true /* Remove credit link */,
   moreInfoLink: true /* Show more info link */,
   useExternalCss: false /* If false, the tarteaucitron.css file will be loaded */,
-
-  //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for multisite */
-
   readmoreLink:
     "/politique-confidentialite#cookie" /* Change the default readmore link */
 });
