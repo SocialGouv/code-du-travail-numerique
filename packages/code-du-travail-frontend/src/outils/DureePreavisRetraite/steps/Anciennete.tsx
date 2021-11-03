704e3a8dd9b286f6c59684f7a8d61bcef6545cb0
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-03 18:46:07 +0100

    fix: form and code

--- packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/steps/Anciennete.tsx
+++ packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/steps/Anciennete.tsx
@@ -10,18 +10,34 @@ import { mapToPublicodesSituation } from "../../publicodes/Utils";
 
 function AncienneteStep({ form }: WizardStepProps): JSX.Element {
   const publicodesContext = usePublicodes();
-  const [overrideQuestion, setOverrideQuestion] = useState<string>(null);
+  const [question, setQuestion] = useState<JSX.Element>(
+    <>
+      Le salarié a-t-il plus de 2 ans d&apos;ancienneté dans l&apos;entreprise{" "}
+      <Small>(à partir de 2 ans + 1 jour)</Small>
+      &nbsp;?
+    </>
+  );
 
   useEffect(() => {
     if (
       form.getState().values.ccn.num === 2264 &&
       form.getState().values["contrat salarié - mise à la retraite"] === "oui"
     ) {
-      setOverrideQuestion(
-        "Le salarié a-t-il plus de 5 ans d'ancienneté dans l'entreprise ?"
+      setQuestion(
+        <>
+          Le salarié a-t-il plus de 5 ans d&apos;ancienneté dans
+          l&apos;entreprise ? <Small>(à partir de 5 ans + 1 jour)</Small>
+        </>
       );
       form.change("seniorityValue", "61");
     } else {
+      setQuestion(
+        <>
+          Le salarié a-t-il plus de 2 ans d&apos;ancienneté dans
+          l&apos;entreprise <Small>(à partir de 2 ans + 1 jour)</Small>
+          &nbsp;?
+        </>
+      );
       form.change("seniorityValue", "25");
     }
   }, []);
@@ -34,41 +50,24 @@ function AncienneteStep({ form }: WizardStepProps): JSX.Element {
 
   return (
     <>
-      {overrideQuestion ? (
-        <YesNoQuestion
-          name="seniorityMaximum"
-          label={overrideQuestion}
-          onChange={() => {
-            form.change("contrat salarié - ancienneté", undefined);
-          }}
-        />
-      ) : (
-        <YesNoQuestion
-          label={
-            <>
-              Le salarié a-t-il plus de 2 ans d&apos;ancienneté dans
-              l&apos;entreprise <Small>(à partir de 2 ans + 1 jour)</Small>
-              &nbsp;?
-            </>
-          }
-          name="seniorityMaximum"
-          tooltip={{
-            content: (
-              <p>
-                L&apos;ancienneté du salarié est habituellement mentionnée sur
-                le&nbsp;
-                <b>bulletin de salaire</b>.
-              </p>
-            ),
-            help: "",
-          }}
-          onChange={() => {
-            form.change("contrat salarié - ancienneté", undefined);
-            form.change("seniorityValue", "25");
-          }}
-        />
-      )}
-
+      <YesNoQuestion
+        label={question}
+        name="seniorityMaximum"
+        tooltip={{
+          content: (
+            <p>
+              L&apos;ancienneté du salarié est habituellement mentionnée sur
+              le&nbsp;
+              <b>bulletin de salaire</b>.
+            </p>
+          ),
+          help: "",
+        }}
+        onChange={() => {
+          form.change("contrat salarié - ancienneté", undefined);
+          form.change("seniorityValue", "25");
+        }}
+      />
       {form.getState().values.seniorityMaximum === false && (
         <TextQuestion
           name="contrat salarié - ancienneté"
