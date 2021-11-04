46d60abf06e41e1bbf608cbe02b2a3bd65a8f138
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2021-11-04 17:31:19 +0100

    feat: tarteaucitron added (#3920)

---
+++ packages/code-du-travail-frontend/src/lib/tracking/EventTracker.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+import { URL_TRACKED } from "./constants";
+
+export default function EventTracker(): JSX.Element {
+  const router = useRouter();
+
+  const urlToTrack = React.useMemo(
+    () => URL_TRACKED.find((urlTracked) => urlTracked.url === router.pathname),
+    [router.pathname]
+  );
+
+  React.useEffect(() => {
+    if (typeof window !== "undefined" && typeof window.gtag !== "undefined") {
+      window.gtag("event", "conversion", {
+        allow_custom_scripts: true,
+        send_to: `DC-3048978/emplo253/${urlToTrack.type}+unique`,
+        u1: `${window.location.origin}${urlToTrack.url}`,
+      });
+    }
+  }, [urlToTrack, router.pathname]);
+
+  return (
+    <>
+      {urlToTrack && (
+        <script
+          type="text/javascript"
+          src="/static/tarteaucitron/initTarteaucitron.js"
+        />
+      )}
+    </>
+  );
+}
