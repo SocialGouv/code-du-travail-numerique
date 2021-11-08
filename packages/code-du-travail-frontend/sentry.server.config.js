02325a9dcd4f87c5e8626ae737a037bc187dd370
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-08 10:47:38 +0100

    fix: config

--- packages/code-du-travail-frontend/sentry.server.config.js
+++ packages/code-du-travail-frontend/sentry.server.config.js
@@ -4,8 +4,22 @@
 
 import * as Sentry from "@sentry/nextjs";
 
+function getSentryCspUrl() {
+  const PACKAGE_VERSION = process.env.VERSION || "";
+  const PORT = parseInt(process.env.FRONTEND_PORT, 10) || 3000;
+  const FRONTEND_HOST = process.env.FRONTEND_HOST || `http://localhost:${PORT}`;
+  const isProduction = FRONTEND_HOST.includes("code.travail.gouv.fr");
+  const environment = isProduction ? "production" : "preproduction";
+  if (process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN) {
+    return `${
+      process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+    }&sentry_environment=${environment}&sentry_release=${PACKAGE_VERSION}`;
+  }
+  return "";
+}
+
 Sentry.init({
-  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || "",
+  dsn: getSentryCspUrl(),
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
