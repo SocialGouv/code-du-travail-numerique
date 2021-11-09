caef4bae94def6a31475feeca4573dcaa6ab3bec
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-09 10:27:44 +0100

    fix: env

--- packages/code-du-travail-frontend/sentry.client.config.js
+++ packages/code-du-travail-frontend/sentry.client.config.js
@@ -4,19 +4,8 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-const PORT = parseInt(process.env.FRONTEND_PORT, 10) || 3000;
-const FRONTEND_HOST = process.env.FRONTEND_HOST || `http://localhost:${PORT}`;
-const isProduction = FRONTEND_HOST.includes("code.travail.gouv.fr");
-const isPreproduction = FRONTEND_HOST.includes(
-  "preprod.dev.fabrique.social.gouv.fr"
-);
-
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || "",
-  environment: isProduction
-    ? "production"
-    : isPreproduction
-    ? "preproduction"
-    : "dev",
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENV || "dev",
   tracesSampleRate: 1.0,
 });
