d28536098a9e8ccc2c209339fbc18472fa987f20
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-16 18:52:46 +0100

    feat: add notif

--- packages/code-du-travail-modeles/src/__test__/batiment-ouvriers-parisien/depart-mise.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/batiment-ouvriers-parisien/depart-mise.spec.ts
@@ -1,5 +1,6 @@
 import Engine from "publicodes";
 
+import { getNotifications } from "../..";
 import { mergeModels } from "../../internal/merger";
 import { getReferences } from "../../utils/GetReferences";
 import {
@@ -82,5 +83,36 @@ describe("Préavis de retraite de la CC 1740", () => {
         );
       }
     );
+  });
+
+  describe("Vérification des notifications", () => {
+    test("Pour un salarié en départ à la retraite, une notification doit s'afficher", () => {
+      const notifications = getNotifications(
+        engine.setSituation({
+          "contrat salarié . ancienneté": 5,
+          "contrat salarié . convention collective": "'IDCC1740'",
+          "contrat salarié . mise à la retraite": "non",
+          "contrat salarié . travailleur handicapé": "non",
+        })
+      );
+      expect(notifications).toHaveLength(1);
+      expect(notifications[0].description).toBe(
+        "Le salarié doit notifier à l’employeur son départ en retraite par une lettre recommandée avec accusé de réception. Le départ en retraite aura lieu le premier jour d’un mois civil. Exemple: Si le 6 mars 2020 un salarié notifie sa décision de partir à la retraite à son employeur, le préavis à effectuer débutera le 1er avril 2020."
+      );
+    });
+    test("Pour un salarié en mise à la retraite, une notification doit s'afficher", () => {
+      const notifications = getNotifications(
+        engine.setSituation({
+          "contrat salarié . ancienneté": 5,
+          "contrat salarié . convention collective": "'IDCC1740'",
+          "contrat salarié . mise à la retraite": "oui",
+          "contrat salarié . travailleur handicapé": "non",
+        })
+      );
+      expect(notifications).toHaveLength(1);
+      expect(notifications[0].description).toBe(
+        "L’employeur doit notifier au salarié sa mise à la retraite par lettre recommandée avec accusé de réception. La mise à la retraite aura lieu le premier jour d'un mois civil. Exemple : Si le 6 mars 2020 un employeur notifie sa décision de mettre à la retraite son salarié, le préavis à effectuer débutera le 1er avril 2020."
+      );
+    });
   });
 });
