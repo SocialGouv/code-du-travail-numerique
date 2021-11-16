8853e7fd53de8e51078e3857c614d246062fa1f8
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-16 19:34:02 +0100

    fix: cc

--- packages/code-du-travail-frontend/server/server.js
+++ packages/code-du-travail-frontend/server/server.js
@@ -5,13 +5,12 @@ const { getKoaServer } = require("./koaServer");
  * this env variable is use to target developpement / staging deployement
  * in order to block indexing bot using a x-robot-header and an appropriate robots.txt
  */
-const NEXT_PUBLIC_IS_PRODUCTION_DEPLOYMENT =
+const IS_PRODUCTION_DEPLOYMENT =
   process.env.NEXT_PUBLIC_IS_PRODUCTION_DEPLOYMENT === "true";
 const PORT = parseInt(process.env.FRONTEND_PORT, 10) || 3000;
 const FRONTEND_HOST = process.env.FRONTEND_HOST || `http://localhost:${PORT}`;
 const PROD_HOSTNAME = process.env.PROD_HOSTNAME || "code.travail.gouv.fr";
-const PUBLIC_SENTRY_DSN =
-  process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.PUBLIC_SENTRY_DSN || "";
+const PUBLIC_SENTRY_DSN = process.env.SENTRY_DSN || "";
 const PACKAGE_VERSION = process.env.VERSION || "";
 
 const dev = process.env.NODE_ENV !== "production";
@@ -33,7 +32,7 @@ nextApp.prepare().then(async () => {
 
   - process.env.NODE_ENV : ${process.env.NODE_ENV || "development"}
   - version: ${PACKAGE_VERSION}
-  - is deployed on prod(${PROD_HOSTNAME}): ${NEXT_PUBLIC_IS_PRODUCTION_DEPLOYMENT}
+  - is deployed on prod(${PROD_HOSTNAME}): ${IS_PRODUCTION_DEPLOYMENT}
   - sentry: ${PUBLIC_SENTRY_DSN},
 
 `);
