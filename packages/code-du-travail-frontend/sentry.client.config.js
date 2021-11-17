badeda9bd8623f156bd4e9924b839d9b14b98ed7
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-17 10:10:18 +0100

    fix: sentry

--- packages/code-du-travail-frontend/sentry.client.config.js
+++ packages/code-du-travail-frontend/sentry.client.config.js
@@ -6,6 +6,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || "",
-  environment: process.env.NEXT_PUBLIC_SENTRY_ENV | "dev",
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENV || "dev",
   tracesSampleRate: 1.0,
 });
