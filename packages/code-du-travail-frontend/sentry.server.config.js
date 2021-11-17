a69ef3ceffd6f8935e996bdd513a320a6d240f77
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-17 12:07:25 +0100

    fix: env

--- packages/code-du-travail-frontend/sentry.server.config.js
+++ packages/code-du-travail-frontend/sentry.server.config.js
@@ -5,7 +5,9 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.SENTRY_PUBLIC_DSN || "",
-  environment: process.env.SENTRY_ENV || "dev",
+  dsn:
+    process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_PUBLIC_DSN || "",
+  environment:
+    process.env.NEXT_PUBLIC_SENTRY_ENV || process.env.SENTRY_ENV || "dev",
   tracesSampleRate: 1.0,
 });
