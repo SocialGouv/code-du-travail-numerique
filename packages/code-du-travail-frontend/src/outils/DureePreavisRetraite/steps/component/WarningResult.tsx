890bee74246979d79baa8edc9210af230b46830f
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-19 17:33:49 +0100

    fix: cc

--- packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/steps/component/WarningResult.tsx
+++ packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/steps/component/WarningResult.tsx
@@ -1,3 +1,4 @@
+import { supportedCcn as retraiteData } from "@socialgouv/modeles-social";
 import React from "react";
 
 import Disclaimer from "../../../common/Disclaimer";
@@ -12,8 +13,15 @@ type Props = {
 const title = "Attention il peut exister une durée plus favorable";
 
 const WarningResult: React.FC<Props> = ({ publicodesContext, data }) => {
+  const isSupported = React.useMemo(() => {
+    if (data && data.ccn) {
+      const idccInfo = retraiteData.find((item) => item.idcc == data.ccn.num);
+      return !!(idccInfo && idccInfo.preavisRetraite);
+    }
+    return false;
+  }, [data]);
   if (publicodesContext.result.value === 0) {
-    if (data.ccn) {
+    if (isSupported) {
       return (
         <Disclaimer
           title={"Attention il peut quand même exister une durée de préavis"}
@@ -45,49 +53,80 @@ const WarningResult: React.FC<Props> = ({ publicodesContext, data }) => {
     data["contrat salarié - mise à la retraite"] === "oui" ? "mise" : "depart";
 
   if (type === "depart") {
+    if (isSupported) {
+      return (
+        <Disclaimer title={title}>
+          <p>
+            Un accord collectif d’entreprise, le contrat de travail ou un usage
+            peut prévoir une durée de préavis<sup>*</sup> ou une condition
+            d’ancienneté<sup>*</sup> plus favorable pour le salarié. Dans ce
+            cas, c’est cette durée ou cette ancienneté plus favorable qui
+            s’applique au salarié.
+          </p>
+
+          <SmallText>
+            <sup>*</sup>&nbsp;durée de préavis plus favorable pour le salarié =
+            durée plus courte.
+          </SmallText>
+          <SmallText>
+            <sup>*</sup>&nbsp;condition d’ancienneté plus favorable pour le
+            salarié = condition d’ancienneté moins restrictive et conduisant à
+            une durée de préavis plus courte.
+          </SmallText>
+        </Disclaimer>
+      );
+    } else {
+      return (
+        <Disclaimer
+          title={"Attention il peut quand même exister une durée de préavis"}
+        >
+          <p>
+            Une convention collective de branche, un accord collectif
+            d’entreprise, le contrat de travail ou un usage peut prévoir une
+            durée de préavis. Dans ce cas, cette durée doit s’appliquer.
+          </p>
+          <p>Nous vous conseillons de vérifiez cela.</p>
+        </Disclaimer>
+      );
+    }
+  }
+
+  if (isSupported) {
     return (
       <Disclaimer title={title}>
         <p>
-          Une convention collective de branche, un accord collectif
-          d’entreprise, le contrat de travail ou un usage peut prévoir une durée
-          de préavis<sup>*</sup> ou une condition d’ancienneté
-          <sup>*</sup> plus favorable pour le salarié. Dans ce cas, c’est cette
-          durée ou cette ancienneté plus favorable qui s’applique au salarié.
+          Un accord collectif d’entreprise, le contrat de travail ou un usage
+          peut prévoir une durée de préavis<sup>*</sup> ou une condition
+          d’ancienneté<sup>*</sup> plus favorable pour le salarié. Dans ce cas,
+          c’est cette durée ou cette ancienneté plus favorable qui s’applique au
+          salarié.
         </p>
 
         <SmallText>
           <sup>*</sup>&nbsp;durée de préavis plus favorable pour le salarié =
-          durée plus courte.
+          durée plus longue.
         </SmallText>
         <SmallText>
           <sup>*</sup>&nbsp;condition d’ancienneté plus favorable pour le
           salarié = condition d’ancienneté moins restrictive et conduisant à une
-          durée de préavis plus courte.
+          durée de préavis plus longue.
         </SmallText>
       </Disclaimer>
     );
+  } else {
+    return (
+      <Disclaimer
+        title={"Attention il peut quand même exister une durée de préavis"}
+      >
+        <p>
+          Une convention collective de branche, un accord collectif
+          d’entreprise, le contrat de travail ou un usage peut prévoir une durée
+          de préavis. Dans ce cas, cette durée doit s’appliquer.
+        </p>
+        <p>Nous vous conseillons de vérifiez cela.</p>
+      </Disclaimer>
+    );
   }
-
-  return (
-    <Disclaimer title={title}>
-      <p>
-        Un accord collectif d’entreprise, le contrat de travail ou un usage peut
-        prévoir une durée de préavis<sup>*</sup> ou une condition d’ancienneté
-        <sup>*</sup> plus favorable pour le salarié. Dans ce cas, c’est cette
-        durée ou cette ancienneté plus favorable qui s’applique au salarié.
-      </p>
-
-      <SmallText>
-        <sup>*</sup>&nbsp;durée de préavis plus favorable pour le salarié =
-        durée plus longue.
-      </SmallText>
-      <SmallText>
-        <sup>*</sup>&nbsp;condition d’ancienneté plus favorable pour le salarié
-        = condition d’ancienneté moins restrictive et conduisant à une durée de
-        préavis plus longue.
-      </SmallText>
-    </Disclaimer>
-  );
 };
 
 export default WarningResult;
