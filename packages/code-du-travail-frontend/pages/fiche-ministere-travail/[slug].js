5905abb3e20652226a92c1ff11d7e9663a328be8
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-19 19:02:59 +0100

    fix: image

--- packages/code-du-travail-frontend/pages/fiche-ministere-travail/[slug].js
+++ packages/code-du-travail-frontend/pages/fiche-ministere-travail/[slug].js
@@ -19,13 +19,11 @@ const fetchSheetMT = ({ slug }) => fetch(`${API_URL}/sheets-mt/${slug}`);
 const buildAccordionSections = (sections) =>
   sections
     .filter((section) => section.anchor)
-    .map(({ anchor, html, title }) => {
-      return {
-        body: <TabContent>{html}</TabContent>,
-        id: anchor,
-        title,
-      };
-    });
+    .map(({ anchor, html, title }) => ({
+      body: <TabContent>{html}</TabContent>,
+      id: anchor,
+      title,
+    }));
 
 const Fiche = ({ data = { _source: {} }, anchor }) => {
   const {
