c61968dabc05a9271b7c8832237b125ca83871b2
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-11-29 17:17:09 +0100

    fix: warning result

--- packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/steps/component/WarningResult.tsx
+++ packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/steps/component/WarningResult.tsx
@@ -15,47 +15,51 @@ export const titreFavorable =
 export const titrePreavis =
   "Attention il peut quand même exister une durée de préavis";
 
+export enum StatusCcn {
+  CCN_SUPPORTED = "CCN_SUPPORTED",
+  CCN_UNSUPPORTED = "CCN_UNSUPPORTED",
+  CC_UNSELECTED = "CC_UNSELECTED",
+}
+
 const WarningResult: React.FC<Props> = ({
   resultValueInDays,
   ccNumber,
   type,
 }) => {
-  const isSupported = React.useMemo(() => {
-    if (ccNumber) {
-      const idccInfo = supportedCcn.find((item) => item.idcc == ccNumber);
-      return !!(idccInfo && idccInfo.preavisRetraite);
-    }
-    return false;
+  const statusCcn: StatusCcn = React.useMemo(() => {
+    const idccInfo = supportedCcn.find((item) => item.idcc == ccNumber);
+    const find = !!(idccInfo && idccInfo.preavisRetraite);
+    return find && ccNumber
+      ? StatusCcn.CCN_SUPPORTED
+      : !find && ccNumber
+      ? StatusCcn.CCN_UNSUPPORTED
+      : StatusCcn.CC_UNSELECTED;
   }, [ccNumber]);
-  if (resultValueInDays === 0) {
-    if (isSupported) {
-      return (
-        <Disclaimer title={titrePreavis}>
+
+  return (
+    <Disclaimer title={resultValueInDays === 0 ? titrePreavis : titreFavorable}>
+      {resultValueInDays === 0 && statusCcn === StatusCcn.CCN_SUPPORTED ? (
+        <>
           <p>
             Un accord collectif d’entreprise, le contrat de travail ou un usage
             peut prévoir une durée de préavis. Dans ce cas, cette durée doit
             s’appliquer.
           </p>
           <p>Nous vous conseillons de vérifiez cela.</p>
-        </Disclaimer>
-      );
-    } else {
-      return (
-        <Disclaimer title={titrePreavis}>
+        </>
+      ) : resultValueInDays === 0 &&
+        (statusCcn === StatusCcn.CC_UNSELECTED ||
+          statusCcn === StatusCcn.CCN_UNSUPPORTED) ? (
+        <>
           <p>
             Une convention collective de branche, un accord collectif
             d’entreprise, le contrat de travail ou un usage peut prévoir une
             durée de préavis. Dans ce cas, cette durée doit s’appliquer.
           </p>
           <p>Nous vous conseillons de vérifiez cela.</p>
-        </Disclaimer>
-      );
-    }
-  }
-  if (type === "depart") {
-    if (isSupported) {
-      return (
-        <Disclaimer title={titrePreavis}>
+        </>
+      ) : type === "depart" && statusCcn === StatusCcn.CCN_SUPPORTED ? (
+        <>
           <p>
             Un accord collectif d’entreprise, le contrat de travail ou un usage
             peut prévoir une durée de préavis<sup>*</sup> ou une condition
@@ -73,56 +77,76 @@ const WarningResult: React.FC<Props> = ({
             salarié = condition d’ancienneté moins restrictive et conduisant à
             une durée de préavis plus courte.
           </SmallText>
-        </Disclaimer>
-      );
-    } else {
-      return (
-        <Disclaimer title={titreFavorable}>
+        </>
+      ) : type === "mise" && statusCcn === StatusCcn.CCN_SUPPORTED ? (
+        <>
+          <p>
+            Un accord collectif d’entreprise, le contrat de travail ou un usage
+            peut prévoir une durée de préavis<sup>*</sup> ou une condition
+            d’ancienneté<sup>*</sup> plus favorable pour le salarié. Dans ce
+            cas, c’est cette durée ou cette ancienneté plus favorable qui
+            s’applique au salarié.
+          </p>
+
+          <SmallText>
+            <sup>*</sup>&nbsp;durée de préavis plus favorable pour le salarié =
+            durée plus longue.
+          </SmallText>
+          <SmallText>
+            <sup>*</sup>&nbsp;condition d’ancienneté plus favorable pour le
+            salarié = condition d’ancienneté moins restrictive et conduisant à
+            une durée de préavis plus longue.
+          </SmallText>
+        </>
+      ) : type === "depart" &&
+        (statusCcn === StatusCcn.CC_UNSELECTED ||
+          statusCcn === StatusCcn.CCN_UNSUPPORTED) ? (
+        <>
           <p>
             Une convention collective de branche, un accord collectif
             d’entreprise, le contrat de travail ou un usage peut prévoir une
-            durée de préavis. Dans ce cas, cette durée doit s’appliquer.
+            durée de préavis<sup>*</sup> ou une condition d’ancienneté
+            <sup>*</sup> plus favorable pour le salarié. Dans ce cas, c’est
+            cette durée ou cette ancienneté plus favorable qui s’applique au
+            salarié.
           </p>
-          <p>Nous vous conseillons de vérifiez cela.</p>
-        </Disclaimer>
-      );
-    }
-  }
 
-  if (isSupported) {
-    return (
-      <Disclaimer title={titrePreavis}>
-        <p>
-          Un accord collectif d’entreprise, le contrat de travail ou un usage
-          peut prévoir une durée de préavis<sup>*</sup> ou une condition
-          d’ancienneté<sup>*</sup> plus favorable pour le salarié. Dans ce cas,
-          c’est cette durée ou cette ancienneté plus favorable qui s’applique au
-          salarié.
-        </p>
+          <SmallText>
+            <sup>*</sup>&nbsp;durée de préavis plus favorable pour le salarié =
+            durée plus courte.
+          </SmallText>
+          <SmallText>
+            <sup>*</sup>&nbsp;condition d’ancienneté plus favorable pour le
+            salarié = condition d’ancienneté moins restrictive et conduisant à
+            une durée de préavis plus courte.
+          </SmallText>
+        </>
+      ) : type === "mise" &&
+        (statusCcn === StatusCcn.CC_UNSELECTED ||
+          statusCcn === StatusCcn.CCN_UNSUPPORTED) ? (
+        <>
+          <p>
+            Une convention collective de branche, un accord collectif
+            d’entreprise, le contrat de travail ou un usage peut prévoir une
+            durée de préavis<sup>*</sup> ou une condition d’ancienneté
+            <sup>*</sup> plus favorable pour le salarié. Dans ce cas, c’est
+            cette durée ou cette ancienneté plus favorable qui s’applique au
+            salarié.
+          </p>
 
-        <SmallText>
-          <sup>*</sup>&nbsp;durée de préavis plus favorable pour le salarié =
-          durée plus longue.
-        </SmallText>
-        <SmallText>
-          <sup>*</sup>&nbsp;condition d’ancienneté plus favorable pour le
-          salarié = condition d’ancienneté moins restrictive et conduisant à une
-          durée de préavis plus longue.
-        </SmallText>
-      </Disclaimer>
-    );
-  } else {
-    return (
-      <Disclaimer title={titreFavorable}>
-        <p>
-          Une convention collective de branche, un accord collectif
-          d’entreprise, le contrat de travail ou un usage peut prévoir une durée
-          de préavis. Dans ce cas, cette durée doit s’appliquer.
-        </p>
-        <p>Nous vous conseillons de vérifiez cela.</p>
-      </Disclaimer>
-    );
-  }
+          <SmallText>
+            <sup>*</sup>&nbsp;durée de préavis plus favorable pour le salarié =
+            durée plus longue.
+          </SmallText>
+          <SmallText>
+            <sup>*</sup>&nbsp;condition d’ancienneté plus favorable pour le
+            salarié = condition d’ancienneté moins restrictive et conduisant à
+            une durée de préavis plus longue.
+          </SmallText>
+        </>
+      ) : null}
+    </Disclaimer>
+  );
 };
 
 export default WarningResult;
