88cee0869d43acb28cc1d32fa329173e31527ca0
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-12-24 17:56:48 +0100

    fix: cc bug

--- packages/code-du-travail-frontend/src/outils/common/PubliReferences.tsx
+++ packages/code-du-travail-frontend/src/outils/common/PubliReferences.tsx
@@ -10,21 +10,25 @@ type Props = {
 
 const PubliReferences: React.FC<Props> = ({ references }) => (
   <>
-    {references.length > 0 && (
+    {references && references.length > 0 && (
       <>
         <SectionTitle>Sources</SectionTitle>
         <ul>
           {references.map(({ article, url }, id) => (
-            <li key={`${url}-${id}`}>
-              <A11yLink
-                href={url}
-                target="_blank"
-                rel="noopener noreferrer"
-                title={`Consultez l’${article.toLowerCase()}`}
-              >
-                {article}
-              </A11yLink>
-            </li>
+            <>
+              {article && (
+                <li key={`${url}-${id}`}>
+                  <A11yLink
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title={`Consultez l’${article.toLowerCase()}`}
+                  >
+                    {article}
+                  </A11yLink>
+                </li>
+              )}
+            </>
           ))}
         </ul>
       </>
