5bcce3fdb6a806acaf48a7e7055a9560591bf21a
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-12-27 11:59:05 +0100

    fix: test

---
+++ packages/code-du-travail-modeles/src/__test__/3239-particuliers-employeurs-domicile/preavis-retraite/depart-mise.spec.ts
@@ -0,0 +1,140 @@
+import Engine from "publicodes";
+
+import { mergeModels } from "../../../internal/merger";
+import { getReferences } from "../../../utils/GetReferences";
+import {
+  DepartRetraiteReferences,
+  MiseRetraiteReferences,
+} from "../../common/legal-references";
+
+const engine = new Engine(mergeModels());
+
+const ReferencesParticulierEmployeur = [
+  {
+    article: "Article 162-5",
+    url: "",
+  },
+  {
+    article: "Article 162-4-1",
+    url: "",
+  },
+];
+
+const ReferencesAssistantsMaternels = [
+  {
+    article: "Article 120",
+    url: "",
+  },
+];
+
+const DepartRetraiteCcReferencesParticulierEmployeurs = [
+  ...DepartRetraiteReferences,
+  ...ReferencesParticulierEmployeur,
+];
+
+const DepartRetraiteCcReferencesAssistantsMaternels = [
+  ...DepartRetraiteReferences,
+  ...ReferencesAssistantsMaternels,
+];
+
+const MiseRetraiteCcReferencesParticulierEmployeurs = [
+  ...MiseRetraiteReferences,
+  ...ReferencesParticulierEmployeur,
+];
+
+const MiseRetraiteCcReferencesAssistantsMaternels = [
+  ...MiseRetraiteReferences,
+  ...ReferencesAssistantsMaternels,
+];
+
+type InputType = {
+  category: string;
+  seniority: number;
+  expectedResult: number;
+  expectedReferences: { article: string; url: string }[];
+  expectedPeriod: "jour" | "mois";
+};
+
+describe("Préavis de retraite de la CC 3239", () => {
+  describe("Vérification des départs à la retraite et des références juridiques", () => {
+    test.each`
+      category                                           | seniority | expectedResult | expectedPeriod | expectedReferences
+      ${"Salariés du particulier employeur"}             | ${2}      | ${7}           | ${"jour"}      | ${DepartRetraiteCcReferencesParticulierEmployeurs}
+      ${"Salariés du particulier employeur"}             | ${6}      | ${1}           | ${"mois"}      | ${DepartRetraiteCcReferencesParticulierEmployeurs}
+      ${"Salariés du particulier employeur"}             | ${12}     | ${1}           | ${"mois"}      | ${DepartRetraiteCcReferencesParticulierEmployeurs}
+      ${"Salariés du particulier employeur"}             | ${24}     | ${2}           | ${"mois"}      | ${DepartRetraiteCcReferencesParticulierEmployeurs}
+      ${"Assistants maternels du particulier employeur"} | ${2}      | ${8}           | ${"jour"}      | ${DepartRetraiteCcReferencesAssistantsMaternels}
+      ${"Assistants maternels du particulier employeur"} | ${6}      | ${15}          | ${"jour"}      | ${DepartRetraiteCcReferencesAssistantsMaternels}
+      ${"Assistants maternels du particulier employeur"} | ${12}     | ${1}           | ${"mois"}      | ${DepartRetraiteCcReferencesAssistantsMaternels}
+      ${"Assistants maternels du particulier employeur"} | ${24}     | ${1}           | ${"mois"}      | ${DepartRetraiteCcReferencesAssistantsMaternels}
+    `(
+      "Pour un $category possédant $seniority mois d'ancienneté, son préavis devrait être $expectedResult mois",
+      ({
+        category,
+        seniority,
+        expectedResult,
+        expectedReferences,
+        expectedPeriod,
+      }: InputType) => {
+        const situation = engine.setSituation({
+          "contrat salarié . ancienneté": seniority,
+          "contrat salarié . convention collective": "'IDCC3239'",
+          "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . catégorie professionnelle": `'${category}'`,
+          "contrat salarié . mise à la retraite": "non",
+          "contrat salarié . travailleur handicapé": "non",
+        });
+        const result = situation.evaluate(
+          "contrat salarié . préavis de retraite"
+        );
+        const references = getReferences(situation);
+
+        expect(result.nodeValue).toEqual(expectedResult);
+        expect(result.unit?.numerators).toEqual([expectedPeriod]);
+        expect(result.missingVariables).toEqual({});
+        expect(references).toHaveLength(expectedReferences.length);
+        expect(references).toEqual(expect.arrayContaining(expectedReferences));
+      }
+    );
+  });
+
+  describe("Vérification des mises à la retraite et des références juridiques", () => {
+    test.each`
+      category                                           | seniority | expectedResult | expectedPeriod | expectedReferences
+      ${"Salariés du particulier employeur"}             | ${2}      | ${7}           | ${"jour"}      | ${MiseRetraiteCcReferencesParticulierEmployeurs}
+      ${"Salariés du particulier employeur"}             | ${6}      | ${1}           | ${"mois"}      | ${MiseRetraiteCcReferencesParticulierEmployeurs}
+      ${"Salariés du particulier employeur"}             | ${12}     | ${1}           | ${"mois"}      | ${MiseRetraiteCcReferencesParticulierEmployeurs}
+      ${"Salariés du particulier employeur"}             | ${24}     | ${2}           | ${"mois"}      | ${MiseRetraiteCcReferencesParticulierEmployeurs}
+      ${"Assistants maternels du particulier employeur"} | ${2}      | ${8}           | ${"jour"}      | ${MiseRetraiteCcReferencesAssistantsMaternels}
+      ${"Assistants maternels du particulier employeur"} | ${6}      | ${1}           | ${"mois"}      | ${MiseRetraiteCcReferencesAssistantsMaternels}
+      ${"Assistants maternels du particulier employeur"} | ${12}     | ${1}           | ${"mois"}      | ${MiseRetraiteCcReferencesAssistantsMaternels}
+      ${"Assistants maternels du particulier employeur"} | ${24}     | ${2}           | ${"mois"}      | ${MiseRetraiteCcReferencesAssistantsMaternels}
+    `(
+      "Pour un $category possédant $seniority mois d'ancienneté, son préavis devrait être $expectedResult mois",
+      ({
+        category,
+        seniority,
+        expectedResult,
+        expectedReferences,
+        expectedPeriod,
+      }: InputType) => {
+        const situation = engine.setSituation({
+          "contrat salarié . ancienneté": seniority,
+          "contrat salarié . convention collective": "'IDCC3239'",
+          "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . catégorie professionnelle": `'${category}'`,
+          "contrat salarié . mise à la retraite": "oui",
+          "contrat salarié . travailleur handicapé": "non",
+        });
+        const result = situation.evaluate(
+          "contrat salarié . préavis de retraite"
+        );
+        const references = getReferences(situation);
+
+        expect(result.nodeValue).toEqual(expectedResult);
+        expect(result.unit?.numerators).toEqual([expectedPeriod]);
+        expect(result.missingVariables).toEqual({});
+        expect(references).toHaveLength(expectedReferences.length);
+        expect(references).toEqual(expect.arrayContaining(expectedReferences));
+      }
+    );
+  });
+});
