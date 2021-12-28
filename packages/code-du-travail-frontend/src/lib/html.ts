5cb6094fc453a509faa7870556ca2e81769d4b52
maxgfr <25312957+maxgfr@users.noreply.github.com>
2021-12-28 11:17:52 +0100

    remove unsued code

--- packages/code-du-travail-frontend/src/lib/html.ts
+++ packages/code-du-travail-frontend/src/lib/html.ts
@@ -6,6 +6,10 @@ export const htmlParser = (html: string): string => {
   // FIXME: Remove style from docx converter
   $("style").remove();
 
+  // https://travail-emploi.gouv.fr/le-ministere-en-action/coronavirus-covid-19/questions-reponses-par-theme/article/mesures-de-prevention-dans-l-entreprise-contre-la-covid-19
+  $("button").remove();
+  $(".oembed-source").remove();
+
   $("dl").replaceWith(function () {
     const src = $(this).find("source").attr("srcset");
     return src
