c98826a4b6f9b33ccce942976a656787c82c8705
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2021-12-30 16:02:42 +0100

    fix(react-ui): Update icon (svgr) (#4139)

--- packages/react-ui/src/icons/components/bicolor/Calculator.js
+++ packages/react-ui/src/icons/components/bicolor/Calculator.js
@@ -1,32 +1,31 @@
 import * as React from "react";
+import { memo } from "react";
 
-function SvgCalculator(props) {
-  return (
-    <svg
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 52 52"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M11 2h30a3 3 0 013 3v42a3 3 0 01-3 3H11a3 3 0 01-3-3V24s0-1 1-1 1 1 1 1v23a1 1 0 001 1h30a1 1 0 001-1V5a1 1 0 00-1-1H11a1 1 0 00-1 1v10s0 1-1 1-1-1-1-1V5a3 3 0 013-3z"
-        fill="#4E6896"
-      />
-      <path d="M9 20a1 1 0 100-2 1 1 0 000 2z" fill="#4E6896" />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M14 12a1 1 0 011-1h22a1 1 0 011 1v6a1 1 0 01-1 1H15a1 1 0 01-1-1v-6zm2 5v-4h20v4H16z"
-        fill="#FF7067"
-      />
-      <path
-        d="M15 26a1 1 0 100 2h3a1 1 0 100-2h-3zm10 0a1 1 0 100 2h3a1 1 0 100-2h-3zm8 1a1 1 0 011-1h3a1 1 0 110 2h-3a1 1 0 01-1-1zm-18 6a1 1 0 100 2h3a1 1 0 100-2h-3zm9 1a1 1 0 011-1h3a1 1 0 110 2h-3a1 1 0 01-1-1zm10-1a1 1 0 100 2h3a1 1 0 100-2h-3zm-20 8a1 1 0 011-1h3a1 1 0 110 2h-3a1 1 0 01-1-1zm11-1a1 1 0 100 2h3a1 1 0 100-2h-3zm8 1a1 1 0 011-1h3a1 1 0 110 2h-3a1 1 0 01-1-1z"
-        fill="#FF7067"
-      />
-    </svg>
-  );
-}
+const SvgCalculator = (props) => (
+  <svg
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 52 52"
+    aria-hidden="true"
+    {...props}
+  >
+    <path
+      d="M11 2h30a3 3 0 0 1 3 3v42a3 3 0 0 1-3 3H11a3 3 0 0 1-3-3V24s0-1 1-1 1 1 1 1v23a1 1 0 0 0 1 1h30a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v10s0 1-1 1-1-1-1-1V5a3 3 0 0 1 3-3z"
+      fill="#4E6896"
+    />
+    <path d="M9 20a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" fill="#4E6896" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M14 12a1 1 0 0 1 1-1h22a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1H15a1 1 0 0 1-1-1v-6zm2 5v-4h20v4H16z"
+      fill="#FF7067"
+    />
+    <path
+      d="M15 26a1 1 0 1 0 0 2h3a1 1 0 1 0 0-2h-3zm10 0a1 1 0 1 0 0 2h3a1 1 0 1 0 0-2h-3zm8 1a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1zm-18 6a1 1 0 1 0 0 2h3a1 1 0 1 0 0-2h-3zm9 1a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1zm10-1a1 1 0 1 0 0 2h3a1 1 0 1 0 0-2h-3zm-20 8a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1zm11-1a1 1 0 1 0 0 2h3a1 1 0 1 0 0-2h-3zm8 1a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1z"
+      fill="#FF7067"
+    />
+  </svg>
+);
 
-const MemoSvgCalculator = React.memo(SvgCalculator);
-export default MemoSvgCalculator;
+const Memo = memo(SvgCalculator);
+export default Memo;
--- packages/react-ui/src/icons/components/monochrome/ShareFacebook.js
+++ packages/react-ui/src/icons/components/monochrome/ShareFacebook.js
@@ -1,21 +1,20 @@
 import * as React from "react";
+import { memo } from "react";
 
-function SvgShareFacebook(props) {
-  return (
-    <svg
-      viewBox="0 0 16 16"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M11.7 1H9.6a3.5 3.5 0 00-3.5 3.5v2.1H4v2.8h2.1V15h2.8V9.4H11l.7-2.8H8.9V4.5a.7.7 0 01.7-.7h2.1V1z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
+const SvgShareFacebook = (props) => (
+  <svg
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    {...props}
+  >
+    <path
+      d="M11.7 1H9.6a3.5 3.5 0 0 0-3.5 3.5v2.1H4v2.8h2.1V15h2.8V9.4H11l.7-2.8H8.9V4.5a.7.7 0 0 1 .7-.7h2.1V1z"
+      fill="currentColor"
+    />
+  </svg>
+);
 
-const MemoSvgShareFacebook = React.memo(SvgShareFacebook);
-export default MemoSvgShareFacebook;
+const Memo = memo(SvgShareFacebook);
+export default Memo;
