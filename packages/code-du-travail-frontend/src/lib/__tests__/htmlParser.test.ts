a617e090cedcde4c6b364f2619337a0dc028cec5
carolineBda <caroline.bourdeu@gmail.com>
2022-01-05 12:12:15 +0100

    add more coverage

--- packages/code-du-travail-frontend/src/lib/__tests__/htmlParser.test.ts
+++ packages/code-du-travail-frontend/src/lib/__tests__/htmlParser.test.ts
@@ -2,8 +2,9 @@ import { htmlParser } from "..";
 
 describe("htmlParser", () => {
   test.each`
-    input                       | expected
-    ${"<button>hello</button>"} | ${""}
+    input                                        | expected
+    ${"<button>hello</button>"}                  | ${""}
+    ${'<div class=".oembed-source">hello</div>'} | ${""}
   `("should return $expected for $input", ({ input, expected }) => {
     expect(htmlParser(input)).toBe(expected);
   });
