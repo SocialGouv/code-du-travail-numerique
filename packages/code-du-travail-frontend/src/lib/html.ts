c655f792538c9e69edab784b0e7bac4c6bb7d0f1
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2022-01-07 14:52:32 +0100

    fix(frontend): remove comment for cheerio (#4163)

--- packages/code-du-travail-frontend/src/lib/html.ts
+++ packages/code-du-travail-frontend/src/lib/html.ts
@@ -3,7 +3,6 @@ import cheerio from "cheerio";
 export const htmlParser = (html: string): string => {
   const $ = cheerio.load(html, null, false);
 
-  // FIXME: Remove style from docx converter
   $("style").remove();
 
   // https://travail-emploi.gouv.fr/le-ministere-en-action/coronavirus-covid-19/questions-reponses-par-theme/article/mesures-de-prevention-dans-l-entreprise-contre-la-covid-19
