021ecb538cd6264fc197ee344506e0a7723f2531
Remi Mélisson <rmelisson@gmail.com>
2022-01-27 15:55:27 +0100

    use prod url

--- packages/code-du-travail-frontend/src/conventions/Search/api/enterprises.service.ts
+++ packages/code-du-travail-frontend/src/conventions/Search/api/enterprises.service.ts
@@ -47,8 +47,7 @@ const siretNumberError =
   "Veuillez indiquer un numéro Siret (14 chiffres uniquement)";
 
 const ENTERPRISE_API_URL =
-  // "https://search-recherche-entreprises.fabrique.social.gouv.fr/api/v1";
-  "https://search-recherche-entreprises-remi-siege-n-fields-3x4l.dev.fabrique.social.gouv.fr/api/v1";
+  "https://search-recherche-entreprises.fabrique.social.gouv.fr/api/v1";
 
 const makeSearchUrl = ({ query, address }) => {
   const params: { k: string; v: string }[] = [
