8646178a45eacf02331e3ac2b9fb74833eeb974b
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-02-17 18:53:19 +0100

    fix: rule

--- packages/code-du-travail-modeles/src/__test__/common/extract-implemented-cc.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/common/extract-implemented-cc.spec.ts
@@ -7,8 +7,7 @@ const engine = new Engine(mergeModels());
 
 test("Check all agreements have an cdtn with idcc property", () => {
   Object.values(engine.getParsedRules())
-    .filter((rule) => {
-      // @ts-expect-error
+    .filter((rule: any) => {
       const applicableSi = rule.rawNode["applicable si"];
       if (applicableSi !== undefined && typeof applicableSi === "string") {
         return applicableSi.startsWith("convention collective = 'IDCC");
