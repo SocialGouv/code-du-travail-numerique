fb8a9ed7800b256a67970cf86179375ca9f22df6
carolineBda <caroline.bourdeu@gmail.com>
2022-02-21 10:23:18 +0100

    fix: use official URL for recherche-entreprises

--- packages/code-du-travail-api/src/routes/enterprises/index.ts
+++ packages/code-du-travail-api/src/routes/enterprises/index.ts
@@ -13,7 +13,7 @@ const index = `${ES_INDEX_PREFIX}-${CDTN_ADMIN_VERSION}_${DOCUMENTS}`;
 
 const router = new Router({ prefix: API_BASE_URL });
 const ENTERPRISE_API_URL =
-  "https://search-recherche-entreprises.fabrique.social.gouv.fr/api/v1";
+  "https://api.recherche-entreprises.fabrique.social.gouv.fr/api/v1";
 
 /**
  * Agreement type from @socialgouv/kali-data/data/index.json
