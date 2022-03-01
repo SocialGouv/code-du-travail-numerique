3db18f889ff85eee6b0eacae4eaefc9e4c50f98a
Martial Maillot <martial.maillot@gmail.com>
2022-03-01 14:17:59 +0100

    fix(simulators): information steps always empty

--- packages/code-du-travail-frontend/src/outils/common/__tests__/StepInformations.test.tsx
+++ packages/code-du-travail-frontend/src/outils/common/__tests__/StepInformations.test.tsx
@@ -1,9 +1,13 @@
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import { Form } from "react-final-form";
 
+import { generateAgreement } from "../../../../test/generateAgreement";
+import { EmbeddedInjectedForm } from "../../../../test/TestForm";
 import { MatomoActionEvent } from "../../../lib";
-import { StepInformations } from "../StepInformations";
+import { StepInformations, StepInformationsProps } from "../StepInformations";
+import { FormContent } from "../type/WizardType";
 
 describe("<StepInformations />", () => {
   it("should render", () => {
@@ -18,5 +22,50 @@ describe("<StepInformations />", () => {
       </Form>
     );
     expect(container).toMatchSnapshot();
+  });
+
+  it("should render questions for specific agreement", () => {
+    const { getByText, getAllByRole, getByRole } = render(
+      <EmbeddedInjectedForm<FormContent, Omit<StepInformationsProps, "form">>
+        Step={StepInformations}
+        formData={{
+          ccn: { route: "agreement", selected: generateAgreement(1483) },
+        }}
+        props={{
+          actionEvent: MatomoActionEvent.PREAVIS_DEMISSION,
+        }}
+      />
+    );
+
+    expect(getByText(/Statut du salarié/)).toBeInTheDocument();
+    expect(
+      getByText(/Quelle est la catégorie professionnelle du salarié \?/)
+    ).toBeInTheDocument();
+    expect(getAllByRole("option").length).toBe(4);
+    expect(
+      (getByRole("option", { name: "..." }) as HTMLOptionElement).selected
+    ).toBeTruthy();
+    expect(getByRole("option", { name: "Employés" })).toBeInTheDocument();
+    expect(
+      getByRole("option", { name: "Agents de maîtrise" })
+    ).toBeInTheDocument();
+    expect(getByRole("option", { name: "Cadres" })).toBeInTheDocument();
+    userEvent.selectOptions(
+      // Find the select element
+      getByRole("combobox"),
+      // Find and select the Ireland option
+      getByRole("option", { name: "Employés" })
+    );
+    expect(
+      getByText(/Quelle est l'ancienneté du salarié \?/)
+    ).toBeInTheDocument();
+    expect(getAllByRole("option").length).toBe(7);
+    expect(
+      (getAllByRole("option", { name: "..." })[1] as HTMLOptionElement).selected
+    ).toBeTruthy();
+    expect(
+      getByRole("option", { name: "Moins de 6 mois" })
+    ).toBeInTheDocument();
+    expect(getByRole("option", { name: "Plus de 6 mois" })).toBeInTheDocument();
   });
 });
