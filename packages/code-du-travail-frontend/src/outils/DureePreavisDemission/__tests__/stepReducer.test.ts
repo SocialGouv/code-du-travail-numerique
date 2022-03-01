b6a263b4805df063e73b64c8f8463b2053ae36ec
Martial Maillot <martial.maillot@gmail.com>
2022-03-01 11:11:39 +0100

    fix(simulators): steps skipped on simulators

---
+++ packages/code-du-travail-frontend/src/outils/DureePreavisDemission/__tests__/stepReducer.test.ts
@@ -0,0 +1,89 @@
+import { generateAgreement } from "../../../../test/generateAgreement";
+import { ActionName, SkipFn } from "../../common/type/WizardType";
+import { initialState, stepReducer } from "../stepReducer";
+
+describe("initialSteps", () => {
+  it("should return default steps", () => {
+    expect(initialState.steps.map(({ name }) => ({ name }))).toEqual([
+      { name: "intro" },
+      { name: "info_cc" },
+      { name: "infos" },
+      { name: "results" },
+    ]);
+  });
+  it("handles reset action", () => {
+    expect(
+      stepReducer(
+        {
+          stepIndex: 1,
+          steps: [],
+        },
+        { type: ActionName.reset }
+      )
+    ).toEqual(initialState);
+  });
+  it("handles setIndex action", () => {
+    expect(
+      stepReducer(initialState, { payload: 1, type: ActionName.setStepIndex })
+    ).toEqual({ stepIndex: 1, steps: initialState.steps });
+  });
+
+  describe("Skip the information steps", () => {
+    const informationStep = initialState.steps.filter(
+      (step) => step.name == "infos"
+    )[0];
+
+    it("should contains an information step", () => {
+      expect(informationStep).toBeDefined();
+    });
+
+    it("should define a skip function", () => {
+      expect(informationStep.skip).toBeDefined();
+    });
+
+    const skip = informationStep.skip as SkipFn;
+
+    it(`should not skip the step for the supported agreement 16 with criteria`, () => {
+      expect(
+        skip({
+          ccn: {
+            route: "agreement",
+            selected: generateAgreement(16),
+          },
+        })
+      ).toBeFalsy();
+    });
+
+    it(`should skip the step for the supported agreement 29 without criteria`, () => {
+      expect(
+        skip({
+          ccn: {
+            route: "agreement",
+            selected: generateAgreement(1480),
+          },
+        })
+      ).toBeTruthy();
+    });
+
+    it(`should skip the step for not supported agreement 9999`, () => {
+      expect(
+        skip({
+          ccn: {
+            route: "agreement",
+            selected: generateAgreement(9999),
+          },
+        })
+      ).toBeTruthy();
+    });
+
+    it(`should skip the step for legal`, () => {
+      expect(
+        skip({
+          ccn: {
+            route: "not-selected",
+          },
+        })
+      ).toBeTruthy();
+    });
+  });
+});
