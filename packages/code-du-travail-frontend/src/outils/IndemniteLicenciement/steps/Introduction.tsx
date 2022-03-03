7bf61157e66c58ae024850d0772230a87743c755
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-03 11:59:29 +0100

    fix: intro

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Introduction.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Introduction.tsx
@@ -3,14 +3,13 @@ import React from "react";
 const StepIntro = (): JSX.Element => (
   <>
     <p>
-      Ce simulateur est un outil qui permet d’estimer le montant de l’indemnité
-      minimale de licenciement d’un salarié.
+      Ce simulateur permet d’estimer le montant de l’indemnité légale de
+      licenciement d’un salarié.
     </p>
     <p>
-      Afin de pouvoir remplir les renseignements demandés, munissez-vous des
-      informations relatives au licenciement (dates d’entrée et de sortie de
-      l’entreprise, courrier de licenciement, derniers bulletins de salaire,
-      etc.).
+      Vous aurez besoin d’informations telles que les dates d’entrée et de
+      sortie de l’entreprise, la date de notification du licenciement et le
+      montant des derniers salaires.
     </p>
   </>
 );
