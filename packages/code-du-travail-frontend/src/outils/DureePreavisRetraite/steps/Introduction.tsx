1875a3e826e5e5e08137d665116f0d11aea61e92
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2022-03-07 16:00:23 +0100

    fix(frontend): reduce introduction (#4279)

--- packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/steps/Introduction.tsx
+++ packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/steps/Introduction.tsx
@@ -4,19 +4,11 @@ const IntroductionStep = (): JSX.Element => (
   <>
     <p>
       Ce simulateur vous permet de calculer la durée de préavis à respecter en
-      cas de départ ou de mise à la retraite, telle qu’elle est prévue par la
-      loi et la convention collective applicable.
+      cas de départ ou de mise à la retraite.
     </p>
     <p>
-      En cas de départ ou de mise à la retraite, le salarié ne peut pas quitter
-      l’entreprise immédiatement. Il doit y rester durant une certaine période
-      (appelée préavis ou parfois délai congé).
-    </p>
-    <p>
-      Vous aurez besoin d’informations concernant la situation du salarié, comme
-      sa convention collective, sa catégorie ou son coefficient. La plupart de
-      ces informations se trouvent sur le bulletin de salaire ou le contrat de
-      travail.
+      Des informations disponibles sur le bulletin de salaire pourraient être
+      utiles pour réaliser cette simulation.
     </p>
   </>
 );
