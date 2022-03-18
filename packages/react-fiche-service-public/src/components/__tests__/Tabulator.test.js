7c06d63a85050bc36717291f6e5df726274022b4
carolineBda <caroline.bourdeu@gmail.com>
2022-03-18 15:46:52 +0100

    fix(hn in tabs): when tabs do not have headings level as parameter, then starts at H2 not H1

--- packages/react-fiche-service-public/src/components/__tests__/Tabulator.test.js
+++ packages/react-fiche-service-public/src/components/__tests__/Tabulator.test.js
@@ -1,24 +1,25 @@
-import React from "react";
 import { render } from "@testing-library/react";
+import React from "react";
+
 import Tabulator from "../Tabulator";
 import tabulatorDataMock from "./mocks/tabulatorData.json";
 
 describe("<Tabulator />", () => {
   it("should have two different levels of headings", () => {
     const { container } = render(
-      <Tabulator data={tabulatorDataMock} headingLevel={0} />,
+      <Tabulator data={tabulatorDataMock} headingLevel={0} />
     );
     expect(container).toMatchSnapshot();
   });
   it("should incease heading levels if not 0", () => {
     const { container } = render(
-      <Tabulator data={tabulatorDataMock} headingLevel={1} />,
+      <Tabulator data={tabulatorDataMock} headingLevel={1} />
     );
     expect(container).toMatchSnapshot();
   });
   it("should render", () => {
     const { container } = render(
-      <Tabulator data={tabulatorDataMock} headingLevel={2} />,
+      <Tabulator data={tabulatorDataMock} headingLevel={2} />
     );
     expect(container).toMatchSnapshot();
   });
