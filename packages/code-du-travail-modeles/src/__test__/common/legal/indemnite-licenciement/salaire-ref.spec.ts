bb7731d6c35b80b358beaa6ca610a2abd5155d4c
carolineBda <caroline.bourdeu@gmail.com>
2022-03-24 14:07:23 +0100

    Handle specific cases for CC

--- packages/code-du-travail-modeles/src/__test__/common/legal/indemnite-licenciement/salaire-ref.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/common/legal/indemnite-licenciement/salaire-ref.spec.ts
@@ -62,4 +62,51 @@ describe("Salaire de référénce pour un employé", () => {
       expect(result.unit?.numerators).toEqual(["€"]);
     }
   );
+  test.each`
+    salary1 | salary2 | salary3 | salaryPreavis1 | salaryPreavis2 | salaryPreavis3 | preavis  | salaireRef
+    ${0}    | ${0}    | ${0}    | ${0}           | ${0}           | ${0}           | ${"non"} | ${0}
+    ${1000} | ${2000} | ${3000} | ${0}           | ${0}           | ${0}           | ${"non"} | ${2000}
+    ${1000} | ${1000} | ${1000} | ${0}           | ${0}           | ${0}           | ${"non"} | ${1000}
+    ${1000} | ${1000} | ${1000} | ${4000}        | ${0}           | ${0}           | ${"oui"} | ${2000}
+    ${1000} | ${1000} | ${1000} | ${4000}        | ${4000}        | ${0}           | ${"oui"} | ${3000}
+    ${1000} | ${1000} | ${1000} | ${7000}        | ${7000}        | ${7000}        | ${"oui"} | ${7000}
+  `(
+    "POC avec préavis: $preavis. SalaireRef: $salaireRef",
+    ({
+      salary1,
+      salary2,
+      salary3,
+      salaryPreavis1,
+      salaryPreavis2,
+      salaryPreavis3,
+      preavis,
+      salaireRef,
+    }) => {
+      const result = engine
+        .setSituation({
+          "contrat salarié . convention collective": "''",
+          "contrat salarié . indemnité de licenciement . moyenne des salaires des 3 derniers mois si préavis . préavis":
+            preavis,
+          "contrat salarié . indemnité de licenciement . moyenne des salaires des 3 derniers mois si préavis . préavis . salaire du mois 1":
+            salaryPreavis1,
+          "contrat salarié . indemnité de licenciement . moyenne des salaires des 3 derniers mois si préavis . préavis . salaire du mois 2":
+            salaryPreavis2,
+          "contrat salarié . indemnité de licenciement . moyenne des salaires des 3 derniers mois si préavis . préavis . salaire du mois 3":
+            salaryPreavis3,
+          "contrat salarié . indemnité de licenciement . même salaire sur les 12 derniers mois":
+            "non",
+          "contrat salarié . indemnité de licenciement . salaire du mois 1":
+            salary1,
+          "contrat salarié . indemnité de licenciement . salaire du mois 2":
+            salary2,
+          "contrat salarié . indemnité de licenciement . salaire du mois 3":
+            salary3,
+        })
+        .evaluate(
+          "contrat salarié . indemnité de licenciement . moyenne des salaires des 3 derniers mois si préavis"
+        );
+      expect(result.nodeValue).toEqual(salaireRef);
+      expect(result.unit?.numerators).toEqual(["€"]);
+    }
+  );
 });
