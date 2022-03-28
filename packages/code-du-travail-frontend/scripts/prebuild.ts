f86a508e25209ce055c774fa8da6967ffd508256
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-28 20:16:58 +0200

    fix: robots prod

--- packages/code-du-travail-frontend/scripts/prebuild.ts
+++ packages/code-du-travail-frontend/scripts/prebuild.ts
@@ -3,14 +3,14 @@ import fs from "fs";
 
 export const filePath = path.join(__dirname, "../public/robots.txt");
 
-export const generateRobotsTxt = (isOnProduction: boolean, host: string) => {
+export const generateRobotsTxt = (isOnProduction: boolean) => {
   const robotsDev = ["User-agent: *", "Disallow: /"].join("\n");
   const robotsProd = [
     "User-agent: *",
     "Disallow: /assets/",
     "Disallow: /images/",
     "",
-    `Sitemap: https://${host}/sitemap.xml`,
+    `Sitemap: https://code.travail.gouv.fr/sitemap.xml`,
   ].join("\n");
 
   const robot = isOnProduction ? robotsProd : robotsDev;
@@ -20,8 +20,7 @@ export const generateRobotsTxt = (isOnProduction: boolean, host: string) => {
 
 const run = () => {
   generateRobotsTxt(
-    process.env.NEXT_PUBLIC_IS_PRODUCTION_DEPLOYMENT ? true : false,
-    process.env.FRONTEND_HOST ?? "localhost"
+    process.env.NEXT_PUBLIC_IS_PRODUCTION_DEPLOYMENT ? true : false
   );
   console.log("Robots.txt generated.");
 };
