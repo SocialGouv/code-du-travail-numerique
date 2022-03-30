d78c908acaaefd4eee5750d68220a8c84181d727
Caroline <4971715+carolineBda@users.noreply.github.com>
2022-03-30 10:08:28 +0200

    feat(sitemap): serve sitemap directly under /sitemap.xml (to avoid redirect and avoid having 2 duplicated content served under 2 differents url (#4329)

---
+++ packages/code-du-travail-frontend/pages/sitemap.xml.ts
@@ -0,0 +1,35 @@
+import http from "http";
+import https from "https";
+import getConfig from "next/config";
+
+const {
+  publicRuntimeConfig: {
+    AZURE_BASE_URL = "https://cdtnadminprod.blob.core.windows.net",
+  },
+} = getConfig();
+const Sitemap = () => {};
+
+export const getServerSideProps = async ({ res }) => {
+  return new Promise(function SitemapStream(resolve, reject) {
+    const get = AZURE_BASE_URL.startsWith("https") ? https.get : http.get;
+    const sitempaReq = get(
+      `${AZURE_BASE_URL}/sitemap/sitemap.xml`,
+      (response) => {
+        response.pipe(res);
+        response.on("end", () => {
+          resolve({
+            props: {},
+          });
+        });
+      }
+    );
+
+    sitempaReq.on("error", (error) => {
+      console.error(error);
+      reject();
+      res.status(500).json({ error, message: "stream error", statusCode: 500 });
+    });
+  });
+};
+
+export default Sitemap;
