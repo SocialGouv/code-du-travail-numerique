aa831f2796b24c24c5ff510fa19490947e2fa874
Martial Maillot <martial.maillot@gmail.com>
2022-04-07 16:25:55 +0200

    fix: compilation issue

--- packages/code-du-travail-modeles/src/utils/constants.ts
+++ packages/code-du-travail-modeles/src/utils/constants.ts
@@ -1,6 +1,8 @@
+import ccnSupported from "./ccn-supported.json";
+
 export type AgreementInfo = {
   idcc: number;
   preavisRetraite: boolean;
 };
 
-export const supportedCcn: AgreementInfo[] = [];
+export const supportedCcn: AgreementInfo[] = ccnSupported;
