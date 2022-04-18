e14de5f35444b5da8d04ac89e779f6b15748a4c8
Martial Maillot <martial.maillot@gmail.com>
2022-04-18 20:44:30 +0200

    feat(simulators): Extraction du composant décorateur du simulateur (#4382)
    * feat(simulators): Extraction du composant décorateur du simulateur
    
    * feat(simulators): correction suite aux retours

---
+++ packages/code-du-travail-frontend/src/outils/Components/SimulatorDecorator/__tests__/SimulatorDecorator.test.tsx
@@ -0,0 +1,43 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import SimulatorDecorator from "../index";
+import { FormApi } from "final-form";
+
+type FormState = {
+  name?: string;
+};
+
+const callback: (values: FormState, form: FormApi<FormState>) => void = () => {
+  /* nothing to do */
+};
+const onFormStepSubmit = jest.fn(callback);
+
+describe("SimulatorDecorator", () => {
+  describe("Render a step", () => {
+    it("should show all information provided to the decorator", () => {
+      const { getByText } = render(
+        <SimulatorDecorator<FormState>
+          initialValues={{ name: "John" }}
+          title={{ title: "Titre du simulateur" }}
+          navigation={{ showNext: true }}
+          steps={{
+            activeIndex: 0,
+            steps: [],
+          }}
+          onFormStepSubmit={onFormStepSubmit}
+          showDebug={false}
+          renderStep={(values) => <>Nom: {values.getState().values.name}</>}
+          annotations={<>Rendu des annotations</>}
+        />
+      );
+      expect(getByText(/Nom: John/)).toBeInTheDocument();
+      expect(getByText(/Rendu des annotations/)).toBeInTheDocument();
+      expect(getByText(/Titre du simulateur/)).toBeInTheDocument();
+
+      const startButton = getByText(/Commencer/);
+      expect(startButton).toBeInTheDocument();
+      startButton.click();
+      expect(onFormStepSubmit).toBeCalledTimes(1);
+    });
+  });
+});
