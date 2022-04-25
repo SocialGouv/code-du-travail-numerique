6c5d11bd84e6ec37922d5d99b8b66b1ace134094
Caroline <4971715+carolineBda@users.noreply.github.com>
2022-04-25 16:21:34 +0200

    Update packages/code-du-travail-frontend/src/outils/DureePreavisDemission/steps/__tests__/Agreement.test.tsx
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>

--- packages/code-du-travail-frontend/src/outils/DureePreavisDemission/steps/__tests__/Agreement.test.tsx
+++ packages/code-du-travail-frontend/src/outils/DureePreavisDemission/steps/__tests__/Agreement.test.tsx
@@ -5,7 +5,7 @@ describe("<AgreementStep />", () => {
   it("should render", () => {
     const { getAllByRole, queryByText } = renderForm(AgreementStep);
 
-    expect(getAllByRole("radio").length).toBe(2);
+    expect(getAllByRole("radio")).toHaveLength(2);
     expect(queryByText(/Je ne souhaite pas renseigner/)).not.toBeInTheDocument();
     expect(
       queryByText(/La convention collective est nécessaire pour obtenir/)
