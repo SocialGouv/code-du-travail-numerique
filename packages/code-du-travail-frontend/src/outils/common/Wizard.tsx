7dd7c8a7d6d3a603622cc5beac08682184047468
carolineBda <caroline.bourdeu@gmail.com>
2022-04-27 17:27:11 +0200

    refactor

--- packages/code-du-travail-frontend/src/outils/common/Wizard.tsx
+++ packages/code-du-travail-frontend/src/outils/common/Wizard.tsx
@@ -63,9 +63,7 @@ function Wizard({
       `click_previous_${title}`,
       state.steps[nextStepIndex].name,
     ]);
-    if (window) {
-      window.scrollTo(0, 0);
-    }
+    window?.scrollTo(0, 0);
   };
   const nextStep = (values) => {
     let nextStepIndex = stepIndex;
@@ -85,9 +83,7 @@ function Wizard({
       `view_step_${title}`,
       state.steps[nextStepIndex].name,
     ]);
-    if (window) {
-      window.scrollTo(0, 0);
-    }
+    window?.scrollTo(0, 0);
   };
 
   const previousVisible = stepIndex > 0;
