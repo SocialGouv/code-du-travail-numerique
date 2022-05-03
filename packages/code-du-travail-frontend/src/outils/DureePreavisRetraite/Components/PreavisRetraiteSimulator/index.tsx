264b5597164e12c75b66e9b8d4dabb19fbc3ffcc
Martial Maillot <martial.maillot@gmail.com>
2022-05-03 09:47:05 +0200

    feat(simulators): show debug info only in dev env

--- packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/Components/PreavisRetraiteSimulator/index.tsx
+++ packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/Components/PreavisRetraiteSimulator/index.tsx
@@ -1,19 +1,17 @@
-import { DebugInfo } from "../index";
+import {DebugInfo} from "../index";
 import React from "react";
-import { usePreavisRetraiteStore } from "../../state";
-import { Simulator } from "../../../Simulator";
-import { StepName } from "../../steps";
-import { PreavisRetraiteFormState } from "../../form";
-import { steps } from "./steps";
+import {usePreavisRetraiteStore} from "../../state";
+import {Simulator} from "../../../Simulator";
+import {StepName} from "../../steps";
+import {PreavisRetraiteFormState} from "../../form";
+import {steps} from "./steps";
 
-const PreavisRetraiteSimulator = ({
-  title,
-  icon,
-}: {
+type Props = {
   icon: string;
   title: string;
-}): JSX.Element => {
-  const { onChange, onStepChange } = usePreavisRetraiteStore((state) => ({
+}
+const PreavisRetraiteSimulator = ({title, icon,}: Props): JSX.Element => {
+  const {onChange, onStepChange} = usePreavisRetraiteStore((state) => ({
     onChange: state.onFormValuesChange,
     onStepChange: state.onStepChange,
   }));
@@ -23,7 +21,13 @@ const PreavisRetraiteSimulator = ({
       title={title}
       icon={icon}
       duration="5 min"
-      debug={<DebugInfo />}
+      debug={
+        process.env.NODE_ENV !== "production" &&
+        process.env.NODE_ENV !== "test" ? (
+          <DebugInfo/>
+        ) : (
+          <></>
+        )}
       onFormValuesChange={onChange}
       onStepChange={onStepChange}
       steps={steps}
