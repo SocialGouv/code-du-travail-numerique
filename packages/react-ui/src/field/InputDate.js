a283c4185e24cac291b05c9eb32b5e5c82830f41
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-05-16 14:50:21 +0200

    fix: job

--- packages/react-ui/src/field/InputDate.js
+++ packages/react-ui/src/field/InputDate.js
@@ -6,7 +6,6 @@ import { box, breakpoints, fonts, spacings } from "../theme.js";
 import { INPUT_HEIGHT } from "./Input";
 
 export const InputDate = ({ value, invalid, onChange, ...props }) => {
-  const inputRef = React.createRef();
   const [date, setDate] = React.useState(value ?? "");
   const [isValid, setIsValid] = React.useState(true ?? !invalid);
   const [isFocus, setIsFocus] = React.useState(false);
@@ -26,22 +25,29 @@ export const InputDate = ({ value, invalid, onChange, ...props }) => {
     } else if (newValue.length <= 10) {
       setDate(newValue);
     }
-    const splitParts = newValue.split("/");
-    const day = isNaN(Number(splitParts[0])) ? null : Number(splitParts[0]);
-    const month = isNaN(Number(splitParts[1])) ? null : Number(splitParts[1]);
-    const year = isNaN(Number(splitParts[2])) ? null : Number(splitParts[2]);
-    const isYearValid = year && year >= 1900 && year <= 2100;
-    const isMonthValid = month && month >= 1 && month <= 12;
-    const isDayValid = day && day >= 1 && day <= 31;
-    const isValidDate = /^\d{2}\/\d{2}\/\d{4}$/.test(newValue);
-    const isValid = isYearValid && isMonthValid && isDayValid && isValidDate;
-    setIsValid(isValid);
+    if (newValue.length <= 10) {
+      const splitParts = newValue.split("/");
+      const day = isNaN(Number(splitParts[0])) ? null : Number(splitParts[0]);
+      const month = isNaN(Number(splitParts[1])) ? null : Number(splitParts[1]);
+      const year = isNaN(Number(splitParts[2])) ? null : Number(splitParts[2]);
+      const isYearValid = year && year >= 1900 && year <= 2100;
+      const isMonthValid = month && month >= 1 && month <= 12;
+      const isDayValid = day && day >= 1 && day <= 31;
+      const isValidDate = /^\d{2}\/\d{2}\/\d{4}$/.test(newValue);
+      const isValid = isYearValid && isMonthValid && isDayValid && isValidDate;
+      setIsValid(isValid);
+    }
     if (onChange) onChange(newValue);
   };
 
-  const onClickDatePicker = () => {
-    console.log(inputRef.current);
-    inputRef.current.datepicker();
+  const onChangeDatePicker = (event) => {
+    const date = event.target.value;
+    const realDate = date.split("-");
+    const year = realDate[0] ?? "";
+    const month = realDate[1] ?? "";
+    const day = realDate[2] ?? "";
+    const newValue = `${day}/${month}/${year}`;
+    setDate(newValue);
   };
 
   const formatDate = () => {
@@ -63,17 +69,13 @@ export const InputDate = ({ value, invalid, onChange, ...props }) => {
         onBlur={() => setIsFocus(false)}
         {...props}
       />
-      <StyledDiv onClick={onClickDatePicker}>
-        <DatePickerIcon width={spacings.larger} height={spacings.larger} />
-      </StyledDiv>
-      <InvisibleInputDate
+      <DatePickerInputDate
         type="date"
-        placeholder="JJ/MM/AAAA"
         min="1900-01-01"
         max="2100-01-01"
         maxlength="11"
         value={formatDate()}
-        ref={inputRef}
+        onChange={onChangeDatePicker}
       />
     </StyledWrapper>
   );
@@ -91,9 +93,9 @@ InputDate.propTypes = {
 const StyledWrapper = styled.div`
   display: flex;
   flex-direction: row;
-
-  width: 100%;
-  max-width: fit-content;
+  @media (max-width: ${breakpoints.mobile}) {
+    width: 100%;
+  }
   height: ${INPUT_HEIGHT};
 
   background: ${({ theme }) => theme.white};
@@ -115,13 +117,6 @@ const StyledWrapper = styled.div`
   }
 `;
 
-const StyledDiv = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-`;
-
 const StyledInput = styled.input`
   padding: 0 ${spacings.medium};
   padding-right: ${(props) => (props.hasIcon ? "5rem" : spacings.medium)};
@@ -133,16 +128,19 @@ const StyledInput = styled.input`
   border-color: transparent;
   outline: none;
   border-radius: ${box.borderRadius};
-  width: 130px;
+  width: 160px;
+  @media (max-width: ${breakpoints.mobile}) {
+    width: 100%;
+  }
 
   &::placeholder {
     color: ${({ theme }) => theme.placeholder};
   }
 `;
 
-const InvisibleInputDate = styled(StyledInput)`
-  display: block;
-  width: 60px;
+const DatePickerInputDate = styled(StyledInput)`
+  width: 40px;
+  padding: 0;
   &::-webkit-outer-spin-button,
   &::-webkit-inner-spin-button {
     margin: 0;
@@ -153,27 +151,11 @@ const InvisibleInputDate = styled(StyledInput)`
     display: block;
     width: ${spacings.large};
     height: ${spacings.large};
-    margin-right: -${spacings.medium};
     color: rgba(0, 0, 0, 0);
     background-color: ${({ theme }) => theme.placeholder};
     cursor: pointer;
     opacity: 1;
     mask-image: url("data:image/svg+xml;,${encodeURIComponent(iconDateSvg)}");
+    mask-repeat: no-repeat;
   }
 `;
-
-const DatePickerIcon = (props) => (
-  <svg
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 32 32"
-    {...props}
-  >
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M20.947 7.601h1.601c1.472 0 2.521 1.198 2.521 2.669v2.306l-.001 10.392c0 1.472-1.049 2.669-2.521 2.669H8.669A2.672 2.672 0 0 1 6 22.967l.001-12.697a2.672 2.672 0 0 1 2.67-2.669h1.331V6h.999v1.601H20V6h.947v1.601zm1.6 17.036c.883 0 1.454-.786 1.454-1.67v-10.33h-17L7 22.967c0 .884.785 1.67 1.668 1.67h13.879zm-15.548-13h17.002l.001-1.367c0-.883-.57-1.601-1.453-1.601h-1.6v1.068H20V8.669h-9v1.068h-1V8.669H8.669C7.786 8.669 7 9.387 7 10.27v1.367zm9.677 6.277h-2.135a1.07 1.07 0 0 1-1.068-1.068v-2.135a1.07 1.07 0 0 1 1.068-1.068h2.135a1.07 1.07 0 0 1 1.068 1.068v2.135a1.07 1.07 0 0 1-1.068 1.068zm0-3.203h-2.135v2.135h2.136v-2.135zm3.203 3.203h2.135a1.07 1.07 0 0 0 1.068-1.068v-2.135a1.07 1.07 0 0 0-1.068-1.068H19.88a1.07 1.07 0 0 0-1.068 1.068v2.135a1.07 1.07 0 0 0 1.068 1.068zm0-3.203h2.135l.001 2.135H19.88v-2.135zm-8.54 8.541H9.204a1.07 1.07 0 0 1-1.068-1.068v-2.135a1.07 1.07 0 0 1 1.068-1.068h2.135a1.07 1.07 0 0 1 1.068 1.068v2.135a1.07 1.07 0 0 1-1.068 1.068zm0-3.203H9.204v2.135h2.136l-.001-2.135zm3.202 3.203h2.135a1.07 1.07 0 0 0 1.068-1.068v-2.135a1.07 1.07 0 0 0-1.068-1.068h-2.135a1.07 1.07 0 0 0-1.068 1.068v2.135a1.07 1.07 0 0 0 1.068 1.068zm0-3.203h2.135l.001 2.135h-2.136v-2.135zm7.473 3.203H19.88a1.07 1.07 0 0 1-1.068-1.068v-2.135a1.07 1.07 0 0 1 1.068-1.068h2.135a1.07 1.07 0 0 1 1.068 1.068v2.135a1.07 1.07 0 0 1-1.068 1.068zm0-3.203H19.88v2.135h2.136v-2.135z"
-      fill="currentColor"
-    />
-  </svg>
-);
