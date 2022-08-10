35addc5bde44bd33ed053b551346dfddfe6cef54
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2022-08-10 16:45:32 +0200

    feat(indemnite-licenciement): ajout de la CC 1979 (#4511)

--- packages/code-du-travail-modeles/src/plugins/types.ts
+++ packages/code-du-travail-modeles/src/plugins/types.ts
@@ -2,6 +2,7 @@ export enum SupportedCcIndemniteLicenciement {
   IDCC1516 = "IDCC1516",
   IDCC2511 = "IDCC2511",
   IDCC2264 = "IDCC2264",
+  IDCC1979 = "IDCC1979",
   default = "default",
   legal = "legal",
 }
