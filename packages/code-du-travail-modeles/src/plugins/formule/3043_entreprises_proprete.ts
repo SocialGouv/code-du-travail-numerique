2817f0ae4b5481a3ad70512bfd30b19fa457edf6
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-08-10 16:18:18 +0200

    fix: cc

---
+++ packages/code-du-travail-modeles/src/plugins/formule/3043_entreprises_proprete.ts
@@ -0,0 +1,49 @@
+import { round } from "../../utils";
+import type { SupportedCcIndemniteLicenciement } from "..";
+import type { Formula, FormulaProps, IFormula } from "./types";
+
+export class Formula3043
+  implements IFormula<SupportedCcIndemniteLicenciement.IDCC3043>
+{
+  computeFormula({
+    seniority,
+    refSalary,
+  }: FormulaProps<SupportedCcIndemniteLicenciement.IDCC3043>): Formula {
+    let formula = "";
+    const explanations = [];
+    const an = round(seniority) < 2 ? "an" : "ans";
+    if (seniority > 2) {
+      if (seniority < 6) {
+        formula = `1 / 10 * Sref * A`;
+        explanations.push(`A : Ancienneté totale (${round(seniority)} ${an})`);
+      } else if (seniority <= 10) {
+        formula = `(1/10*Sref*A1) + (1/6*Sref*A2)`;
+        const anWithout = round(seniority - 5) < 2 ? "an" : "ans";
+        explanations.push(
+          `A1: Années d'ancienneté pour la fraction des 5 premières années (5 ans)`
+        );
+        explanations.push(
+          `A2 : Années d'ancienneté pour la fraction de 6 ans à 10 ans révolus (${round(
+            seniority - 5
+          )} ${anWithout})`
+        );
+      } else {
+        formula = `(1/10*Sref*A1) + (1/6*Sref*A2) + (1/5*Sref*A3)`;
+        const anWithout = round(seniority - 10) < 2 ? "an" : "ans";
+        explanations.push(
+          `A1: Années d'ancienneté pour la fraction des 5 premières années (5 ans)`
+        );
+        explanations.push(
+          `A2 : Années d'ancienneté pour la fraction de 6 ans à 10 ans révolus (5 ans)`
+        );
+        explanations.push(
+          `A3 : Années d'ancienneté au-delà de 10 ans révolus (${round(
+            seniority - 10
+          )} ${anWithout})`
+        );
+      }
+      explanations.push(`Sref : Salaire de référence (${round(refSalary)} €)`);
+    }
+    return { explanations, formula };
+  }
+}
--- packages/code-du-travail-modeles/src/plugins/formule/index.ts
+++ packages/code-du-travail-modeles/src/plugins/formule/index.ts
@@ -2,6 +2,7 @@ import { SupportedCcIndemniteLicenciement } from "../types";
 import { Formula1516 } from "./1516_organismes_formation";
 import { Formula1979 } from "./1979_hotels_cafes_restaurants";
 import { Formula2264 } from "./2264_hospitalisation_privee";
+import { Formula3043 } from "./3043_entreprises_proprete";
 import { FormulaLegal } from "./legal";
 import type { IFormula } from "./types";
 
@@ -14,6 +15,8 @@ export class FormuleFactory {
         return new Formula2264() as IFormula<T>;
       case SupportedCcIndemniteLicenciement.IDCC1979:
         return new Formula1979() as IFormula<T>;
+      case SupportedCcIndemniteLicenciement.IDCC3043:
+        return new Formula3043() as IFormula<T>;
       case SupportedCcIndemniteLicenciement.default:
       default:
         return new FormulaLegal() as IFormula<T>;
