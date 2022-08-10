48792840ff897f4d0ad24ec70f2cfbbe9418aaa6
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-08-10 17:25:27 +0200

    fix

--- packages/code-du-travail-modeles/src/plugins/formule/3043_entreprises_proprete.ts
+++ packages/code-du-travail-modeles/src/plugins/formule/3043_entreprises_proprete.ts
@@ -11,25 +11,27 @@ export class Formula3043
   }: FormulaProps<SupportedCcIndemniteLicenciement.IDCC3043>): Formula {
     let formula = "";
     const explanations = [];
-    const an = round(seniority) < 2 ? "an" : "ans";
+    const year = round(seniority) < 2 ? "an" : "ans";
     if (seniority > 2) {
       if (seniority < 6) {
         formula = `1 / 10 * Sref * A`;
-        explanations.push(`A : Ancienneté totale (${round(seniority)} ${an})`);
+        explanations.push(
+          `A : Ancienneté totale (${round(seniority)} ${year})`
+        );
       } else if (seniority <= 10) {
         formula = `(1 / 10 * Sref * A1) + (1 / 6 * Sref * A2)`;
-        const anWithout = round(seniority - 5) < 2 ? "an" : "ans";
+        const yearAfterDiff = round(seniority - 5) < 2 ? "an" : "ans";
         explanations.push(
           `A1: Années d'ancienneté pour la fraction des 5 premières années (5 ans)`
         );
         explanations.push(
           `A2 : Années d'ancienneté pour la fraction de 6 ans à 10 ans révolus (${round(
             seniority - 5
-          )} ${anWithout})`
+          )} ${yearAfterDiff})`
         );
       } else {
         formula = `(1 / 10 * Sref * A1) + (1 / 6 * Sref * A2) + (1 / 5 * Sref * A3)`;
-        const anWithout = round(seniority - 10) < 2 ? "an" : "ans";
+        const yearAfterDiff = round(seniority - 10) < 2 ? "an" : "ans";
         explanations.push(
           `A1: Années d'ancienneté pour la fraction des 5 premières années (5 ans)`
         );
@@ -39,7 +41,7 @@ export class Formula3043
         explanations.push(
           `A3 : Années d'ancienneté au-delà de 10 ans révolus (${round(
             seniority - 10
-          )} ${anWithout})`
+          )} ${yearAfterDiff})`
         );
       }
       explanations.push(`Sref : Salaire de référence (${round(refSalary)} €)`);
