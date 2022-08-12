905ce767d214cfa9315b8c669778c98a6301a0e8
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-08-12 11:44:30 +0200

    feat: add cc

---
+++ packages/code-du-travail-modeles/src/plugins/anciennete/1090_automobiles.ts
@@ -0,0 +1,38 @@
+import { differenceInMonths, parse } from "date-fns";
+
+import type { SupportedCcIndemniteLicenciement } from "..";
+import type { ISeniority, Motif, SeniorityProps } from "./types";
+
+export class Seniority1090
+  implements ISeniority<SupportedCcIndemniteLicenciement.default>
+{
+  protected motifs: Motif[];
+
+  constructor(motifs: Motif[]) {
+    this.motifs = motifs;
+  }
+
+  computeSeniority({
+    dateEntree,
+    dateSortie,
+    absencePeriods = [],
+  }: SeniorityProps<SupportedCcIndemniteLicenciement.default>): number {
+    const dEntree = parse(dateEntree, "dd/MM/yyyy", new Date());
+    const dSortie = parse(dateSortie, "dd/MM/yyyy", new Date());
+    const totalAbsence =
+      absencePeriods
+        .filter((period) => Boolean(period.durationInMonth))
+        .reduce((total, item) => {
+          const m = this.motifs.find((motif) => motif.label === item.motif);
+          if (
+            !m ||
+            !item.durationInMonth ||
+            (m.key === "absenceMaladieNonPro" && item.durationInMonth > 6)
+          ) {
+            return total;
+          }
+          return total + item.durationInMonth * m.value;
+        }, 0) / 12;
+    return differenceInMonths(dSortie, dEntree) / 12 - totalAbsence;
+  }
+}
