6b3f5798313a4a82cc99cfa57cbf520c98c1c6db
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-08-12 17:01:47 +0200

    fix: ca marche

--- packages/code-du-travail-modeles/src/__test__/413-handicap/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/413-handicap/indemnite-licenciement/indemnite.spec.ts
@@ -13,12 +13,16 @@ describe("Indemnité conventionnel de licenciement pour la CC 413", () => {
       ${"Non-cadres"}                                                                                                                  | ${1.75}    | ${2000}    | ${0}
       ${"Non-cadres"}                                                                                                                  | ${2}       | ${2100}    | ${2100}
       ${"Non-cadres"}                                                                                                                  | ${15}      | ${2100}    | ${12600}
+      ${"Non-cadres"}                                                                                                                  | ${9}       | ${3650}    | ${16425}
+      ${"Non-cadres"}                                                                                                                  | ${100}     | ${3650}    | ${21900}
       ${"Cadres"}                                                                                                                      | ${0}       | ${2000}    | ${0}
       ${"Cadres"}                                                                                                                      | ${23 / 12} | ${2000}    | ${0}
       ${"Cadres"}                                                                                                                      | ${2}       | ${2000}    | ${0}
       ${"Cadres"}                                                                                                                      | ${25 / 12} | ${2000}    | ${4166.67}
       ${"Cadres"}                                                                                                                      | ${3}       | ${3650}    | ${10950}
+      ${"Cadres"}                                                                                                                      | ${4}       | ${3650}    | ${14600}
       ${"Cadres"}                                                                                                                      | ${14}      | ${3650}    | ${43800}
+      ${"Cadres"}                                                                                                                      | ${100}     | ${3650}    | ${43800}
       ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${0}       | ${2000}    | ${0}
       ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${23 / 12} | ${2000}    | ${0}
       ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${2}       | ${2000}    | ${0}
@@ -26,6 +30,7 @@ describe("Indemnité conventionnel de licenciement pour la CC 413", () => {
       ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${3}       | ${2000}    | ${6000}
       ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${10}      | ${3633.33} | ${36333.3}
       ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${14}      | ${2000}    | ${28000}
+      ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${40}      | ${2000}    | ${36000}
     `(
       "ancienneté: $seniority an, salaire de référence: $salary, => $expectedCompensation €",
       ({ seniority, salary, expectedCompensation, category }) => {
@@ -53,31 +58,26 @@ describe("Indemnité conventionnel de licenciement pour la CC 413", () => {
 
   describe("Cas mixte", () => {
     test.each`
-      seniorityNonCadres | category                                                                                                                         | seniority  | salary     | expectedCompensation
-      ${0}               | ${"Cadres"}                                                                                                                      | ${0}       | ${2000}    | ${0}
-      ${0}               | ${"Cadres"}                                                                                                                      | ${23 / 12} | ${2000}    | ${0}
-      ${0}               | ${"Cadres"}                                                                                                                      | ${2}       | ${2000}    | ${0}
-      ${0}               | ${"Cadres"}                                                                                                                      | ${25 / 12} | ${2000}    | ${4166.67}
-      ${0}               | ${"Cadres"}                                                                                                                      | ${3}       | ${3650}    | ${10950}
-      ${0}               | ${"Cadres"}                                                                                                                      | ${14}      | ${3650}    | ${43800}
-      ${0}               | ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${0}       | ${2000}    | ${0}
-      ${0}               | ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${23 / 12} | ${2000}    | ${0}
-      ${0}               | ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${2}       | ${2000}    | ${0}
-      ${0}               | ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${25 / 12} | ${2000}    | ${4166.67}
-      ${0}               | ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${3}       | ${2000}    | ${6000}
-      ${0}               | ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${10}      | ${3633.33} | ${36333.3}
-      ${0}               | ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${14}      | ${2000}    | ${28000}
+      seniorityNonCadres | category                                                                                                                         | seniority | salary     | expectedCompensation
+      ${9}               | ${"Cadres"}                                                                                                                      | ${13}     | ${3650}    | ${31025}
+      ${10}              | ${"Cadres"}                                                                                                                      | ${25}     | ${3650}    | ${73000}
+      ${4}               | ${"Cadres directeurs généraux, directeurs de centre de formation en travail social et directeurs d'établissement ou de service"} | ${8}      | ${3633.33} | ${21799.98}
     `(
       "ancienneté: $seniority an, salaire de référence: $salary, seniorityNonCadres: $seniorityNonCadres => $expectedCompensation €",
-      ({ seniority, salary, expectedCompensation, seniorityNonCadres }) => {
+      ({
+        seniority,
+        salary,
+        expectedCompensation,
+        seniorityNonCadres,
+        category,
+      }) => {
         const result = engine
           .setSituation({
-            "contrat salarié . convention collective": "'IDCC2264'",
-            "contrat salarié . convention collective . établissement handicap . indemnité de licenciement . catégorie professionnelle":
-              "'Cadres'",
-            "contrat salarié . convention collective . établissement handicap . indemnité de licenciement . catégorie professionnelle . cadre . non cadre durant une période":
+            "contrat salarié . convention collective": "'IDCC0413'",
+            "contrat salarié . convention collective . établissement handicap . indemnité de licenciement . catégorie professionnelle": `'${category}'`,
+            "contrat salarié . convention collective . établissement handicap . indemnité de licenciement . catégorie professionnelle . cadre et non cadre durant une période":
               "oui",
-            "contrat salarié . convention collective . établissement handicap . indemnité de licenciement . catégorie professionnelle . cadre . non cadre durant une période . temps":
+            "contrat salarié . convention collective . établissement handicap . indemnité de licenciement . catégorie professionnelle . cadre et non cadre durant une période . temps":
               seniorityNonCadres,
             "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
               seniority,
