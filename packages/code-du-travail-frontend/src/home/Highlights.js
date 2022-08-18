64719e2f95fc66a2b997ea48fe4fa9dfe7e6c125
Victor Zeinstra <zeinsta.victor@gmail.com>
2022-08-18 13:51:21 +0200

    chore: fix pipeline

--- packages/code-du-travail-frontend/src/home/Highlights.js
+++ packages/code-du-travail-frontend/src/home/Highlights.js
@@ -1,7 +1,7 @@
 import { Container, Grid, PageTitle, Section } from "@socialgouv/cdtn-ui";
 import React from "react";
 
-import { ListLink } from "../../src/search/SearchResults/Results.tsx";
+import { ListLink } from "../../src/search/SearchResults/Results";
 
 export const Highlights = ({ highlights = [] }) => {
   if (!highlights.length) return null;
