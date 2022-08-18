4a3901e858007d79d17acfa8a1918854c46cdbe2
Martial Maillot <martial.maillot@gmail.com>
2022-08-18 16:53:44 +0200

    feat(simulators): Ajout CC 1518 au simulateur d'indemnité de licenciement (#4521)

--- packages/code-du-travail-modeles/src/plugins/anciennete/motifs.ts
+++ packages/code-du-travail-modeles/src/plugins/anciennete/motifs.ts
@@ -1,4 +1,5 @@
 import { SupportedCcIndemniteLicenciement } from "../types";
+import { MOTIFS_1518 } from "./1518_education_loisirs";
 import { MOTIFS_2511 } from "./2511_sport";
 import { MOTIFS_3043 } from "./3043_entreprises_proprete";
 import { LEGAL_MOTIFS } from "./legal";
@@ -10,6 +11,8 @@ export const getMotifs = (idcc: SupportedCcIndemniteLicenciement): Motif[] => {
       return MOTIFS_2511;
     case SupportedCcIndemniteLicenciement.IDCC3043:
       return MOTIFS_3043;
+    case SupportedCcIndemniteLicenciement.IDCC1518:
+      return MOTIFS_1518;
     case SupportedCcIndemniteLicenciement.default:
     default:
       return LEGAL_MOTIFS;
