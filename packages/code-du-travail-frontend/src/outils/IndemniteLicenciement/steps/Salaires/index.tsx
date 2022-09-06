b88dbbe3cfecece8f33f5b8f180c42df2eddd55b
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-09-06 15:57:34 +0200

    fix: component

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Salaires/index.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Salaires/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useIndemniteLicenciementStore } from "../../store";
 import { RadioQuestion, TextQuestion } from "../../../Components";
-import { TempsPartiel, SalaireTempsPlein } from "./components";
+import { TempsPartiel, SalaireTempsPlein, TooltipSalary } from "./components";
 import { SupportedCcIndemniteLicenciement } from "@socialgouv/modeles-social";
 import { IndemniteLicenciementStepName } from "../..";
 import { AgreementsInjector } from "../../agreements";
@@ -107,54 +107,7 @@ const StepSalaires = () => {
               title="Salaires mensuels bruts des 12 derniers mois et primes des 3 derniers mois précédant la notification du licenciement"
               subTitle="Indiquez le montant des salaires (en incluant les primes et avantages en nature) dans le premier champ et le montant des primes dans le second champ (uniquement pour les 3 derniers mois)              "
               tooltip={{
-                content: (
-                  <>
-                    <p>
-                      Prendre en compte le salaire fixe ou variable (ex :
-                      commissions), les primes et avantages en nature. Les
-                      éléments de rémunération suivants sont{" "}
-                      <strong>à exclure</strong> :
-                    </p>
-                    <ul>
-                      <li>
-                        <strong>
-                          Les remboursements de frais professionnels
-                        </strong>{" "}
-                        (exemple : frais de repas, frais d’utilisation d’un
-                        véhicule personnel à des fins professionnelles, frais de
-                        transport, frais de logement)
-                      </li>
-                      <li>
-                        <strong>
-                          L’indemnité compensatrice de congés payés
-                        </strong>{" "}
-                        : somme versée par l’employeur en contrepartie des jours
-                        de congés payés acquis mais non pris par le salarié
-                      </li>
-                      <li>
-                        <strong>L’indemnité compensatrice de préavis</strong> :
-                        si l’employeur décide lui seul de dispenser le salarié
-                        d’exécuter un préavis obligatoire
-                      </li>
-                      <li>
-                        <strong>
-                          Une prime ou gratification librement versée par
-                          l’employeur
-                        </strong>{" "}
-                        à une catégorie de salariés et/ou à l’occasion d’un
-                        évènement unique (exemple : prime exceptionnelle de
-                        pouvoir d’achat)
-                      </li>
-                      <li>
-                        <strong>
-                          La participation aux résultats de l’entreprise
-                        </strong>{" "}
-                        (exemple : prime d’intéressement, prime de
-                        participation)
-                      </li>
-                    </ul>
-                  </>
-                ),
+                content: <TooltipSalary />,
               }}
               onSalariesChange={onSalariesChange}
               salaryPeriods={salaryPeriods}
