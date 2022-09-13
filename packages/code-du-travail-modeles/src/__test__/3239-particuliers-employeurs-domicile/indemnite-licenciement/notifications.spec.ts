deef80d454314d7e5b3fc373bb45afcd9e72278f
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-09-13 10:26:00 +0200

    fix: notif

--- packages/code-du-travail-modeles/src/__test__/3239-particuliers-employeurs-domicile/indemnite-licenciement/notifications.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/3239-particuliers-employeurs-domicile/indemnite-licenciement/notifications.spec.ts
@@ -13,7 +13,7 @@ const notificationBloquante =
   "<strong>L’indemnité légale de licenciement n’est pas dûe en cas de suspension, modification ou au retrait de l'agrément de l'assistant maternel.</strong> Lorsqu’il est invoqué, le motif doit apparaître précisément dans le courrier. Reportez vous à la lettre de notification de licenciement.";
 
 describe("Notification bloquante et non bloquante pour la CC 3239", () => {
-  describe("Cas standard", () => {
+  describe("Cas standard pour les salariés du particulier employeur", () => {
     test.each`
       category                                  | seniority | salary
       ${CatPro3239.salarieParticulierEmployeur} | ${0}      | ${2000}
@@ -21,12 +21,6 @@ describe("Notification bloquante et non bloquante pour la CC 3239", () => {
       ${CatPro3239.salarieParticulierEmployeur} | ${2}      | ${2000}
       ${CatPro3239.salarieParticulierEmployeur} | ${10}     | ${2000}
       ${CatPro3239.salarieParticulierEmployeur} | ${12}     | ${2000}
-      ${CatPro3239.assistantMaternel}           | ${0}      | ${2000}
-      ${CatPro3239.assistantMaternel}           | ${8 / 12} | ${2000}
-      ${CatPro3239.assistantMaternel}           | ${9 / 12} | ${2000}
-      ${CatPro3239.assistantMaternel}           | ${2}      | ${2000}
-      ${CatPro3239.assistantMaternel}           | ${10}     | ${2000}
-      ${CatPro3239.assistantMaternel}           | ${12}     | ${2000}
     `(
       "ancienneté: $seniority an, salaire de référence: $salary, catégorie $category => $expectedCompensation €",
       ({ seniority, salary, category }) => {
@@ -45,27 +39,26 @@ describe("Notification bloquante et non bloquante pour la CC 3239", () => {
         expect(notificationsBloquantes).toHaveLength(0);
 
         const notifications = getNotifications(situation);
-        expect(notifications).toHaveLength(1);
-        expect(notifications[0].description).toBe(notification);
+        expect(notifications).toHaveLength(0);
       }
     );
 
-    describe("Assistante maternelle qui a sélectionné comme motif un retrait d'agrément", () => {
+    describe("Cas standard pour les assistantes maternelles", () => {
       test.each`
-        seniority | salary
-        ${0}      | ${2000}
-        ${8 / 12} | ${2000}
-        ${9 / 12} | ${2000}
-        ${2}      | ${2000}
-        ${10}     | ${2000}
-        ${12}     | ${2000}
+        category                        | seniority | salary
+        ${CatPro3239.assistantMaternel} | ${0}      | ${2000}
+        ${CatPro3239.assistantMaternel} | ${8 / 12} | ${2000}
+        ${CatPro3239.assistantMaternel} | ${9 / 12} | ${2000}
+        ${CatPro3239.assistantMaternel} | ${2}      | ${2000}
+        ${CatPro3239.assistantMaternel} | ${10}     | ${2000}
+        ${CatPro3239.assistantMaternel} | ${12}     | ${2000}
       `(
-        "ancienneté: $seniority an, salaire de référence: $salary, => $expectedCompensation €",
-        ({ seniority, salary }) => {
+        "ancienneté: $seniority an, salaire de référence: $salary, catégorie $category => $expectedCompensation €",
+        ({ seniority, salary, category }) => {
           const situation = engine.setSituation({
             "contrat salarié . convention collective": "'IDCC3239'",
-            "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle": `'${CatPro3239.assistantMaternel}'`,
-            "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle . assistante maternelle . type de licenciement": `'Oui'`,
+            "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle": `'${category}'`,
+            "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle . assistante maternelle . type de licenciement": `'Non'`,
             "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
               seniority,
             "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
@@ -74,16 +67,50 @@ describe("Notification bloquante et non bloquante pour la CC 3239", () => {
           });
 
           const notificationsBloquantes = getNotificationsBloquantes(situation);
-          expect(notificationsBloquantes).toHaveLength(1);
-          expect(notificationsBloquantes[0].description).toBe(
-            notificationBloquante
-          );
+          expect(notificationsBloquantes).toHaveLength(0);
 
           const notifications = getNotifications(situation);
           expect(notifications).toHaveLength(1);
           expect(notifications[0].description).toBe(notification);
         }
       );
+
+      describe("Assistante maternelle qui a sélectionné comme motif un retrait d'agrément", () => {
+        test.each`
+          seniority | salary
+          ${0}      | ${2000}
+          ${8 / 12} | ${2000}
+          ${9 / 12} | ${2000}
+          ${2}      | ${2000}
+          ${10}     | ${2000}
+          ${12}     | ${2000}
+        `(
+          "ancienneté: $seniority an, salaire de référence: $salary, => $expectedCompensation €",
+          ({ seniority, salary }) => {
+            const situation = engine.setSituation({
+              "contrat salarié . convention collective": "'IDCC3239'",
+              "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle": `'${CatPro3239.assistantMaternel}'`,
+              "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle . assistante maternelle . type de licenciement": `'Oui'`,
+              "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
+                seniority,
+              "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
+                salary,
+              "indemnité de licenciement": "oui",
+            });
+
+            const notificationsBloquantes =
+              getNotificationsBloquantes(situation);
+            expect(notificationsBloquantes).toHaveLength(1);
+            expect(notificationsBloquantes[0].description).toBe(
+              notificationBloquante
+            );
+
+            const notifications = getNotifications(situation);
+            expect(notifications).toHaveLength(1);
+            expect(notifications[0].description).toBe(notification);
+          }
+        );
+      });
     });
   });
 });
