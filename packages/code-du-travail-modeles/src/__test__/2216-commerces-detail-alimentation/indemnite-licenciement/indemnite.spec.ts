274f55035d94db3432ad800a7cbbbad4466440d4
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-09-16 15:34:17 +0200

    fix: rules

--- packages/code-du-travail-modeles/src/__test__/2216-commerces-detail-alimentation/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/2216-commerces-detail-alimentation/indemnite-licenciement/indemnite.spec.ts
@@ -1,34 +1,67 @@
 import Engine from "publicodes";
 
 import { mergeModels } from "../../../internal/merger";
-import { CatPro3239 } from "../../../plugins/salaire-reference/3239_particuliers_employeurs_domicile";
+import { CatPro2216 } from "../../../plugins/formule/2216_commerces_detail_alimentation";
 
 const engine = new Engine(mergeModels());
 
-describe("Indemnité conventionnel de licenciement pour la CC 3239", () => {
+describe("Indemnité conventionnel de licenciement pour la CC 2216", () => {
   describe("Cas standard", () => {
     test.each`
-      category                                  | seniority | salary  | expectedCompensation
-      ${CatPro3239.salarieParticulierEmployeur} | ${0}      | ${2000} | ${0}
-      ${CatPro3239.salarieParticulierEmployeur} | ${8 / 12} | ${2000} | ${333.33}
-      ${CatPro3239.salarieParticulierEmployeur} | ${2}      | ${2000} | ${1000}
-      ${CatPro3239.salarieParticulierEmployeur} | ${10}     | ${2000} | ${5000}
-      ${CatPro3239.salarieParticulierEmployeur} | ${12}     | ${2000} | ${6333.33}
-      ${CatPro3239.assistantMaternel}           | ${0}      | ${2000} | ${0}
-      ${CatPro3239.assistantMaternel}           | ${8 / 12} | ${2000} | ${0}
-      ${CatPro3239.assistantMaternel}           | ${9 / 12} | ${2000} | ${25}
-      ${CatPro3239.assistantMaternel}           | ${2}      | ${2000} | ${25}
-      ${CatPro3239.assistantMaternel}           | ${10}     | ${2000} | ${25}
-      ${CatPro3239.assistantMaternel}           | ${12}     | ${2000} | ${25}
+      category               | isEconomicFiring | age   | seniority | salary  | expectedCompensation
+      ${CatPro2216.employes} | ${false}         | ${50} | ${0}      | ${2000} | ${0}
+      ${CatPro2216.employes} | ${true}          | ${50} | ${0}      | ${2000} | ${0}
+      ${CatPro2216.employes} | ${true}          | ${51} | ${0}      | ${2000} | ${0}
+      ${CatPro2216.employes} | ${false}         | ${50} | ${8 / 12} | ${2000} | ${0}
+      ${CatPro2216.employes} | ${true}          | ${50} | ${8 / 12} | ${2000} | ${0}
+      ${CatPro2216.employes} | ${true}          | ${51} | ${8 / 12} | ${2000} | ${0}
+      ${CatPro2216.employes} | ${false}         | ${50} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.employes} | ${true}          | ${50} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.employes} | ${true}          | ${51} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.agents}   | ${false}         | ${50} | ${0}      | ${2000} | ${0}
+      ${CatPro2216.agents}   | ${true}          | ${50} | ${0}      | ${2000} | ${0}
+      ${CatPro2216.agents}   | ${true}          | ${51} | ${0}      | ${2000} | ${0}
+      ${CatPro2216.agents}   | ${false}         | ${50} | ${8 / 12} | ${2000} | ${0}
+      ${CatPro2216.agents}   | ${true}          | ${50} | ${8 / 12} | ${2000} | ${0}
+      ${CatPro2216.agents}   | ${true}          | ${51} | ${8 / 12} | ${2000} | ${0}
+      ${CatPro2216.agents}   | ${false}         | ${50} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.agents}   | ${true}          | ${50} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.agents}   | ${true}          | ${51} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${0}      | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${0}      | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${0}      | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${8 / 12} | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${8 / 12} | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${8 / 12} | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${8}      | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${8}      | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${8}      | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${22}     | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${22}     | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${22}     | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${44}     | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${44}     | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${44}     | ${2000} | ${0}
     `(
       "ancienneté: $seniority an, salaire de référence: $salary, catégorie $category => $expectedCompensation €",
-      ({ seniority, salary, expectedCompensation, category }) => {
+      ({
+        category,
+        isEconomicFiring,
+        age,
+        seniority,
+        salary,
+        expectedCompensation,
+      }) => {
         const situation = engine.setSituation({
-          "contrat salarié . convention collective": "'IDCC3239'",
-          "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle": `'${category}'`,
-          "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle . assistante maternelle . type de licenciement": `'Non'`,
-          "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle . assistante maternelle . type de licenciement . autres . total salaires":
-            salary,
+          "contrat salarié . convention collective": "'IDCC2216'",
+          "contrat salarié . convention collective . commerce gros et detail alimentation . indemnité de licenciement . catégorie professionnelle": `'${category}'`,
+          "contrat salarié . convention collective . commerce gros et detail alimentation . indemnité de licenciement . catégorie professionnelle . licenciement économique":
+            isEconomicFiring ? `'Oui'` : `'Non'`,
+          "contrat salarié . convention collective . commerce gros et detail alimentation . indemnité de licenciement . catégorie professionnelle . licenciement économique . age":
+            age,
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
             seniority,
           "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
@@ -44,41 +77,5 @@ describe("Indemnité conventionnel de licenciement pour la CC 3239", () => {
         expect(result.nodeValue).toEqual(expectedCompensation);
       }
     );
-
-    describe("Assistante maternelle qui a sélectionné comme motif un retrait d'agrément", () => {
-      test.each`
-        seniority | salary  | expectedCompensation
-        ${0}      | ${2000} | ${false}
-        ${8 / 12} | ${2000} | ${false}
-        ${9 / 12} | ${2000} | ${false}
-        ${2}      | ${2000} | ${false}
-        ${10}     | ${2000} | ${false}
-        ${12}     | ${2000} | ${false}
-      `(
-        "ancienneté: $seniority an, salaire de référence: $salary, => $expectedCompensation €",
-        ({ seniority, salary, expectedCompensation }) => {
-          const situation = engine.setSituation({
-            "contrat salarié . convention collective": "'IDCC3239'",
-            "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle": `'${CatPro3239.assistantMaternel}'`,
-            "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle . assistante maternelle . type de licenciement": `'Oui'`,
-            "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle . assistante maternelle . type de licenciement . autres . total salaires":
-              salary,
-            "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
-              seniority,
-            "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
-              salary,
-            "indemnité de licenciement": "oui",
-          });
-
-          const result = situation.evaluate(
-            "contrat salarié . indemnité de licenciement . résultat conventionnel"
-          );
-
-          expect(result.missingVariables).toEqual({});
-          expect(result.unit?.numerators).toEqual(["€"]);
-          expect(result.nodeValue).toEqual(expectedCompensation);
-        }
-      );
-    });
   });
 });
