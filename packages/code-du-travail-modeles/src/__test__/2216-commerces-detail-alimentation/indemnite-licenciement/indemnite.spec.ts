5d576bee234460b2215d544120065e7ee459ba69
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-09-16 16:44:22 +0200

    fix: cc

--- packages/code-du-travail-modeles/src/__test__/2216-commerces-detail-alimentation/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/2216-commerces-detail-alimentation/indemnite-licenciement/indemnite.spec.ts
@@ -12,39 +12,67 @@ describe("Indemnité conventionnel de licenciement pour la CC 2216", () => {
       ${CatPro2216.employes} | ${false}         | ${50} | ${0}      | ${2000} | ${0}
       ${CatPro2216.employes} | ${true}          | ${50} | ${0}      | ${2000} | ${0}
       ${CatPro2216.employes} | ${true}          | ${51} | ${0}      | ${2000} | ${0}
-      ${CatPro2216.employes} | ${false}         | ${50} | ${8 / 12} | ${2000} | ${0}
-      ${CatPro2216.employes} | ${true}          | ${50} | ${8 / 12} | ${2000} | ${0}
-      ${CatPro2216.employes} | ${true}          | ${51} | ${8 / 12} | ${2000} | ${0}
-      ${CatPro2216.employes} | ${false}         | ${50} | ${11}     | ${2000} | ${0}
-      ${CatPro2216.employes} | ${true}          | ${50} | ${11}     | ${2000} | ${0}
-      ${CatPro2216.employes} | ${true}          | ${51} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.employes} | ${false}         | ${50} | ${8 / 12} | ${2000} | ${333.33}
+      ${CatPro2216.employes} | ${true}          | ${50} | ${8 / 12} | ${2000} | ${333.33}
+      ${CatPro2216.employes} | ${true}          | ${51} | ${8 / 12} | ${2000} | ${400}
+      ${CatPro2216.employes} | ${false}         | ${50} | ${11}     | ${2000} | ${5666.67}
+      ${CatPro2216.employes} | ${true}          | ${50} | ${11}     | ${2000} | ${5666.67}
+      ${CatPro2216.employes} | ${true}          | ${51} | ${11}     | ${2000} | ${6800}
       ${CatPro2216.agents}   | ${false}         | ${50} | ${0}      | ${2000} | ${0}
       ${CatPro2216.agents}   | ${true}          | ${50} | ${0}      | ${2000} | ${0}
       ${CatPro2216.agents}   | ${true}          | ${51} | ${0}      | ${2000} | ${0}
-      ${CatPro2216.agents}   | ${false}         | ${50} | ${8 / 12} | ${2000} | ${0}
-      ${CatPro2216.agents}   | ${true}          | ${50} | ${8 / 12} | ${2000} | ${0}
-      ${CatPro2216.agents}   | ${true}          | ${51} | ${8 / 12} | ${2000} | ${0}
-      ${CatPro2216.agents}   | ${false}         | ${50} | ${11}     | ${2000} | ${0}
-      ${CatPro2216.agents}   | ${true}          | ${50} | ${11}     | ${2000} | ${0}
-      ${CatPro2216.agents}   | ${true}          | ${51} | ${11}     | ${2000} | ${0}
+      ${CatPro2216.agents}   | ${false}         | ${50} | ${8 / 12} | ${2000} | ${333.33}
+      ${CatPro2216.agents}   | ${true}          | ${50} | ${8 / 12} | ${2000} | ${333.33}
+      ${CatPro2216.agents}   | ${true}          | ${51} | ${8 / 12} | ${2000} | ${400}
+      ${CatPro2216.agents}   | ${false}         | ${50} | ${11}     | ${2000} | ${5666.67}
+      ${CatPro2216.agents}   | ${true}          | ${50} | ${11}     | ${2000} | ${5666.67}
+      ${CatPro2216.agents}   | ${true}          | ${51} | ${11}     | ${2000} | ${6800}
       ${CatPro2216.cadres}   | ${false}         | ${50} | ${0}      | ${2000} | ${0}
       ${CatPro2216.cadres}   | ${true}          | ${50} | ${0}      | ${2000} | ${0}
       ${CatPro2216.cadres}   | ${true}          | ${51} | ${0}      | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${false}         | ${50} | ${8 / 12} | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${true}          | ${50} | ${8 / 12} | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${true}          | ${51} | ${8 / 12} | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${false}         | ${50} | ${8}      | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${true}          | ${50} | ${8}      | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${true}          | ${51} | ${8}      | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${false}         | ${50} | ${11}     | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${true}          | ${50} | ${11}     | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${true}          | ${51} | ${11}     | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${false}         | ${50} | ${22}     | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${true}          | ${50} | ${22}     | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${true}          | ${51} | ${22}     | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${false}         | ${50} | ${44}     | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${true}          | ${50} | ${44}     | ${2000} | ${0}
-      ${CatPro2216.cadres}   | ${true}          | ${51} | ${44}     | ${2000} | ${0}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${8 / 12} | ${2000} | ${400}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${8 / 12} | ${2000} | ${480}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${8 / 12} | ${2000} | ${480}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${8}      | ${2000} | ${4800}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${8}      | ${2000} | ${5760}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${8}      | ${2000} | ${5760}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${11}     | ${2000} | ${6800}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${11}     | ${2000} | ${8160}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${11}     | ${2000} | ${8160}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${22}     | ${2000} | ${16000}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${22}     | ${2000} | ${19200}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${22}     | ${2000} | ${19200}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${44}     | ${2000} | ${36666.67}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${44}     | ${2000} | ${44000}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${44}     | ${2000} | ${44000}
+      ${CatPro2216.employes} | ${false}         | ${45} | ${0.5}    | ${2500} | ${0}
+      ${CatPro2216.employes} | ${false}         | ${45} | ${0.67}   | ${2500} | ${418.75}
+      ${CatPro2216.employes} | ${false}         | ${45} | ${10}     | ${2500} | ${6250}
+      ${CatPro2216.employes} | ${false}         | ${45} | ${11}     | ${2500} | ${7083.33}
+      ${CatPro2216.employes} | ${true}          | ${50} | ${20}     | ${2500} | ${14583.33}
+      ${CatPro2216.employes} | ${true}          | ${51} | ${20}     | ${2500} | ${17500}
+      ${CatPro2216.employes} | ${true}          | ${53} | ${8}      | ${2500} | ${6000}
+      ${CatPro2216.agents}   | ${false}         | ${45} | ${0.5}    | ${2500} | ${0}
+      ${CatPro2216.agents}   | ${false}         | ${45} | ${0.67}   | ${2500} | ${418.75}
+      ${CatPro2216.agents}   | ${false}         | ${45} | ${10}     | ${2500} | ${6250}
+      ${CatPro2216.agents}   | ${false}         | ${45} | ${11}     | ${2500} | ${7083.33}
+      ${CatPro2216.agents}   | ${true}          | ${50} | ${20}     | ${2500} | ${14583.33}
+      ${CatPro2216.agents}   | ${true}          | ${51} | ${20}     | ${2500} | ${17500}
+      ${CatPro2216.agents}   | ${true}          | ${53} | ${8}      | ${2500} | ${6000}
+      ${CatPro2216.cadres}   | ${false}         | ${45} | ${0.91}   | ${2500} | ${682.5}
+      ${CatPro2216.cadres}   | ${false}         | ${45} | ${5}      | ${2500} | ${3750}
+      ${CatPro2216.cadres}   | ${false}         | ${45} | ${6}      | ${2500} | ${4500}
+      ${CatPro2216.cadres}   | ${false}         | ${45} | ${15}     | ${2500} | ${12500}
+      ${CatPro2216.cadres}   | ${false}         | ${45} | ${22}     | ${2500} | ${20000}
+      ${CatPro2216.cadres}   | ${false}         | ${45} | ${38}     | ${2500} | ${40000}
+      ${CatPro2216.cadres}   | ${false}         | ${45} | ${40}     | ${2500} | ${42500}
+      ${CatPro2216.cadres}   | ${false}         | ${45} | ${41}     | ${2500} | ${43333.33}
+      ${CatPro2216.cadres}   | ${true}          | ${33} | ${5}      | ${2500} | ${3750}
+      ${CatPro2216.cadres}   | ${true}          | ${40} | ${15}     | ${2500} | ${12500}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${22}     | ${2500} | ${24000}
+      ${CatPro2216.cadres}   | ${true}          | ${58} | ${38}     | ${2500} | ${48000}
+      ${CatPro2216.cadres}   | ${true}          | ${55} | ${40}     | ${2500} | ${51000}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${41}     | ${2500} | ${52000}
     `(
       "ancienneté: $seniority an, salaire de référence: $salary, catégorie $category => $expectedCompensation €",
       ({
--- packages/code-du-travail-modeles/src/__test__/2216-commerces-detail-alimentation/indemnite-licenciement/références.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/2216-commerces-detail-alimentation/indemnite-licenciement/références.spec.ts
@@ -1,110 +1,128 @@
 import Engine from "publicodes";
 
 import { mergeModels } from "../../../internal/merger";
-import { CatPro3239 } from "../../../plugins/salaire-reference/3239_particuliers_employeurs_domicile";
+import { CatPro2216 } from "../../../plugins/formule/2216_commerces_detail_alimentation";
 import { getReferences } from "../../../utils";
 
 const engine = new Engine(mergeModels());
 
-const ReferencesPe = [
+const referencesEmployes = [
   {
-    article: "Article 47-1",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942116?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942116",
+    article:
+      "Article 7 de l’Annexe I Employés et ouvriers, personnel de livraison",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000041517490?idConteneur=KALICONT000005635085",
   },
   {
-    article: "Article 47-2",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942117?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942117",
+    article:
+      "Article 6 de l’Annexe I Employés et ouvriers, personnel de livraison",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000041517485?idConteneur=KALICONT000005635085&origin=list#KALIARTI000041517485",
   },
   {
-    article: "Article 48-1-3-1-1",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942139?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942139",
-  },
-  {
-    article: "Article 48-1-3-4",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942144?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942144",
-  },
-  {
-    article: "Article 49",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942151?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942151",
-  },
-  {
-    article: "Article 60",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942169?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942169",
-  },
-  {
-    article: "Article 142",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942377?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942377",
-  },
-  {
-    article: "Article 163-1",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942458?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942458",
+    article: "Article 3.13",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000039111891?idConteneur=KALICONT000005635085&origin=list#KALIARTI000039111891",
   },
 ];
 
-const ReferencesAssMat = [
+const referencesAgents = [
   {
-    article: "Article 47-1",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942116?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942116",
+    article:
+      "Article 7 de l’Annexe II relative aux agents de maîtrise et techniciens",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000041517524",
   },
   {
-    article: "Article 47-2",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942117?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942117",
+    article:
+      "Article 6 de l’Annexe II relative aux agents de maîtrise et techniciens",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000041517519?idConteneur=KALICONT000005635085&origin=list#KALIARTI000041517519",
   },
   {
-    article: "Article 48-1-3-1-1",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942139?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942139",
-  },
-  {
-    article: "Article 48-1-3-4",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942144?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942144",
-  },
-  {
-    article: "Article 49",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942151?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942151",
-  },
-  {
-    article: "Article 60",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942169?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942169",
-  },
-  {
-    article: "Article 90-1",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942236?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942236",
-  },
-  {
-    article: "Article 121-1",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000043942321?idConteneur=KALICONT000044594539&origin=list#KALIARTI000043942321",
+    article: "Article 3.13",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000039111891?idConteneur=KALICONT000005635085&origin=list#KALIARTI000039111891",
   },
 ];
 
-describe("Références juridiques pour la CC 3239", () => {
-  test("Notifications de l'assistante maternelle", () => {
-    const situation = engine.setSituation({
-      "contrat salarié . convention collective": "'IDCC3239'",
-      "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle": `'${CatPro3239.assistantMaternel}'`,
-      "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle . assistante maternelle . type de licenciement": `'Non'`,
-      "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle . assistante maternelle . type de licenciement . autres . total salaires": 20000,
-      "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année": 2,
-      "indemnité de licenciement": "oui",
-    });
+const referencesCadres = [
+  {
+    article: "Article 7 de l’Annexe III Cadres",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000041517561",
+  },
+  {
+    article: "Article 6 de l’Annexe III Cadres",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000041517556?idConteneur=KALICONT000005635085&origin=list#KALIARTI000041517556",
+  },
+  {
+    article: "Article 3.13",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000039111891?idConteneur=KALICONT000005635085&origin=list#KALIARTI000039111891",
+  },
+];
 
-    const result = getReferences(situation, "résultat conventionnel");
+describe("Références juridique pour l'indemnité conventionnel de licenciement pour la CC 2216", () => {
+  describe("Cas standard", () => {
+    test.each`
+      category               | isEconomicFiring | age   | seniority | salary  | expectedReferences
+      ${CatPro2216.employes} | ${false}         | ${50} | ${0}      | ${2000} | ${referencesEmployes}
+      ${CatPro2216.employes} | ${true}          | ${50} | ${0}      | ${2000} | ${referencesEmployes}
+      ${CatPro2216.employes} | ${true}          | ${51} | ${0}      | ${2000} | ${referencesEmployes}
+      ${CatPro2216.employes} | ${false}         | ${50} | ${8 / 12} | ${2000} | ${referencesEmployes}
+      ${CatPro2216.employes} | ${true}          | ${50} | ${8 / 12} | ${2000} | ${referencesEmployes}
+      ${CatPro2216.employes} | ${true}          | ${51} | ${8 / 12} | ${2000} | ${referencesEmployes}
+      ${CatPro2216.employes} | ${false}         | ${50} | ${11}     | ${2000} | ${referencesEmployes}
+      ${CatPro2216.employes} | ${true}          | ${50} | ${11}     | ${2000} | ${referencesEmployes}
+      ${CatPro2216.employes} | ${true}          | ${51} | ${11}     | ${2000} | ${referencesEmployes}
+      ${CatPro2216.agents}   | ${false}         | ${50} | ${0}      | ${2000} | ${referencesAgents}
+      ${CatPro2216.agents}   | ${true}          | ${50} | ${0}      | ${2000} | ${referencesAgents}
+      ${CatPro2216.agents}   | ${true}          | ${51} | ${0}      | ${2000} | ${referencesAgents}
+      ${CatPro2216.agents}   | ${false}         | ${50} | ${8 / 12} | ${2000} | ${referencesAgents}
+      ${CatPro2216.agents}   | ${true}          | ${50} | ${8 / 12} | ${2000} | ${referencesAgents}
+      ${CatPro2216.agents}   | ${true}          | ${51} | ${8 / 12} | ${2000} | ${referencesAgents}
+      ${CatPro2216.agents}   | ${false}         | ${50} | ${11}     | ${2000} | ${referencesAgents}
+      ${CatPro2216.agents}   | ${true}          | ${50} | ${11}     | ${2000} | ${referencesAgents}
+      ${CatPro2216.agents}   | ${true}          | ${51} | ${11}     | ${2000} | ${referencesAgents}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${0}      | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${0}      | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${0}      | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${8 / 12} | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${8 / 12} | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${8 / 12} | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${8}      | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${8}      | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${8}      | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${11}     | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${11}     | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${11}     | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${22}     | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${22}     | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${22}     | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${false}         | ${50} | ${44}     | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${50} | ${44}     | ${2000} | ${referencesCadres}
+      ${CatPro2216.cadres}   | ${true}          | ${51} | ${44}     | ${2000} | ${referencesCadres}
+    `(
+      "ancienneté: $seniority an, salaire de référence: $salary, catégorie $category => $expectedReferences",
+      ({
+        category,
+        isEconomicFiring,
+        age,
+        seniority,
+        salary,
+        expectedReferences,
+      }) => {
+        const situation = engine.setSituation({
+          "contrat salarié . convention collective": "'IDCC2216'",
+          "contrat salarié . convention collective . commerce gros et detail alimentation . indemnité de licenciement . catégorie professionnelle": `'${category}'`,
+          "contrat salarié . convention collective . commerce gros et detail alimentation . indemnité de licenciement . catégorie professionnelle . licenciement économique":
+            isEconomicFiring ? `'Oui'` : `'Non'`,
+          "contrat salarié . convention collective . commerce gros et detail alimentation . indemnité de licenciement . catégorie professionnelle . licenciement économique . age":
+            age,
+          "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
+            seniority,
+          "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
+            salary,
+          "indemnité de licenciement": "oui",
+        });
 
-    expect(result).toHaveLength(ReferencesAssMat.length);
-    expect(result).toEqual(expect.arrayContaining(ReferencesAssMat));
-  });
-
-  test("Notifications du salarié du particulier employeur", () => {
-    const situation = engine.setSituation({
-      "contrat salarié . convention collective": "'IDCC3239'",
-      "contrat salarié . convention collective . particuliers employeurs et emploi à domicile . indemnité de licenciement . catégorie professionnelle": `'${CatPro3239.salarieParticulierEmployeur}'`,
-      "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année": 2,
-      "contrat salarié . indemnité de licenciement . salaire de référence conventionnel": 2000,
-      "indemnité de licenciement": "oui",
-    });
-
-    const result = getReferences(situation, "résultat conventionnel");
-
-    expect(result).toHaveLength(ReferencesPe.length);
-    expect(result).toEqual(expect.arrayContaining(ReferencesPe));
+        const result = getReferences(situation, "résultat conventionnel");
+        expect(result).toHaveLength(expectedReferences.length);
+        expect(result).toEqual(expect.arrayContaining(expectedReferences));
+      }
+    );
   });
 });
