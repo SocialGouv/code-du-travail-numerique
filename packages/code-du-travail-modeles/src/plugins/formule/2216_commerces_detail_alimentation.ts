ba9abea2760ed7b54fac626374a3a60c40d52bc2
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-09-16 15:16:11 +0200

    fix: formules

--- packages/code-du-travail-modeles/src/plugins/formule/2216_commerces_detail_alimentation.ts
+++ packages/code-du-travail-modeles/src/plugins/formule/2216_commerces_detail_alimentation.ts
@@ -28,7 +28,7 @@ export class Formula2216
     age,
   }: FormulaProps<SupportedCcIndemniteLicenciement.IDCC2216>): Formula {
     let formula = "";
-    const explanations = [];
+    let explanations: string[] = [];
     const year = round(seniority) < 2 ? "an" : "ans";
     if (
       (category === CatPro2216.employes || category === CatPro2216.agents) &&
@@ -39,14 +39,15 @@ export class Formula2216
         refSalary,
         seniority,
       });
+      formula = res.formula;
+      explanations = res.explanations;
       if (isEconomicFiring && age > 50 && seniority <= 10) {
-        res.formula += ` + ( 20% * (1 / 4 * Sref * A) )`;
+        formula += ` + ( 20% * (1 / 4 * Sref * A) )`;
         explanations.push(`20% : majoration pour motif économique`);
       } else if (isEconomicFiring && age > 50 && seniority > 10) {
-        res.formula += ` + ( 20% * (1 / 4 * Sref * A) + 20% * (1 / 3 * Sref * A) )`;
+        formula += ` + ( 20% * (1 / 4 * Sref * A) + 20% * (1 / 3 * Sref * A) )`;
         explanations.push(`20% : majoration pour motif économique`);
       }
-      return res;
     } else if (
       category === CatPro2216.cadres &&
       seniority >= 8 / 12 &&
@@ -56,61 +57,57 @@ export class Formula2216
       explanations.push(`A : Ancienneté totale (${round(seniority)} ${year})`);
       explanations.push(`Sref : Salaire de référence (${round(refSalary)} €)`);
       if (isEconomicFiring && age >= 50) {
-        formula += ` + ( 20% * (1 / 4 * Sref * A))`;
+        formula += ` + ( 20% * (1 / 4 * Sref * A) )`;
         explanations.push(`20% : majoration pour motif économique`);
       }
-    } else if (
-      category === CatPro2216.cadres &&
-      seniority > 5 &&
-      seniority <= 40
-    ) {
-      formula = `( 3 / 10 * Sref * A1 ) + ( 4 / 10 * Sref * A2 ) + ( 5 / 10 * Sref * A3 )`;
-      const maxSeniority1 = Math.min(Math.abs(round(seniority - 10)), 10);
+    } else if (category === CatPro2216.cadres && seniority > 5) {
+      formula = `( 3 / 10 * Sref * A1 )`;
+      const maxSeniority1 = Math.min(round(seniority), 10);
       const yearAfterDiff = maxSeniority1 < 2 ? "an" : "ans";
       explanations.push(
         `A1: Années de présence pour la tranche jusqu'à 10 ans (${maxSeniority1} ${yearAfterDiff})`
       );
-      const maxSeniority2 = Math.min(Math.abs(round(seniority - 20)), 20);
-      const yearAfterDiff2 = maxSeniority2 < 2 ? "an" : "ans";
-      explanations.push(
-        `A2: Années de présence pour la tranche de 10 à 20 ans (${maxSeniority2} ${yearAfterDiff2})`
-      );
-      const maxSeniority3 = Math.abs(round(seniority - 20));
-      const yearAfterDiff3 = maxSeniority3 < 2 ? "an" : "ans";
-      explanations.push(
-        `A3: Années de présence pour la tranche au-delà de 20 ans (${maxSeniority3} ${yearAfterDiff3})`
-      );
       if (isEconomicFiring && age >= 50) {
-        formula += ` + ( 20% * ( 3 / 10 * Sref * A1 ) + 20% * ( 4 / 10 * Sref * A2 ) + 20% * ( 5 / 10 * Sref * A3 ))`;
-        explanations.push(`20% : majoration pour motif économique`);
+        formula += ` + ( 20% * ( 3 / 10 * Sref * A1 ) )`;
       }
-    } else if (category === CatPro2216.cadres && seniority > 40) {
-      formula = `( 3 / 10 * Sref * A1 ) + ( 4 / 10 * Sref * A2 ) + ( 5 / 10 * Sref * A3 )`;
-      const maxSeniority1 = Math.min(Math.abs(round(seniority - 10)), 10);
-      const yearAfterDiff = maxSeniority1 < 2 ? "an" : "ans";
-      explanations.push(
-        `A1: Années de présence pour la tranche jusqu'à 10 ans (${maxSeniority1} ${yearAfterDiff})`
-      );
-      const maxSeniority2 = Math.min(Math.abs(round(seniority - 20)), 20);
-      const yearAfterDiff2 = maxSeniority2 < 2 ? "an" : "ans";
-      explanations.push(
-        `A2: Années de présence pour la tranche de 10 à 20 ans (${maxSeniority2} ${yearAfterDiff2})`
-      );
-      const maxSeniority3 = Math.min(Math.abs(round(seniority - 40)), 20);
-      const yearAfterDiff3 = maxSeniority3 < 2 ? "an" : "ans";
-      explanations.push(
-        `A3: Années de présence pour la tranche au-delà de 20 ans (${maxSeniority3} ${yearAfterDiff3})`
-      );
-      const maxSeniority4 = Math.abs(round(seniority - 40));
-      const yearAfterDiff4 = maxSeniority4 < 2 ? "an" : "ans";
-      explanations.push(
-        `A4: Années de présence pour la tranche au-delà de 40 ans (${maxSeniority4} ${yearAfterDiff4})`
-      );
-      if (isEconomicFiring && age >= 50) {
-        formula += ` + ( 20% * ( 3 / 10 * Sref * A1 ) + 20% * ( 4 / 10 * Sref * A2 ) + 20% * ( 5 / 10 * Sref * A3 ) + 20% * ( 5 / 10 * Sref * A4 ))`;
+      if (seniority > 10) {
+        formula += ` + ( 4 / 10 * Sref * A2 )`;
+        const maxSeniority2 = Math.min(round(seniority - 10), 10);
+        const yearAfterDiff2 = maxSeniority2 < 2 ? "an" : "ans";
+        explanations.push(
+          `A2: Années de présence pour la tranche de 10 à 20 ans (${maxSeniority2} ${yearAfterDiff2})`
+        );
+        if (isEconomicFiring && age >= 50) {
+          formula += ` + ( 20% * ( 4 / 10 * Sref * A2 ))`;
+        }
+      }
+      if (seniority > 20) {
+        formula += ` + ( 5 / 10 * Sref * A3 )`;
+        const maxSeniority3 = Math.min(round(seniority - 20), 20);
+        const yearAfterDiff3 = maxSeniority3 < 2 ? "an" : "ans";
+        explanations.push(
+          `A3: Années de présence pour la tranche de 20 ans à 40 ans (${maxSeniority3} ${yearAfterDiff3})`
+        );
+        if (isEconomicFiring && age >= 50) {
+          formula += ` + ( 20% * ( 5 / 10 * Sref * A3 ))`;
+        }
+      }
+      if (seniority > 40) {
+        formula += ` + ( 1 / 3 * Sref * A4 )`;
+        const maxSeniority4 = round(seniority - 40);
+        const yearAfterDiff4 = maxSeniority4 < 2 ? "an" : "ans";
+        explanations.push(
+          `A4: Années de présence pour la tranche au-delà de 40 ans (${maxSeniority4} ${yearAfterDiff4})`
+        );
+        if (isEconomicFiring && age >= 50) {
+          formula += ` + ( 20% * ( 1 / 3 * Sref * A4 ))`;
+        }
+      }
+      if (isEconomicFiring) {
         explanations.push(`20% : majoration pour motif économique`);
       }
     }
+    console.log(formula, explanations);
     return { explanations, formula };
   }
 }
