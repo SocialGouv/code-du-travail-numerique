7f9e2982ff68325dc7f727493bd0c6fe764609ee
Victor Zeinstra <zeinsta.victor@gmail.com>
2022-09-22 11:32:17 +0200

    fix: svg bottom margin

--- packages/react-ui/src/icons/components/monochrome/ArrowTurn.js
+++ packages/react-ui/src/icons/components/monochrome/ArrowTurn.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { memo } from "react";
 
 const SvgArrowTurn = (props) => (
-  <svg {...props} viewBox="0 0 24 24" width="16" height="16">
+  <svg {...props} viewBox="0 0 22 22" width="16" height="16">
     <path d="M5.828 7l2.536 2.536L6.95 10.95 2 6l4.95-4.95 1.414 1.414L5.828 5H13a8 8 0 1 1 0 16H4v-2h9a6 6 0 1 0 0-12H5.828z" />
   </svg>
 );
