288bcfd83a084377030ce294d41354253e42bdfe
carolineBda <caroline.bourdeu@gmail.com>
2022-09-26 15:07:33 +0200

    fix build

--- packages/react-ui/src/field/test.js
+++ packages/react-ui/src/field/test.js
@@ -1,3 +1,5 @@
+import "regenerator-runtime/runtime";
+
 import { fireEvent, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
