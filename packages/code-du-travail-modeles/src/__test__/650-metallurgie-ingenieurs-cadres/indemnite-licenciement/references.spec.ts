4ea6260515acbe103b56e86f6e02f829dd432291
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-09-27 11:31:19 +0200

    fix: reference

--- packages/code-du-travail-modeles/src/__test__/650-metallurgie-ingenieurs-cadres/indemnite-licenciement/references.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/650-metallurgie-ingenieurs-cadres/indemnite-licenciement/references.spec.ts
@@ -73,7 +73,7 @@ describe("Références juridique pour l'indemnité conventionnel de licenciement
     );
   });
 
-  describe("Cas avec une question", () => {
+  describe("Cas avec une réponse Non", () => {
     test.each`
       age   | seniority | salary
       ${54} | ${1}      | ${2562}
@@ -90,6 +90,38 @@ describe("Références juridique pour l'indemnité conventionnel de licenciement
             age,
           "contrat salarié . convention collective . métallurgie ingénieurs et cadres . indemnité de licenciement . age 55 ans":
             "'Non'",
+          "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
+            seniority,
+          "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
+            salary,
+          "indemnité de licenciement": "oui",
+        });
+
+        const result = getReferences(situation, "résultat conventionnel");
+
+        expect(result).toHaveLength(expectedReferences.length);
+        expect(result).toEqual(expect.arrayContaining(expectedReferences));
+      }
+    );
+  });
+
+  describe("Cas avec une réponse Oui", () => {
+    test.each`
+      age   | seniority | salary
+      ${54} | ${1}      | ${2562}
+      ${54} | ${5}      | ${2562}
+      ${54} | ${7.91}   | ${2562}
+      ${54} | ${8}      | ${2668}
+      ${54} | ${19}     | ${2668}
+    `(
+      "age: $age, ancienneté: $seniority an, salaire de référence: $salary, catégorie $category",
+      ({ seniority, salary, age }) => {
+        const situation = engine.setSituation({
+          "contrat salarié . convention collective": "'IDCC0650'",
+          "contrat salarié . convention collective . métallurgie ingénieurs et cadres . indemnité de licenciement . age":
+            age,
+          "contrat salarié . convention collective . métallurgie ingénieurs et cadres . indemnité de licenciement . age 55 ans":
+            "'Oui'",
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
             seniority,
           "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
