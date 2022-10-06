9ac579b7c7312e255915a4743fbf00755993ea90
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-10-06 18:15:47 +0200

    fix: correction

---
+++ packages/code-du-travail-modeles/src/plugins/salaire-reference/573_commerces_de_gros.ts
@@ -0,0 +1,34 @@
+import { nonNullable, rankByMonthArrayDescFrench, sum } from "../../utils";
+import type { SalaryPeriods, SupportedCcIndemniteLicenciement } from "..";
+import { ReferenceSalaryLegal } from "./legal";
+import type { IReferenceSalary, ReferenceSalaryProps } from "./types";
+
+export type CC573ReferenceSalaryProps = {
+  salaires: SalaryPeriods[];
+  isEconomicFiring: boolean;
+};
+
+export class ReferenceSalary573
+  implements IReferenceSalary<SupportedCcIndemniteLicenciement.IDCC0573>
+{
+  /**
+   * Règle :
+   * - si le licenciement est pour des raisons économiques
+   * S / 12
+   * S : total des salaires perçus lors des 12 derniers mois précédant le jour de l'envoi de la lettre de licenciement (brut)
+   * - sinon c'est le légal qui s'applique
+   **/
+  computeReferenceSalary({
+    salaires = [],
+    isEconomicFiring = false,
+  }: ReferenceSalaryProps<SupportedCcIndemniteLicenciement.IDCC0573>): number {
+    if (isEconomicFiring) {
+      const rankedSalaires = rankByMonthArrayDescFrench(salaires);
+      const salaryValues = rankedSalaires
+        .map((a) => a.value)
+        .filter(nonNullable);
+      return sum(salaryValues) / salaires.length;
+    }
+    return new ReferenceSalaryLegal().computeReferenceSalary({ salaires });
+  }
+}
--- packages/code-du-travail-modeles/src/plugins/salaire-reference/types.ts
+++ packages/code-du-travail-modeles/src/plugins/salaire-reference/types.ts
@@ -1,5 +1,6 @@
 import type { SupportedCcIndemniteLicenciement } from "..";
 import type { CC16ReferenceSalaryProps } from "./16_transports_routiers";
+import type { CC573ReferenceSalaryProps } from "./573_commerces_de_gros";
 import type { CC1486ReferenceSalaryProps } from "./1486_bureaux_etudes_techniques";
 import type { CC1516ReferenceSalaryProps } from "./1516_organismes_formation";
 import type { CC1527ReferenceSalaryProps } from "./1527-immobilier";
@@ -27,4 +28,6 @@ export type ReferenceSalaryProps<T> =
     ? CC3239ReferenceSalaryProps
     : T extends SupportedCcIndemniteLicenciement.IDCC0016
     ? CC16ReferenceSalaryProps
+    : T extends SupportedCcIndemniteLicenciement.IDCC0573
+    ? CC573ReferenceSalaryProps
     : LegalReferenceSalaryProps;
