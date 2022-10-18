9e8dcb040e22ac92489c0a7fd275d2d20c7ee84e
Victor Zeinstra <zeinsta.victor@gmail.com>
2022-10-18 11:56:46 +0200

    chore: ut

--- packages/code-du-travail-frontend/__tests__/pages/outils/indemnite-licenciement-cc.test.tsx
+++ packages/code-du-travail-frontend/__tests__/pages/outils/indemnite-licenciement-cc.test.tsx
@@ -1,79 +1,100 @@
 import {
   default as Outils,
   getServerSideProps,
-  Props,
 } from "../../../pages/outils/[slug]";
 
-import { act } from "react-dom/test-utils";
-import { render, fireEvent, RenderResult } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  RenderResult,
+  waitFor,
+} from "@testing-library/react";
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
-    json: () => Promise.resolve({ relatedItems: [] }),
+    ok: true,
+    json: () =>
+      Promise.resolve({
+        relatedItems: [],
+        hits: {
+          hits: [
+            {
+              _source: {
+                num: 16,
+                shortTitle:
+                  "Transports routiers et activités auxiliaires du transport",
+                id: "KALICONT000005635624",
+                title:
+                  "Transports routiers et activités auxiliaires du transport",
+                url: "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000005635624",
+                slug: "16-transports-routiers-et-activites-auxiliaires-du-transport",
+              },
+            },
+          ],
+        },
+      }),
   })
 ) as jest.Mock;
 
-describe.each([
-  {
-    slug: "indemnite-licenciement",
-  },
-])(
-  `Etant donné les paramètres:
-        - slug: $slug,
-    `,
-  ({ slug }) => {
-    describe("Quand j'arrive sur l'outil d'idemnité licenciement", () => {
-      let rendering;
-      beforeEach(async () => {
-        await act(async () => {
-          // @ts-ignore
-          const { props }: Props = await getServerSideProps({
-            query: { slug },
-          });
-          rendering = await render(<Outils {...props} />);
-        });
-      });
-      describe("Quand je réponds aux questions du contrat de travail", () => {
-        beforeEach(async () => {
-          await act(async () => {
-            await fireEvent.click(rendering?.queryByText("Commencer"));
-            await fireEvent.click(
-              rendering.queryByText("Contrat à durée indeterminé (CDI)")
-            );
-            await fireEvent.click(rendering.queryAllByText("Non")[0]);
-            await fireEvent.click(rendering.queryAllByText("Non")[1]);
-            await fireEvent.click(rendering.queryByText("Suivant"));
-          });
-        });
-        it("doit afficher la question sur la connaissance de la cc", () => {
-          expect(
-            rendering.queryByText(
-              "Quel est le nom de la convention collective applicable ?"
-            )
-          ).toBeInTheDocument();
-        });
-        describe("Quand je clique sur 'je sais'", () => {
-          beforeEach(async () => {
-            await act(async () => {
-              await fireEvent.click(
-                rendering.queryByText(
-                  "Je sais quelle est ma convention collective (je la saisis)"
-                )
-              );
-            });
-          });
-          it("doit afficher la partie de sélection de cc", () => {
-            expect(
-              rendering.queryByText(
-                "Précisez et sélectionnez votre convention collective"
-              )
-            ).toBeInTheDocument();
-            // expect(
-            //   rendering.getByRole("input", { name: "agreement-search" })
-            // ).toBeInTheDocument();
-          });
-        });
-      });
+jest.spyOn(Storage.prototype, "setItem");
+jest.spyOn(Storage.prototype, "getItem");
+
+describe("indemnite-licenciement", () => {
+  let rendering: RenderResult;
+  test(`
+    - Vérifier l'affichage de la question cc
+    - Vérifier l'affichage de la recherche
+  `, async () => {
+    //@ts-ignore
+    const result: any = await getServerSideProps({
+      query: { slug: "indemnite-licenciement" },
     });
-  }
-);
+    const props = result.props;
+    rendering = await render(<Outils {...props} />);
+    await fireEvent.click(rendering?.queryByText("Commencer") as HTMLElement);
+    await fireEvent.click(
+      rendering.queryByText("Contrat à durée indeterminé (CDI)") as HTMLElement
+    );
+    await fireEvent.click(rendering.queryAllByText("Non")[0] as HTMLElement);
+    await fireEvent.click(rendering.queryAllByText("Non")[1] as HTMLElement);
+    await fireEvent.click(rendering.queryByText("Suivant") as HTMLElement);
+
+    // Vérifier l'affichage de la question cc
+    expect(
+      rendering.queryByText(
+        "Quel est le nom de la convention collective applicable ?"
+      )
+    ).toBeInTheDocument();
+
+    // Vérifier l'affichage de la recherche
+    await fireEvent.click(
+      rendering.queryByText(
+        "Je sais quelle est ma convention collective (je la saisis)"
+      ) as HTMLElement
+    );
+    expect(
+      rendering.queryByText(
+        "Précisez et sélectionnez votre convention collective"
+      )
+    ).toBeInTheDocument();
+    expect(rendering.getByTestId("input-agreement")).toBeInTheDocument();
+    await fireEvent.change(rendering.getByTestId("input-agreement"), {
+      target: { value: "1" },
+    });
+    await waitFor(async () =>
+      expect(
+        rendering.queryByText(
+          "Transports routiers et activités auxiliaires du transport"
+        )
+      ).toBeInTheDocument()
+    );
+    fireEvent.click(
+      rendering.queryByText(
+        "Transports routiers et activités auxiliaires du transport"
+      ) as HTMLElement
+    );
+    expect(
+      rendering.queryByText(/Vous avez sélectionné la convention collective/)
+    ).toBeInTheDocument();
+  });
+});
--- packages/code-du-travail-frontend/__tests__/pages/outils/indemnite-licenciement-contract.test.tsx
+++ packages/code-du-travail-frontend/__tests__/pages/outils/indemnite-licenciement-contract.test.tsx
@@ -1,10 +1,8 @@
 import {
   default as Outils,
   getServerSideProps,
-  Props,
 } from "../../../pages/outils/[slug]";
 
-import { act } from "react-dom/test-utils";
 import { render, fireEvent, RenderResult } from "@testing-library/react";
 
 global.fetch = jest.fn(() =>
@@ -13,123 +11,81 @@ global.fetch = jest.fn(() =>
   })
 ) as jest.Mock;
 
-describe("Quand j'arrive sur l'outil d'idemnité licenciement", () => {
-  let rendering;
-  let props: Props;
+describe("indemnite-licenciement", () => {
+  let rendering: RenderResult;
   beforeAll(async () => {
     //@ts-ignore
     const result: any = await getServerSideProps({
       query: { slug: "indemnite-licenciement" },
     });
-    props = result.props;
+    const props = result.props;
+    rendering = await render(<Outils {...props} />);
   });
-  beforeEach(async () => {
-    await act(async () => {
-      rendering = await render(<Outils {...props} />);
-    });
-  });
-  it("doit afficher l'intro", () => {
+  test(`
+    - Vérifier l'affichage de l'intro
+    - Vérifier l'affichage de la partie contrat
+    - Vérifier l'ouverture des tooltips de la partie contrat
+  `, async () => {
+    // Vérifier l'affichage de l'intro
     expect(
       rendering.queryByText("Calculer l'indemnité de licenciement")
     ).toBeInTheDocument();
     expect(rendering.queryByText("Commencer")).toBeInTheDocument();
-  });
-  describe("Quand je clique sur le bouton commencer", () => {
-    beforeEach(
-      async () =>
-        await act(async () => {
-          await fireEvent.click(rendering.queryByText("Commencer"));
-        })
-    );
-    it("doit afficher les questions & réponses de la partie contrat", () => {
-      expect(
-        rendering.queryByText("Quel est le type du contrat de travail ?")
-      ).toBeInTheDocument();
-      expect(
-        rendering.queryByText(
-          "Contrat à durée determiné (CDD) ou contrat d’intérim"
-        )
-      ).toBeInTheDocument();
-      expect(
-        rendering.queryByText("Contrat à durée indeterminé (CDI)")
-      ).toBeInTheDocument();
-      expect(
-        rendering.queryByText(
-          "Le licenciement est-il dû à une faute grave (ou lourde) ?"
-        )
-      ).toBeInTheDocument();
-      expect(rendering.queryAllByText("Oui")[0]).toBeInTheDocument();
-      expect(rendering.queryAllByText("Non")[0]).toBeInTheDocument();
-      expect(
-        rendering.queryByText(
-          "Le licenciement est-il dû à une inaptitude suite à un accident du travail ou maladie professionnelle reconnue ?"
-        )
-      ).toBeInTheDocument();
-      expect(rendering.queryAllByText("Oui")[1]).toBeInTheDocument();
-      expect(rendering.queryAllByText("Non")[1]).toBeInTheDocument();
-    });
 
-    describe("Quand je clique sur la 1e réponse à la question type de contrat", () => {
-      beforeEach(
-        async () =>
-          await act(async () => {
-            await fireEvent.click(
-              rendering.queryByText(
-                "Contrat à durée determiné (CDD) ou contrat d’intérim"
-              )
-            );
-          })
-      );
-      it("doit afficher l'info bulle", () => {
-        expect(
-          rendering.queryByText("indemnité de précarité (nouvelle fenêtre)")
-        ).toBeInTheDocument();
-      });
-      describe("Quand je clique sur la 2e réponse à la question type de contrat", () => {
-        beforeEach(
-          async () =>
-            await act(async () => {
-              await fireEvent.click(
-                rendering.queryByText("Contrat à durée indeterminé (CDI)")
-              );
-            })
-        );
-        it("ne doit plus afficher l'info bulle", () => {
-          expect(
-            rendering.queryByText("indemnité de précarité (nouvelle fenêtre)")
-          ).not.toBeInTheDocument();
-        });
-      });
-      describe("Quand je clique sur la 1e réponse à la question faute grave", () => {
-        beforeEach(
-          async () =>
-            await act(async () => {
-              await fireEvent.click(rendering.queryAllByText("Oui")[0]);
-            })
-        );
-        it("doit afficher l'info bulle", () => {
-          expect(
-            rendering.queryByText(
-              "L’indemnité légale de licenciement n’est pas dûe en cas de faute grave."
-            )
-          ).toBeInTheDocument();
-        });
-        describe("Quand je clique sur la 2e réponse à la question type de contrat", () => {
-          beforeEach(
-            async () =>
-              await act(async () => {
-                await fireEvent.click(rendering.queryAllByText("Non")[0]);
-              })
-          );
-          it("ne doit plus afficher l'info bulle", () => {
-            expect(
-              rendering.queryByText(
-                "L’indemnité légale de licenciement n’est pas dûe en cas de faute grave."
-              )
-            ).not.toBeInTheDocument();
-          });
-        });
-      });
-    });
+    // Vérifier l'affichage de la partie contrat
+    await fireEvent.click(rendering.queryByText("Commencer") as HTMLElement);
+    expect(
+      rendering.queryByText("Quel est le type du contrat de travail ?")
+    ).toBeInTheDocument();
+    expect(
+      rendering.queryByText(
+        "Contrat à durée determiné (CDD) ou contrat d’intérim"
+      )
+    ).toBeInTheDocument();
+    expect(
+      rendering.queryByText("Contrat à durée indeterminé (CDI)")
+    ).toBeInTheDocument();
+    expect(
+      rendering.queryByText(
+        "Le licenciement est-il dû à une faute grave (ou lourde) ?"
+      )
+    ).toBeInTheDocument();
+    expect(rendering.queryAllByText("Oui")[0]).toBeInTheDocument();
+    expect(rendering.queryAllByText("Non")[0]).toBeInTheDocument();
+    expect(
+      rendering.queryByText(
+        "Le licenciement est-il dû à une inaptitude suite à un accident du travail ou maladie professionnelle reconnue ?"
+      )
+    ).toBeInTheDocument();
+    expect(rendering.queryAllByText("Oui")[1]).toBeInTheDocument();
+    expect(rendering.queryAllByText("Non")[1]).toBeInTheDocument();
+
+    // Vérifier l'ouverture des tooltips de la partie contrat
+    await fireEvent.click(
+      rendering.queryByText(
+        "Contrat à durée determiné (CDD) ou contrat d’intérim"
+      ) as HTMLElement
+    );
+    expect(
+      rendering.queryByText("indemnité de précarité (nouvelle fenêtre)")
+    ).toBeInTheDocument();
+    await fireEvent.click(
+      rendering.queryByText("Contrat à durée indeterminé (CDI)") as HTMLElement
+    );
+    expect(
+      rendering.queryByText("indemnité de précarité (nouvelle fenêtre)")
+    ).not.toBeInTheDocument();
+    await fireEvent.click(rendering.queryAllByText("Oui")[0]);
+    expect(
+      rendering.queryByText(
+        "L’indemnité légale de licenciement n’est pas dûe en cas de faute grave."
+      )
+    ).toBeInTheDocument();
+    await fireEvent.click(rendering.queryAllByText("Non")[0]);
+    expect(
+      rendering.queryByText(
+        "L’indemnité légale de licenciement n’est pas dûe en cas de faute grave."
+      )
+    ).not.toBeInTheDocument();
   });
 });
