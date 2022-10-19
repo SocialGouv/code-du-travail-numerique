c240d4256ab2c59d2f5127b80bbef3299a1fb472
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-10-19 19:31:21 +0200

    fix: tu

--- packages/code-du-travail-modeles/src/plugins/formule/573_commerces_de_gros.ts
+++ packages/code-du-travail-modeles/src/plugins/formule/573_commerces_de_gros.ts
@@ -31,7 +31,16 @@ export class Formula573
     const explanations = [];
     const roundSeniority = round(seniority);
     const year = roundSeniority < 2 ? "an" : "ans";
-
+    const roundSeniority2 = Math.min(round(seniority), 9);
+    const year2 = roundSeniority2 < 2 ? "an" : "ans";
+    const roundSeniority3 = Math.max(0, Math.min(round(seniority - 9), 10));
+    const year3 = roundSeniority3 < 2 ? "an" : "ans";
+    const roundSeniority4 = Math.max(0, round(seniority - 19));
+    const year4 = roundSeniority4 < 2 ? "an" : "ans";
+    const roundSeniority5 = Math.max(0, round(seniority - 10));
+    const year5 = roundSeniority5 < 2 ? "an" : "ans";
+    const roundSeniority6 = Math.max(0, round(seniority - 9));
+    const year6 = roundSeniority6 < 2 ? "an" : "ans";
     switch (true) {
       case category === CatPro573.autres && seniority >= 1 && seniority <= 10: {
         formula = `1 / 5 * Sref * A`;
@@ -42,13 +51,11 @@ export class Formula573
       }
       case category === CatPro573.autres && seniority > 10: {
         formula = `1 / 5 * Sref * A1 + 2 / 15 * Sref * A2`;
-        const roundSeniority2 = round(seniority - 10);
-        const year2 = roundSeniority2 < 2 ? "an" : "ans";
         explanations.push(
           `A1: Années d'ancienneté au total (${roundSeniority} ${year})`
         );
         explanations.push(
-          `A2: Années d'ancienneté au delà de 10 ans (${roundSeniority2} ${year2})`
+          `A2: Années d'ancienneté au delà de 10 ans (${roundSeniority5} ${year5})`
         );
         break;
       }
@@ -57,14 +64,12 @@ export class Formula573
         age >= 55 &&
         typeLicenciement === LicenciementEconomique.oui: {
         formula = `2 / 10 * Sref * A1 + 3 / 10 * Sref * A2 + 20% (2 / 10 * Sref * A1 + 3 / 10 * Sref)`;
-        const roundSeniority2 = Math.max(round(seniority), 9);
-        const roundSeniority3 = round(seniority - 9);
-        const year3 = roundSeniority3 < 2 ? "an" : "ans";
+
         explanations.push(
-          `A1: Années de présence dans la tranche de 0 à 9 ans inclus (${roundSeniority2} ${year})`
+          `A1: Années de présence dans la tranche de 0 à 9 ans inclus (${roundSeniority2} ${year2})`
         );
         explanations.push(
-          `A2: Années de présence dans la tranche à partir de 10 ans (${roundSeniority3} ${year3})`
+          `A2: Années de présence dans la tranche à partir de 10 ans (${roundSeniority6} ${year6})`
         );
         break;
       }
@@ -80,50 +85,54 @@ export class Formula573
         explanations.push(
           `A1: Années d'ancienneté au total (${roundSeniority} ${year})`
         );
-        explanations.push("A2: Années d'ancienneté au delà de 10 ans");
+        explanations.push(
+          `A2: Années d'ancienneté au delà de 10 ans (${roundSeniority5} ${year5})`
+        );
         break;
       }
       case category === CatPro573.cadres && seniority >= 1 && seniority <= 5: {
         formula = `2 / 10 * Sref * A`;
-        explanations.push("A: Années d'ancienneté au total");
+        explanations.push(
+          `A: Années d'ancienneté au total (${roundSeniority} ${year})`
+        );
         break;
       }
       case category === CatPro573.cadres && seniority >= 15 && age >= 55: {
         formula = `3 / 10 * Sref * A1 + 4 / 10 * Sref * A2 + 5 / 10 * Sref * A3 + 20% ( 3 / 10 * Sref * A1 + 4 / 10 * Sref * A2 + 5 / 10 * Sref * A3 )`;
         explanations.push(
-          "A1: Années de présence dans la tranche de 0 à 9 ans inclus"
+          `A1: Années de présence dans la tranche de 0 à 9 ans inclus (${roundSeniority2} ${year2})`
         );
         explanations.push(
-          "A2: Années de présence dans la tranche de 10 à 19 ans inclus"
+          `A2: Années de présence dans la tranche de 10 à 19 ans inclus (${roundSeniority3} ${year3})`
         );
         explanations.push(
-          "A3: Années de présence dans la tranche à partir de 20 ans"
+          `A3: Années de présence dans la tranche à partir de 20 ans (${roundSeniority4} ${year4})`
         );
         break;
       }
       case category === CatPro573.cadres && seniority >= 15 && age >= 50: {
         formula = `3 / 10 * Sref * A1 + 4 / 10 * Sref * A2 + 5 / 10 * Sref * A3 + 15% ( 3 / 10 * Sref * A1 + 4 / 10 * Sref * A2 + 5 / 10 * Sref )`;
         explanations.push(
-          "A1: Années de présence dans la tranche de 0 à 9 ans inclus"
+          `A1: Années de présence dans la tranche de 0 à 9 ans inclus (${roundSeniority2} ${year2})`
         );
         explanations.push(
-          "A2: Années de présence dans la tranche de 10 à 19 ans inclus"
+          `A2: Années de présence dans la tranche de 10 à 19 ans inclus (${roundSeniority3} ${year3})`
         );
         explanations.push(
-          "A3: Années de présence dans la tranche à partir de 20 ans"
+          `A3: Années de présence dans la tranche à partir de 20 ans (${roundSeniority4} ${year4})`
         );
         break;
       }
       case category === CatPro573.cadres && seniority > 5: {
         formula = `3 / 10 * Sref * A1 + 4 / 10 * Sref * A2 + 5 / 10 * Sref * A3`;
         explanations.push(
-          "A1: Années de présence dans la tranche de 0 à 9 ans inclus"
+          `A1: Années de présence dans la tranche de 0 à 9 ans inclus (${roundSeniority2} ${year2})`
         );
         explanations.push(
-          "A2: Années de présence dans la tranche de 10 à 19 ans inclus"
+          `A2: Années de présence dans la tranche de 10 à 19 ans inclus (${roundSeniority3} ${year3})`
         );
         explanations.push(
-          "A3: Années de présence dans la tranche à partir de 20 ans"
+          `A3: Années de présence dans la tranche à partir de 20 ans (${roundSeniority4} ${year4})`
         );
         break;
       }
@@ -132,8 +141,12 @@ export class Formula573
       explanations.push(`Sref : Salaire de référence (${round(refSalary)} €)`);
     }
 
+    const filteredExplanations = explanations.filter(
+      (el) => !el.includes("(0 an)")
+    );
+
     return {
-      explanations,
+      explanations: filteredExplanations,
       formula,
     };
   }
