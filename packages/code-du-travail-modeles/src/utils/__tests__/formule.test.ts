4062ad10e316a74f7359704bcd5c124a2a87970d
carolineBda <caroline.bourdeu@gmail.com>
2022-10-26 16:20:13 +0200

    fix

--- packages/code-du-travail-modeles/src/utils/__tests__/formule.test.ts
+++ packages/code-du-travail-modeles/src/utils/__tests__/formule.test.ts
@@ -43,24 +43,22 @@ describe("Formula", () => {
   describe("Check formula for CC when fold back to droit du travail", () => {
     test.each`
       age   | seniority | expectedFormula                            | expectedExplanations
-      ${50} | ${5}      | ${"(1/4 * Sref * A1)"}                     | ${["A1 : Ancienneté totale (5 ans)", "Sref : Salaire de référence (2300 €)"]}
+      ${50} | ${5}      | ${"1/4 * Sref * A"}                        | ${["A : Ancienneté totale (5 ans)", "Sref : Salaire de référence (2300 €)"]}
       ${50} | ${16.4}   | ${"(1/4 * Sref * A1) + (1/3 * Sref * A2)"} | ${["A1 : Ancienneté totale (16.4 ans)", "A2: Années d'ancienneté au-delà de 10 ans (6.4 ans)", "Sref : Salaire de référence (2300 €)"]}
     `(
       "Avec une séniorité de $seniority ans: $expectedFormula",
-      ({ seniority }) => {
+      ({ seniority, expectedFormula, expectedExplanations }) => {
         const situation = engine.setSituation({
           "contrat salarié . convention collective": "'IDCC1090'",
-          "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
+          "contrat salarié . indemnité de licenciement . ancienneté en année":
             seniority,
           "contrat salarié . indemnité de licenciement . salaire de référence conventionnel": 2300,
           "indemnité de licenciement": "oui",
         });
         const formule = getFormule(situation);
 
-        expect(formule.formula).toEqual(
-          "(1/4 * Sref * A1) + (1/3 * Sref * A2)"
-        );
-        expect(formule.explanations).toEqual([]);
+        expect(formule.formula).toEqual(expectedFormula);
+        expect(formule.explanations).toEqual(expectedExplanations);
       }
     );
   });
