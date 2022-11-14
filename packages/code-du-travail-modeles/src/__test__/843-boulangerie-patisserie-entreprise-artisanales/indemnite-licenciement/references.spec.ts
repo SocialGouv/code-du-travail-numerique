e364f7ae051d019eff482c80b5aef9e0d350a208
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2022-11-14 11:05:29 +0100

    feat(indemnité-licenciemnt): ajout de la cc 843 (#4614)

--- packages/code-du-travail-modeles/src/__test__/843-boulangerie-patisserie-entreprise-artisanales/indemnite-licenciement/references.spec.ts
+++ packages/code-du-travail-modeles/src/__test__/843-boulangerie-patisserie-entreprise-artisanales/indemnite-licenciement/references.spec.ts
@@ -7,12 +7,12 @@ const engine = new Engine(mergeIndemniteLicenciementModels());
 
 const References = [
   {
-    article: "Article 6.2.1",
-    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALISCTA000005719412/?idConteneur=KALICONT000005635826",
+    article: "Article 33",
+    url: "https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000005873155/?idConteneur=KALICONT000005635886",
   },
 ];
 
-describe("Vérification des références juridiques pour la CC 787", () => {
+describe("Vérification des références juridiques pour la CC 843", () => {
   test.each`
     seniority | expectedReferences
     ${5}      | ${References}
@@ -23,7 +23,7 @@ describe("Vérification des références juridiques pour la CC 787", () => {
     ({ seniority, expectedReferences }) => {
       const result = getReferences(
         engine.setSituation({
-          "contrat salarié . convention collective": "'IDCC0787'",
+          "contrat salarié . convention collective": "'IDCC0843'",
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
             seniority,
           "contrat salarié . indemnité de licenciement . salaire de référence conventionnel": 1000,
