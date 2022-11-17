ded5f397171635e85b13344a8a64a32db331085b
Martial Maillot <martial.maillot@gmail.com>
2022-11-17 10:51:27 +0100

    fix(preavis-retraite): Forcer publicodes à calculer la prochaine question au changement d'origine (#4669)

--- packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/__tests__/ui.ts
+++ packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/__tests__/ui.ts
@@ -4,19 +4,29 @@ import { ui as uiCommon } from "../../__tests__/ui";
 export const ui = {
   ...uiCommon,
   origin: {
-    departRetraiteOui: byTestId("origin.isRetirementMandatory-depart"),
-    departRetraiteNon: byTestId("origin.isRetirementMandatory-mise"),
+    mise: byTestId("mise-a-la-retraite"),
+    depart: byTestId("depart-a-la-retraite"),
   },
-  agreement1351: {
-    searchResult: byText(/Entreprises de prévention et de sécurité/),
-    categoryProInput: byTestId(
-      "infos.contrat salarié - convention collective - prevention sécurité entreprise - catégorie professionnelle"
+  information: {
+    handicap: byText(
+      /Le salarié concerné est-il reconnu en tant que travailleur handicapé/
     ),
-    disableWorkerYesInput: byTestId(
-      "infos.contrat salarié - travailleur handicapé-oui"
-    ),
-    disableWorkerNoInput: byTestId(
-      "infos.contrat salarié - travailleur handicapé-non"
-    ),
+    agreement1517: {
+      categoryPro: byText(/Quelle est la catégorie professionnelle du salarié/),
+      categoryProInput: byTestId(
+        "infos.contrat salarié - convention collective - commerces de detail non alimentaires - catégorie professionnelle"
+      ),
+    },
+    agreement1351: {
+      categoryProInput: byTestId(
+        "infos.contrat salarié - convention collective - prevention sécurité entreprise - catégorie professionnelle"
+      ),
+      disableWorkerYesInput: byTestId(
+        "infos.contrat salarié - travailleur handicapé-oui"
+      ),
+      disableWorkerNoInput: byTestId(
+        "infos.contrat salarié - travailleur handicapé-non"
+      ),
+    }
   },
 };
