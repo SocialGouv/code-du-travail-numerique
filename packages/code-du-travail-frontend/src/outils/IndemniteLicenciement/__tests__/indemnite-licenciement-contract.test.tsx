8c673a1ba62fae2b26e050dcd3b57400b8929eb5
Victor Zeinstra <zeinsta.victor@gmail.com>
2022-11-25 14:56:53 +0100

    feat: display questions one by one in contract part

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/__tests__/indemnite-licenciement-contract.test.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/__tests__/indemnite-licenciement-contract.test.tsx
@@ -8,8 +8,9 @@ import { render, fireEvent } from "@testing-library/react";
 
 test(`
   - Vérifier l'affichage de l'intro
-  - Vérifier l'affichage des questions de contrat
-  - Vérifier les bulles d'info
+  - Vérifier l'affichage de la question type contrat
+  - Vérifier l'affichage de la question faute grave
+  - Vérifier l'affichage de la question inaptitude
 `, async () => {
   await render(
     <CalculateurIndemnite
@@ -24,27 +25,32 @@ test(`
   expect(ui.introduction.startButton.query()).toBeInTheDocument();
   fireEvent.click(ui.introduction.startButton.get());
 
-  // Vérifier l'affichage des questions de contrat
+  // Vérifier l'affichage de la question type contrat
   expect(ui.contract.type.question.query()).toBeInTheDocument();
   expect(ui.contract.type.cdi.query()).toBeInTheDocument();
   expect(ui.contract.type.cdd.query()).toBeInTheDocument();
-
-  expect(ui.contract.fauteGrave.question.query()).toBeInTheDocument();
-  expect(ui.contract.fauteGrave.oui.query()).toBeInTheDocument();
-  expect(ui.contract.fauteGrave.non.query()).toBeInTheDocument();
-
-  expect(ui.contract.inaptitude.question.query()).toBeInTheDocument();
-  expect(ui.contract.inaptitude.oui.query()).toBeInTheDocument();
-  expect(ui.contract.inaptitude.non.query()).toBeInTheDocument();
-
-  // Vérifier l'affichage des bulles d'info
+  expect(ui.contract.fauteGrave.question.query()).not.toBeInTheDocument();
+  expect(ui.contract.fauteGrave.oui.query()).not.toBeInTheDocument();
+  expect(ui.contract.fauteGrave.non.query()).not.toBeInTheDocument();
+  expect(ui.contract.inaptitude.question.query()).not.toBeInTheDocument();
+  expect(ui.contract.inaptitude.oui.query()).not.toBeInTheDocument();
+  expect(ui.contract.inaptitude.non.query()).not.toBeInTheDocument();
   fireEvent.click(ui.contract.type.cdd.get());
   expect(ui.contract.type.alert.query()).toBeInTheDocument();
   fireEvent.click(ui.contract.type.cdi.get());
   expect(ui.contract.type.alert.query()).not.toBeInTheDocument();
 
+  // Vérifier l'affichage de la question faute grave
+  expect(ui.contract.fauteGrave.question.query()).toBeInTheDocument();
+  expect(ui.contract.fauteGrave.oui.query()).toBeInTheDocument();
+  expect(ui.contract.fauteGrave.non.query()).toBeInTheDocument();
   fireEvent.click(ui.contract.fauteGrave.oui.get());
   expect(ui.contract.fauteGrave.alert.query()).toBeInTheDocument();
   fireEvent.click(ui.contract.fauteGrave.non.get());
   expect(ui.contract.fauteGrave.alert.query()).not.toBeInTheDocument();
+
+  // Vérifier l'affichage de la question inaptitude
+  expect(ui.contract.inaptitude.question.query()).toBeInTheDocument();
+  expect(ui.contract.inaptitude.oui.query()).toBeInTheDocument();
+  expect(ui.contract.inaptitude.non.query()).toBeInTheDocument();
 });
