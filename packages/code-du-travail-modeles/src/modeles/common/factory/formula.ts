b3bc5d2e609bcee0c684517faa78faa12bfafafa
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-11-25 12:04:23 +0100

    fix: cc

--- packages/code-du-travail-modeles/src/modeles/common/factory/formula.ts
+++ packages/code-du-travail-modeles/src/modeles/common/factory/formula.ts
@@ -4,7 +4,6 @@ import { Formula650 } from "../../conventions/650_metallurgie_ingenieurs_cadres"
 import { Formula1516 } from "../../conventions/1516_organismes_formation";
 import { Formula1596 } from "../../conventions/1596_batiment_ouvriers_employes";
 import { Formula1597 } from "../../conventions/1597_batiment_employes_ouvriers_bis";
-import { Formula3043 } from "../../conventions/3043_entreprises_proprete";
 import { Formula3127 } from "../../conventions/3127_entreprises_services_a_la_personne";
 import type { IFormula } from "..";
 import { SupportedCcIndemniteLicenciement } from "../types";
@@ -16,8 +15,6 @@ export class FormuleFactory {
     switch (idcc) {
       case SupportedCcIndemniteLicenciement.IDCC1516:
         return new Formula1516() as IFormula<T>;
-      case SupportedCcIndemniteLicenciement.IDCC3043:
-        return new Formula3043() as IFormula<T>;
       case SupportedCcIndemniteLicenciement.IDCC3127:
         return new Formula3127() as IFormula<T>;
       case SupportedCcIndemniteLicenciement.IDCC1596:
