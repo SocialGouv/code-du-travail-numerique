bfa015f5884d61be3b77cff1a6d913d83fe67e2d
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-12-05 15:16:28 +0100

    fix: file

--- packages/code-du-travail-frontend/src/conventions/Search/api/__mocks__/agreements.service.ts
+++ packages/code-du-travail-frontend/src/conventions/Search/api/__mocks__/agreements.service.ts
@@ -1,44 +1,41 @@
-export const searchagreements = (query: string) => {
+export const searchAgreements = (query: string) => {
   switch (query) {
     case "16":
-      return promise.resolve(
-        [
+      return Promise.resolve([
         {
-          url: "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000005635624",
+          url:
+            "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000005635624",
           id: "KALICONT000005635624",
           num: 16,
-          shorttitle:
-          "Transports routiers et activités auxiliaires du transport",
+          shortTitle:
+            "Transports routiers et activités auxiliaires du transport",
           slug: "16-transports-routiers-et-activites-auxiliaires-du-transport",
           title: "Transports routiers et activités auxiliaires du transport",
         },
-        ]
-      );
+      ]);
     case "1351":
-      return promise.resolve(
-        [
+      return Promise.resolve([
         {
-          url: "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000005635405",
+          url:
+            "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000005635405",
           id: "KALICONT000005635405",
           num: 1351,
-          shorttitle: "Entreprises de prévention et de sécurité",
+          shortTitle: "Entreprises de prévention et de sécurité",
           slug: "1351-entreprises-de-prevention-et-de-securite",
           title: "Entreprises de prévention et de sécurité",
         },
-        ]
-      );
+      ]);
     case "3239":
-      return promise.resolve(
-        [
+      return Promise.resolve([
         {
-          url: "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000044594539",
+          url:
+            "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000044594539",
           id: "KALICONT000044594539",
           num: 3239,
-          shorttitle: "Particuliers employeurs et emploi à domicile",
+          shortTitle: "Particuliers employeurs et emploi à domicile",
           slug: "3239-particuliers-employeurs-et-emploi-a-domicile",
           title: "Particuliers employeurs et emploi à domicile",
         },
-        ]
-      );
+      ]);
   }
-}
+};
