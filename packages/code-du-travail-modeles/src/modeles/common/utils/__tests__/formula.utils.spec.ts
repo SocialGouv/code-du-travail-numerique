36893756367c91a9067b80858468f26c97f99f8d
carolineBda <caroline.bourdeu@gmail.com>
2022-12-05 17:32:57 +0100

    add formula

--- packages/code-du-travail-modeles/src/modeles/common/utils/__tests__/formula.utils.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/common/utils/__tests__/formula.utils.spec.ts
@@ -1,39 +1,51 @@
-import { cleanFormula } from "../formula";
+import { cleanFormula, removePartFromFormula } from "../formula";
 
-describe("cleanFormula", () => {
-  test("should remove good part from formula", () => {
-    const formula = "[20% * A1][ + A2]";
-    const explanations = ["A1 : Quantité (12 litres)"];
-    expect(cleanFormula(formula, explanations)).toEqual("20% * A1");
+describe("formule utils", () => {
+  describe("cleanFormula", () => {
+    test("should do nothing if nothing to clean", () => {
+      const formula = "20% * A1 + A2";
+      expect(cleanFormula(formula)).toEqual("20% * A1 + A2");
+    });
+
+    test("should clean square brackets", () => {
+      const formula = "[20% * A1][ + A2]";
+      expect(cleanFormula(formula)).toEqual("20% * A1 + A2");
+    });
+
+    test("should remove spaces at the beginning", () => {
+      expect(cleanFormula(" 20% * A1 + A2")).toEqual("20% * A1 + A2");
+    });
+
+    test("should remove + at the beginning", () => {
+      expect(cleanFormula("+ 20% * A1 + A2")).toEqual("20% * A1 + A2");
+    });
   });
+  describe("removePartFromFormula", () => {
+    test("should remove good part from formula", () => {
+      const formula = "[20% * A1][ + A2]";
+      expect(removePartFromFormula(formula, "A2 : prix")).toEqual("[20% * A1]");
+    });
 
-  test("should good part with + from formula", () => {
-    const formula = "[20% * A1][ + A2]";
-    const explanations = ["A2 : Quantité (12 litres)"];
-    expect(cleanFormula(formula, explanations)).toEqual("A2");
-  });
+    test("should good part with + from formula", () => {
+      const formula = "[20% * A1][ + A2]";
+      expect(removePartFromFormula(formula, "A1 : Quantité")).toEqual(
+        "[ + A2]"
+      );
+    });
 
-  test("should work with more complexe example", () => {
-    const formula = "[20% * A1 * A2][ + A3][ + A4]";
-    const explanations = [
-      "A1 : Prix (20 euros)",
-      "A2 : Quantité (12 litres)",
-      "A3 : Majoration (30 euros)",
-    ];
-    expect(cleanFormula(formula, explanations)).toEqual("20% * A1 * A2 + A3");
-  });
+    test("should work with more complexe example", () => {
+      const formula = "[20% * A1 * A2][ + A3][ + A4]";
+      expect(removePartFromFormula(formula, "A4 :  je suis vide")).toEqual(
+        "[20% * A1 * A2][ + A3]"
+      );
+    });
 
-  test("should work with a real example", () => {
-    const formula =
-      "[(1 / 5 * Sref * A1)][ + (2 / 5 * Sref * A2)][ + (1 / 2 * Sref * A3)][ + (Sref * A4)]";
-    const explanations = [
-      "A1 : Ancienneté de 10 ans ou moins dans la fonction de non-cadre (1 an)",
-      "A3 : Années d'ancienneté dans la fonction de cadre jusqu'à 5 ans (5 ans)",
-      "A4 : Années d'ancienneté dans la fonction de cadre supérieures à 5 ans (10 ans)",
-      "Sref : Salaire de référence (1000 €)",
-    ];
-    expect(cleanFormula(formula, explanations)).toEqual(
-      "(1 / 5 * Sref * A1) + (1 / 2 * Sref * A3) + (Sref * A4)"
-    );
+    test("should work with a real example", () => {
+      const formula =
+        "[(1 / 5 * Sref * A1)][ + (2 / 5 * Sref * A2)][ + (1 / 2 * Sref * A3)][ + (Sref * A4)]";
+      expect(removePartFromFormula(formula, "A2: Années")).toEqual(
+        "[(1 / 5 * Sref * A1)][ + (1 / 2 * Sref * A3)][ + (Sref * A4)]"
+      );
+    });
   });
 });
