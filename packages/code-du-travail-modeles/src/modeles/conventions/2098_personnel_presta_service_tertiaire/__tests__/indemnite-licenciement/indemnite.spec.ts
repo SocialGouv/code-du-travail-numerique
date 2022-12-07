e0d31e04074a0ed70b420d6bf183411208de631d
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-12-07 17:52:37 +0100

    fix: tu

--- packages/code-du-travail-modeles/src/modeles/conventions/2098_personnel_presta_service_tertiaire/__tests__/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/2098_personnel_presta_service_tertiaire/__tests__/indemnite-licenciement/indemnite.spec.ts
@@ -19,8 +19,10 @@ describe("Calcul de l'indemnité de licenciement CC 2098", () => {
             "contrat salarié . convention collective . personnel presta service tertiaire . inaptitude suite à un accident non professionnelle":
               "'Oui'",
             "contrat salarié . indemnité de licenciement": "oui",
-            "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année": seniority,
-            "contrat salarié . indemnité de licenciement . ancienneté en année": seniority,
+            "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
+              seniority,
+            "contrat salarié . indemnité de licenciement . ancienneté en année":
+              seniority,
             "contrat salarié . indemnité de licenciement . inaptitude suite à un accident ou maladie professionnelle":
               "non",
             "contrat salarié . indemnité de licenciement . salaire de référence": 2800,
@@ -57,7 +59,8 @@ describe("Calcul de l'indemnité de licenciement CC 2098", () => {
             "contrat salarié . convention collective . personnel presta service tertiaire . inaptitude suite à un accident non professionnelle":
               "'Non'",
             "contrat salarié . indemnité de licenciement": "oui",
-            "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année": seniority,
+            "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
+              seniority,
             "contrat salarié . indemnité de licenciement . salaire de référence conventionnel": 2800,
           })
           .evaluate(
@@ -71,39 +74,42 @@ describe("Calcul de l'indemnité de licenciement CC 2098", () => {
     );
 
     test.each`
-      seniority | age   | expectedCompensation
-      ${1}      | ${35} | ${0}
-      ${2}      | ${35} | ${2223.6}
-      ${7}      | ${35} | ${8523.8}
-      ${14}     | ${35} | ${20383}
-      ${22}     | ${35} | ${37801.2}
-      ${33}     | ${35} | ${62260.8}
-      ${1}      | ${54} | ${0}
-      ${2}      | ${54} | ${2445.96}
-      ${7}      | ${54} | ${9376.18}
-      ${14}     | ${54} | ${22421.3}
-      ${22}     | ${54} | ${41581.32}
-      ${33}     | ${54} | ${66708}
-      ${1}      | ${57} | ${0}
-      ${2}      | ${57} | ${2779.5}
-      ${7}      | ${57} | ${10654.75}
-      ${14}     | ${57} | ${25478.75}
-      ${22}     | ${57} | ${47251.5}
-      ${33}     | ${57} | ${66708}
+      seniority | age   | salaireRef | expectedCompensation
+      ${1}      | ${35} | ${3706}    | ${0}
+      ${2}      | ${35} | ${3706}    | ${2223.6}
+      ${7}      | ${35} | ${3706}    | ${8523.8}
+      ${14}     | ${35} | ${3706}    | ${20383}
+      ${22}     | ${35} | ${3706}    | ${37801.2}
+      ${33}     | ${35} | ${3706}    | ${62260.8}
+      ${1}      | ${54} | ${3706}    | ${0}
+      ${2}      | ${54} | ${3706}    | ${2445.96}
+      ${7}      | ${54} | ${3706}    | ${9376.18}
+      ${14}     | ${54} | ${3706}    | ${22421.3}
+      ${22}     | ${54} | ${3706}    | ${41581.32}
+      ${33}     | ${54} | ${3706}    | ${66708}
+      ${1}      | ${57} | ${3706}    | ${0}
+      ${2}      | ${57} | ${3706}    | ${2779.5}
+      ${7}      | ${57} | ${3706}    | ${10654.75}
+      ${14}     | ${57} | ${3706}    | ${25478.75}
+      ${22}     | ${57} | ${3706}    | ${47251.5}
+      ${33}     | ${57} | ${3706}    | ${66708}
     `(
       "Cadres, avec une ancienneté $seniority ans, un age $age ans, un salaire de référence 3706€ => une compensation de base de $expectedCompensation €",
       ({ salaireRef, expectedCompensation, seniority, age }) => {
         const result = engine
           .setSituation({
             "contrat salarié . convention collective": "'IDCC2098'",
-            "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . cadres . age": age,
+            "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . cadres . age":
+              age,
             "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . catégorie professionnelle":
               "'Cadres'",
             "contrat salarié . convention collective . personnel presta service tertiaire . inaptitude suite à un accident non professionnelle":
               "'Non'",
             "contrat salarié . indemnité de licenciement": "oui",
-            "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année": seniority,
-            "contrat salarié . indemnité de licenciement . salaire de référence conventionnel": salaireRef,
+            "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
+              seniority,
+            "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
+              salaireRef,
           })
           .evaluate(
             "contrat salarié . indemnité de licenciement . résultat conventionnel"
