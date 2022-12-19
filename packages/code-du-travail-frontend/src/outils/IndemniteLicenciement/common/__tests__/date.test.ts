03efeb8a4a292baaa1ee065c26378fe06f3c1d32
Victor Zeinstra <zeinsta.victor@gmail.com>
2022-12-19 12:56:47 +0100

    chore: TU

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/common/__tests__/date.test.ts
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/common/__tests__/date.test.ts
@@ -2,7 +2,6 @@ import { dateOneDayLater } from "../date";
 
 describe("dateOneDayLater", () => {
   test("increment one day", () => {
-    const newDate = dateOneDayLater("31/01/2022");
-    expect(newDate).toBe("01/02/2022");
+    expect(dateOneDayLater("31/01/2022")).toBe("01/02/2022");
   });
 });
