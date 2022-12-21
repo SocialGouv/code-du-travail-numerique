8f63d3665a5c0b90c63014c4d3343add8fdff2ea
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-12-21 20:30:57 +0100

    fix: code

--- packages/code-du-travail-modeles/src/modeles/base/requiredSeniority.ts
+++ packages/code-du-travail-modeles/src/modeles/base/requiredSeniority.ts
@@ -11,5 +11,5 @@ export function computeRequiredSeniority({
 }: RequiredSeniorityProps): number {
   const dEntree = parse(dateEntree, "dd/MM/yyyy", new Date());
   const dNotification = parse(dateNotification, "dd/MM/yyyy", new Date());
-  return differenceInMonths(dNotification, dEntree);
+  return differenceInMonths(dNotification, dEntree) / 12;
 }
