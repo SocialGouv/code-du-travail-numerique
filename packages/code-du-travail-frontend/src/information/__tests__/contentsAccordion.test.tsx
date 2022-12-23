04d9d29b368745f09b89478343d897b9913817ed
Victor Zeinstra <zeinstra.victor@gmail.com>
2022-12-23 12:16:29 +0100

    fix: références manquante dans les sections page info (#4822)
    Co-authored-by: Victor Zeinstra <zeinsta.victor@gmail.com>

--- packages/code-du-travail-frontend/src/information/__tests__/contentsAccordion.test.tsx
+++ packages/code-du-travail-frontend/src/information/__tests__/contentsAccordion.test.tsx
@@ -5,49 +5,76 @@ import { ContentType, SectionDisplayMode } from "cdtn-types";
 
 jest.mock("../htmlProcess.service");
 
-test(`Information Contents
-  - Vérifier l'affichage en mode accordéon
-  - Vérifier l'ouverture des accordéons
-`, async () => {
-  await render(
-    <Contents
-      anchor={[]}
-      dismissalProcess={false}
-      sectionDisplayMode={SectionDisplayMode.accordion}
-      contents={[
-        {
-          name: "accordion1",
-          title: "Accordion1",
-          blocks: [
-            {
-              type: ContentType.markdown,
-              markdown: "myText1",
-              html: "myText1",
-            },
-          ],
-        },
-        {
-          name: "accordion2",
-          title: "Accordion2",
-          blocks: [
-            {
-              type: ContentType.markdown,
-              markdown: "myText2",
-              html: "myText2",
-            },
-          ],
-        },
-      ]}
-    />
-  );
-  // Vérifier l'affichage en mode accordéon
-  expect(screen.queryByText("Accordion1")).toBeInTheDocument();
-  expect(ui.accordionButton(0).query()).toBeInTheDocument();
-  expect(screen.queryByText("Accordion2")).toBeInTheDocument();
-  expect(ui.accordionButton(1).query()).toBeInTheDocument();
-  // Vérifier l'ouverture des accordéons
-  fireEvent.click(ui.accordionButton(0).get());
-  expect(screen.queryByText("myText1")).toBeInTheDocument();
-  fireEvent.click(ui.accordionButton(1).get());
-  expect(screen.queryByText("myText2")).toBeInTheDocument();
+describe("Information Contents", () => {
+  beforeEach(() => {
+    render(
+      <Contents
+        anchor={[]}
+        dismissalProcess={false}
+        sectionDisplayMode={SectionDisplayMode.accordion}
+        contents={[
+          {
+            name: "accordion1",
+            title: "Accordion1",
+            blocks: [
+              {
+                type: ContentType.markdown,
+                markdown: "myText1",
+                html: "myText1",
+              },
+            ],
+            references: [
+              {
+                label: "referenceLabel",
+                links: [
+                  {
+                    id: "id1",
+                    slug: "slug1",
+                    title: "referenceLink1",
+                    type: "external",
+                    url: "url1",
+                  },
+                  {
+                    id: "id2",
+                    slug: "slug2",
+                    title: "referenceLink2",
+                    type: "external",
+                    url: "url2",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "accordion2",
+            title: "Accordion2",
+            blocks: [
+              {
+                type: ContentType.markdown,
+                markdown: "myText2",
+                html: "myText2",
+              },
+            ],
+            references: [],
+          },
+        ]}
+      />
+    );
+  });
+  test("Vérifier l'affichage en mode accordéon", () => {
+    expect(screen.queryByText("Accordion1")).toBeInTheDocument();
+    expect(ui.accordionButton(0).query()).toBeInTheDocument();
+    expect(screen.queryByText("Accordion2")).toBeInTheDocument();
+    expect(ui.accordionButton(1).query()).toBeInTheDocument();
+  });
+  test("Vérifier l'ouverture des accordéons", () => {
+    fireEvent.click(ui.accordionButton(0).get());
+    expect(screen.queryByText("myText1")).toBeInTheDocument();
+    fireEvent.click(ui.accordionButton(1).get());
+    expect(screen.queryByText("myText2")).toBeInTheDocument();
+  });
+  test("Vérifier l'affichage des références", () => {
+    expect(screen.queryByText("referenceLink1")).toBeInTheDocument();
+    expect(screen.queryByText("referenceLink2")).toBeInTheDocument();
+  });
 });
