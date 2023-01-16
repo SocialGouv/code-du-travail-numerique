467451c1ace19b6899e1d01e91ddfd9ba2d70491
Caroline <4971715+carolineBda@users.noreply.github.com>
2023-01-16 12:13:33 +0100

    fix(2098): refactor missing vars + missing parts of formula when majoration (#4877)

--- packages/code-du-travail-modeles/src/modeles/conventions/2098_personnel_presta_service_tertiaire/__tests__/indemnite-licenciement/formule.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/2098_personnel_presta_service_tertiaire/__tests__/indemnite-licenciement/formule.spec.ts
@@ -5,7 +5,7 @@ describe("Formule de l'indemnité de licenciement - CC 2098", () => {
     test.each`
       seniority | expectedFormula                            | expectedExplanations
       ${7 / 12} | ${""}                                      | ${[]}
-      ${0.67}   | ${"(1/4 * Sref * A1)"}                     | ${["A1 : Ancienneté de 10 ans ou moins (0.67 an)", "Sref : Salaire de référence (2800 €)"]}
+      ${0.67}   | ${"1/4 * Sref * A"}                        | ${["A : Ancienneté totale (0.67 an)", "Sref : Salaire de référence (2800 €)"]}
       ${18}     | ${"(1/4 * Sref * A1) + (1/3 * Sref * A2)"} | ${["A1 : Ancienneté de 10 ans ou moins (10 ans)", "A2 : Ancienneté au-delà de 10 ans (8 ans)", "Sref : Salaire de référence (2800 €)"]}
     `(
       "Avec une ancienneté $seniority ans, un salaire de référence $salaireRef € => $expectedFormula",
@@ -44,7 +44,7 @@ describe("Formule de l'indemnité de licenciement - CC 2098", () => {
           "contrat salarié . convention collective": "'IDCC2098'",
           "contrat salarié . convention collective . personnel presta service tertiaire . inaptitude suite à un accident non professionnelle":
             "'Non'",
-          "contrat salarié . convention collective . personnel presta service tertiaire . inaptitude suite à un accident non professionnelle . autre licenciement . catégorie professionnelle":
+          "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . catégorie professionnelle":
             "'Non-cadres'",
           "contrat salarié . indemnité de licenciement": "oui",
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année": seniority,
@@ -59,17 +59,23 @@ describe("Formule de l'indemnité de licenciement - CC 2098", () => {
     );
 
     test.each`
-      seniority  | age   | expectedFormula                                                                        | expectedExplanations
-      ${23 / 12} | ${35} | ${""}                                                                                  | ${[]}
-      ${2}       | ${35} | ${"3/10 * Sref * A"}                                                                   | ${["A: Années d'ancienneté à compter de la 1re année (2 ans)", "Sref : Salaire de référence (2800 €)"]}
-      ${7}       | ${35} | ${"(3/10 * Sref * A1) + (4/10 * Sref * A2)"}                                           | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (2 ans)", "Sref : Salaire de référence (2800 €)"]}
-      ${14}      | ${35} | ${"(3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3)"}                      | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (5 ans)", "A3: Années d'ancienneté au-delà de la 10e année (4 ans)", "Sref : Salaire de référence (2800 €)"]}
-      ${22}      | ${35} | ${"(3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3) + (6/10 * Sref * A4)"} | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (5 ans)", "A3: Années d'ancienneté au-delà de la 10e année (5 ans)", "A4: Années d'ancienneté au-delà de la 15e année (7 ans)", "Sref : Salaire de référence (2800 €)"]}
-      ${2}       | ${54} | ${"3/10 * Sref * A"}                                                                   | ${["A: Années d'ancienneté à compter de la 1re année (2 ans)", "Sref : Salaire de référence (2800 €)"]}
-      ${7}       | ${54} | ${"(3/10 * Sref * A1) + (4/10 * Sref * A2)"}                                           | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (2 ans)", "Sref : Salaire de référence (2800 €)"]}
-      ${14}      | ${54} | ${"(3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3)"}                      | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (5 ans)", "A3: Années d'ancienneté au-delà de la 10e année (4 ans)", "Sref : Salaire de référence (2800 €)"]}
-      ${22}      | ${54} | ${"(3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3) + (6/10 * Sref * A4)"} | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (5 ans)", "A3: Années d'ancienneté au-delà de la 10e année (5 ans)", "A4: Années d'ancienneté au-delà de la 15e année (7 ans)", "Sref : Salaire de référence (2800 €)"]}
-      ${35}      | ${55} | ${"18 * Sref"}                                                                         | ${["Sref : Salaire de référence (2800 €)"]}
+      seniority  | age   | expectedFormula                                                                                                                                                                        | expectedExplanations
+      ${23 / 12} | ${35} | ${""}                                                                                                                                                                                  | ${[]}
+      ${2}       | ${35} | ${"3/10 * Sref * A"}                                                                                                                                                                   | ${["A: Années d'ancienneté à compter de la 1re année (2 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${7}       | ${35} | ${"(3/10 * Sref * A1) + (4/10 * Sref * A2)"}                                                                                                                                           | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (2 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${14}      | ${35} | ${"(3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3)"}                                                                                                                      | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (5 ans)", "A3: Années d'ancienneté au-delà de la 10e année (4 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${22}      | ${35} | ${"(3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3) + (6/10 * Sref * A4)"}                                                                                                 | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (5 ans)", "A3: Années d'ancienneté au-delà de la 10e année (5 ans)", "A4: Années d'ancienneté au-delà de la 15e année (7 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${35}      | ${35} | ${"18 * Sref"}                                                                                                                                                                         | ${["Sref : Salaire de référence (2800 €)"]}
+      ${2}       | ${54} | ${"(3/10 * Sref * A) + (10% * (3/10 * Sref * A))"}                                                                                                                                     | ${["A: Années d'ancienneté à compter de la 1re année (2 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${7}       | ${54} | ${"((3/10 * Sref * A1) + (4/10 * Sref * A2)) + (10% * ((3/10 * Sref * A1) + (4/10 * Sref * A2)))"}                                                                                     | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (2 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${14}      | ${54} | ${"((3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3)) + (10% * ((3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3)))"}                                           | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (5 ans)", "A3: Années d'ancienneté au-delà de la 10e année (4 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${22}      | ${54} | ${"((3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3) + (6/10 * Sref * A4)) + (10% * ((3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3) + (6/10 * Sref * A4)))"} | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (5 ans)", "A3: Années d'ancienneté au-delà de la 10e année (5 ans)", "A4: Années d'ancienneté au-delà de la 15e année (7 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${35}      | ${54} | ${"18 * Sref"}                                                                                                                                                                         | ${["Sref : Salaire de référence (2800 €)"]}
+      ${2}       | ${56} | ${"(3/10 * Sref * A) + (25% * (3/10 * Sref * A))"}                                                                                                                                     | ${["A: Années d'ancienneté à compter de la 1re année (2 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${7}       | ${56} | ${"((3/10 * Sref * A1) + (4/10 * Sref * A2)) + (25% * ((3/10 * Sref * A1) + (4/10 * Sref * A2)))"}                                                                                     | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (2 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${14}      | ${56} | ${"((3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3)) + (25% * ((3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3)))"}                                           | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (5 ans)", "A3: Années d'ancienneté au-delà de la 10e année (4 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${22}      | ${56} | ${"((3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3) + (6/10 * Sref * A4)) + (25% * ((3/10 * Sref * A1) + (4/10 * Sref * A2) + (5/10 * Sref * A3) + (6/10 * Sref * A4)))"} | ${["A1: Années d'ancienneté à compter de la 1re année (5 ans)", "A2: Années d'ancienneté au-delà de la 5e année (5 ans)", "A3: Années d'ancienneté au-delà de la 10e année (5 ans)", "A4: Années d'ancienneté au-delà de la 15e année (7 ans)", "Sref : Salaire de référence (2800 €)"]}
+      ${35}      | ${56} | ${"18 * Sref"}                                                                                                                                                                         | ${["Sref : Salaire de référence (2800 €)"]}
     `(
       "Cadres, avec une ancienneté $seniority ans, un age $age ans => $expectedFormula",
       ({ seniority, age, expectedFormula, expectedExplanations }) => {
@@ -77,9 +83,9 @@ describe("Formule de l'indemnité de licenciement - CC 2098", () => {
           "contrat salarié . convention collective": "'IDCC2098'",
           "contrat salarié . convention collective . personnel presta service tertiaire . inaptitude suite à un accident non professionnelle":
             "'Non'",
-          "contrat salarié . convention collective . personnel presta service tertiaire . inaptitude suite à un accident non professionnelle . autre licenciement . catégorie professionnelle":
+          "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . catégorie professionnelle":
             "'Cadres'",
-          "contrat salarié . convention collective . personnel presta service tertiaire . inaptitude suite à un accident non professionnelle . autre licenciement . catégorie professionnelle . cadres . age": age,
+          "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . cadres . age": age,
           "contrat salarié . indemnité de licenciement": "oui",
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année": seniority,
           "contrat salarié . indemnité de licenciement . ancienneté requise en année": seniority,
