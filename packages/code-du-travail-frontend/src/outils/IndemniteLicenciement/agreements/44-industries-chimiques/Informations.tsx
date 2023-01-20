8c2460aa6cd0aabe81fedee2704eff2b3bdc2c7d
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-01-20 17:49:52 +0100

    fix: info

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/agreements/44-industries-chimiques/Informations.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/agreements/44-industries-chimiques/Informations.tsx
@@ -1,9 +1,12 @@
 import { useIndemniteLicenciementStore } from "../../store";
 
 export default function Agreement44Informations() {
-  const { hasVariablePay } = useIndemniteLicenciementStore((state) => ({
-    hasVariablePay: state.agreement44Data.input.hasVariablePay,
-  }));
+  const { hasVariablePay, knowingLastSalary, lastMonthSalary } =
+    useIndemniteLicenciementStore((state) => ({
+      hasVariablePay: state.agreement44Data.input.hasVariablePay,
+      lastMonthSalary: state.agreement44Data.input.lastMonthSalary,
+      knowingLastSalary: state.agreement44Data.input.knowingLastSalary,
+    }));
 
   return (
     <>
@@ -11,6 +14,24 @@ export default function Agreement44Informations() {
         <li>
           Les salaires indiqués comportent une partie variable &nbsp;:&nbsp;
           {hasVariablePay === "oui" ? "Oui" : "Non"}
+        </li>
+      )}
+
+      {knowingLastSalary && (
+        <li>
+          Le salarié connaissait le salaire du mois précédent la date de
+          notification de son licenciement&nbsp;?&nbsp;
+          {knowingLastSalary === "oui" ? "Oui" : "Non"}
+        </li>
+      )}
+      {lastMonthSalary && (
+        <li>
+          Le salaire du mois précédent la date de notification de son
+          licenciement est de&nbsp;:&nbsp;
+          {lastMonthSalary.value} €{" "}
+          {lastMonthSalary.prime
+            ? "dont (${lastMonthSalary.prime} € de prime)"
+            : ""}
         </li>
       )}
     </>
