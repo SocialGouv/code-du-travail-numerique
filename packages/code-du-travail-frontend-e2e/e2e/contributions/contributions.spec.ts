8f5f6a0be042b3db9b6e952d455b01d396c21b6f
carolineBda <caroline.bourdeu@gmail.com>
2023-01-23 09:57:07 +0100

    add test e2e

--- packages/code-du-travail-frontend-e2e/e2e/contributions/contributions.spec.ts
+++ packages/code-du-travail-frontend-e2e/e2e/contributions/contributions.spec.ts
@@ -1,4 +1,22 @@
 describe("Contributions", () => {
+  it("je vois la liste de toutes les contributions par thèmes", () => {
+    cy.visit("/contribution");
+    cy.get("h1").should("have.text", "Ma convention collective");
+    cy.get("body").should(
+      "contain",
+      "Retrouvez les questions/réponses fréquentes organisées par thème pour votre convention collective"
+    );
+    cy.get('h2[class^="contribution__"]').should("have.length", 126);
+    cy.get('ul *[class^="convention-collective__ListItem"]').should(
+      "have.length",
+      126
+    );
+    cy.get('ul *[class^="convention-collective__ListItem"]').first().click();
+    cy.url().should(
+      "include",
+      "/contribution/quelle-peut-etre-la-duree-maximale-dun-cdd"
+    );
+  });
   it("je vois une page contribution", () => {
     cy.visit("/contribution/quelle-peut-etre-la-duree-maximale-dun-cdd");
     cy.get("h1").should(
