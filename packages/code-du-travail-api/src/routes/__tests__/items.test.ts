c1d0bed0907b8c51d14075ff37c011976ac3037b
carolineBda <caroline.bourdeu@gmail.com>
2023-01-25 13:41:42 +0100

    fix tests

--- packages/code-du-travail-api/src/routes/__tests__/items.test.ts
+++ packages/code-du-travail-api/src/routes/__tests__/items.test.ts
@@ -4,40 +4,44 @@ import request from "supertest";
 import router from "../items";
 
 jest.mock("@socialgouv/cdtn-elasticsearch");
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require("./__mocking__/mockNLP");
 
 const app = new Koa();
 app.use(router.routes());
 
-test("return item from its id", async () => {
-  const response = await request(app.callback()).get(`/api/v1/items/9`);
-  expect(response.status).toBe(200);
-  expect(response.body).toMatchSnapshot();
-});
+describe("Items", () => {
+  test("return item from its id", async () => {
+    const response = await request(app.callback()).get(`/api/v1/items/9`);
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchSnapshot();
+  });
 
-test("return item from source and slug", async () => {
-  const response = await request(app.callback()).get(
-    `/api/v1/items/fiches_service_public/demission-dun-salarie`
-  );
-  expect(response.status).toBe(200);
-  expect(response.body).toMatchSnapshot();
-});
+  test("return item from source and slug", async () => {
+    const response = await request(app.callback()).get(
+      `/api/v1/items/fiches_service_public/demission-dun-salarie`
+    );
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchSnapshot();
+  });
 
-test("ensure related items do not include queried item", async () => {
-  const source = "fiches_service_public";
-  const slug = "demission-dun-salarie";
-  const response = await request(app.callback()).get(
-    `/api/v1/items/${source}/${slug}`
-  );
-  expect(response.status).toBe(200);
-  expect(
-    response.body.relatedItems.map(({ slug, source }) => ({ slug, source }))
-  ).not.toContainEqual({ slug, source });
-});
+  test("ensure related items do not include queried item", async () => {
+    const source = "fiches_service_public";
+    const slug = "demission-dun-salarie";
+    const response = await request(app.callback()).get(
+      `/api/v1/items/${source}/${slug}`
+    );
+    expect(response.status).toBe(200);
+    expect(
+      response.body.relatedItems.map(({ slug, source }) => ({ slug, source }))
+    ).not.toContainEqual({ slug, source });
+  });
 
-test("return item from its url", async () => {
-  const response = await request(app.callback()).get(
-    `/api/v1/items?url=https://www.service-public.fr/particuliers/vosdroits/F2883`
-  );
-  expect(response.status).toBe(200);
-  expect(response.body).toMatchSnapshot();
+  test("return item from its url", async () => {
+    const response = await request(app.callback()).get(
+      `/api/v1/items?url=https://www.service-public.fr/particuliers/vosdroits/F2883`
+    );
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchSnapshot();
+  });
 });
--- packages/code-du-travail-api/src/routes/__tests__/stats.test.ts
+++ packages/code-du-travail-api/src/routes/__tests__/stats.test.ts
@@ -36,7 +36,7 @@ describe("Stats", () => {
     expect(response.status).toBe(200);
     expect(response.body).toMatchInlineSnapshot(`
       Object {
-        "nbDocuments": 25,
+        "nbDocuments": 27,
         "nbPageViews": 10,
         "nbSearches": 4,
         "nbVisits": 20,
