5fa1fce3ccb94886930b65a1e5929d059d842968
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-01-26 10:25:19 +0100

    fix: empty commit

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/agreements/messageMotifExample.ts
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/agreements/messageMotifExample.ts
@@ -18,6 +18,6 @@ export const getMessageMotifExample = (
     case isSalariePartEmployeur:
       return "L'absence du particulier employeur, les congés payés, le congé de maternité, de paternité ou d'adoption, le congé de présence parentale, l'arrêt de travail lié à un accident du travail, ou une maladie professionnelle ou un accident de trajet, le congé lié à la formation professionnelle (CIF, projet de transition professionnelle), le congé de solidarité internationale, et le congé de solidarité familiale sont déjà pris en compte dans l'ancienneté et ne sont pas des périodes à renseigner ci-après :";
     default:
-      return "Les congés payés, le congé de maternité ou d'adoption, le congé de présence parental, l'arrêt de travail lié à un accident du travail ou une maladie professionnelle, le congé individuel de formation (CIF), le congé de solidarité internationale, le congé de solidarité familiale et le stage de fin d'étude de plus de 2 mois sont déjà prises en compte dans l'ancienneté et ne sont pas des périodes à renseigner ci-après :";
+      return "Les congés payés, le congé de maternité ou d'adoption, le congé de présence parentale, l'arrêt de travail lié à un accident du travail ou une maladie professionnelle, le congé lié à la formation professionnelle (CIF, projet de transition professionnelle), le congé de solidarité internationale, le congé de solidarité familiale et le stage de fin d'étude de plus de 2 mois sont déjà pris en compte dans l'ancienneté et ne sont pas des périodes à renseigner ci-après :";
   }
 };
