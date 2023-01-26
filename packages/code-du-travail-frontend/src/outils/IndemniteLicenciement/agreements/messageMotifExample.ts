8c571c68696046ce604f922eb0f964fc7e48ea9d
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-01-26 17:59:25 +0100

    fix: sans switch true

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/agreements/messageMotifExample.ts
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/agreements/messageMotifExample.ts
@@ -12,12 +12,9 @@ export const getMessageMotifExample = (
   const isAssMat = categoryPro3239 === `'${CatPro3239.assistantMaternel}'`;
   const isSalariePartEmployeur =
     categoryPro3239 === `'${CatPro3239.salarieParticulierEmployeur}'`;
-  switch (true) {
-    case isAssMat:
-      return "L'absence de l'enfant gardé, les congés payés, le congé de maternité, de paternité ou d'adoption, le congé de présence parentale, l'arrêt de travail lié à un accident du travail, ou une maladie professionnelle ou un accident de trajet, le congé lié à la formation professionnelle (CIF, projet de transition professionnelle), le congé de solidarité internationale, et le congé de solidarité familiale sont déjà pris en compte dans l'ancienneté et ne sont pas des périodes à renseigner ci-après :";
-    case isSalariePartEmployeur:
-      return "L'absence du particulier employeur, les congés payés, le congé de maternité, de paternité ou d'adoption, le congé de présence parentale, l'arrêt de travail lié à un accident du travail, ou une maladie professionnelle ou un accident de trajet, le congé lié à la formation professionnelle (CIF, projet de transition professionnelle), le congé de solidarité internationale, et le congé de solidarité familiale sont déjà pris en compte dans l'ancienneté et ne sont pas des périodes à renseigner ci-après :";
-    default:
-      return "Les congés payés, le congé de maternité ou d'adoption, le congé de présence parentale, l'arrêt de travail lié à un accident du travail ou une maladie professionnelle, le congé lié à la formation professionnelle (CIF, projet de transition professionnelle), le congé de solidarité internationale, le congé de solidarité familiale et le stage de fin d'étude de plus de 2 mois sont déjà pris en compte dans l'ancienneté et ne sont pas des périodes à renseigner ci-après :";
-  }
+  if (isAssMat)
+    return "L'absence de l'enfant gardé, les congés payés, le congé de maternité, de paternité ou d'adoption, le congé de présence parentale, l'arrêt de travail lié à un accident du travail, ou une maladie professionnelle ou un accident de trajet, le congé lié à la formation professionnelle (CIF, projet de transition professionnelle), le congé de solidarité internationale, et le congé de solidarité familiale sont déjà pris en compte dans l'ancienneté et ne sont pas des périodes à renseigner ci-après :";
+  else if (isSalariePartEmployeur)
+    return "L'absence du particulier employeur, les congés payés, le congé de maternité, de paternité ou d'adoption, le congé de présence parentale, l'arrêt de travail lié à un accident du travail, ou une maladie professionnelle ou un accident de trajet, le congé lié à la formation professionnelle (CIF, projet de transition professionnelle), le congé de solidarité internationale, et le congé de solidarité familiale sont déjà pris en compte dans l'ancienneté et ne sont pas des périodes à renseigner ci-après :";
+  return "Les congés payés, le congé de maternité ou d'adoption, le congé de présence parentale, l'arrêt de travail lié à un accident du travail ou une maladie professionnelle, le congé lié à la formation professionnelle (CIF, projet de transition professionnelle), le congé de solidarité internationale, le congé de solidarité familiale et le stage de fin d'étude de plus de 2 mois sont déjà pris en compte dans l'ancienneté et ne sont pas des périodes à renseigner ci-après :";
 };
