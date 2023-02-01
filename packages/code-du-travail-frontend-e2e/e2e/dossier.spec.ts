e020b04487706db1ce2fa8cb04f38f37e8ffde01
Martial Maillot <martial.maillot@gmail.com>
2023-02-01 11:32:37 +0100

    fix: e2e test

--- packages/code-du-travail-frontend-e2e/e2e/dossier.spec.ts
+++ packages/code-du-travail-frontend-e2e/e2e/dossier.spec.ts
@@ -1,7 +1,7 @@
 describe("Dossiers", () => {
   it("je vois une page dossier", () => {
     cy.visit("/dossiers/ministere-du-travail-notre-dossier-sur-le-coronavirus");
-    cy.get("h1").should("have.text", "Covid-19 : fin du protocole sanitaire");
+    cy.get("h1").should("have.text", "Covid-19");
     cy.get("body").should("contain", "Sommaire");
 
     cy.contains(
