7e2c8e0ddce22bdc6ae3e5000b981d7ed101d7d6
carolineBda <caroline.bourdeu@gmail.com>
2023-02-06 16:42:13 +0100

    fix test

--- packages/code-du-travail-frontend-e2e/e2e/dossier.spec.ts
+++ packages/code-du-travail-frontend-e2e/e2e/dossier.spec.ts
@@ -1,16 +1,17 @@
 describe("Dossiers", () => {
   it("je vois une page dossier", () => {
     cy.visit("/dossiers/ministere-du-travail-notre-dossier-sur-le-coronavirus");
-    cy.get("h1").should("have.text", "Covid-19 : fin du protocole sanitaire");
+    cy.get("h1").should("have.text", "Covid-19");
     cy.get("body").should("contain", "Sommaire");
 
-    cy.contains(
-      "Covid-19 : le régime post-crise sanitaire à compter du 14 mars 2022"
-    ).click();
-    cy.get("h1").should("have.text", "Covid-19 : fin du protocole sanitaire");
+    cy.contains("Covid-19 : le régime post-crise sanitaire").click();
+    cy.get("h1").should(
+      "have.text",
+      "Covid-19 : le régime post-crise sanitaire"
+    );
     cy.get("body").should(
       "contain",
-      "Un allègement des mesures à partir du 14 mars 2022"
+      "Le passe vaccinal et le passe sanitaire ne sont plus requis"
     );
   });
 });
