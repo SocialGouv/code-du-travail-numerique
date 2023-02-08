98bb65d2060ff0f687f1bed0fde27a19434c82a6
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-02-08 10:11:14 +0100

    chore: change host env

--- packages/code-du-travail-frontend/scripts/prebuild.ts
+++ packages/code-du-travail-frontend/scripts/prebuild.ts
@@ -34,7 +34,7 @@ export const generateWidgetScript = (isOnProduction: boolean, host: string) => {
 
 const run = () => {
   const isOnProduction = !!process.env.PRODUCTION;
-  const host = process.env.NEXT_PUBLIC_SITE_URL ?? "localhost:3000";
+  const host = process.env.FRONTEND_HOST ?? "localhost:3000";
   generateRobotsTxt(isOnProduction, host);
   console.log("Robots.txt generated.");
   generateWidgetScript(isOnProduction, host);
