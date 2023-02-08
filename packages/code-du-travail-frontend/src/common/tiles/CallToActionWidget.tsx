e5a1babfd29350b4c709fd23ad8c742f853fb7ea
carolineBda <caroline.bourdeu@gmail.com>
2023-02-08 16:59:39 +0100

    fix widget

--- packages/code-du-travail-frontend/src/common/tiles/CallToActionWidget.tsx
+++ packages/code-du-travail-frontend/src/common/tiles/CallToActionWidget.tsx
@@ -1,6 +1,5 @@
 import { Paragraph, theme } from "@socialgouv/cdtn-ui";
 import React from "react";
-import Link from "next/link";
 import { CallToActionTile } from "./CallToAction";
 import styled from "styled-components";
 
@@ -17,20 +16,18 @@ export default function CallToActionWidget({
   image,
 }: Props): JSX.Element {
   return (
-    <Link href={link} passHref>
-      <CallToActionTile
-        noCustom
-        action="Installer"
-        title={title}
-        titleTagType="h2"
-        href={link}
-      >
-        <>
-          <Image src={`/static/assets/img/${image}`} alt={"Widget " + title} />
-          <ParagraphNarrow>{description}</ParagraphNarrow>
-        </>
-      </CallToActionTile>
-    </Link>
+    <CallToActionTile
+      noCustom
+      action="Installer"
+      title={title}
+      titleTagType="h2"
+      href={link}
+    >
+      <>
+        <Image src={`/static/assets/img/${image}`} alt={"Widget " + title} />
+        <ParagraphNarrow>{description}</ParagraphNarrow>
+      </>
+    </CallToActionTile>
   );
 }
 
