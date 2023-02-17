f85515995a1a4b332e93416f897d324a752ae86e
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-02-17 16:21:50 +0100

    feat: add home

--- packages/code-du-travail-frontend/pages/api/home.ts
+++ packages/code-du-travail-frontend/pages/api/home.ts
@@ -2,8 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { HomeController } from "../../src/api";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const controller = new HomeController(req, res);
   if (req.method === "GET") {
-    const controller = new HomeController();
     const data = controller.get();
     res.status(200).json(data);
   }
