803099d3492735ee1e6feb5b95bff0236d1b7bde
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-02-21 14:47:53 +0100

    fix: pod

--- .k8s/components/www.ts
+++ .k8s/components/www.ts
@@ -3,15 +3,10 @@ import { ok } from "assert";
 
 import { create } from "@socialgouv/kosko-charts/components/app";
 import { addWaitForHttp } from "@socialgouv/kosko-charts/utils/addWaitForHttp";
-import { addEnv } from "@socialgouv/kosko-charts/utils/addEnv";
-import { getIngressHost } from "@socialgouv/kosko-charts/utils/getIngressHost";
 import { getGithubRegistryImagePath } from "@socialgouv/kosko-charts/utils/getGithubRegistryImagePath";
-import { EnvVar } from "kubernetes-models/v1/EnvVar";
 import type { Deployment } from "kubernetes-models/apps/v1/Deployment";
 import type { IIoK8sApiCoreV1HTTPGetAction } from "kubernetes-models/v1";
 import { HorizontalPodAutoscaler } from "kubernetes-models/autoscaling/v2beta2/HorizontalPodAutoscaler";
-
-import getApiManifests from "./api";
 
 // all probes httpGet
 const httpGet: IIoK8sApiCoreV1HTTPGetAction = {
@@ -21,8 +16,6 @@ const httpGet: IIoK8sApiCoreV1HTTPGetAction = {
 
 export default async () => {
   // extract computed url from API manifests for the frontend
-  const apiManifests = await getApiManifests();
-
   const productionConfig = {
     domain: "travail.gouv.fr",
     subdomain: "code",
