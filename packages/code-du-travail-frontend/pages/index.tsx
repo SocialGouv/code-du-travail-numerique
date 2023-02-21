b32c65dd6b4a09618b7e8e96fa741a06192ac385
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-02-21 17:23:13 +0100

    fix: build

--- packages/code-du-travail-frontend/pages/index.tsx
+++ packages/code-du-travail-frontend/pages/index.tsx
@@ -84,6 +84,7 @@ export async function getStaticProps() {
       agreements,
     },
     revalidate: 60 * 10, // 10 minutes
+    fallback: "blocking", // to block generation at build time
   };
 }
 
