964842f2e206b0edbd9e4d14a96eaf5fc206e74f
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-02-23 15:30:17 +0100

    fix: build

--- packages/code-du-travail-frontend/src/api/modules/home/controller.ts
+++ packages/code-du-travail-frontend/src/api/modules/home/controller.ts
@@ -1,5 +1,6 @@
 import { ElasticSearchItem } from "cdtn-types";
 import { NextApiRequest, NextApiResponse } from "next";
+import { NotFoundError } from "../../utils";
 import { getBySlugsAgreements } from "../agreements";
 import { getBySlugsContributions } from "../contributions";
 import { getBySlugHighlights } from "../highlights";
@@ -27,39 +28,47 @@ export class HomeController {
 
   public async get() {
     // modifier en passant en fonction
-    const themes = await getAllThemes();
-    const highlights = await getBySlugHighlights("homepage");
-    const tools = await getAllTools(
-      // modifier en passant un array
-      "df,dsds,dsds,dsds",
-      "simulateur-embauche,indemnite-licenciement,preavis-demission,convention-collective"
-    );
-    const modeles = await getBySlugsModeles([
-      "lettre-de-demission", // modfier en passant les id
-      "rupture-du-contrat-en-periode-dessai-par-le-salarie",
-      "rupture-de-periode-dessai-par-lemployeur",
-      "convocation-a-un-entretien-prealable-au-licenciement-pour-motif-personnel",
-    ]);
-    const contributions = await getBySlugsContributions([
-      "quelle-est-la-duree-du-preavis-en-cas-de-demission",
-      "les-conges-pour-evenements-familiaux",
-      "en-cas-darret-maladie-du-salarie-lemployeur-doit-il-assurer-le-maintien-de-salaire",
-      "est-il-obligatoire-davoir-un-contrat-de-travail-ecrit-et-signe",
-    ]);
-    const agreements = await getBySlugsAgreements([
-      "2609-batiment-etam",
-      "650-metallurgie-ingenieurs-et-cadres",
-      "3239-particuliers-employeurs-et-emploi-a-domicile",
-      "573-commerces-de-gros",
-    ]);
-    const response: GetHomePage = {
-      themes,
-      highlights,
-      tools,
-      modeles,
-      contributions,
-      agreements,
-    };
-    this.res.status(200).json(response);
+    try {
+      const themes = await getAllThemes();
+      const highlights = await getBySlugHighlights("homepage");
+      const tools = await getAllTools(
+        // modifier en passant un array
+        "df,dsds,dsds,dsds",
+        "simulateur-embauche,indemnite-licenciement,preavis-demission,convention-collective"
+      );
+      const modeles = await getBySlugsModeles([
+        "lettre-de-demission", // modfier en passant les id
+        "rupture-du-contrat-en-periode-dessai-par-le-salarie",
+        "rupture-de-periode-dessai-par-lemployeur",
+        "convocation-a-un-entretien-prealable-au-licenciement-pour-motif-personnel",
+      ]);
+      const contributions = await getBySlugsContributions([
+        "quelle-est-la-duree-du-preavis-en-cas-de-demission",
+        "les-conges-pour-evenements-familiaux",
+        "en-cas-darret-maladie-du-salarie-lemployeur-doit-il-assurer-le-maintien-de-salaire",
+        "est-il-obligatoire-davoir-un-contrat-de-travail-ecrit-et-signe",
+      ]);
+      const agreements = await getBySlugsAgreements([
+        "2609-batiment-etam",
+        "650-metallurgie-ingenieurs-et-cadres",
+        "3239-particuliers-employeurs-et-emploi-a-domicile",
+        "573-commerces-de-gros",
+      ]);
+      const response: GetHomePage = {
+        themes,
+        highlights,
+        tools,
+        modeles,
+        contributions,
+        agreements,
+      };
+      this.res.status(200).json(response);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        this.res.status(404).json({ message: "Not found" });
+      } else {
+        this.res.status(500).json({ message: "Internal server error" });
+      }
+    }
   }
 }
