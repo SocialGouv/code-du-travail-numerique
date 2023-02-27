2ecc0239984650a861fbf6ee5fe511e20d2b652b
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-02-27 16:15:15 +0100

    fix: merge

---
+++ packages/cdtn-types/build/index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ContentType = exports.BlockDisplayMode = exports.SectionDisplayMode = void 0;
+var SectionDisplayMode;
+(function (SectionDisplayMode) {
+    SectionDisplayMode["accordion"] = "accordion";
+    SectionDisplayMode["tab"] = "tab";
+})(SectionDisplayMode = exports.SectionDisplayMode || (exports.SectionDisplayMode = {}));
+var BlockDisplayMode;
+(function (BlockDisplayMode) {
+    BlockDisplayMode["line"] = "line";
+    BlockDisplayMode["square"] = "square";
+})(BlockDisplayMode = exports.BlockDisplayMode || (exports.BlockDisplayMode = {}));
+var ContentType;
+(function (ContentType) {
+    ContentType["markdown"] = "markdown";
+    ContentType["graphic"] = "graphic";
+    ContentType["content"] = "content";
+})(ContentType = exports.ContentType || (exports.ContentType = {}));
