24d0abcc4016fd6d1c85d5d8ef0b514a73e68946
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-03-08 17:31:05 +0100

    fix: code

--- packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/state/createContext.ts
+++ packages/code-du-travail-frontend/src/outils/DureePreavisRetraite/state/createContext.ts
@@ -1,10 +1,15 @@
-import createContext from "zustand/context";
-import { StoreApi } from "zustand";
+import { createContext } from "react";
+import { StoreApi, useStore } from "zustand";
 import { PreavisRetraiteStore } from "./types";
 
-const { Provider, useStore } = createContext<StoreApi<PreavisRetraiteStore>>();
+const PreavisRetraiteContext = createContext<StoreApi<PreavisRetraiteStore>>(
+  {} as StoreApi<PreavisRetraiteStore>
+);
+
+const { Provider } = PreavisRetraiteContext;
 
 export {
   Provider as PreavisRetraiteProvider,
+  PreavisRetraiteContext,
   useStore as usePreavisRetraiteStore,
 };
