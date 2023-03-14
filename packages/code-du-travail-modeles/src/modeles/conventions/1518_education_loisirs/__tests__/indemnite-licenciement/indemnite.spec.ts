86cc4e655965258b96fa4102b61c85d73dd4c0c0
Martial Maillot <martial.maillot@gmail.com>
2023-03-14 16:44:48 +0100

    feat: improve unit test

--- packages/code-du-travail-modeles/src/modeles/conventions/1518_education_loisirs/__tests__/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/1518_education_loisirs/__tests__/indemnite-licenciement/indemnite.spec.ts
@@ -8,24 +8,25 @@ const engine = new IndemniteLicenciementPublicodes(
 describe("CC 1518", () => {
   describe("Calcul de l'indemnité de licenciement", () => {
     test.each`
-      seniority     | salaireRef | expectedCompensation
-      ${7 / 12}     | ${2500}    | ${0}
-      ${8 / 12}     | ${2500}    | ${0}
-      ${9 / 12}     | ${2500}    | ${468.75}
-      ${1}          | ${2500}    | ${625}
-      ${0.75}       | ${2500}    | ${468.75}
-      ${2 - 1 / 12} | ${2500}    | ${1197.92}
-      ${8}          | ${2500}    | ${5000}
+      seniorityRight | seniority     | salaireRef | expectedCompensation
+      ${7 / 12}      | ${7 / 12}     | ${2500}    | ${0}
+      ${8 / 12}      | ${8 / 12}     | ${2500}    | ${0}
+      ${8 / 12}      | ${1}          | ${2500}    | ${0}
+      ${9 / 12}      | ${9 / 12}     | ${2500}    | ${468.75}
+      ${9 / 12}      | ${1}          | ${2500}    | ${625}
+      ${9 / 12}      | ${0.75}       | ${2500}    | ${468.75}
+      ${9 / 12}      | ${2 - 1 / 12} | ${2500}    | ${1197.92}
+      ${9 / 12}      | ${8}          | ${2500}    | ${5000}
     `(
       "Avec une ancienneté $seniority ans, un salaire de référence $salaireRef € => une compensation de base de $expectedCompensation €",
-      ({ salaireRef, expectedCompensation, seniority }) => {
+      ({ salaireRef, expectedCompensation, seniority, seniorityRight }) => {
         const { result, missingArgs } = engine.setSituation(
           {
             "contrat salarié . convention collective": "'IDCC1518'",
             "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
               seniority,
             "contrat salarié . indemnité de licenciement . ancienneté conventionnelle requise en année":
-              seniority,
+              seniorityRight,
             "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
               salaireRef,
           },
--- packages/code-du-travail-modeles/src/modeles/conventions/3043_entreprises_proprete/__tests__/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/3043_entreprises_proprete/__tests__/indemnite-licenciement/indemnite.spec.ts
@@ -7,27 +7,27 @@ const engine = new IndemniteLicenciementPublicodes(
 
 describe("Indemnité conventionnel de licenciement pour la CC 3043", () => {
   test.each`
-    seniority | salary  | expectedCompensation
-    ${0}      | ${2000} | ${0}
-    ${1}      | ${2000} | ${0}
-    ${1.91}   | ${2000} | ${0}
-    ${2}      | ${2000} | ${0}
-    ${2.08}   | ${2000} | ${416}
-    ${5}      | ${2000} | ${1000}
-    ${5.91}   | ${2000} | ${1182}
-    ${6}      | ${2000} | ${1333.33}
-    ${9.91}   | ${2000} | ${2636.67}
-    ${12}     | ${2000} | ${3466.67}
+    seniorityRight | seniority | salary  | expectedCompensation
+    ${0}           | ${3}      | ${2000} | ${0}
+    ${1}           | ${3}      | ${2000} | ${0}
+    ${1.91}        | ${3}      | ${2000} | ${0}
+    ${2}           | ${3}      | ${2000} | ${0}
+    ${25 / 12}     | ${2.08}   | ${2000} | ${416}
+    ${25 / 12}     | ${5}      | ${2000} | ${1000}
+    ${25 / 12}     | ${5.91}   | ${2000} | ${1182}
+    ${25 / 12}     | ${6}      | ${2000} | ${1333.33}
+    ${25 / 12}     | ${9.91}   | ${2000} | ${2636.67}
+    ${25 / 12}     | ${12}     | ${2000} | ${3466.67}
   `(
     "ancienneté: $seniority an, salaire de référence: $salary, => $expectedCompensation €",
-    ({ seniority, salary, expectedCompensation }) => {
+    ({ seniority, seniorityRight, salary, expectedCompensation }) => {
       const { result, missingArgs } = engine.setSituation(
         {
           "contrat salarié . convention collective": "'IDCC3043'",
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
             seniority,
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle requise en année":
-            seniority,
+            seniorityRight,
           "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
             salary,
         },
