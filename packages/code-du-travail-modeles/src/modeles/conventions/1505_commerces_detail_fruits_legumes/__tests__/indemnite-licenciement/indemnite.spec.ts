22ffe1f2c7edbd638cd9f10ba982fe04c412ce03
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-03-16 14:43:16 +0100

    chore: update tests

--- packages/code-du-travail-modeles/src/modeles/conventions/1505_commerces_detail_fruits_legumes/__tests__/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/1505_commerces_detail_fruits_legumes/__tests__/indemnite-licenciement/indemnite.spec.ts
@@ -7,22 +7,24 @@ const engine = new IndemniteLicenciementPublicodes(
 
 describe("Calcul de l'indemnité de licenciement CC 1505", () => {
   test.each`
-    seniority | expectedCompensation
-    ${6 / 12} | ${0}
-    ${0.67}   | ${435.5}
-    ${6}      | ${3900}
-    ${11}     | ${7366.67}
-    ${20}     | ${15166.67}
+    seniorityRight | seniority | expectedCompensation
+    ${6 / 12}      | ${6 / 12} | ${0}
+    ${0.67}        | ${0.67}   | ${435.5}
+    ${3}           | ${6}      | ${3900}
+    ${6}           | ${6}      | ${3900}
+    ${11}          | ${11}     | ${7366.67}
+    ${14}          | ${11}     | ${7366.67}
+    ${20}          | ${20}     | ${15166.67}
   `(
-    "Avec une ancienneté $seniority ans et salaire de ref 2600€ => une compensation de base de $expectedCompensation €",
-    ({ expectedCompensation, seniority }) => {
+    "Avec une ancienneté $seniority ans, une ancienneté requise $seniorityRight et salaire de ref 2600€ => une compensation de base de $expectedCompensation €",
+    ({ expectedCompensation, seniority, seniorityRight }) => {
       const { result, missingArgs } = engine.setSituation(
         {
           "contrat salarié . convention collective": "'IDCC1505'",
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
             seniority,
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle requise en année":
-            seniority,
+            seniorityRight,
           "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
             "2600",
         },
