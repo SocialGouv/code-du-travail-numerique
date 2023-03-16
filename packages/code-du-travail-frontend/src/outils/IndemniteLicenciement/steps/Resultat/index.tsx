534ac10b86f7bab940e9196b1e5a82391e54eb57
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-03-16 09:07:27 +0100

    fix: event

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/index.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/index.tsx
@@ -28,17 +28,6 @@ const StepResult = () => {
     init();
   }, []);
 
-  useEffect(() => {
-    matopush([
-      MatomoBaseEvent.TRACK_EVENT,
-      MatomoBaseEvent.OUTIL,
-      MatomoActionEvent.INDEMNITE_LICENCIEMENT,
-      isEligible
-        ? IndemniteLicenciementStepName.Resultat
-        : MatomoSimulatorEvent.STEP_RESULT_INELIGIBLE,
-    ]);
-  }, [isEligible]);
-
   return <>{isEligible ? <Eligible /> : <Ineligible />}</>;
 };
 
