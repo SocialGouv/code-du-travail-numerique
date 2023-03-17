536c83700b9af3ef71d57c0c0ed83f0925abb3c8
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-03-17 15:07:27 +0100

    fix: tu

--- packages/code-du-travail-modeles/src/modeles/conventions/2614_travaux_publics/__tests__/indemnite-licenciement/anciennete.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/2614_travaux_publics/__tests__/indemnite-licenciement/anciennete.spec.ts
@@ -4,46 +4,26 @@ import {
   SupportedCcIndemniteLicenciement,
 } from "../../../../common";
 
-describe("CC 1517", () => {
-  describe("Calcul de l'ancienneté pour le calcul", () => {
+describe("CC 2614", () => {
+  describe("Calcul de l'ancienneté", () => {
     test.each`
-      absences                                                             | entryDate       | exitDate        | expectedAnciennete
-      ${[]}                                                                | ${"20/02/2020"} | ${"20/02/2021"} | ${1}
-      ${[{ durationInMonth: 1, motif: { key: MotifKeys.maladieNonPro } }]} | ${"20/02/2020"} | ${"20/02/2021"} | ${0.9166666666666666}
+      absences                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        | entryDate       | exitDate        | expectedAnciennete
+      ${[]}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           | ${"20/02/2020"} | ${"20/02/2021"} | ${1}
+      ${[{ motif: { key: MotifKeys.maladieNonPro } }, { motif: { key: MotifKeys.accidentTrajet } }, { motif: { key: MotifKeys.congesSabbatique } }, { motif: { key: MotifKeys.congesCreationEntreprise } }, { motif: { key: MotifKeys.congesParentalEducation } }, { motif: { key: MotifKeys.congesSansSolde } }, { motif: { key: MotifKeys.greve } }, { motif: { key: MotifKeys.miseAPied } }, { motif: { key: MotifKeys.congesPaternite } }]}                                                                                                                                                                                       | ${"20/02/2020"} | ${"20/02/2021"} | ${1}
+      ${[{ durationInMonth: 0, motif: { key: MotifKeys.maladieNonPro } }, { durationInMonth: 0, motif: { key: MotifKeys.accidentTrajet } }, { durationInMonth: 0, motif: { key: MotifKeys.congesSabbatique } }, { durationInMonth: 0, motif: { key: MotifKeys.congesCreationEntreprise } }, { durationInMonth: 0, motif: { key: MotifKeys.congesParentalEducation } }, { durationInMonth: 0, motif: { key: MotifKeys.congesSansSolde } }, { durationInMonth: 0, motif: { key: MotifKeys.greve } }, { durationInMonth: 0, motif: { key: MotifKeys.miseAPied } }, { durationInMonth: 0, motif: { key: MotifKeys.congesPaternite } }]}   | ${"20/02/2020"} | ${"20/02/2021"} | ${1}
+      ${[{ durationInMonth: 0, motif: { key: MotifKeys.maladieNonPro } }, { durationInMonth: 0, motif: { key: MotifKeys.accidentTrajet } }, { durationInMonth: 0, motif: { key: MotifKeys.congesSabbatique } }, { durationInMonth: "1", motif: { key: MotifKeys.congesCreationEntreprise } }, { durationInMonth: 0, motif: { key: MotifKeys.congesParentalEducation } }, { durationInMonth: 0, motif: { key: MotifKeys.congesSansSolde } }, { durationInMonth: 0, motif: { key: MotifKeys.greve } }, { durationInMonth: 0, motif: { key: MotifKeys.miseAPied } }, { durationInMonth: 0, motif: { key: MotifKeys.congesPaternite } }]} | ${"20/02/2020"} | ${"20/02/2021"} | ${0.9166666666666666}
+      ${[{ durationInMonth: 0.5, motif: { key: MotifKeys.maladieNonPro } }, { durationInMonth: 0.75, motif: { key: MotifKeys.maladieNonPro } }, { durationInMonth: 2.5, motif: { key: MotifKeys.maladieNonPro } }]}                                                                                                                                                                                                                                                                                                                                                                                                                   | ${"01/01/2008"} | ${"01/01/2021"} | ${13}
+      ${[{ durationInMonth: 20, motif: { key: MotifKeys.maladieNonPro } }]}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           | ${"01/01/2022"} | ${"01/02/2023"} | ${1.0833333333333333}
     `(
       "Calcul de l'ancienneté avec $entryDate et $exitDate en attendant $expectedAnciennete an",
       ({ absences, entryDate, exitDate, expectedAnciennete }) => {
         const seniority = new SeniorityFactory().create(
-          SupportedCcIndemniteLicenciement.IDCC1517
+          SupportedCcIndemniteLicenciement.IDCC2614
         );
 
         const result = seniority.computeSeniority({
           absencePeriods: absences,
           dateEntree: entryDate,
-          dateSortie: exitDate,
-        });
-
-        expect(result.value).toEqual(expectedAnciennete);
-      }
-    );
-  });
-
-  describe("Calcul de l'ancienneté pour le droit", () => {
-    test.each`
-      absences                                                             | entryDate       | exitDate        | expectedAnciennete
-      ${[]}                                                                | ${"20/02/2020"} | ${"20/02/2021"} | ${1}
-      ${[{ durationInMonth: 1, motif: { key: MotifKeys.maladieNonPro } }]} | ${"20/02/2020"} | ${"20/02/2021"} | ${0.9166666666666666}
-    `(
-      "Calcul de l'ancienneté avec $entryDate et $exitDate en attendant $expectedAnciennete an",
-      ({ absences, entryDate, exitDate, expectedAnciennete }) => {
-        const seniority = new SeniorityFactory().create(
-          SupportedCcIndemniteLicenciement.IDCC1517
-        );
-
-        const result = seniority.computeRequiredSeniority({
-          absencePeriods: absences,
-          dateEntree: entryDate,
-          dateNotification: "01/01/1979",
           dateSortie: exitDate,
         });
 
