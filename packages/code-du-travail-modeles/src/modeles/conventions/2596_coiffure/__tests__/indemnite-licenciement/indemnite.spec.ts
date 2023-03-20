c0df4107c4f62098a979640d372e8d34bc78caad
carolineBda <caroline.bourdeu@gmail.com>
2023-03-20 12:15:08 +0100

    add seniorityRight test cases

--- packages/code-du-travail-modeles/src/modeles/conventions/2596_coiffure/__tests__/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/2596_coiffure/__tests__/indemnite-licenciement/indemnite.spec.ts
@@ -7,21 +7,23 @@ const engine = new IndemniteLicenciementPublicodes(
 
 describe("Indemnité conventionnel de licenciement pour la CC 2596", () => {
   test.each`
-    seniority | salary  | expectedCompensation
-    ${6 / 12} | ${2600} | ${0}
-    ${8 / 12} | ${2600} | ${433.33}
-    ${5}      | ${2600} | ${3250}
-    ${15}     | ${2600} | ${10833.33}
+    seniorityRight | seniority | salary  | expectedCompensation
+    ${6 / 12}      | ${6 / 12} | ${2600} | ${0}
+    ${6 / 12}      | ${8 / 12} | ${2600} | ${0}
+    ${8 / 12}      | ${8 / 12} | ${2600} | ${433.33}
+    ${8 / 12}      | ${5}      | ${2600} | ${3250}
+    ${8 / 12}      | ${5}      | ${2600} | ${3250}
+    ${8 / 12}      | ${15}     | ${2600} | ${10833.33}
   `(
-    "Non-cadres: ancienneté: $seniority an, salaire de référence: $salary, => $expectedCompensation €",
-    ({ seniority, salary, expectedCompensation }) => {
+    "Non-cadres: ancienneté: $seniority an, ancienneté requise : $seniorityRight an,  salaire de référence: $salary, => $expectedCompensation €",
+    ({ seniority, seniorityRight, salary, expectedCompensation }) => {
       const { result, missingArgs } = engine.setSituation(
         {
           "contrat salarié . convention collective": "'IDCC2596'",
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
             seniority,
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle requise en année":
-            seniority,
+            seniorityRight,
           "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
             salary,
           "contrat salarié . convention collective . coiffure . indemnité de licenciement . catégorie professionnelle":
@@ -35,22 +37,22 @@ describe("Indemnité conventionnel de licenciement pour la CC 2596", () => {
     }
   );
   test.each`
-    seniority | salary  | expectedCompensation
-    ${6 / 12} | ${2873} | ${359.13}
-    ${9 / 12} | ${2873} | ${538.69}
-    ${8}      | ${2877} | ${5754}
-    ${20}     | ${2891} | ${14455}
-    ${25}     | ${2891} | ${17346}
+    seniorityRight | seniority | salary  | expectedCompensation
+    ${0}           | ${6 / 12} | ${2873} | ${359.13}
+    ${0}           | ${9 / 12} | ${2873} | ${538.69}
+    ${0}           | ${8}      | ${2877} | ${5754}
+    ${0}           | ${20}     | ${2891} | ${14455}
+    ${0}           | ${25}     | ${2891} | ${17346}
   `(
-    "Cadres: ancienneté: $seniority an, salaire de référence: $salary, => $expectedCompensation €",
-    ({ seniority, salary, expectedCompensation }) => {
+    "Cadres: ancienneté: $seniority an, ancienneté requise : $seniorityRight an, salaire de référence: $salary, => $expectedCompensation €",
+    ({ seniority, seniorityRight, salary, expectedCompensation }) => {
       const { result, missingArgs } = engine.setSituation(
         {
           "contrat salarié . convention collective": "'IDCC2596'",
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
             seniority,
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle requise en année":
-            seniority,
+            seniorityRight,
           "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
             salary,
           "contrat salarié . convention collective . coiffure . indemnité de licenciement . catégorie professionnelle":
