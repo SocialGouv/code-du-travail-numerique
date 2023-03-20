32f6a258136d127218ccf0c855d414d8b153b345
carolineBda <caroline.bourdeu@gmail.com>
2023-03-20 15:30:29 +0100

    tst

--- packages/code-du-travail-modeles/src/modeles/conventions/1516_organismes_formation/__tests__/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/1516_organismes_formation/__tests__/indemnite-licenciement/indemnite.spec.ts
@@ -7,15 +7,15 @@ const engine = new IndemniteLicenciementPublicodes(
 
 describe("Indemnité conventionnel de licenciement pour la CC 1516", () => {
   test.each`
-    seniority  | seniorityRight | salary  | expectedCompensation
-    ${0}       | ${0}           | ${0}    | ${0}
-    ${18 / 12} | ${18 / 12}     | ${3037} | ${0}
-    ${2}       | ${2}           | ${3037} | ${0}
-    ${2.1}     | ${2}           | ${3037} | ${0}
-    ${2.1}     | ${2.1}         | ${3037} | ${1275.54}
-    ${23}      | ${23}          | ${3037} | ${16399.8}
-    ${26}      | ${26}          | ${3037} | ${18222}
-    ${42}      | ${42}          | ${3037} | ${18222}
+    seniorityRight | seniority  | salary  | expectedCompensation
+    ${0}           | ${0}       | ${0}    | ${0}
+    ${18 / 12}     | ${18 / 12} | ${3037} | ${0}
+    ${2}           | ${2}       | ${3037} | ${0}
+    ${2}           | ${2.1}     | ${3037} | ${0}
+    ${2.1}         | ${2.1}     | ${3037} | ${1275.54}
+    ${23}          | ${23}      | ${3037} | ${16399.8}
+    ${26}          | ${26}      | ${3037} | ${18222}
+    ${42}          | ${42}      | ${3037} | ${18222}
   `(
     "ancienneté: $seniority an, ancienneté requise: $seniorityRight an, salaire de référence: $salary, => $expectedCompensation €",
     ({ seniority, seniorityRight, salary, expectedCompensation }) => {
