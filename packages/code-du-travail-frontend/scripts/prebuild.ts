95c2488ab49eec481ddc2b134c372f50aec3c6ba
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-03-20 14:42:42 +0100

    fix: comme avant

--- packages/code-du-travail-frontend/scripts/prebuild.ts
+++ packages/code-du-travail-frontend/scripts/prebuild.ts
@@ -31,9 +31,9 @@ export const generateWidgetScript = (host: string) => {
 };
 
 const run = () => {
-  const isOnDev = !process.env.NEXT_PUBLIC_IS_PRODUCTION_DEPLOYMENT;
+  const isProduction = !!process.env.NEXT_PUBLIC_IS_PRODUCTION_DEPLOYMENT;
   const host = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
-  generateRobotsTxt(isOnDev ? false : true, host);
+  generateRobotsTxt(isProduction, host);
   console.log("Robots.txt generated.");
   generateWidgetScript(host);
   console.log("widget.js generated.");
