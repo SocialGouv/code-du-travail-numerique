6017c5c6824edc3d2e6ac8c45b10fcbee48a4368
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-03-20 14:28:07 +0100

    fix: image

--- packages/code-du-travail-frontend/cypress.config.js
+++ packages/code-du-travail-frontend/cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.TEST_BASEURL ?? "http://localhost:3000",
     specPattern: "e2e/**/*.spec.{js,jsx,ts,tsx}",
-    supportFile: "support/index.ts",
+    supportFile: "e2e/utils/index.ts",
     viewportHeight: 1000,
     viewportWidth: 1280,
     chromeWebSecurity: false,
