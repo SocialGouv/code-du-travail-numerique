465a1c65a18421f6712ad070b9edb1ae21f5363d
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-03-23 15:08:24 +0100

    fix: api migration

--- packages/code-du-travail-frontend/src/api/modules/items/controller.ts
+++ packages/code-du-travail-frontend/src/api/modules/items/controller.ts
@@ -1,3 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { DEFAULT_ERROR_500_MESSAGE, NotFoundError } from "../../utils";
+import { getAllModeles } from "./service";
+
+export class ModelesController {
+  private req: NextApiRequest;
+  private res: NextApiResponse;
+
+  constructor(req: NextApiRequest, res: NextApiResponse) {
+    this.req = req;
+    this.res = res;
+  }
+
+  public async get() {
+    try {
+      const response = await getAllModeles();
+      this.res.status(200).json(response);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        this.res.status(404).json({ message: error.message });
+      } else {
+        this.res.status(500).json({
+          message: DEFAULT_ERROR_500_MESSAGE,
+        });
+      }
+    }
+  }
+}
+
 import { elasticDocumentsIndex, elasticsearchClient } from "../../utils";
 import { getDocumentBody, getSearchBySourceSlugBody } from "./queries";
 
