2d2c2a3f02ca0bc8455eb40285cc67883bff8113
carolineBda <caroline.bourdeu@gmail.com>
2023-03-30 14:26:10 +0200

    remove memoizee

--- packages/code-du-travail-frontend/src/conventions/Search/api/enterprises.service.ts
+++ packages/code-du-travail-frontend/src/conventions/Search/api/enterprises.service.ts
@@ -1,5 +1,4 @@
 import debounce from "debounce-promise";
-import memoizee from "memoizee";
 import { API_URL } from "../../../config";
 
 import { Agreement } from "./type";
@@ -35,51 +34,48 @@ const siretLengthError =
 const siretNumberError =
   "Veuillez indiquer un numéro Siret (14 chiffres uniquement)";
 
-const apiEnterprises = memoizee(
-  function createFetcher(
-    query: string,
-    address: string | undefined | null = undefined
-  ): Promise<Enterprise[]> {
-    if (/^\d{2,8}$/.test(query.replace(/\s/g, ""))) {
-      return Promise.reject(siretSirenError);
-    }
-    if (
-      /^\d{10,13}$/.test(query.replace(/\s/g, "")) ||
-      /^\d{15,}$/.test(query.replace(/\s/g, ""))
-    ) {
-      return Promise.reject(siretLengthError);
-    }
-    if (/\D+\d{14}/.test(query.replace(/\s/g, ""))) {
-      return Promise.reject(siretNumberError);
-    }
+const apiEnterprises = function createFetcher(
+  query: string,
+  address: string | undefined | null = undefined
+): Promise<Enterprise[]> {
+  if (/^\d{2,8}$/.test(query.replace(/\s/g, ""))) {
+    return Promise.reject(siretSirenError);
+  }
+  if (
+    /^\d{10,13}$/.test(query.replace(/\s/g, "")) ||
+    /^\d{15,}$/.test(query.replace(/\s/g, ""))
+  ) {
+    return Promise.reject(siretLengthError);
+  }
+  if (/\D+\d{14}/.test(query.replace(/\s/g, ""))) {
+    return Promise.reject(siretNumberError);
+  }
 
-    const url = `${API_URL}/enterprises?q=${encodeURIComponent(query)}${
-      address ? `&a=${encodeURIComponent(address)}` : ""
-    }`;
+  const url = `${API_URL}/enterprises?q=${encodeURIComponent(query)}${
+    address ? `&a=${encodeURIComponent(address)}` : ""
+  }`;
 
-    // if (/^\d{14}$/.test(query.replace(/\s/g, ""))) {
-    //   url = `${ENTERPRISE_API_URL}/etablissement/${query}`;
-    // } else if (/^\d{9}$/.test(query.replace(/\s/g, ""))) {
-    //   url = `${ENTERPRISE_API_URL}/entreprise/${query}`;
-    // }
+  // if (/^\d{14}$/.test(query.replace(/\s/g, ""))) {
+  //   url = `${ENTERPRISE_API_URL}/etablissement/${query}`;
+  // } else if (/^\d{9}$/.test(query.replace(/\s/g, ""))) {
+  //   url = `${ENTERPRISE_API_URL}/entreprise/${query}`;
+  // }
 
-    return fetch(url)
-      .then(async (response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        if (response.status === 404) {
-          return { entreprises: [] };
-        }
-        const errorMessage = await response.text();
-        return Promise.reject(errorMessage);
-      })
-      .then((result: ApiEnterpriseData) => {
-        return result.entreprises;
-      });
-  },
-  { length: false }
-);
+  return fetch(url)
+    .then(async (response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      if (response.status === 404) {
+        return { entreprises: [] };
+      }
+      const errorMessage = await response.text();
+      return Promise.reject(errorMessage);
+    })
+    .then((result: ApiEnterpriseData) => {
+      return result.entreprises;
+    });
+};
 
 const searchEnterprises = debounce(apiEnterprises, 300);
 
