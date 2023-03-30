7b586e3f7261a46995f8f5e8804e1f5dc858f925
carolineBda <caroline.bourdeu@gmail.com>
2023-03-30 14:00:46 +0200

    fix help on question

--- packages/code-du-travail-frontend/src/questionnaire/Components/Question/Question.tsx
+++ packages/code-du-travail-frontend/src/questionnaire/Components/Question/Question.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { DossierLicenciementContext, useStore } from "../../store";
 import { Response } from "./Response";
 import { ShowInfo } from "./ShowInfo";
-import { Fieldset, Legend } from "@socialgouv/cdtn-ui";
+import { Fieldset, Legend, Text } from "@socialgouv/cdtn-ui";
 import { InfoBulle } from "../../../outils/common/InfoBulle";
 import { trackClickHelp } from "../../tracking";
 import { useContext } from "react";
@@ -19,20 +19,22 @@ export const Question = ({ widgetMode }: QuestionProps) => {
     <ShowInfo slug={lastResponse.slug} widgetMode={widgetMode}></ShowInfo>
   ) : (
     <Fieldset>
-      <QuestionHeaderWrapper>
-        <LegendWrapper>{currentQuestion?.text}</LegendWrapper>
-        {currentQuestion?.info && (
-          <StyledInfoBulle
-            title={"Plus d'informations"}
-            dataTestid={`Tooltip-${currentQuestion?.text}`}
-            onVisibilityChange={() => {
-              trackClickHelp(currentQuestion.trackingName);
-            }}
-          >
-            {currentQuestion?.info}
-          </StyledInfoBulle>
-        )}
-      </QuestionHeaderWrapper>
+      <Legend>
+        <Text fontWeight="600">
+          {currentQuestion?.text}
+          {currentQuestion?.info && (
+            <StyledInfoBulle
+              title={"Plus d'informations"}
+              dataTestid={`Tooltip-${currentQuestion?.text}`}
+              onVisibilityChange={() => {
+                trackClickHelp(currentQuestion.trackingName);
+              }}
+            >
+              {currentQuestion.info}
+            </StyledInfoBulle>
+          )}
+        </Text>
+      </Legend>
 
       {currentQuestion?.responses.map((response, index: number) => (
         <Response
@@ -48,20 +50,6 @@ export const Question = ({ widgetMode }: QuestionProps) => {
 
 const StyledInfoBulle = styled(InfoBulle)`
   padding: 0;
-`;
-
-const LegendWrapper = styled(Legend)`
-  font-weight: 600;
-  float: left;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 16px;
-`;
-
-const QuestionHeaderWrapper = styled.div`
-  margin: 0 0 4px;
-  display: inline-block;
 `;
 
 const Description = styled.i`
