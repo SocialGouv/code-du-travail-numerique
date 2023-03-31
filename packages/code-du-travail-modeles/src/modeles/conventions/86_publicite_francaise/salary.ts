4ba9be000270ff1f9e337525a4ed98440959167b
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-03-31 17:54:15 +0200

    fix: cc

---
+++ packages/code-du-travail-modeles/src/modeles/conventions/86_publicite_francaise/salary.ts
@@ -0,0 +1,22 @@
+import type {
+  IReferenceSalary,
+  ReferenceSalaryProps,
+  SupportedCcIndemniteLicenciement,
+} from "../../common";
+import { nonNullable, rankByMonthArrayDescFrench } from "../../common";
+
+export class ReferenceSalary86
+  implements IReferenceSalary<SupportedCcIndemniteLicenciement.IDCC0086>
+{
+  /**
+   * - S
+   * S : salaire perçu par le salarié lors du dernier mois précédant le jour de l'envoi de la lettre de licenciement (brut)
+   */
+  computeReferenceSalary({
+    salaires = [],
+  }: ReferenceSalaryProps<SupportedCcIndemniteLicenciement.IDCC0086>): number {
+    const rankedSalaires = rankByMonthArrayDescFrench(salaires);
+    const salaryValues = rankedSalaires.map((a) => a.value).filter(nonNullable);
+    return salaryValues[0] ?? 0;
+  }
+}
