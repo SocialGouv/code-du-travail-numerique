9469cd5089821d946402ad27f8447c6057c2ea2c
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-03-31 09:32:40 +0200

    chore: test

--- packages/code-du-travail-frontend/pages/widgets/search.tsx
+++ packages/code-du-travail-frontend/pages/widgets/search.tsx
@@ -1,14 +1,21 @@
 import { useIframeResizer } from "../../src/common/hooks";
 import { SearchWidget } from "../../src/search/SearchWidget";
-import { push } from "@socialgouv/matomo-next";
+import { init, push } from "@socialgouv/matomo-next";
 import { useEffect } from "react";
+import { PIWIK_SITE_ID, PIWIK_URL, SITE_URL } from "../../src/config";
 
 function Widgets(): JSX.Element {
   useIframeResizer();
   useEffect(() => {
-    push(["setCookieSameSite", "None"]);
-    // @ts-ignore
-    push(["setSecureCookie", true]);
+    init({
+      siteId: PIWIK_SITE_ID,
+      url: PIWIK_URL,
+      onInitialization: () => {
+        push(["setCookieSameSite", "None"]);
+        // @ts-ignore
+        push(["setSecureCookie", true]);
+      },
+    });
   }, []);
 
   return <SearchWidget></SearchWidget>;
