487ef35c95b272012a027dba7d087cd14f6bcaa9
carolineBda <caroline.bourdeu@gmail.com>
2023-04-05 15:08:16 +0200

    fix formula

--- packages/code-du-travail-modeles/src/modeles/conventions/1606_bricolage/__tests__/indemnite-licenciement/formule.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/1606_bricolage/__tests__/indemnite-licenciement/formule.spec.ts
@@ -33,12 +33,12 @@ describe("Formule indemnité licenciement - 1606", () => {
   );
 
   test.each`
-    seniority  | age   | expectedFormula                               | expectedExplanations
-    ${11 / 12} | ${38} | ${""}                                         | ${[]}
-    ${25 / 12} | ${38} | ${"2% * Sref * A"}                            | ${["A : Ancienneté totale (≈ 2.08 ans : valeur arrondie)", "Sref : Salaire de référence (1000 €)"]}
-    ${25 / 12} | ${50} | ${"(2% * Sref * A) + (50% * (2% * Sref * A)"} | ${["A : Ancienneté totale (≈ 2.08 ans : valeur arrondie)", "Sref : Salaire de référence (1000 €)"]}
-    ${30}      | ${38} | ${"2% * Sref * A"}                            | ${["A : Ancienneté totale (30 ans)", "Sref : Salaire de référence (1000 €)"]}
-    ${30}      | ${50} | ${"(2% * Sref * A) + (50% * (2% * Sref * A)"} | ${["A : Ancienneté totale (30 ans)", "Sref : Salaire de référence (1000 €)"]}
+    seniority  | age   | expectedFormula                                | expectedExplanations
+    ${11 / 12} | ${38} | ${""}                                          | ${[]}
+    ${25 / 12} | ${38} | ${"2% * Sref * A"}                             | ${["A : Ancienneté totale (≈ 2.08 ans : valeur arrondie)", "Sref : Salaire de référence (1000 €)"]}
+    ${25 / 12} | ${50} | ${"(2% * Sref * A) + (50% * (2% * Sref * A))"} | ${["A : Ancienneté totale (≈ 2.08 ans : valeur arrondie)", "Sref : Salaire de référence (1000 €)"]}
+    ${30}      | ${38} | ${"2% * Sref * A"}                             | ${["A : Ancienneté totale (30 ans)", "Sref : Salaire de référence (1000 €)"]}
+    ${30}      | ${50} | ${"(2% * Sref * A) + (50% * (2% * Sref * A))"} | ${["A : Ancienneté totale (30 ans)", "Sref : Salaire de référence (1000 €)"]}
   `(
     "Formule cadres $expectedFormula avec $seniority an$s et age: age",
     ({ seniority, age, expectedFormula, expectedExplanations }) => {
