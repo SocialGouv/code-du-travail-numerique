9aa7a1c564aeff4fe7892d0d9cd01fd3b607215d
carolineBda <caroline.bourdeu@gmail.com>
2023-04-05 15:29:59 +0200

    Fix références

--- packages/code-du-travail-modeles/src/modeles/conventions/1606_bricolage/__tests__/indemnite-licenciement/references.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/1606_bricolage/__tests__/indemnite-licenciement/references.spec.ts
@@ -7,31 +7,41 @@ const engine = new IndemniteLicenciementPublicodes(
 
 const References = [
   {
-    article: "Article 9.2",
-    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000005775560?idConteneur=KALICONT000005635435",
+    article: "Article 9.2.4",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000005870734?idConteneur=KALICONT000005635871",
+  },
+];
+const ReferencesCadres50 = [
+  {
+    article: "Article 9.2.4",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000005870734?idConteneur=KALICONT000005635871",
+  },
+  {
+    article: "Article 10 de l'Annexe Cadres",
+    url: "https://www.legifrance.gouv.fr/conv_coll/article/KALIARTI000005870788?idConteneur=KALICONT000005635871",
   },
 ];
 
 describe("Vérification des références juridiques pour la CC 2511", () => {
   test.each`
-    seniority | inaptitude | expectedReferences
-    ${5}      | ${"non"}   | ${References}
-    ${5}      | ${"oui"}   | ${References}
-    ${6}      | ${"non"}   | ${References}
-    ${6}      | ${"oui"}   | ${References}
-    ${24}     | ${"non"}   | ${References}
-    ${24}     | ${"oui"}   | ${References}
+    age   | catPro            | expectedReferences
+    ${24} | ${"'Cadres'"}     | ${References}
+    ${24} | ${"'Non-Cadres'"} | ${References}
+    ${50} | ${"'Cadres'"}     | ${ReferencesCadres50.concat(References)}
+    ${50} | ${"'Non-Cadres'"} | ${References}
   `(
-    "pour un employé avec une ancienneté de $seniority mois",
-    ({ seniority, inaptitude, expectedReferences }) => {
+    "pour un $catPro avec une age de $age ans",
+    ({ catPro, age, expectedReferences }) => {
       engine.setSituation({
         "contrat salarié . convention collective": "'IDCC1606'",
         "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
-          seniority,
+          "3",
         "contrat salarié . indemnité de licenciement . ancienneté conventionnelle requise en année":
-          seniority,
-        "contrat salarié . indemnité de licenciement . inaptitude suite à un accident ou maladie professionnelle":
-          inaptitude,
+          "3",
+        "contrat salarié . convention collective . bricolage . catégorie professionnelle":
+          catPro,
+        "contrat salarié . convention collective . bricolage . indemnité de licenciement . cadres . age":
+          age,
         "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
           "1000",
       });
