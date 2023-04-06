7214147e6dedd2dd74b73fa4d0b200ebe9448e04
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2023-04-06 10:45:52 +0200

    fix(pages): remove last `getInitialProps` and transform some files in `tsx` (#5091)Co-authored-by: Caroline <4971715+carolineBda@users.noreply.github.com>
    * fix: pages
    
    * fix: pages
    
    * fix: recherche
    
    * fix: recherche
    
    * fix: build
    
    * Update packages/code-du-travail-frontend/pages/modeles-de-courriers/index.tsx
    
    Co-authored-by: Caroline <4971715+carolineBda@users.noreply.github.com>
    
    * fix: error
    
    * fix: cors
    
    ---------
    
    Co-authored-by: Caroline <4971715+carolineBda@users.noreply.github.com>

--- packages/code-du-travail-frontend/src/api/middleware.ts
+++ packages/code-du-travail-frontend/src/api/middleware.ts
@@ -2,20 +2,15 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Cors from "cors";
 import { IS_PROD } from "../config";
 
-// Initializing the cors middleware
-// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-const cors = Cors();
+const cors = Cors({ origin: IS_PROD ? false : "*" });
 
 export function runMiddleware(req: NextApiRequest, res: NextApiResponse) {
   return new Promise((resolve, reject) => {
-    if (!IS_PROD) {
-      cors(req, res, (result: any) => {
-        if (result instanceof Error) {
-          return reject(result);
-        }
-
-        return resolve(result);
-      });
-    }
+    cors(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
   });
 }
