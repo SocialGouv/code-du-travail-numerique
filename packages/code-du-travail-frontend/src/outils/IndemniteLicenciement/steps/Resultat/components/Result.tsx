49082f162b420df46f739ae0661cbbe778be20de
carolineBda <caroline.bourdeu@gmail.com>
2023-04-12 11:33:12 +0200

    feat(result): add notification + remove "brut"

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/components/Result.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/components/Result.tsx
@@ -13,36 +13,52 @@ type Props = {
   resultMessage: string;
 };
 
-export default function Result(props: Props) {
+export default function Result({
+  notifications = [],
+  resultMessage,
+  maxResult,
+}: Props) {
+  const notifs = [
+    {
+      dottedName: "congé paternité",
+      description:
+        "Depuis le 11 mars 2023 les périodes d’absence pour congé paternité ne sont plus retirées de l’ancienneté du salarié. Si le salarié a pris un congé paternité avant cette date, il peut être décompté de son ancienneté et de ce fait, donner lieu à un montant d’indemnité moins favorable que celui de notre simulateur.",
+    } as Notification,
+  ].concat(notifications);
   return (
     <>
       <SectionTitle hasSmallMarginTop>Indemnité</SectionTitle>
       <p>
-        {props.resultMessage}{" "}
-        <HighlightResult>
-          {props.maxResult.replace(".", ",")}&nbsp;€&nbsp;brut.
-        </HighlightResult>
-        {props.notifications && props.notifications.length > 0 && (
-          <NoticeNote
-            numberOfElements={props.notifications.length}
-            currentElement={0}
-            isList
-          />
-        )}
+        {resultMessage}{" "}
+        <HighlightResult>{maxResult.replace(".", ",")}&nbsp;€.</HighlightResult>
+        <NoticeNote
+          numberOfElements={notifs.length}
+          currentElement={0}
+          isList
+        />
       </p>
-      {props.notifications && props.notifications.length > 0 && (
-        <SmallText>
-          {props.notifications.map((notification, index) => (
-            <>
-              <NoticeNote
-                numberOfElements={props.notifications!.length}
-                currentElement={1 + index}
-              />
-              {notification.description}
-            </>
-          ))}
-        </SmallText>
-      )}
+      <p>
+        Ce montant est exonéré d’impôt sur le revenu et de cotisations sociales
+        sous certaines conditions (en savoir plus sur l’exonération de l’
+        <a
+          href="/fiche-service-public/indemnite-de-licenciement-du-salarie-en-cdi"
+          target="_blank"
+        >
+          indemnité de licenciement
+        </a>
+        )
+      </p>
+      <SmallText>
+        {notifs.map((notification, index) => (
+          <span key={index}>
+            <NoticeNote
+              numberOfElements={notifs.length}
+              currentElement={1 + index}
+            />
+            {notification.description}
+          </span>
+        ))}
+      </SmallText>
     </>
   );
 }
--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/components/__tests__/Result.test.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/components/__tests__/Result.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import Result from "../Result";
 
@@ -8,5 +8,10 @@ describe("<Result />", () => {
       <Result maxResult="2000" resultMessage="Le résultat est :" />
     );
     expect(queryByText(/2000/i)).toBeInTheDocument();
+    expect(
+      queryByText(
+        /Depuis le 11 mars 2023 les périodes d’absence pour congé paternité/
+      )
+    ).toBeInTheDocument();
   });
 });
