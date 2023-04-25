d68625d011f329c925813db1a30a193679e383a5
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-04-25 15:48:11 +0200

    feat: ajout de la CC 1043 à l'indemnité licenciement

---
+++ packages/code-du-travail-modeles/src/modeles/conventions/1043_gardien_concierge/__tests__/indemnite-licenciement/indemnite.spec.ts
@@ -0,0 +1,37 @@
+import { IndemniteLicenciementPublicodes } from "../../../../../publicodes";
+
+const engine = new IndemniteLicenciementPublicodes(
+  modelsIndemniteLicenciement,
+  "1043"
+);
+
+describe("CC 1043", () => {
+  describe("Calcul de l'indemnité de licenciement", () => {
+    test.each`
+      seniorityRight | seniority | salaireRef | expectedCompensation
+      ${0.91}        | ${1}      | ${2464}    | ${0}
+      ${0.91}        | ${7}      | ${2464}    | ${3449.6}
+      ${0.91}        | ${19}     | ${2464}    | ${13305.6}
+      ${0.91}        | ${27}     | ${2464}    | ${21847.47}
+    `(
+      "Avec une ancienneté $seniority ans, un salaire de référence $salaireRef € => une compensation de base de $expectedCompensation €",
+      ({ salaireRef, expectedCompensation, seniority, seniorityRight }) => {
+        const { result, missingArgs } = engine.setSituation(
+          {
+            "contrat salarié . convention collective": "'IDCC1043'",
+            "contrat salarié . indemnité de licenciement . ancienneté conventionnelle en année":
+              seniority,
+            "contrat salarié . indemnité de licenciement . ancienneté conventionnelle requise en année":
+              seniorityRight,
+            "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
+              salaireRef,
+          },
+          "contrat salarié . indemnité de licenciement . résultat conventionnel"
+        );
+        expect(result.unit?.numerators).toEqual(["€"]);
+        expect(missingArgs).toEqual([]);
+        expect(result.value).toEqual(expectedCompensation);
+      }
+    );
+  });
+});
