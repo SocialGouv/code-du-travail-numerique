04fa7f0946ebc6950014e6a69e0509c946d4a761
carolineBda <caroline.bourdeu@gmail.com>
2023-05-03 13:53:54 +0200

    revert

--- packages/code-du-travail-modeles/src/modeles/common/utils/global.ts
+++ packages/code-du-travail-modeles/src/modeles/common/utils/global.ts
@@ -1,3 +1,7 @@
+function parseIdcc(idcc: string): number {
+  return parseInt(idcc, 10);
+}
+
 function formatIdcc(num: number | string): string {
   return `0000${num}`.slice(-4);
 }
@@ -14,4 +18,4 @@ function extractMdxContentUrl(
   )?.[0];
 }
 
-export { extractMdxContentUrl, formatIdcc };
+export { extractMdxContentUrl, formatIdcc, parseIdcc };
