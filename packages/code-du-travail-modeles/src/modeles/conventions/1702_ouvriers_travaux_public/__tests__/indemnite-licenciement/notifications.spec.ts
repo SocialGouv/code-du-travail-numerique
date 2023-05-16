49db7dc76231e5df1b812637aabc1debf83a1aa3
Martial Maillot <martial.maillot@gmail.com>
2023-05-16 09:54:03 +0200

    fix: feedbacks

--- packages/code-du-travail-modeles/src/modeles/conventions/1702_ouvriers_travaux_public/__tests__/indemnite-licenciement/notifications.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/1702_ouvriers_travaux_public/__tests__/indemnite-licenciement/notifications.spec.ts
@@ -5,14 +5,10 @@ const engine = new IndemniteLicenciementPublicodes(
   "1702"
 );
 
-const notification =
-  "Le montant de l’indemnité doit être calculé sur la base des salaires (reconstitués) que la salarié aurait dû percevoir au cours des 3 derniers mois précédant la rupture du contrat s’il n’avait pas été en arrêt de travail. Pour simplifier l'utilisation de ce simulateur, la calcul se base ici sur les salaires perçus avant l'arrêt travail et non sur les salaires reconstitués. Le montant de l'indemnité pourrait donc être plus important que celui donné par le simulateur.";
-
 describe("Notification pour la CC 1702", () => {
   test("l'utilisateur est en arrêt de travail", () => {
     engine.setSituation({
       "contrat salarié . convention collective": "'IDCC1702'",
-      "contrat salarié . indemnité de licenciement . arrêt de travail": "oui",
       "contrat salarié . convention collective . ouvriers travaux public . indemnité de licenciement . age":
         "40",
       "contrat salarié . convention collective . ouvriers travaux public . indemnité de licenciement . licenciement économique": `'Non'`,
@@ -20,19 +16,21 @@ describe("Notification pour la CC 1702", () => {
         "10",
       "contrat salarié . indemnité de licenciement . ancienneté conventionnelle requise en année":
         "10",
+      "contrat salarié . indemnité de licenciement . arrêt de travail": "oui",
       "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
         "2500",
     });
 
     const notifications = engine.getNotifications();
     expect(notifications).toHaveLength(1);
-    expect(notifications[0].description).toBe(notification);
+    expect(notifications[0].description).toBe(
+      "Le montant de l’indemnité doit être calculé sur la base des salaires (reconstitués) que le salarié aurait dû percevoir au cours des 3 derniers mois précédant la rupture du contrat s’il n’avait pas été en arrêt de travail. Pour simplifier l'utilisation de ce simulateur, le calcul se base ici sur les salaires perçus avant l'arrêt travail et non sur les salaires reconstitués. Le montant de l'indemnité pourrait donc être plus important que celui donné par le simulateur."
+    );
   });
 
   test("l'utilisateur est en licenciement économique", () => {
     engine.setSituation({
       "contrat salarié . convention collective": "'IDCC1702'",
-      "contrat salarié . indemnité de licenciement . arrêt de travail": "non",
       "contrat salarié . convention collective . ouvriers travaux public . indemnité de licenciement . age":
         "40",
       "contrat salarié . convention collective . ouvriers travaux public . indemnité de licenciement . licenciement économique": `'Oui'`,
@@ -40,6 +38,7 @@ describe("Notification pour la CC 1702", () => {
         "10",
       "contrat salarié . indemnité de licenciement . ancienneté conventionnelle requise en année":
         "10",
+      "contrat salarié . indemnité de licenciement . arrêt de travail": "non",
       "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
         "2500",
     });
