b25cf49090cddc8274372796b6be6080864c351e
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-05-25 15:58:18 +0200

    fix: code

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/components/ForMoreInfo.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/components/ForMoreInfo.tsx
@@ -2,17 +2,22 @@ import Link from "next/link";
 
 export default function ForMoreInfo() {
   return (
-    <p>
-      Pour en savoir plus sur l’indemnité de licenciement et son mode de calcul,
-      consultez{" "}
-      <Link
-        href={`/fiche-service-public/indemnite-de-licenciement-du-salarie-en-cdi`}
-        passHref
-        target={"_blank"}
-      >
-        cet article
-      </Link>
-      .
-    </p>
+    <>
+      <p>
+        Pour en savoir plus sur l’indemnité de licenciement et son mode de calcul,
+        consultez{" "}
+        <Link
+          href={`/fiche-service-public/indemnite-de-licenciement-du-salarie-en-cdi`}
+          passHref
+          target={"_blank"}
+        >
+          cet article
+        </Link>
+        .
+      </p>
+      <i>
+        Le montant donné n’est qu’une estimation, il est donné à titre indicatif. Pour simplifier l’utilisation de ce simulateur, certains paramètres complexes n’ont pas été pris en compte dans le calcul de l’indemnité et peuvent donner lieu à un montant différent. Par exemple, les absences de moins d’un mois ou les contrats antérieurs au CDI ne sont pas pris en compte dans le calcul de l’ancienneté du salarié.
+      </i>
+    </>
   );
 }
