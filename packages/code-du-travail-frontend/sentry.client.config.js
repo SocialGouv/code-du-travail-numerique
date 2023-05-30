abbdde31f16d6e1bfe59fab1c03739f4a5a0ccf9
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-05-30 11:36:31 +0200

    fix: ref name

--- packages/code-du-travail-frontend/sentry.client.config.js
+++ packages/code-du-travail-frontend/sentry.client.config.js
@@ -11,7 +11,8 @@ Sentry.init({
     process.env.NEXT_PUBLIC_SENTRY_ENV || process.env.SENTRY_ENV || "dev",
   tracesSampleRate: 1.0,
   release: process.env.NEXT_PUBLIC_SENTRY_RELEASE || process.env.SENTRY_RELEASE,
-  replaysSessionSampleRate: 1.0,
+
+  replaysSessionSampleRate: 0.01,
   replaysOnErrorSampleRate: 1.0,
-  integrations: [new Sentry.Replay()],
+  integrations: [new Sentry.Replay({ maskAllText: true, blockAllMedia: true })],
 });
