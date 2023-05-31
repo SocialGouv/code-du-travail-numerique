3cb24495974b4c2d65c8e477958099ee635d871a
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-05-31 10:45:05 +0200

    fix: review branch

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/components/ForMoreInfo.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/components/ForMoreInfo.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import styled from "styled-components";
+import { theme } from "@socialgouv/cdtn-ui";
 
 export default function ForMoreInfo() {
   return (
     <>
       <p>
-        Pour en savoir plus sur l’indemnité de licenciement et son mode de calcul,
-        consultez{" "}
+        Pour en savoir plus sur l’indemnité de licenciement et son mode de
+        calcul, consultez{" "}
         <Link
           href={`/fiche-service-public/indemnite-de-licenciement-du-salarie-en-cdi`}
           passHref
@@ -15,9 +17,19 @@ export default function ForMoreInfo() {
         </Link>
         .
       </p>
-      <i>
-        Le montant donné n’est qu’une estimation, il est donné à titre indicatif. Pour simplifier l’utilisation de ce simulateur, certains paramètres complexes n’ont pas été pris en compte dans le calcul de l’indemnité et peuvent donner lieu à un montant différent. Par exemple, les absences de moins d’un mois ou les contrats antérieurs au CDI ne sont pas pris en compte dans le calcul de l’ancienneté du salarié.
-      </i>
+      <StyledI>
+        Le montant donné n’est qu’une estimation, il est donné à titre
+        indicatif. Pour simplifier l’utilisation de ce simulateur, certains
+        paramètres complexes n’ont pas été pris en compte dans le calcul de
+        l’indemnité et peuvent donner lieu à un montant différent. Par exemple,
+        les absences de moins d’un mois ou les contrats antérieurs au CDI ne
+        sont pas pris en compte dans le calcul de l’ancienneté du salarié.
+      </StyledI>
     </>
   );
 }
+const { fonts } = theme;
+
+const StyledI = styled.i`
+  font-size: ${fonts.sizes.small};
+`;
