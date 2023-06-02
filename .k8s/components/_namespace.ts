9af94763356e2ccb67d031af0a613964b960bcac
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-06-02 10:55:51 +0200

    fix: janitor

--- .k8s/components/_namespace.ts
+++ .k8s/components/_namespace.ts
@@ -1,7 +1,3 @@
 import { createNamespace } from "@socialgouv/kosko-charts/components/namespace";
 
-export default createNamespace({
-  annotations: {
-    "janitor/ttl": "365d",
-  },
-});
+export default createNamespace();
