191cba3373ced59725710b9c0d7befa5065f6ebe
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-06-05 20:15:22 +0200

    chore(ci): use kontinuous deploy (#4519)
    Co-authored-by: Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
    Co-authored-by: Jo <jo@surikat.pro>
    Co-authored-by: maxgfr <25312957+maxgfr@users.noreply.github.com>

--- .k8s/components/netpol.ts
+++
@@ -1,3 +0,0 @@
-import { create } from "@socialgouv/kosko-charts/components/netpol";
-
-export default create();
--- .k8s/components/yaml.ts
+++
@@ -1,8 +0,0 @@
-import env from "@kosko/env";
-import { importYamlFolder } from "@socialgouv/kosko-charts/components/yaml";
-import path from "path";
-
-const manifests = importYamlFolder(
-  path.join(__dirname, "..", `environments/${env.env}/yaml`)
-);
-export default manifests;
