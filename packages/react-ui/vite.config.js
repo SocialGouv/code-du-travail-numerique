18b795c068c4fafc57dfa28cb5b45e1fe11c12f9
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-06-07 12:28:50 +0200

    fix(storybook): typescript error in production environment

--- packages/react-ui/vite.config.js
+++ packages/react-ui/vite.config.js
@@ -1,20 +1,9 @@
 import react from "@vitejs/plugin-react";
 import fs from "fs/promises";
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/index.ts"),
-      // the proper extensions will be added
-      fileName: "react-ui",
-
-      name: "@socialgouv/react-ui",
-    },
-  },
   esbuild: {
     exclude: [],
     include: /(src|\.storybook)\/.*\.[tj]sx?$/,
@@ -28,7 +17,7 @@ export default defineConfig({
           setup(build) {
             build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
               contents: await fs.readFile(args.path, "utf8"),
-              loader: "jsx",
+              loader: "tsx",
             }));
           },
         },
