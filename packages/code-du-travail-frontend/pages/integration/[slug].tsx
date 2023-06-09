80c3f738b9d66837c39602fad9a856ff31c137dc
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-06-09 16:23:28 +0200

    fix: protocol

--- packages/code-du-travail-frontend/pages/integration/[slug].tsx
+++ packages/code-du-travail-frontend/pages/integration/[slug].tsx
@@ -8,6 +8,7 @@ import { integrationData, IntegrationContainer } from "../../src/integration";
 import { SITE_URL } from "../../src/config";
 
 const IntegrationPage = (props): JSX.Element => {
+  console.log(props);
   const {
     description,
     metaDescription,
