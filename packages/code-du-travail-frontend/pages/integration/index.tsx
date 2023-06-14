d8392437d18d1315119e640dbd0f594f944149c4
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-06-14 17:15:25 +0200

    fix: wording

--- packages/code-du-travail-frontend/pages/integration/index.tsx
+++ packages/code-du-travail-frontend/pages/integration/index.tsx
@@ -12,7 +12,7 @@ const FindAnswerPage = (): JSX.Element => {
     <Layout>
       <Metas
         title="Widgets pour intégrer le Code du travail numérique à votre site"
-        description="L’équipe du Code du travail numérique vous propose d’intégrer son moteur de recherche et certains de ses simulateurs sur votre site grâce à un module (widget)."
+        description="L’équipe du Code du travail numérique vous propose d’intégrer son moteur de recherche, ses modèles de courriers ainsi que certains de ses simulateurs et outils sur votre site grâce à un module (widget)."
       />
 
       <Section>
