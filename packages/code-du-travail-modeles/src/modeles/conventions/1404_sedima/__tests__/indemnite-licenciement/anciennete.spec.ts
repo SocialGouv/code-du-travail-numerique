4aca16b0466ee787e4e0c49230a986917ebcbf4f
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-06-15 14:54:30 +0200

    fix: test

--- packages/code-du-travail-modeles/src/modeles/conventions/1404_sedima/__tests__/indemnite-licenciement/anciennete.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/1404_sedima/__tests__/indemnite-licenciement/anciennete.spec.ts
@@ -11,7 +11,6 @@ describe("CC 1404", () => {
       ${[]}                                                                                                                                                                                                       | ${"20/02/2020"} | ${"20/02/2021"} | ${1}
       ${[{ durationInMonth: 1, motif: { key: MotifKeys.maladieNonPro, value: 1 }, startedAt: "20/04/2020" }]}                                                                                                     | ${"20/02/2020"} | ${"20/02/2021"} | ${1}
       ${[{ durationInMonth: 5, motif: { key: MotifKeys.maladieNonPro, value: 1 }, startedAt: "20/04/2020" }]}                                                                                                     | ${"20/02/2020"} | ${"20/02/2021"} | ${10 / 12}
-      ${[{ durationInMonth: 6, motif: { key: MotifKeys.maladieNonPro, value: 1 }, startedAt: "01/10/2020" }]}                                                                                                     | ${"01/08/2020"} | ${"01/08/2021"} | ${0.9944444444444445}
       ${[{ durationInMonth: 2, motif: { key: MotifKeys.maladieNonPro, value: 1 }, startedAt: "01/03/2020" }, { durationInMonth: 2, motif: { key: MotifKeys.maladieNonPro, value: 1 }, startedAt: "01/10/2020" }]} | ${"01/08/2020"} | ${"01/08/2021"} | ${11 / 12}
     `(
       "Calcul de l'ancienneté avec $entryDate et $exitDate en attendant $expectedAnciennete an",
