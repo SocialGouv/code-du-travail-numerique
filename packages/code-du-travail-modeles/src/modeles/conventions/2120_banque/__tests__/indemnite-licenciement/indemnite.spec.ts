be9bbef0106b8527612091c59de32f3111c51e7d
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-06-20 20:06:22 +0200

    fix: tu

--- packages/code-du-travail-modeles/src/modeles/conventions/2120_banque/__tests__/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/2120_banque/__tests__/indemnite-licenciement/indemnite.spec.ts
@@ -8,37 +8,37 @@ const engine = new IndemniteLicenciementPublicodes(
 
 describe("Indemnité conventionnel de licenciement pour la CC 2120", () => {
   test.each`
-    categoriePro    | semestresAvant2002 | semestresApres2002 | licenciementEco       | licenciementDisciplinaire | seniorityRight | seniority | salary  | expectedCompensation
-    ${"Non-cadres"} | ${0}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.non}     | ${0}           | ${0}      | ${2000} | ${0}
-    ${"Non-cadres"} | ${0}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${0.5}         | ${0.5}    | ${2000} | ${0}
-    ${"Non-cadres"} | ${0}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${0.67}        | ${0.67}   | ${2000} | ${335}
-    ${"Non-cadres"} | ${0}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${0.67}        | ${15}     | ${2000} | ${8333.33}
-    ${"Non-cadres"} | ${1}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${0.91}        | ${0.91}   | ${1991} | ${0}
-    ${"Non-cadres"} | ${2}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${1}      | ${1991} | ${1991}
-    ${"Non-cadres"} | ${6}               | ${34}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${20}     | ${1991} | ${22896.5}
-    ${"Non-cadres"} | ${6}               | ${25}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${15.67}  | ${1991} | ${18416.75}
-    ${"Non-cadres"} | ${6}               | ${19}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${12.5}   | ${1991} | ${15430.25}
-    ${"Cadres"}     | ${1}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${0.91}        | ${0.91}   | ${3064} | ${0}
-    ${"Cadres"}     | ${2}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${1}      | ${3064} | ${3064}
-    ${"Cadres"}     | ${6}               | ${34}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${20}     | ${3064} | ${35236}
-    ${"Cadres"}     | ${6}               | ${25}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${15.67}  | ${3064} | ${28342}
-    ${"Cadres"}     | ${6}               | ${19}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${12.5}   | ${3064} | ${23746}
-    ${"Non-cadres"} | ${0}               | ${1}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${0.91}        | ${0.91}   | ${1991} | ${0}
-    ${"Non-cadres"} | ${2}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${1}      | ${1991} | ${1991}
-    ${"Non-cadres"} | ${4}               | ${36}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${20}     | ${1991} | ${21901}
-    ${"Non-cadres"} | ${4}               | ${27}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${15.67}  | ${1991} | ${17421.25}
-    ${"Non-cadres"} | ${4}               | ${21}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${12.5}   | ${1991} | ${14434.75}
-    ${"Non-cadres"} | ${0}               | ${1}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${0.91}        | ${0.91}   | ${2772} | ${0}
-    ${"Non-cadres"} | ${2}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${1}      | ${2772} | ${2485.24}
-    ${"Non-cadres"} | ${6}               | ${34}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${20}     | ${2772} | ${26305.32}
-    ${"Non-cadres"} | ${6}               | ${25}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${15.67}  | ${2772} | ${21315.72}
-    ${"Non-cadres"} | ${6}               | ${19}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${12.5}   | ${2772} | ${17989.32}
-    ${"Cadres"}     | ${0}               | ${1}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${0.91}        | ${0.91}   | ${2772} | ${0}
-    ${"Cadres"}     | ${2}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${1}      | ${2772} | ${2485.24}
-    ${"Cadres"}     | ${6}               | ${34}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${20}     | ${2772} | ${26305.32}
-    ${"Cadres"}     | ${6}               | ${25}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${15.67}  | ${2772} | ${21315.72}
-    ${"Cadres"}     | ${6}               | ${19}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${12.5}   | ${2772} | ${17989.32}
-    ${"Non-cadres"} | ${1}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${0.91}        | ${0.91}   | ${2772} | ${0}
+    entryDate       | categoriePro    | semestresAvant2002 | semestresApres2002 | licenciementEco       | licenciementDisciplinaire | seniorityRight | seniority | salary  | expectedCompensation
+    ${"01/01/1999"} | ${"Non-cadres"} | ${0}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.non}     | ${0}           | ${0}      | ${2000} | ${0}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${0}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${0.5}         | ${0.5}    | ${2000} | ${0}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${0}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${0.67}        | ${0.67}   | ${2000} | ${335}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${0}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${0.67}        | ${15}     | ${2000} | ${8333.33}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${1}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${0.91}        | ${0.91}   | ${1991} | ${0}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${2}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${1}      | ${1991} | ${1991}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${6}               | ${34}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${20}     | ${1991} | ${22896.5}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${6}               | ${25}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${15.67}  | ${1991} | ${18416.75}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${6}               | ${19}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${12.5}   | ${1991} | ${15430.25}
+    ${"01/01/1999"} | ${"Cadres"}     | ${1}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${0.91}        | ${0.91}   | ${3064} | ${0}
+    ${"01/01/1999"} | ${"Cadres"}     | ${2}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${1}      | ${3064} | ${3064}
+    ${"01/01/1999"} | ${"Cadres"}     | ${6}               | ${34}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${20}     | ${3064} | ${35236}
+    ${"01/01/1999"} | ${"Cadres"}     | ${6}               | ${25}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${15.67}  | ${3064} | ${28342}
+    ${"01/01/1999"} | ${"Cadres"}     | ${6}               | ${19}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${12.5}   | ${3064} | ${23746}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${0}               | ${1}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${0.91}        | ${0.91}   | ${1991} | ${0}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${2}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${1}      | ${1991} | ${1991}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${4}               | ${36}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${20}     | ${1991} | ${21901}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${4}               | ${27}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${15.67}  | ${1991} | ${17421.25}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${4}               | ${21}              | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${1}           | ${12.5}   | ${1991} | ${14434.75}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${0}               | ${1}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${0.91}        | ${0.91}   | ${2772} | ${0}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${2}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${1}      | ${2772} | ${2485.24}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${6}               | ${34}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${20}     | ${2772} | ${26305.32}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${6}               | ${25}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${15.67}  | ${2772} | ${21315.72}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${6}               | ${19}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${12.5}   | ${2772} | ${17989.32}
+    ${"01/01/1999"} | ${"Cadres"}     | ${0}               | ${1}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${0.91}        | ${0.91}   | ${2772} | ${0}
+    ${"01/01/1999"} | ${"Cadres"}     | ${2}               | ${0}               | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${1}      | ${2772} | ${2485.24}
+    ${"01/01/1999"} | ${"Cadres"}     | ${6}               | ${34}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${20}     | ${2772} | ${26305.32}
+    ${"01/01/1999"} | ${"Cadres"}     | ${6}               | ${25}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${15.67}  | ${2772} | ${21315.72}
+    ${"01/01/1999"} | ${"Cadres"}     | ${6}               | ${19}              | ${QuestionOuiNon.non} | ${QuestionOuiNon.oui}     | ${1}           | ${12.5}   | ${2772} | ${17989.32}
+    ${"01/01/1999"} | ${"Non-cadres"} | ${1}               | ${0}               | ${QuestionOuiNon.oui} | ${QuestionOuiNon.non}     | ${0.91}        | ${0.91}   | ${2772} | ${0}
   `(
     "$#) Catégorie pro $categoriePro, seniorityRight: $seniorityRight an, semestresAvant2002 $semestresAvant2002, semestresApres2002 $semestresApres2002, licenciementDisciplinaire $licenciementDisciplinaire, licenciementEco $licenciementEco, salaire de référence: $salary => $expectedCompensation",
     ({
@@ -51,6 +51,7 @@ describe("Indemnité conventionnel de licenciement pour la CC 2120", () => {
       salary,
       expectedCompensation,
       seniority,
+      entryDate,
     }) => {
       const { result, missingArgs } = engine.setSituation(
         {
@@ -66,6 +67,8 @@ describe("Indemnité conventionnel de licenciement pour la CC 2120", () => {
             seniority,
           "contrat salarié . indemnité de licenciement . ancienneté conventionnelle requise en année":
             seniorityRight,
+          "contrat salarié . indemnité de licenciement . date d'entrée":
+            entryDate,
           "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
             salary,
         },
