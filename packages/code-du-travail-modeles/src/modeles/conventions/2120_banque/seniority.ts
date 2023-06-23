9b2651036c9c5c8dc72592d6fcf79b9819fd34a0
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-06-23 15:37:06 +0200

    fix: seniority

--- packages/code-du-travail-modeles/src/modeles/conventions/2120_banque/seniority.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/2120_banque/seniority.ts
@@ -13,12 +13,6 @@ import { MotifKeys } from "../../common/motif-keys";
 import { SeniorityDefault } from "../../common/seniority";
 
 const MOTIFS_2120 = LEGAL_MOTIFS.map((item) => {
-  if (item.key === MotifKeys.maladieNonPro) {
-    return {
-      ...item,
-      value: 0,
-    };
-  }
   return {
     ...item,
     startAt: () => true,
