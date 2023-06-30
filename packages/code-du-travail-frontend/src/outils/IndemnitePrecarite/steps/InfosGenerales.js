8e3bcd86a306b9189d94f684877062cd0361bf3e
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-06-30 09:42:54 +0200

    fix: question sans interuption

--- packages/code-du-travail-frontend/src/outils/IndemnitePrecarite/steps/InfosGenerales.js
+++ packages/code-du-travail-frontend/src/outils/IndemnitePrecarite/steps/InfosGenerales.js
@@ -79,6 +79,7 @@ function StepInfosGenerales({ form }) {
                 [
                   "cttFormation",
                   "propositionCDIFindeContrat",
+                  "propositionCDIFinContrat",
                   "refusSouplesse",
                   "ruptureContratFauteGrave",
                   "cttFormation",
