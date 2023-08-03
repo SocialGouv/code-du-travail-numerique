5610c890c32b3d10edf3558cabf60c929b7b7208
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2023-08-03 17:11:38 +0200

    Update packages/code-du-travail-frontend/src/outils/common/Feedback/QuestionnaireAdvanced.tsx
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>

--- packages/code-du-travail-frontend/src/outils/common/Feedback/QuestionnaireAdvanced.tsx
+++ packages/code-du-travail-frontend/src/outils/common/Feedback/QuestionnaireAdvanced.tsx
@@ -64,7 +64,7 @@ export const QuestionnaireAdvanced = ({
             setStatusExplanation(status);
           }}
           dataTestId="resultClarity"
-        ></StyledQuestionnaireItem>
+        />
         <QuestionnaireText
           title="Vous souhaitez nous en dire davantage ?"
           placeholder="ex: la question sur la date de début du contrat n'est pas claire"
