bea5be023d0e840efd2a04900a283597d48941a5
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-08-08 19:03:54 +0200

    fix: msg

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/components/ErrorPublicodes.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/steps/Resultat/components/ErrorPublicodes.tsx
@@ -10,7 +10,8 @@ export default function ErrorPublicodes() {
       <Paragraph noMargin>
         Nous n&apos;avons pas pu calculer votre indemnité de licenciement, en
         raison d&apos;une erreur liée à notre moteur de calcul. Veuillez
-        vérifier les informations saisies.
+        vérifier les informations saisies ou rafraîchir la page si le problème
+        persiste.
       </Paragraph>
     </div>
   );
