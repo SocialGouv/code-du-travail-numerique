9d35d7c18a94f1faa88c1801b52ab3e8d4541f2f
carolineBda <caroline.bourdeu@gmail.com>
2023-09-22 14:17:23 +0200

    remove sentry replay

--- packages/code-du-travail-frontend/sentry.client.config.js
+++ packages/code-du-travail-frontend/sentry.client.config.js
@@ -3,7 +3,6 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
-import { Replay } from "@sentry/browser";
 
 Sentry.init({
   dsn:
@@ -12,8 +11,4 @@ Sentry.init({
     process.env.NEXT_PUBLIC_SENTRY_ENV || process.env.SENTRY_ENV || "dev",
   tracesSampleRate: 0.2,
   release: process.env.NEXT_PUBLIC_SENTRY_RELEASE || process.env.SENTRY_RELEASE,
-
-  replaysSessionSampleRate: 0.01,
-  replaysOnErrorSampleRate: 1.0,
-  integrations: [new Replay()],
 });
