88ead81f55e16d4d458cb7b044d24fe499859541
Caroline <4971715+carolineBda@users.noreply.github.com>
2023-10-19 09:23:52 +0200

    feat(accessibilité): un seul lien sur les tuiles (#5388)

--- packages/code-du-travail-frontend/src/content/Components/ContentList.tsx
+++ packages/code-du-travail-frontend/src/content/Components/ContentList.tsx
@@ -1,21 +1,18 @@
+import { Grid, icons } from "@socialgouv/cdtn-ui";
 import { BlockDisplayMode } from "@socialgouv/cdtn-utils";
-import { icons, theme } from "@socialgouv/cdtn-ui";
+
 import React from "react";
-import styled from "styled-components";
 import { ListLink } from "../../search/SearchResults/Results";
-import useWindowDimensions from "../../common/WindowDimension";
+import styled from "styled-components";
 
 export const ContentList = ({ block, key }) => {
-  let forceBlockDisplayMode;
-  const { width } = useWindowDimensions();
-  if (width < theme.breakpoints.intDesktop) {
-    forceBlockDisplayMode = BlockDisplayMode.line;
-  }
   const { blockDisplayMode, contents } = block;
-  const displayMode = forceBlockDisplayMode || blockDisplayMode;
   return (
     <>
-      <ContentItemList square={displayMode === BlockDisplayMode.square}>
+      <ContentItemList
+        columns={blockDisplayMode === BlockDisplayMode.square ? 2 : 1}
+        square={blockDisplayMode === BlockDisplayMode.square}
+      >
         {contents?.map((item, ContentIndex) => (
           <ListLink
             item={{
@@ -24,7 +21,7 @@ export const ContentList = ({ block, key }) => {
             }}
             key={`${key}-${ContentIndex}`}
             disableAnalytics
-            centerTitle={displayMode === BlockDisplayMode.square}
+            centerTitle={blockDisplayMode === BlockDisplayMode.square}
           ></ListLink>
         ))}
       </ContentItemList>
@@ -32,23 +29,8 @@ export const ContentList = ({ block, key }) => {
   );
 };
 
-const ContentItemList = styled.div`
-  display: grid;
-  grid-template-columns: ${({ square }) =>
-    square ? "repeat(2, 1fr)" : "repeat(1, 1fr)"};
-  margin: ${theme.spacings.medium};
-  column-gap: ${theme.spacings.large};
-  row-gap: ${theme.spacings.large};
-
-  & > div {
-    ${({ square }) => square && "min-height: 341px;"};
-    padding: ${theme.spacings.large} ${theme.spacings.medium};
-    height: 100%;
-  }
-
-  p,
-  a,
-  button {
+const ContentItemList = styled(Grid)`
+  p {
     width: 100%;
     text-align: ${({ square }) => (square ? "center" : "left")};
     justify-content: ${({ square }) => (square ? "center" : "left")};
