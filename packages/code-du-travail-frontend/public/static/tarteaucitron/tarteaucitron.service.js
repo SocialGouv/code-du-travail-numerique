3e3e6ccb9a3f3485d57f7ff9f961f289deb4266d
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-10-26 14:37:03 +0200

    fix: last thing

--- packages/code-du-travail-frontend/public/static/tarteaucitron/tarteaucitron.service.js
+++ packages/code-du-travail-frontend/public/static/tarteaucitron/tarteaucitron.service.js
@@ -1,13 +1,13 @@
 /*global tarteaucitron, ga, Shareaholic, stLight, clicky, top, google, Typekit, FB, ferankReady, IN, stButtons, twttr, PCWidget*/
 /*jslint regexp: true, nomen: true*/
 // google analytics
-tarteaucitron.services._adftrack = {
+tarteaucitron.services.adform = {
   key: "_adftrack",
   type: "analytic",
   name: "Google Analytics",
   uri: "https://support.google.com/analytics/answer/6004245",
   needConsent: true,
-  cookies: ["_adftrack"],
+  cookies: [],
   js: function () {
     "use strict";
     window.dataLayer = window.dataLayer || [];
@@ -15,10 +15,7 @@ tarteaucitron.services._adftrack = {
       "https://s2.adform.net/banners/scripts/st/trackpoint-async.js",
       "",
       function () {
-        window._adftrack = Array.isArray(window._adftrack) ? window._adftrack : (window._adftrack ? [window._adftrack] : []);
-        if (typeof tarteaucitron.user.adftrackMore === "function") {
-          tarteaucitron.user.adftrackMore();
-        }
+        tarteaucitron.user.adftrackMore();
       }
     );
   },
