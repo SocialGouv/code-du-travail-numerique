6670df4392db417186fbd6eb766757925f0a362f
Martial Maillot <martial.maillot@gmail.com>
2023-12-20 17:45:58 +0100

    fix: support du rowspan dans le thead des tableaux

--- packages/code-du-travail-frontend/src/contributions/__tests__/DisplayContentContribution.test.tsx
+++ packages/code-du-travail-frontend/src/contributions/__tests__/DisplayContentContribution.test.tsx
@@ -360,4 +360,54 @@ describe("DisplayContentContribution", () => {
       </p>
     `);
   });
+
+  it(`should render correctly a table with multiple head lines`, () => {
+    const { baseElement } = render(
+      <DisplayContentContribution
+        content={`
+        <table>
+  <tbody>
+    <tr>
+      <td colspan="1" rowspan="2" colwidth="444">
+        <p>
+          <strong>Nature du contrat de mission</strong>
+        </p>
+      </td>
+      <td colspan="2" rowspan="1">
+        <p>
+          <strong>Durée maximale</strong>
+        </p>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="1" rowspan="1">
+        <p>
+          <strong>Contrat de date à date</strong>
+        </p>
+      </td>
+      <td colspan="1" rowspan="1">
+        <p>
+          <strong>Contrat sans terme certain</strong>
+        </p>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="1" rowspan="1" colwidth="444">
+        <p>Remplacement d’un salarié absent ou dont le contrat de travail est suspendu</p>
+      </td>
+      <td colspan="1" rowspan="1">
+        <p>18 mois</p>
+      </td>
+      <td colspan="1" rowspan="1">
+        <p>Fin de l’absence</p>
+      </td>
+    </tr>
+  </tbody>
+</table>
+`}
+      ></DisplayContentContribution>
+    );
+
+    expect(baseElement.firstChild).toMatchSnapshot();
+  });
 });
