fc2385d8010d3ffa24f617a7fd39f4f223ff91f8
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2023-12-26 22:03:25 +0100

    feat(next): replacing call of the API in `getStaticProps` by direct call during build time (#5514)
    * fix: add build time
    
    * fix: replace
    
    * fix: replace
    
    * fix: replace
    
    * fix: replace
    
    * fix: replace
    
    * fix: replace
    
    * fix: replace
    
    * fix: replace
    
    * fix: replace
    
    * fix: replace
    
    * fix: replace
    
    * fix: replace

---
+++ packages/code-du-travail-frontend/src/api/modules/sitemap/controller/get.ts
@@ -0,0 +1,31 @@
+import { Agreement, ElasticSearchItem, Tool } from "@socialgouv/cdtn-utils";
+import { getAllAgreements } from "../../agreements";
+import { getGenericsContributions } from "../../contributions";
+import { getAllModeles } from "../../modeles";
+import { getAllThemesAndSubThemes } from "../../themes";
+import { getAllTools } from "../../tools";
+
+export type GetSitemapPage = {
+  themes: any;
+  tools: Tool[];
+  modeles: ElasticSearchItem[];
+  contributions: ElasticSearchItem[];
+  agreements: Agreement[];
+};
+
+export const getSitemapData = async () => {
+  const themes = await getAllThemesAndSubThemes();
+  const tools = await getAllTools();
+  const modeles = await getAllModeles();
+  const contributions = await getGenericsContributions();
+  const agreements = await getAllAgreements();
+  const response: GetSitemapPage = {
+    themes,
+    tools,
+    modeles,
+    contributions,
+    agreements,
+  };
+
+  return response;
+};
