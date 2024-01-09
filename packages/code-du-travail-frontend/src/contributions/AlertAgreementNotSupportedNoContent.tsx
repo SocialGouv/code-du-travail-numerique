c0ce8fc781d3f43d92bcab962a2ae4f488b52e62
carolineBda <caroline.bourdeu@gmail.com>
2024-01-09 10:46:42 +0100

    fix(alerts): sur les contribs dont la générique ne prévoit rien

--- packages/code-du-travail-frontend/src/contributions/AlertAgreementNotSupportedNoContent.tsx
+++ packages/code-du-travail-frontend/src/contributions/AlertAgreementNotSupportedNoContent.tsx
@@ -27,14 +27,35 @@ export const AlertAgreementNotSupportedNoContent = ({
   );
 };
 
-export const AlertAgreementSupportedNoContent = ({ message }: Props) => {
-  return (
-    <>
-      <Paragraph variant="primary" fontWeight="700" noMargin>
-        Cette convention collective ne prévoit rien
-      </Paragraph>
-      <p>La convention collective sélectionnée ne prévoit rien sur ce sujet.</p>
-      <p>{message}</p>
-    </>
-  );
-};
+export const AlertAgreementSupportedNoContent = () => (
+  <>
+    <Paragraph variant="primary" fontWeight="700" noMargin>
+      Cette convention collective ne prévoit rien
+    </Paragraph>
+    <p>La convention collective sélectionnée ne prévoit rien sur ce sujet.</p>
+    <p>
+      Un accord d’entreprise ou le contrat de travail peut cependant prévoir des
+      règles spécifiques sur ce sujet.
+    </p>
+  </>
+);
+
+export const AlertAgreementSupported = ({
+  showAnswer,
+}: {
+  showAnswer: boolean;
+}) => (
+  <>
+    <Paragraph variant="primary" fontSize="default" fontWeight="700" noMargin>
+      Nous n’avons pas de réponse pour cette convention collective
+    </Paragraph>
+    {showAnswer ? (
+      <p>Vous pouvez consulter les informations générales ci-dessous.</p>
+    ) : (
+      <p>
+        Vous pouvez tout de même poursuivre pour obtenir les informations
+        générales.
+      </p>
+    )}
+  </>
+);
