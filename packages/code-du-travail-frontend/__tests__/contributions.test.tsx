c7ccba19050057ce9d794f7e1c9a586a98852c44
carolineBda <caroline.bourdeu@gmail.com>
2024-02-06 14:46:12 +0100

    add test

---
+++ packages/code-du-travail-frontend/__tests__/contributions.test.tsx
@@ -0,0 +1,34 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+import PageContribution from "../pages/contribution/[slug]";
+
+const contribution = {
+  source: "contributions",
+  linkedContent: [],
+  references: [],
+  idcc: "",
+  ccnShortTitle: "Métallurgie",
+  title: "La période d’essai peut-elle être renouvelée ?",
+};
+describe("<PageContribution />", () => {
+  it("should render title with only question", () => {
+    const { getByRole } = render(
+      <PageContribution isNewContribution={true} contribution={contribution} />
+    );
+    const titreH1 = getByRole("heading", { level: 1 });
+    expect(titreH1.textContent).toBe(
+      "La période d’essai peut-elle être renouvelée ?"
+    );
+  });
+  it("should render title with cc name in it", () => {
+    contribution.idcc = "3248";
+    const { getByRole } = render(
+      <PageContribution isNewContribution={true} contribution={contribution} />
+    );
+    const titreH1 = getByRole("heading", { level: 1 });
+    expect(titreH1.textContent).toBe(
+      "Métallurgie: La période d’essai peut-elle être renouvelée ?"
+    );
+  });
+});
