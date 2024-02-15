1a58ae05c1fd825f4e42685f93fcc76d99057ed6
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-02-15 16:54:26 +0100

    fix: heavy

--- packages/code-du-travail-frontend/cypress/integration/statistiques.spec.ts
+++
@@ -1,9 +0,0 @@
-describe("Statistique", () => {
-  it("should display the page correctly", () => {
-    cy.visit("/stats");
-
-    cy.contains("Statistiques d’utilisation").click();
-
-    cy.contains("Statistiques d’utilisation depuis le");
-  });
-});
