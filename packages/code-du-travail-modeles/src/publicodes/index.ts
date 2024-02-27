9b25ec22295e9fe984f0885b5efc7678a259ae0c
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-02-27 19:28:32 +0100

    fix: done

--- packages/code-du-travail-modeles/src/publicodes/index.ts
+++ packages/code-du-travail-modeles/src/publicodes/index.ts
@@ -2,4 +2,5 @@ export { default as IndemniteLicenciementPublicodes } from "./IndemniteLicenciem
 export { default as PreavisRetraitePublicodes } from "./PreavisRetraitePublicodes";
 export * from "./Publicodes";
 export { default as RuptureConventionnellePublicodes } from "./RuptureConventionnellePublicodes";
+export { default as SingletonPublicodesHelper } from "./SingletonPublicodesHelper";
 export * from "./types";
