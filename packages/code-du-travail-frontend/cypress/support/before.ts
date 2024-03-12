791e952070e33a741d81a1120f3fe1cc0b4c3416
carolineBda <caroline.bourdeu@gmail.com>
2024-03-12 11:24:56 +0100

    generate file from own deploy

--- packages/code-du-travail-frontend/cypress/support/before.ts
+++ packages/code-du-travail-frontend/cypress/support/before.ts
@@ -1,29 +1,25 @@
+// @ts-ignore
 import fs from "fs";
 
 export const downloadAllUrlsToValidate = async () => {
   const urls: string[] = ["/convention-collective", "/contribution"];
 
-  const response = await fetch(
-    "https://code-du-travail-numerique-preprod.dev.fabrique.social.gouv.fr/api/plan-du-site"
-  );
+  const response = await fetch("/api/plan-du-site");
   const data = await response.json();
-  const agreements = data.agreements;
-  const contributions = data.contributions;
-  contributions.forEach((contrib) => {
+
+  data.agreements.forEach((doc) => {
+    urls.push("/convention-collective/" + doc.slug);
+  });
+
+  data.contributions.forEach((contrib) => {
     urls.push("/contribution/" + contrib.generic.slug);
     contrib.agreements.forEach((doc) => {
       urls.push("/contribution/" + doc.slug);
     });
   });
-  agreements.forEach((doc) => {
-    urls.push("/convention-collective/" + doc.slug);
-  });
 
-  fs.writeFile(
-    "./cypress/support/urls-to-validate.json",
-    JSON.stringify(urls)
-  );
+  fs.writeFile("./cypress/support/urls-to-validate.json", JSON.stringify(urls));
   console.log(
-    "Urls to validate saved to file ./cypress/integration/light/urls-to-validate.json"
+    "Urls to validate saved to file ./cypress/support/urls-to-validate.json"
   );
 };
