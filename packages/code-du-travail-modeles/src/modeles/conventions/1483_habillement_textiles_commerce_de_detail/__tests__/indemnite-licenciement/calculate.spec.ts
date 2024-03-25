a8fced14104b655df9a1b881c08a25e77166c386
Victor <victor.zeinstra@gmail.com>
2024-03-25 14:15:00 +0100

    chore: add test on salaryPeriods undefined

--- packages/code-du-travail-modeles/src/modeles/conventions/1483_habillement_textiles_commerce_de_detail/__tests__/indemnite-licenciement/calculate.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/1483_habillement_textiles_commerce_de_detail/__tests__/indemnite-licenciement/calculate.spec.ts
@@ -6,7 +6,7 @@ const engine = new IndemniteLicenciementPublicodes(
 );
 
 describe("Test de la fonctionnalité 'calculate'", () => {
-  test("Vérifier que le salaires est bien pris en compte", () => {
+  test("Vérifier que le fonctionne sans noticeSalaryPeriods", () => {
     const { result, missingArgs, ineligibility } = engine.calculate({
       "contrat salarié . convention collective": "'IDCC1483'",
       "contrat salarié . convention collective . habillement textiles commerce de detail . age":
@@ -31,5 +31,37 @@ describe("Test de la fonctionnalité 'calculate'", () => {
     expect(missingArgs).toEqual([]);
     expect(ineligibility).toBeUndefined();
     expect(result.value).toEqual(1500);
+  });
+  test("Vérifier que le fonctionne sans salaryPeriods", () => {
+    const { missingArgs } = engine.calculate({
+      "contrat salarié . convention collective": "'IDCC1483'",
+      "contrat salarié . convention collective . habillement textiles commerce de detail . age":
+        "50",
+      "contrat salarié . convention collective . habillement textiles commerce de detail . catégorie professionnelle":
+        "'Cadres'",
+      "contrat salarié . indemnité de licenciement . arrêt de travail": "non",
+      "contrat salarié . indemnité de licenciement . date d'entrée":
+        "01/01/2022",
+      "contrat salarié . indemnité de licenciement . date de notification":
+        "01/01/2024",
+      "contrat salarié . indemnité de licenciement . date de sortie":
+        "01/01/2024",
+      "contrat salarié . indemnité de licenciement . inaptitude suite à un accident ou maladie professionnelle":
+        "non",
+      licenciementFauteGrave: "non",
+      noticeSalaryPeriods: undefined,
+      salaryPeriods: undefined,
+      typeContratTravail: "cdi",
+    });
+    expect(missingArgs).toEqual([
+      {
+        indice: 1,
+        name: "contrat salarié - indemnité de licenciement - salaire de référence",
+        rawNode: {
+          nom: "contrat salarié . indemnité de licenciement . salaire de référence",
+          unité: "€",
+        },
+      },
+    ]);
   });
 });
