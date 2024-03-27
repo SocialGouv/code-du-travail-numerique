2e70765dcc1633230021858f51787e20e089587c
Victor Zeinstra <zeinstra.victor@gmail.com>
2024-03-27 10:49:57 +0100

    fix: bug lorsque l'ancienneté arrive à 0 (#5714)
    * fix: bug lorsque l'ancienneté arrive à 0
    
    * chore: clean
    
    * chore: update test
    
    ---------
    
    Co-authored-by: Victor <victor.zeinstra@gmail.com>

--- packages/code-du-travail-modeles/src/modeles/base/__tests__/indemnite-licenciement/calculate.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/base/__tests__/indemnite-licenciement/calculate.spec.ts
@@ -136,5 +136,28 @@ describe("Test de la fonctionnalité 'calculate'", () => {
         "L’indemnité de licenciement n’est pas due lorsque l’ancienneté dans l’entreprise est inférieure à 8 mois."
       );
     });
+
+    test("Vérifier l'inéligibilité sur le bug des 2 mois", () => {
+      const { result, missingArgs, ineligibility } = engine.calculate({
+        absencePeriods:
+          '[{"motif":{"key":"absenceMaladieNonPro","label":"Absence pour maladie non professionnelle","value":1},"durationInMonth":2}]',
+        "contrat salarié . indemnité de licenciement . arrêt de travail": "non",
+        "contrat salarié . indemnité de licenciement . date d'entrée":
+          "01/01/2024",
+        "contrat salarié . indemnité de licenciement . date de notification":
+          "04/03/2024",
+        "contrat salarié . indemnité de licenciement . date de sortie":
+          "26/03/2024",
+        "contrat salarié . indemnité de licenciement . inaptitude suite à un accident ou maladie professionnelle":
+          "non",
+        licenciementFauteGrave: "non",
+        typeContratTravail: "cdi",
+      });
+      expect(missingArgs).toEqual([]);
+      expect(result.value).toEqual(0);
+      expect(ineligibility).toEqual(
+        "L’indemnité de licenciement n’est pas due lorsque l’ancienneté dans l’entreprise est inférieure à 8 mois."
+      );
+    });
   });
 });
