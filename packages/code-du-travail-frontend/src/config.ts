e64ffa8fa3378b8b0fe85d47d96e7189fea47c23
Martial Maillot <martial.maillot@gmail.com>
2024-03-27 17:35:39 +0100

    feat: utilisation de l'url local de l'API pour le ssr

--- packages/code-du-travail-frontend/src/config.ts
+++ packages/code-du-travail-frontend/src/config.ts
@@ -8,8 +8,10 @@ export const BUCKET_DEFAULT_FOLDER =
 export const BUCKET_SITEMAP_FOLDER =
   process.env.NEXT_PUBLIC_BUCKET_SITEMAP_FOLDER ?? "sitemap";
 export const COMMIT = process.env.NEXT_PUBLIC_COMMIT ?? "";
+console.log(`Env SITE_URL : ${process.env.SITE_URL}`)
+console.log(`Env NEXT_PUBLIC_SITE_URL : ${process.env.NEXT_PUBLIC_SITE_URL}`)
 export const SITE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL ?? `http://localhost:3000`;
+  process.env.SITE_URL ?? process.env.NEXT_PUBLIC_SITE_URL ?? `http://localhost:3000`;
 export const PACKAGE_VERSION = version;
 export const PIWIK_SITE_ID = process.env.NEXT_PUBLIC_PIWIK_SITE_ID ?? "";
 export const PIWIK_URL = process.env.NEXT_PUBLIC_PIWIK_URL ?? "";
