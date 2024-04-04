4ef554de1bea31fd8d978a784287a3ee00a80754
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-04-04 19:36:37 +0200

    fix: done

---
+++ packages/code-du-travail-modeles/src/modeles/conventions/2511_sport/__tests__/rupture-conventionnelle/calculate.spec.ts
@@ -0,0 +1,50 @@
+import { RuptureConventionnellePublicodes } from "../../../../../publicodes";
+
+const engine = new RuptureConventionnellePublicodes(
+  modelsRuptureConventionnel,
+  "2511"
+);
+
+describe("Calcul de l'indemnité de rupture conventionnelle pour la CC 2511", () => {
+  test.each`
+    startDate       | endDate         | referenceSalary | referenceAgreementSalary | expectedCompensation
+    ${"01/01/2000"} | ${"01/01/2025"} | ${2500}         | ${2500}                  | ${0}
+    ${"01/06/2024"} | ${"01/01/2025"} | ${2500}         | ${2500}                  | ${0}
+    ${"01/01/2024"} | ${"01/01/2025"} | ${2500}         | ${2500}                  | ${0}
+  `(
+    "avec une date de début $startDate et de fin $endDate",
+    ({
+      endDate,
+      startDate,
+      referenceSalary,
+      referenceAgreementSalary,
+      expectedCompensation,
+    }) => {
+      const { result, missingArgs } = engine.calculate(
+        {
+          "contrat salarié . convention collective": "'IDCC2511'",
+          "contrat salarié . indemnité de licenciement . arrêt de travail":
+            "non",
+          "contrat salarié . indemnité de licenciement . date d'entrée":
+            startDate,
+          "contrat salarié . indemnité de licenciement . date de notification":
+            endDate,
+          "contrat salarié . indemnité de licenciement . date de sortie":
+            endDate,
+          "contrat salarié . indemnité de licenciement . inaptitude suite à un accident ou maladie professionnelle":
+            "non",
+          "contrat salarié . indemnité de licenciement . salaire de référence":
+            referenceSalary,
+          "contrat salarié . indemnité de licenciement . salaire de référence conventionnel":
+            referenceAgreementSalary,
+          licenciementFauteGrave: "non",
+          typeContratTravail: "cdi",
+        },
+        "contrat salarié . indemnité de licenciement . résultat conventionnel"
+      );
+      expect(missingArgs).toEqual([]);
+      expect(result.value).toEqual(expectedCompensation);
+      expect(result.unit?.numerators).toEqual(["€"]);
+    }
+  );
+});
