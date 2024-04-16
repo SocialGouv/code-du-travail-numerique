05668a763f963ebf7d67e66d114171beaf30148d
carolineBda <caroline.bourdeu@gmail.com>
2024-04-16 09:13:36 +0200

    put describe on top of the file

--- packages/code-du-travail-frontend/cypress/integration/heavy/validate-html-contributions.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/heavy/validate-html-contributions.spec.ts
@@ -10,31 +10,29 @@ import urls7 from "../../support/urls-contributions-to-validate-7.json";
 
 import { ConfigData } from "cypress-html-validate";
 
-describe("Validate html for contributions", () => {
-  const localConfig: ConfigData = {
-    rules: {
-      "heading-level": "error",
-      "require-sri": "off",
-      "valid-id": "off",
-      "prefer-native-element": "off",
-      "no-implicit-button-type": "off",
-      "aria-label-misuse": "off",
-      "long-title": "off",
-      "script-type": "off",
-      "no-dup-id": "off",
-      "wcag/h63": "off",
-      "wcag/h32": "off",
-    },
-  };
-  const files = [urls1, urls2, urls3, urls4, urls5, urls6, urls7];
-  for (let i = 0; i < 7; i++) {
-    describe("Part " + (i + 1), () => {
-      files[i].forEach((url) => {
-        it("page should be valid: " + url, () => {
-          cy.visit(url);
-          cy.htmlvalidate(localConfig);
-        });
+const localConfig: ConfigData = {
+  rules: {
+    "heading-level": "error",
+    "require-sri": "off",
+    "valid-id": "off",
+    "prefer-native-element": "off",
+    "no-implicit-button-type": "off",
+    "aria-label-misuse": "off",
+    "long-title": "off",
+    "script-type": "off",
+    "no-dup-id": "off",
+    "wcag/h63": "off",
+    "wcag/h32": "off",
+  },
+};
+const files = [urls1, urls2, urls3, urls4, urls5, urls6, urls7];
+for (let i = 0; i < 7; i++) {
+  describe("Validate html for contributions - part " + (i + 1), () => {
+    files[i].forEach((url) => {
+      it("page should be valid: " + url, () => {
+        cy.visit(url);
+        cy.htmlvalidate(localConfig);
       });
     });
-  }
-});
+  });
+}
