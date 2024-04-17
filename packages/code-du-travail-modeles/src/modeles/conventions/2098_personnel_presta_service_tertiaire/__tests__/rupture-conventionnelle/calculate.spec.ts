e7671dbe393508be9fa1cd22d096accfd3f31739
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2024-04-17 15:28:00 +0200

    feat(rupture-co): ajout de la CC 2098 ; 1486 (#5782)
    * fix: done
    
    * fix: done
    
    * fix: done
    
    * fix: done
    
    * fix: done
    
    * fix: done
    
    * Update packages/code-du-travail-modeles/src/modeles/conventions/1501_restauration_rapide/__tests__/rupture-conventionnelle/calculate.spec.ts
    
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>
    
    * Update packages/code-du-travail-modeles/src/modeles/conventions/1501_restauration_rapide/__tests__/rupture-conventionnelle/calculate.spec.ts
    
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>
    
    * Update packages/code-du-travail-modeles/src/modeles/conventions/1996_pharmacie/__tests__/rupture-conventionnelle/calculate.spec.ts
    
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>
    
    * Update packages/code-du-travail-modeles/src/modeles/conventions/44_industries_chimiques/__tests__/rupture-conventionnelle/calculate.spec.ts
    
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>
    
    * Update packages/code-du-travail-modeles/src/modeles/conventions/1996_pharmacie/__tests__/rupture-conventionnelle/calculate.spec.ts
    
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>
    
    * Update packages/code-du-travail-modeles/src/modeles/conventions/44_industries_chimiques/__tests__/rupture-conventionnelle/calculate.spec.ts
    
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>
    
    * fix: build encore
    
    * fix(ani): done
    
    * fix(ani): done
    
    * fix(ani): done
    
    * fix(ani): done
    
    * fix(ani): done
    
    * fix(e2e): set number of idcc for h2 in contributions
    
    ---------
    
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>

---
+++ packages/code-du-travail-modeles/src/modeles/conventions/2098_personnel_presta_service_tertiaire/__tests__/rupture-conventionnelle/calculate.spec.ts
@@ -0,0 +1,178 @@
+import { RuptureConventionnellePublicodes } from "../../../../../publicodes";
+
+const engine = new RuptureConventionnellePublicodes(
+  modelsRuptureConventionnel,
+  "2098"
+);
+
+describe("Gestion des licenciements pour la CC 2098", () => {
+  describe("Missing variables", () => {
+    test("Sans arguments", () => {
+      const input = {
+        "contrat salarié . convention collective": "'IDCC2098'",
+      };
+
+      const { missingArgs } = engine.calculate(
+        input,
+        "contrat salarié . indemnité de licenciement . résultat conventionnel"
+      );
+      expect(missingArgs).toHaveNextMissingRule(
+        "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . catégorie professionnelle"
+      );
+    });
+
+    test("En étant Non-cadres", () => {
+      const input = {
+        "contrat salarié . convention collective": "'IDCC2098'",
+        "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . catégorie professionnelle": `'Non-cadres'`,
+      };
+
+      const { missingArgs } = engine.calculate(
+        input,
+        "contrat salarié . indemnité de licenciement . résultat conventionnel"
+      );
+      expect(missingArgs).toHaveNextMissingRule(null);
+    });
+
+    test("En étant Cadres", () => {
+      const input = {
+        "contrat salarié . convention collective": "'IDCC2098'",
+        "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . catégorie professionnelle": `'Cadres'`,
+      };
+
+      const { missingArgs } = engine.calculate(
+        input,
+        "contrat salarié . indemnité de licenciement . résultat conventionnel"
+      );
+      expect(missingArgs).toHaveNextMissingRule(
+        "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . cadres . age"
+      );
+    });
+
+    test("En ayant tout complété en étant Cadres", () => {
+      const input = {
+        "contrat salarié . convention collective": "'IDCC2098'",
+        "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . cadres . age":
+          "60",
+        "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . catégorie professionnelle": `'Cadres'`,
+      };
+
+      const { missingArgs } = engine.calculate(
+        input,
+        "contrat salarié . indemnité de licenciement . résultat conventionnel"
+      );
+      expect(missingArgs).toHaveNextMissingRule(null);
+    });
+  });
+
+  describe("Calcul de l'indemnité de licenciement", () => {
+    describe("Cadres", () => {
+      test.each([
+        {
+          age: 45,
+          catPro: "Cadres",
+          dateEntree: "01/01/2000",
+          dateSortie: "01/01/2001",
+          result: 675,
+        },
+        {
+          age: 45,
+          catPro: "Cadres",
+          dateEntree: "01/01/2000",
+          dateSortie: "01/01/2023",
+          result: 18450,
+        },
+        {
+          age: 51,
+          catPro: "Cadres",
+          dateEntree: "01/01/2000",
+          dateSortie: "01/01/2023",
+          result: 18450,
+        },
+        {
+          age: 56,
+          catPro: "Cadres",
+          dateEntree: "01/01/2000",
+          dateSortie: "01/01/2023",
+          result: 18450,
+        },
+      ])(
+        "Licenciement pour inaptitude totale et définitive non consécutive à un accident du travail",
+        (value) => {
+          const { missingArgs, detail } = engine.calculateResult({
+            "contrat salarié . convention collective": "'IDCC2098'",
+            "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . cadres . age": `${value.age}`,
+            "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . catégorie professionnelle": `'${value.catPro}'`,
+            "contrat salarié . indemnité de licenciement . date d'entrée":
+              value.dateEntree,
+            "contrat salarié . indemnité de licenciement . date de notification":
+              value.dateSortie,
+            "contrat salarié . indemnité de licenciement . date de sortie":
+              value.dateSortie,
+            "contrat salarié . indemnité de licenciement . inaptitude suite à un accident ou maladie professionnelle":
+              "non",
+            licenciementFauteGrave: "non",
+            salaryPeriods:
+              '[{"month":"décembre 2024","value":2700},{"month":"novembre 2024","value":2700},{"month":"octobre 2024","value":2700},{"month":"septembre 2024","value":2700},{"month":"août 2024","value":2700},{"month":"juillet 2024","value":2700},{"month":"juin 2024","value":2700},{"month":"mai 2024","value":2700},{"month":"avril 2024","value":2700},{"month":"mars 2024","value":2700},{"month":"février 2024","value":2700},{"month":"janvier 2024","value":2700}]',
+            typeContratTravail: "cdi",
+          });
+          expect(missingArgs).toEqual([]);
+          expect(detail.agreementResult?.value).toEqual(value.result);
+          expect(detail.agreementResult?.unit?.numerators).toEqual(["€"]);
+        }
+      );
+    });
+
+    describe("Non-cadres", () => {
+      test.each([
+        {
+          catPro: "Non-cadres",
+          dateEntree: "01/01/2000",
+          dateSortie: "01/07/2000",
+          result: 0,
+        },
+        {
+          catPro: "Non-cadres",
+          dateEntree: "01/01/2000",
+          dateSortie: "01/10/2000",
+          result: 506.25,
+        },
+        {
+          catPro: "Non-cadres",
+          dateEntree: "01/01/2000",
+          dateSortie: "01/10/2001",
+          result: 1181.25,
+        },
+        {
+          catPro: "Non-cadres",
+          dateEntree: "01/01/2000",
+          dateSortie: "01/10/2005",
+          result: 1639.29,
+        },
+      ])(
+        "Licenciement pour inaptitude totale et définitive non consécutive à un accident du travail",
+        (value) => {
+          const { missingArgs, detail } = engine.calculateResult({
+            "contrat salarié . convention collective": "'IDCC2098'",
+            "contrat salarié . convention collective . personnel presta service tertiaire . autre licenciement . catégorie professionnelle": `'${value.catPro}'`,
+            "contrat salarié . indemnité de licenciement . date d'entrée":
+              value.dateEntree,
+            "contrat salarié . indemnité de licenciement . date de notification":
+              value.dateSortie,
+            "contrat salarié . indemnité de licenciement . date de sortie":
+              value.dateSortie,
+            "contrat salarié . indemnité de licenciement . inaptitude suite à un accident ou maladie professionnelle":
+              "non",
+            licenciementFauteGrave: "non",
+            salaryPeriods:
+              '[{"month":"décembre 2024","value":2700},{"month":"novembre 2024","value":2700},{"month":"octobre 2024","value":2700},{"month":"septembre 2024","value":2700},{"month":"août 2024","value":2700},{"month":"juillet 2024","value":2700},{"month":"juin 2024","value":2700},{"month":"mai 2024","value":2700},{"month":"avril 2024","value":2700},{"month":"mars 2024","value":2700},{"month":"février 2024","value":2700},{"month":"janvier 2024","value":2700}]',
+            typeContratTravail: "cdi",
+          });
+          expect(missingArgs).toEqual([]);
+          expect(detail.agreementResult?.value).toEqual(value.result);
+          expect(detail.agreementResult?.unit?.numerators).toEqual(["€"]);
+        }
+      );
+    });
+  });
+});
