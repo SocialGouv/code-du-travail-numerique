d3953b60acef21bc3ff94253ebc541d4d12db803
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-05-20 13:59:55 +0100

    fix: test

---
+++ packages/code-du-travail-frontend/cypress.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+import htmlvalidate from "cypress-html-validate/plugin";
+
+dotenv.config();
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 20000, // 20s
+  e2e: {
+    baseUrl: process.env.TEST_BASEURL ?? "http://localhost:3000",
+    specPattern: process.env.ALL_TEST
+      ? "cypress/integration/**/*.spec.{js,jsx,ts,tsx}"
+      : "cypress/integration/light/**/*.spec.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/index.ts",
+    viewportHeight: 1000,
+    viewportWidth: 1280,
+    chromeWebSecurity: false,
+    setupNodeEvents(on) {
+      htmlvalidate.install(on);
+    },
+  },
+  video: false,
+});
