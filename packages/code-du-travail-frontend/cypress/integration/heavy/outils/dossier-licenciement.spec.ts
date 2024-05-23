b25ec23b9bba11e3bd61ab1eb55e487144300408
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-05-24 01:07:52 +0200

    fix: zero

--- packages/code-du-travail-frontend/cypress/integration/heavy/outils/dossier-licenciement.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/heavy/outils/dossier-licenciement.spec.ts
@@ -1,10 +1,6 @@
-import "cypress-iframe";
-
 describe("Outil - Dossier Licenciement", () => {
   it("should display the widget", () => {
     cy.visit("https://socialgouv.github.io/cdtn-admin/procedure-licenciement");
-    cy.iframe("#simulateurEmbauche")
-      .contains("Coût total employeur")
-      .should("be.visible");
+    cy.contains("Comprendre sa procédure de licenciement");
   });
 });
