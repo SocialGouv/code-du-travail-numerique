d84819ecdf55fd1b95e423ddf5249b0de10f0a40
Victor <victor.zeinstra@gmail.com>
2024-05-24 09:55:12 +0200

    fix: tu

--- packages/react-ui/src/field/__tests__/field.test.js
+++ packages/react-ui/src/field/__tests__/field.test.js
@@ -1,7 +1,6 @@
 import "regenerator-runtime/runtime";
 
 import { fireEvent, render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import React from "react";
 
 import { InputCheckbox } from "../Checkbox";
