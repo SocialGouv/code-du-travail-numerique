3a732fa913be51e671a0dd7f97f50da37f7ab527
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-05-27 15:15:25 +0200

    fix(docs): ajout de la page sans redirection

--- packages/code-du-travail-frontend/cypress/integration/light/widgets/trouver-sa-cc.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/light/widgets/trouver-sa-cc.spec.ts
@@ -10,4 +10,14 @@ describe("Widget - Trouver sa convention collective", () => {
       .contains("Trouver sa convention collective")
       .should("be.visible");
   });
+
+  it("should display the widget", () => {
+    cy.visit(
+      "https://socialgouv.github.io/cdtn-admin/trouver-sa-convention-collective-sans-redirection"
+    );
+
+    cy.iframe()
+      .contains("Trouver sa convention collective")
+      .should("be.visible");
+  });
 });
