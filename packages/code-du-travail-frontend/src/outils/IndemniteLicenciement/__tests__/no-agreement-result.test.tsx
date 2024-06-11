ff11e336e77c5509e5b521c9d1a00c8fff09a4b3
victor <victor.zeinstra@gmail.com>
2024-06-11 14:39:15 +0200

    fix: bug sur la selection CC qui maj pas moteur publicaode

--- packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/__tests__/no-agreement-result.test.tsx
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/__tests__/no-agreement-result.test.tsx
@@ -1,8 +1,15 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  RenderResult,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import React from "react";
 import { CalculateurIndemniteLicenciement } from "../../../../src/outils";
 import { ui } from "../../CommonIndemniteDepart/__tests__/ui";
 import userEvent from "@testing-library/user-event";
+import { UserAction } from "../../../common";
 
 jest.spyOn(Storage.prototype, "setItem");
 Storage.prototype.getItem = jest.fn(
@@ -17,11 +24,20 @@ Storage.prototype.getItem = jest.fn(
 }
 `
 );
+jest.mock("../../../conventions/Search/api/enterprises.service");
 
 describe("Indemnité licenciement", () => {
+  let rendering: RenderResult;
+  let userAction: UserAction;
   describe("parcours avec la convention collective 413 pour tester le cas où il n'y a pas d'indemnité conventionnel", () => {
     beforeEach(() => {
-      render(<CalculateurIndemniteLicenciement icon={""} title={""} displayTitle={""} />);
+      render(
+        <CalculateurIndemniteLicenciement
+          icon={""}
+          title={""}
+          displayTitle={""}
+        />
+      );
       userEvent.click(ui.introduction.startButton.get());
       userEvent.click(ui.contract.type.cdi.get());
       userEvent.click(ui.contract.fauteGrave.non.get());
@@ -81,5 +97,41 @@ describe("Indemnité licenciement", () => {
         )
       ).not.toBeInTheDocument();
     });
+  });
+  test(`Le résultat doit être légal si la convention collective a été retirée de la sélection`, async () => {
+    rendering = render(
+      <CalculateurIndemniteLicenciement
+        icon={""}
+        title={""}
+        displayTitle={""}
+      />
+    );
+    userAction = new UserAction();
+    userAction
+      .click(ui.introduction.startButton.get())
+      .click(ui.contract.type.cdi.get())
+      .click(ui.contract.fauteGrave.non.get())
+      .click(ui.contract.inaptitude.non.get())
+      .click(ui.contract.arretTravail.non.get())
+      .click(ui.next.get())
+      .click(ui.agreement.unknownAgreement.get())
+      .setInput(ui.agreement.agreementCompanyInput.get(), "bricoman")
+      .click(ui.agreement.agreementCompanySearchButton.get())
+      .click(ui.agreement.agreementCompanySearchButton.get());
+    await waitFor(() => {
+      userAction.click(ui.agreement.searchItem.bricomanie.get());
+    });
+    userAction
+      .click(ui.next.get())
+      .setInput(ui.seniority.startDate.get(), "01/01/2018")
+      .setInput(ui.seniority.notificationDate.get(), "01/01/2024")
+      .setInput(ui.seniority.endDate.get(), "01/01/2024")
+      .click(ui.seniority.hasAbsence.non.get())
+      .click(ui.next.get())
+      .click(ui.salary.hasPartialTime.non.get())
+      .click(ui.salary.hasSameSalary.oui.get())
+      .setInput(ui.salary.sameSalaryValue.get(), "2500")
+      .click(ui.next.get());
+    expect(ui.result.resultatLegal.get()).toHaveTextContent("3 750,00");
   });
 });
