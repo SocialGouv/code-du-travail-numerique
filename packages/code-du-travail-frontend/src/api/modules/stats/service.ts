0006c3e9da0ec1a9233d6bf42671094799c07e53
victor <victor.zeinstra@gmail.com>
2024-06-13 15:21:44 +0200

    feat: divide into batch

--- packages/code-du-travail-frontend/src/api/modules/stats/service.ts
+++ packages/code-du-travail-frontend/src/api/modules/stats/service.ts
@@ -1,4 +1,4 @@
-import { PIWIK_SITE_ID, PIWIK_URL } from "../../../config";
+// import { PIWIK_SITE_ID, PIWIK_URL } from "../../../config";
 import {
   elasticsearchClient,
   elasticDocumentsIndex,
@@ -8,6 +8,12 @@ import { getDocsCountQuery } from "./queries";
 
 export const getStatsService = async () => {
   const body: any = getDocsCountQuery();
+  const refYear = 2020;
+  const now = new Date();
+  const currentYear = now.getFullYear();
+  const numberLoop = currentYear - refYear + 1;
+  const PIWIK_URL = "https://matomo.fabrique.social.gouv.fr";
+  const PIWIK_SITE_ID = "4";
 
   const response = await elasticsearchClient.search<any>({
     body,
@@ -24,20 +30,58 @@ export const getStatsService = async () => {
     nbDocuments += doc_count;
   }
 
-  const URLS = [
-    `${PIWIK_URL}/?module=API&method=VisitsSummary.getVisits&idSite=${PIWIK_SITE_ID}&format=JSON&period=range&date=2020-01-01,today`,
-    `${PIWIK_URL}/?module=API&method=Actions.get&idSite=${PIWIK_SITE_ID}&format=JSON&period=range&date=2020-01-01,today`,
-  ];
+  const generateUrlVisit = (index: number) => {
+    return `${PIWIK_URL}/?module=API&method=VisitsSummary.getVisits&idSite=${PIWIK_SITE_ID}&format=JSON&period=range&date=${
+      refYear + index
+    }-01-01,${refYear + index + 1}-01-01`;
+  };
 
-  const promises = URLS.map((url) =>
-    fetch(url)
+  const generateUrlAction = (index: number) => {
+    return `${PIWIK_URL}/?module=API&method=Actions.get&idSite=${PIWIK_SITE_ID}&format=JSON&period=range&date=${
+      refYear + index
+    }-01-01,${refYear + index + 1}-01-01`;
+  };
+
+  const visitsPromises = Array.from(Array(numberLoop).keys()).map((index) => {
+    const url = generateUrlVisit(index);
+    return fetch(url)
       .then(async (data: Response) => data.json())
       .catch((e: Error) => {
         console.error(e);
         return null;
-      })
+      });
+  });
+
+  const nbVisitDatas = await Promise.all(visitsPromises);
+  const nbVisitData = nbVisitDatas.reduce(
+    (obj, item) => {
+      return {
+        nbVisits: obj.nbVisits + (item.value ?? 0),
+      };
+    },
+    { nbVisits: 0 }
   );
-  const [nbVisitData, infoData] = await Promise.all(promises);
+
+  const actionsPromises = Array.from(Array(numberLoop).keys()).map((index) => {
+    const url = generateUrlAction(index);
+    return fetch(url)
+      .then(async (data: Response) => data.json())
+      .catch((e: Error) => {
+        console.error(e);
+        return null;
+      });
+  });
+
+  const infoDatas = await Promise.all(actionsPromises);
+  const infoData = infoDatas.reduce(
+    (obj, item) => {
+      return {
+        nbPageViews: obj.nbPageViews + (item.nb_pageviews ?? 0),
+        nbSearches: obj.nbSearches + (item.nb_searches ?? 0),
+      };
+    },
+    { nbPageViews: 0, nbSearches: 0 }
+  );
 
   if (!nbVisitData && !infoData) {
     throw new NotFoundError({
@@ -47,10 +91,13 @@ export const getStatsService = async () => {
     });
   }
 
-  return {
+  const result = {
     nbDocuments,
-    nbPageViews: infoData.nb_pageviews,
-    nbSearches: infoData.nb_searches,
-    nbVisits: nbVisitData.value,
+    nbPageViews: infoData.nbPageViews,
+    nbSearches: infoData.nbSearches,
+    nbVisits: nbVisitData.nbVisits,
   };
+
+  console.log(result);
+  return result;
 };
