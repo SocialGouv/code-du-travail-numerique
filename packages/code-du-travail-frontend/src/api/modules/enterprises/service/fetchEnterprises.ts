ef2e074046c56ad00db477a050a95cefdc2e38b8
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-06-14 20:07:35 +0200

    fix: test

--- packages/code-du-travail-frontend/src/api/modules/enterprises/service/fetchEnterprises.ts
+++ packages/code-du-travail-frontend/src/api/modules/enterprises/service/fetchEnterprises.ts
@@ -3,6 +3,7 @@ import { Enterprise } from "../types";
 import { getCodeCommune } from "./fetchCp";
 import { nafMapper } from "./naf";
 import { ApiRechercheEntrepriseResponse } from "./types";
+import { captureException } from "@sentry/nextjs";
 
 export type Convention = {
   idcc: number;
@@ -29,40 +30,54 @@ export const fetchEnterprises = async (
     codeCommune ? `&code_commune=${codeCommune}` : ""
   }`;
 
-  const fetchReq = await fetch(url);
+  try {
+    const fetchReq = await fetch(url);
 
-  const jsonResponse: ApiRechercheEntrepriseResponse = await fetchReq.json();
+    const jsonResponse: ApiRechercheEntrepriseResponse = await fetchReq.json();
 
-  const entreprises = jsonResponse.results.map((result) => {
-    const conventions =
-      result.siege.liste_idcc?.map((idccNumber) => {
-        return {
-          idcc: parseInt(idccNumber, 10),
-          shortTitle: `Convention collective ${idccNumber}`,
-          id: idccNumber,
-          title: `Convention collective ${idccNumber}`,
-        };
-      }) ?? [];
+    const entreprises = jsonResponse.results.map((result) => {
+      const conventions =
+        result.siege.liste_idcc?.map((idccNumber) => {
+          return {
+            idcc: parseInt(idccNumber, 10),
+            shortTitle: `Convention collective ${idccNumber}`,
+            id: idccNumber,
+            title: `Convention collective ${idccNumber}`,
+          };
+        }) ?? [];
+
+      const firstMatchingEtablissement =
+        result.matching_etablissements.length > 0
+          ? {
+              siret: result.matching_etablissements[0].siret,
+              address: result.matching_etablissements[0].adresse,
+            }
+          : { siret: result.siege.siret, address: result.siege.adresse };
+
+      return {
+        activitePrincipale: `${result.activite_principale} - ${
+          nafMapper[result.activite_principale]
+        }`,
+        etablissements: result.nombre_etablissements_ouverts,
+        highlightLabel: result.nom_raison_sociale,
+        label: result.nom_complet,
+        simpleLabel: result.nom_complet,
+        matching: result.nombre_etablissements_ouverts,
+        siren: result.siren,
+        address: result.siege.adresse,
+        firstMatchingEtablissement,
+        conventions,
+      };
+    });
+
     return {
-      activitePrincipale: `${result.activite_principale} - ${
-        nafMapper[result.activite_principale]
-      }`,
-      etablissements: result.nombre_etablissements_ouverts,
-      highlightLabel: result.nom_raison_sociale,
-      label: result.nom_complet,
-      simpleLabel: result.nom_complet,
-      matching: result.nombre_etablissements_ouverts,
-      siren: result.siren,
-      address: result.siege.adresse,
-      firstMatchingEtablissement: {
-        siret: result.matching_etablissements[0].siret,
-        address: result.matching_etablissements[0].adresse,
-      },
-      conventions,
+      entreprises,
     };
-  });
-
-  return {
-    entreprises,
-  };
+  } catch (error) {
+    console.error(error);
+    captureException(error);
+    return {
+      entreprises: [],
+    };
+  }
 };
