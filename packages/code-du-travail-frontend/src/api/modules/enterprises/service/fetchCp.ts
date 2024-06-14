116986c60747f7850611e44b3c1a6303cc2af4ff
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-06-14 16:13:17 +0200

    fix: lessss go

--- packages/code-du-travail-frontend/src/api/modules/enterprises/service/fetchCp.ts
+++ packages/code-du-travail-frontend/src/api/modules/enterprises/service/fetchCp.ts
@@ -2,25 +2,33 @@ import { captureException } from "@sentry/nextjs";
 import { API_GEO_URL } from "../../../../config";
 import { detectIfPostalCode } from "./utils";
 
+type Result = {
+  code: string;
+  nom: string;
+  population: number;
+  _score?: number;
+};
+
 export const getCodeCommune = async (
   postalCodeOrName: string
 ): Promise<string | undefined> => {
   try {
     const isPostalCode = detectIfPostalCode(postalCodeOrName);
     const params = isPostalCode
-      ? `codePostal=${postalCodeOrName}&fields=code`
-      : `nom=${postalCodeOrName}&fields=code`;
+      ? `codePostal=${postalCodeOrName}`
+      : `nom=${postalCodeOrName}`;
     const response = await fetch(`${API_GEO_URL}/communes?${params}`);
     if (!response.ok) {
       throw new Error("Failed to fetch commune code");
     }
-    const result = await response.json();
-    console.log(result);
+    const result: Array<Result> = await response.json();
 
     if (!result || result.length === 0) {
       return undefined;
     }
-    return result[0].code;
+
+    const sortedResult = result.sort((a, b) => b.population - a.population);
+    return sortedResult[0].code;
   } catch (error) {
     captureException(error);
     return undefined;
