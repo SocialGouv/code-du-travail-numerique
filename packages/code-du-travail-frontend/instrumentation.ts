146f8f2ec4f096d27890561e446f552b8144754f
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2024-06-17 16:30:58 +0200

    feat(sentry): upgrade to V8 + use default setup configuration to use `opentelemetry` (#5960)
    * fix: sentry secret
    
    * fix: sentry secret
    
    * fix: sentry secret
    
    * fix: sentry secret
    
    * fix: docker v
    
    * fix: error msg
    
    * fix: instrumentation
    
    * fix: sentry secret
    
    * fix: sentry secret
    
    * fix: credentials
    
    * fix: url

--- packages/code-du-travail-frontend/instrumentation.ts
+++ packages/code-du-travail-frontend/instrumentation.ts
@@ -1,5 +1,5 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./instrumentation.node");
+    await import("./sentry.server.config");
   }
 }
--- packages/code-du-travail-frontend/sentry.client.config.ts
+++ packages/code-du-travail-frontend/sentry.client.config.ts
@@ -9,9 +9,11 @@ Sentry.init({
     process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_PUBLIC_DSN || "",
   environment:
     process.env.NEXT_PUBLIC_SENTRY_ENV || process.env.SENTRY_ENV || "dev",
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 0.05,
   release: process.env.NEXT_PUBLIC_SENTRY_RELEASE || process.env.SENTRY_RELEASE,
-  integrations: [new Sentry.Replay({ maskAllText: true, blockAllMedia: true })],
+  integrations: [
+    Sentry.replayIntegration({ maskAllText: true, blockAllMedia: true }),
+  ],
   replaysSessionSampleRate: 0,
   replaysOnErrorSampleRate: 1.0,
 });
