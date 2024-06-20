2099346e2c46c9145df41913a90b970767f626ed
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-06-20 21:22:36 +0200

    fix: test

--- packages/code-du-travail-frontend/pages/index.tsx
+++ packages/code-du-travail-frontend/pages/index.tsx
@@ -34,20 +34,6 @@ const Home = ({
         description="Posez votre question sur le droit du travail et obtenez une réponse personnalisée à vos questions (contrat de travail, congés payés, formation, démission, indemnités)."
       />
       <SearchHero />
-      <button
-        onClick={() => {
-          throw new Error("test sentry");
-        }}
-      >
-        Throw error
-      </button>
-      <button
-        onClick={() => {
-          throw new Error("bugggg");
-        }}
-      >
-        Throw a second error
-      </button>
       {highlights.length > 0 && (
         <Highlights
           id="highlights-element"
