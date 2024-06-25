d91c95caa16e15e12d440b27c57032d416b2cca6
Caroline <4971715+carolineBda@users.noreply.github.com>
2024-06-25 14:47:10 +0200

    feat(e2e): test la validité de l'html de toutes les pages contributions et convention-collectives (#5686)

--- packages/code-du-travail-frontend/cypress/integration/heavy/validate-html.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/heavy/validate-html.spec.ts
@@ -1,37 +1,29 @@
 import "cypress-html-validate/commands";
 
+import urls from "../../support/urls-to-validate.json";
+import { ConfigData } from "cypress-html-validate";
+
+export const localConfig: ConfigData = {
+  rules: {
+    "heading-level": "error",
+    "require-sri": "off",
+    "valid-id": "off",
+    "prefer-native-element": "off",
+    "no-implicit-button-type": "off",
+    "aria-label-misuse": "off",
+    "long-title": "off",
+    "script-type": "off",
+    "no-dup-id": "off",
+    "wcag/h63": "off",
+    "wcag/h32": "off",
+  },
+};
+
 describe("Validate html", () => {
-  describe("page should be valid", () => {
-    [
-      "/convention-collective",
-      "/convention-collective/3248-metallurgie",
-      "/convention-collective/2941-aide-accompagnement-soins-et-services-a-domicile-bad",
-      "/convention-collective/16-transports-routiers-et-activites-auxiliaires-du-transport",
-      "/contribution",
-      "/contribution/les-conges-pour-evenements-familiaux",
-      "/contribution/2614-les-conges-pour-evenements-familiaux",
-      "/contribution/3248-comment-determiner-lanciennete-du-salarie",
-      "/contribution/292-quelles-sont-les-consequences-du-non-respect-du-preavis-par-le-salarie-ou-lemployeur",
-      "/contribution/2120-quelle-est-la-duree-de-preavis-en-cas-de-licenciement",
-    ].forEach((url) => {
-      it("page: " + url, () => {
-        cy.visit(url);
-        cy.htmlvalidate({
-          rules: {
-            "heading-level": "error",
-            "require-sri": "off",
-            "valid-id": "off",
-            "prefer-native-element": "off",
-            "no-implicit-button-type": "off",
-            "aria-label-misuse": "off",
-            "long-title": "off",
-            "script-type": "off",
-            "no-dup-id": "off",
-            "wcag/h63": "off",
-            "wcag/h32": "off",
-          },
-        });
-      });
+  urls.forEach((url) => {
+    it("page should be valid: " + url, () => {
+      cy.visit(url);
+      cy.htmlvalidate(localConfig);
     });
   });
 });
