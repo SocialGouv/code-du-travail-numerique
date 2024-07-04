4029541fa4d060767a33cd4afa1314aebba43359
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-07-04 16:47:33 +0200

    fix: csp config

--- packages/code-du-travail-frontend/next.config.mjs
+++ packages/code-du-travail-frontend/next.config.mjs
@@ -3,11 +3,11 @@ import MappingReplacement from "./redirects.json" assert { type: "json" };
 
 const ContentSecurityPolicy = `
 img-src 'self' https://travail-emploi.gouv.fr https://www.service-public.fr https://cdtn-prod-public.s3.gra.io.cloud.ovh.net;
-script-src 'self' https://mon-entreprise.urssaf.fr ${
+script-src 'self' https://matomo.fabrique.social.gouv.fr https://mon-entreprise.urssaf.fr ${
   process.env.NEXT_PUBLIC_APP_ENV !== "production" && "'unsafe-eval'"
 };
 frame-src 'self' https://mon-entreprise.urssaf.fr https://matomo.fabrique.social.gouv.fr *.dailymotion.com;
-connect-src 'self' https://geo.api.gouv.fr https://matomo.fabrique.social.gouv.fr;
+connect-src 'self' https://geo.api.gouv.fr;
 `;
 
 const sentryConfig = {
