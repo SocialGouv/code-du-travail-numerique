08e95d14c1a222994519dd040d04d5c9e5fce4d0
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-07-08 16:10:00 +0200

    fix: test

--- packages/code-du-travail-frontend/next.config.mjs
+++ packages/code-du-travail-frontend/next.config.mjs
@@ -6,6 +6,7 @@ img-src 'self' https://travail-emploi.gouv.fr https://www.service-public.fr;
 script-src 'self' https://mon-entreprise.urssaf.fr https://matomo.fabrique.social.gouv.fr ${
   process.env.NEXT_PUBLIC_APP_ENV !== "production" && "'unsafe-eval'"
 };
+connect-src 'self' https://geo.api.gouv.fr https://sentry.fabrique.social.gouv.fr https://matomo.fabrique.social.gouv.fr;
 worker-src 'self' blob:;
 report-uri ${process.env.NEXT_PUBLIC_SENTRY_BASE_URL}/api/${
   process.env.NEXT_PUBLIC_SENTRY_PROJECT_ID
@@ -58,10 +59,6 @@ const moduleExports = {
       {
         key: "Content-Security-Policy",
         value: ContentSecurityPolicy.replace(/\n/g, " ").trim(),
-      },
-      {
-        key: "Report-To",
-        value: `{"group":"csp-endpoint","max_age":10886400,"endpoints":[{"url":"${process.env.NEXT_PUBLIC_SENTRY_BASE_URL}/api/${process.env.NEXT_PUBLIC_SENTRY_PROJECT_ID}/security/?sentry_key=${process.env.NEXT_PUBLIC_SENTRY_PUBLIC_KEY}"}],"include_subdomains":true}`,
       },
     ];
     if (process.env.NEXT_PUBLIC_IS_PRODUCTION_DEPLOYMENT) {
