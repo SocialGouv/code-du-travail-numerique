01838d426a5b240bcec6c693f839b76aaff392d7
Caroline <4971715+carolineBda@users.noreply.github.com>
2024-07-16 18:13:09 +0200

    feat(contributions): ajout de la date de mise à jour sur les contributions (#6034)
    * feat(contributions): ajout de la date de mise à jour sur les contributions
    
    * ts-ignore
    
    * fix tests
    
    * add git ignore
    
    * fix tests
    
    * clean up
    
    * upgrade package
    
    * fix: merge
    
    ---------
    
    Co-authored-by: maxgfr <25312957+maxgfr@users.noreply.github.com>

--- packages/code-du-travail-frontend/cypress/integration/light/contributions/contributions.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/light/contributions/contributions.spec.ts
@@ -25,6 +25,10 @@ describe("Contributions", () => {
       "have.text",
       "La période d’essai peut-elle être renouvelée ?"
     );
+
+    cy.get("div > p > span").invoke('text').should("match", /Mis à jour le/);
+    cy.get("div > p > span").invoke('text').should("match", /\d\d\/\d\d\/\d\d\d\d/);
+
     cy.contains(
       "Accéder aux informations générales sans renseigner ma convention collective"
     ).click();
--- packages/code-du-travail-frontend/pages/contribution/[slug].tsx
+++ packages/code-du-travail-frontend/pages/contribution/[slug].tsx
@@ -61,12 +61,14 @@ function PageContribution(props: Props): React.ReactElement {
     props.contribution.description
   );
 
+
   return (
     <Layout>
       <Metas title={metas.title} description={metas.description} />
       <Answer
         title={getTitleFromNewContrib(props.contribution)}
         breadcrumbs={props.contribution.breadcrumbs}
+        date={props.contribution.date}
       >
         {props.contribution.idcc === "0000" ? (
           <ContributionGeneric
