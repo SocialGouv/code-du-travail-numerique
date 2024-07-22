5c707c6d59405c52305678ef21513ac4c4b95b2b
Martial Maillot <martial.maillot@gmail.com>
2024-07-22 11:18:35 +0200

    fix(rupture-co): désactiver les anciennes règles de la 1486

--- packages/code-du-travail-modeles/src/modeles/conventions/1486_bureaux_etudes_techniques/__tests__/rupture-conventionnelle/calculate.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/1486_bureaux_etudes_techniques/__tests__/rupture-conventionnelle/calculate.spec.ts
@@ -5,7 +5,7 @@ const engine = new RuptureConventionnellePublicodes(
   "1486"
 );
 
-describe("Gestion des licenciements pour la CC 1486", () => {
+describe("Rupture conventionnelle pour la CC 1486", () => {
   describe("Missing variables", () => {
     test("Sans arguments", () => {
       const input = {
@@ -63,40 +63,40 @@ describe("Gestion des licenciements pour la CC 1486", () => {
         catPro: "Ingénieurs et cadres",
         dateEntree: "01/01/2000",
         dateSortie: "01/09/2000",
-        result: 450,
+        result: 600,
       },
       {
         catPro: "Ingénieurs et cadres",
         dateEntree: "01/01/2000",
         dateSortie: "01/01/2001",
-        result: 675,
+        result: 900,
       },
       {
         catPro: "Ingénieurs et cadres",
         dateEntree: "01/01/2000",
         dateSortie: "01/01/2006",
-        result: 4050,
+        result: 5400,
       },
       {
         catPro: "Chargés d'enquête intermittents",
         dateEntree: "01/01/2000",
         dateSortie: "01/09/2000",
-        result: 450,
+        result: 720,
       },
       {
         catPro: "Chargés d'enquête intermittents",
         dateEntree: "01/01/2000",
         dateSortie: "01/01/2002",
-        result: 1350,
+        result: 2160,
       },
       {
         catPro: "Chargés d'enquête intermittents",
         dateEntree: "01/01/2000",
         dateSortie: "01/01/2005",
-        result: 3375,
+        result: 5400,
       },
     ])(
-      "Le licenciement fait suite au refus d'une clause de mobilité",
+      "Calcul pour $catPro avec une entrée dans l'entreprise le $dateEntree et sortie le $dateSortie",
       (value) => {
         const result = engine.calculate({
           "contrat salarié . convention collective": "'IDCC1486'",
@@ -117,6 +117,28 @@ describe("Gestion des licenciements pour la CC 1486", () => {
         expect(result).toAgreementResultBeEqual(value.result, "€");
       }
     );
+  });
+
+  describe("Désactivation des anciennes règles", () => {
+    test("Calcul pour les chargés d'enquête intermittents", () => {
+      const result = engine.calculate({
+        "contrat salarié . convention collective": "'IDCC1486'",
+        "contrat salarié . convention collective . bureaux études techniques . indemnité de licenciement . catégorie professionnelle": `'Chargés d'enquête intermittents'`,
+        "contrat salarié . indemnité de licenciement . date d'entrée":
+          "01/01/2020",
+        "contrat salarié . indemnité de licenciement . date de notification":
+          "01/01/2024",
+        "contrat salarié . indemnité de licenciement . date de sortie":
+          "01/01/2024",
+        "contrat salarié . indemnité de licenciement . inaptitude suite à un accident ou maladie professionnelle":
+          "non",
+        licenciementFauteGrave: "non",
+        salaryPeriods:
+          '[{"month":"décembre 2023","value":2266},{"month":"novembre 2023","value":2250},{"month":"octobre 2023","value":2203},{"month":"septembre 2023","value":2199},{"month":"août 2023","value":2100},{"month":"juillet 2023","value":2020},{"month":"juin 2023","value":2010},{"month":"mai 2023","value":2010},{"month":"avril 2023","value":1930},{"month":"mars 2023","value":1890},{"month":"février 2023","value":1900},{"month":"janvier 2023","value":1800}]',
+        typeContratTravail: "cdi",
+      });
+      expect(result).toAgreementResultBeEqual(3277.07, "€");
+    });
   });
 
   describe("Vérifier qu'il n'y a pas d'ancienneté conventionnelle requise", () => {
@@ -186,7 +208,7 @@ describe("Gestion des licenciements pour la CC 1486", () => {
           '[{"month":"février 2024","value":3000},{"month":"janvier 2024","value":3000}]',
         typeContratTravail: "cdi",
       });
-      expect(result).toAgreementResultBeEqual(100, "€");
+      expect(result).toAgreementResultBeEqual(200, "€");
     });
   });
 });
