12a4d3435a313be52353182ee00f90efba91b4fd
victor <victor.zeinstra@gmail.com>
2024-08-22 17:54:41 +0200

    feat: implement publicode generation

--- packages/code-du-travail-modeles/src/scripts/generatePublicode.ts
+++ packages/code-du-travail-modeles/src/scripts/generatePublicode.ts
@@ -4,14 +4,9 @@ import { generatePublicodeFiles } from "./lib";
 import { generatePreavisLicenciementTree } from "./generatePreavisLicenciementTree";
 
 async function main() {
-  if (process.argv.length < 3) {
-    throw new Error("missing argument 'path'");
-  }
-
-  const path = process.argv[2];
   const dpl = generatePreavisLicenciementTree();
   // console.log("dpl", dpl);
-  await generatePublicodeFiles(dpl, "DureePreavisLicenciement", path);
+  await generatePublicodeFiles(dpl, "DureePreavisLicenciement");
 }
 
 main();
--- packages/code-du-travail-modeles/src/scripts/lib/publicodeGeneration.ts
+++ packages/code-du-travail-modeles/src/scripts/lib/publicodeGeneration.ts
@@ -1,44 +1,145 @@
 import fs from "fs";
 
-import { TreeQuestion } from "./type";
+import { OptionResult, TreeQuestion } from "./type";
+
+function cleanValue(value: string) {
+  const [, newValue] = value.split("|");
+  return newValue.trim();
+}
+
+function generateNamespace(
+  namespace: string[],
+  questionName: string,
+  option: string
+): string {
+  return `
+contrat salarié . convention collective . ${namespace.join(" . ")}:
+  applicable si: ${questionName} = "'${option}'"
+  `;
+}
+
+function generateResult(result: OptionResult, namespace: string[]): string {
+  const namespaceLine = namespace.join(" . ");
+  const refLines = result.refs.map(({ url, label }) => `${label}: ${url}`);
+  const content = `
+contrat salarié . convention collective . ${namespaceLine} . résultat conventionnel:
+  références:
+    ${refLines.join(`
+    `)}
+  `;
+  return content;
+}
+
+function generateQuestions(
+  question: TreeQuestion,
+  namespace: string[]
+): string {
+  let content = "";
+  const namespaceLine = namespace.join(" . ");
+  switch (question.type) {
+    case "select":
+      content = `
+contrat salarié . convention collective . ${namespaceLine} . ${question.name}:
+  titre: ${question.name}
+  question: ${question.text}
+  cdtn:
+    type: liste
+    valeurs:
+      ${question.options.map(
+        ({ text }) => `${cleanValue(text)}: "'${cleanValue(text)}'"`
+      ).join(`
+      `)}
+      `;
+  }
+  const otherOptions = question.options.reduce<string[]>(
+    (arr, { text, nextQuestion, result }) => {
+      arr.push(
+        generateNamespace(
+          [...namespace, cleanValue(text)],
+          question.name,
+          cleanValue(text)
+        )
+      );
+      if (nextQuestion) {
+        arr.push(
+          generateQuestions(nextQuestion, [...namespace, cleanValue(text)])
+        );
+      }
+      if (result) {
+        arr.push(generateResult(result, [...namespace, cleanValue(text)]));
+      }
+      return arr;
+    },
+    []
+  );
+  return `${content}
+          ${otherOptions.join(`
+          `)}`;
+}
+
+function getIdccQuestion(question: TreeQuestion): TreeQuestion | null {
+  if (question.type === "agreement") {
+    return question;
+  }
+  return question.options.reduce<TreeQuestion | null>((result, option) => {
+    if (option.nextQuestion) {
+      const idccQuestion = getIdccQuestion(option.nextQuestion);
+      if (idccQuestion) {
+        result = idccQuestion;
+      }
+    }
+    return result;
+  }, null);
+}
 
 function generatePublicode(
   question: TreeQuestion,
   componentName: string
-): { filename: string; content: string } {
-  const { name } = question;
-  return {
-    filename: `${componentName}.yml`,
-    content: "toto",
-  };
+): { filename: string; content: string }[] {
+  const idccQuestion = getIdccQuestion(question);
+  if (!idccQuestion) {
+    return [];
+  }
+  const pathDir = "src/modeles/conventions";
+  const folders = fs.readdirSync(pathDir);
+  return idccQuestion.options
+    .filter(({ text }) => text === "16")
+    .reduce<{ filename: string; content: string }[]>(
+      (arr, { text, nextQuestion }) => {
+        const foldername = folders.find((folder) =>
+          folder.startsWith(`${text}_`)
+        );
+        const foldernameSplit = foldername?.split("_");
+        const ccName =
+          foldernameSplit?.slice(1, foldernameSplit.length).join(" ") ?? "";
+        if (!foldername) return arr;
+        arr.push({
+          content: nextQuestion
+            ? generateQuestions(nextQuestion, [ccName, componentName])
+            : "",
+          filename: `${pathDir}/${foldername}/${componentName}.yml`,
+        });
+        return arr;
+      },
+      []
+    );
 }
 
 export async function generatePublicodeFiles(
   question: TreeQuestion,
-  componentName: string,
-  path: string
+  componentName: string
 ) {
-  // const tests = generateTest(question, componentName);
-  // console.log(`Generating files for ${componentName}:`);
-  if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
+  const idccQuestion = getIdccQuestion(question);
+  if (!idccQuestion) {
+    return [];
   }
-  const fileContent = `
-    contrat salarié . convention collective . transports routiers . ${componentName}:
-    applicable si: ${componentName}
-    valeur: oui
-  `;
-  const { filename, content } = generatePublicode(question, componentName);
-  fs.writeFile(`${path}/${filename}`, content, function (err) {
-    if (err) throw err;
-    console.log(`${filename} Saved!`);
-  });
-  // await Promise.all(
-  //   tests.map(({ filename, content }) => {
-  //     fs.writeFile(`${path}/${filename}`, content, function (err) {
-  //       if (err) throw err;
-  //       console.log(`${filename} Saved!`);
-  //     });
-  //   })
-  // );
+  const publicodes = generatePublicode(question, componentName);
+  await Promise.all(
+    publicodes.map(({ filename, content }) => {
+      fs.writeFile(`${filename}`, content, function (err) {
+        if (err) throw err;
+        console.log(`${filename} Saved!`);
+      });
+    })
+  );
 }
