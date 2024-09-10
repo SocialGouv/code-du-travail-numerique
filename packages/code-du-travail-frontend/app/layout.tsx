72c2c884a616e18c903ad52bef2e5ca0ef91aea8
carolineBda <caroline.bourdeu@gmail.com>
2024-09-10 17:09:09 +0200

    try with nonce

--- packages/code-du-travail-frontend/app/layout.tsx
+++ packages/code-du-travail-frontend/app/layout.tsx
@@ -7,13 +7,15 @@ import { defaultColorScheme } from "../src/modules/config/defaultColorScheme";
 import { StartDsfr } from "../src/modules/config/StartDsfr";
 import { Metadata } from "next/types";
 import { SITE_URL } from "../src/config";
+import { headers } from "next/headers";
 
 export const metadata: Metadata = {
   title: {
     template: "%s  - Code du travail numérique",
     default: "Code du travail numérique",
   },
-  description: "Posez votre question sur le droit du travail et obtenez une réponse personnalisée à vos questions (contrat de travail, congés payés, formation, démission, indemnités).",
+  description:
+    "Posez votre question sur le droit du travail et obtenez une réponse personnalisée à vos questions (contrat de travail, congés payés, formation, démission, indemnités).",
   metadataBase: new URL(SITE_URL),
   twitter: {
     card: "summary",
@@ -26,25 +28,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const lang = "fr";
-
+  const nonce = headers().get("x-nonce") ?? undefined;
+  console.log("Nonce", nonce);
   return (
-    <html {...getHtmlAttributes({ defaultColorScheme, lang })}>
+    <html
+      {...getHtmlAttributes({ defaultColorScheme, lang })}
+      prefix="og: http://ogp.me/ns#"
+    >
       <head>
         <StartDsfr />
         <DsfrHead
           Link={Link}
           preloadFonts={[
-            //"Marianne-Light",
-            //"Marianne-Light_Italic",
             "Marianne-Regular",
-            //"Marianne-Regular_Italic",
             "Marianne-Medium",
-            //"Marianne-Medium_Italic",
             "Marianne-Bold",
-            //"Marianne-Bold_Italic",
-            //"Spectral-Regular",
-            //"Spectral-ExtraBold"
           ]}
+          nonce={nonce}
         />
       </head>
       <body>
