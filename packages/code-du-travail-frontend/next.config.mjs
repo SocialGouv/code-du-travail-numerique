a422c51647024d87c002ba5dd4d02a2e688deb99
carolineBda <caroline.bourdeu@gmail.com>
2024-09-17 11:10:12 +0200

    update config

--- packages/code-du-travail-frontend/next.config.mjs
+++ packages/code-du-travail-frontend/next.config.mjs
@@ -1,11 +1,12 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import MappingReplacement from "./redirects.json" assert { type: "json" };
 
+const unsafeEval =
+  process.env.NEXT_PUBLIC_APP_ENV === "production" ? "" : "'unsafe-eval'";
+
 const ContentSecurityPolicy = `
 img-src 'self' https://travail-emploi.gouv.fr https://www.service-public.fr https://cdtn-prod-public.s3.gra.io.cloud.ovh.net https://matomo.fabrique.social.gouv.fr data:;
-script-src 'self' https://mon-entreprise.urssaf.fr https://matomo.fabrique.social.gouv.fr ${
-  process.env.NEXT_PUBLIC_APP_ENV !== "production" && "'unsafe-eval'"
-};
+script-src 'self' https://mon-entreprise.urssaf.fr https://matomo.fabrique.social.gouv.fr ${unsafeEval};
 frame-src 'self' https://mon-entreprise.urssaf.fr https://matomo.fabrique.social.gouv.fr *.dailymotion.com;
 connect-src 'self' https://geo.api.gouv.fr https://sentry.fabrique.social.gouv.fr https://matomo.fabrique.social.gouv.fr;
 worker-src 'self' blob:;
