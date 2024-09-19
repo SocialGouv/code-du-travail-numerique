a24252732f71b367d41e67d10ab7d6dc02eacfbc
carolineBda <caroline.bourdeu@gmail.com>
2024-09-19 10:38:24 +0200

    refacto

--- packages/code-du-travail-frontend/src/api/modules/legal-articles/service.ts
+++ packages/code-du-travail-frontend/src/api/modules/legal-articles/service.ts
@@ -1,27 +1,6 @@
-import { SearchRequest } from "@elastic/elasticsearch/lib/api/types";
-import { elasticDocumentsIndex, elasticsearchClient } from "../../utils";
 import { getLegalArticle } from "./queries";
 import { DocumentElasticResult, ElasticLaborCodeArticle } from "./type";
-
-// TODO bouger dans un fichier common ??
-export const getItemBySlug = async <
-  V,
-  K extends keyof DocumentElasticResult<V>
->(
-  fields: K[],
-  body: SearchRequest
-): Promise<DocumentElasticResult<V> | undefined> => {
-  const response = await elasticsearchClient.search<V>({
-    _source: fields,
-    index: elasticDocumentsIndex,
-    ...body,
-  });
-  if (response.hits.hits.length === 0) {
-    return;
-  }
-  const item = response.hits.hits[0];
-  return { ...item._source, _id: item._id } as DocumentElasticResult<V>;
-};
+import { getItemBySlug } from "../items";
 
 export const getLegalArticleBySlug = async (
   slug: string
