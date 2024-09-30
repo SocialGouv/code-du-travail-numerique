dea11824453d50d4adbe58ff978d87a74b100433
carolineBda <caroline.bourdeu@gmail.com>
2024-09-30 16:52:48 +0200

    fix test

--- packages/code-du-travail-modeles/src/modeles/conventions/275_transport_aerien_personnel_au_sol/__tests__/indemnite-licenciement/indemnite.spec.ts
+++ packages/code-du-travail-modeles/src/modeles/conventions/275_transport_aerien_personnel_au_sol/__tests__/indemnite-licenciement/indemnite.spec.ts
@@ -5,7 +5,7 @@ const engine = new IndemniteLicenciementPublicodes(
   "275"
 );
 
-describe("CC 275", () => {
+describe("Calcul de l'indemnité de licenciement pour la CC 275", () => {
   describe("Avant le 31/01/2024", () => {
     describe("Calcul de l'indemnité de licenciement pour Non-cadres", () => {
       test.each`
@@ -103,7 +103,7 @@ describe("CC 275", () => {
   });
 
   describe("Entre le 31/01/2024 et le 30/10/2024", () => {
-    describe("Calcul de l'indemnité de licenciement pour Non-cadres", () => {
+    describe("Pour un Non Cadre", () => {
       test.each`
         seniorityRight | seniority | salaireRef | expectedCompensation
         ${0.61}        | ${0.61}   | ${2500}    | ${0}
@@ -140,7 +140,7 @@ describe("CC 275", () => {
       );
     });
 
-    describe("Calcul de l'indemnité de licenciement pour Cadres", () => {
+    describe("Pour un Cadre", () => {
       test.each`
         seniorityRight | seniority | salaireRef | expectedCompensation | age
         ${0.61}        | ${0.61}   | ${2500}    | ${0}                 | ${45}
@@ -203,17 +203,17 @@ describe("CC 275", () => {
   });
 
   describe("A partir du 30/10/2024", () => {
-    describe("Calcul de l'indemnité de licenciement pour Non-cadres", () => {
+    describe("Pour un Non Cadre", () => {
       test.each`
         seniorityRight | seniority | salaireRef | expectedCompensation
         ${0.61}        | ${0.61}   | ${2500}    | ${0}
         ${0.91}        | ${0.91}   | ${2500}    | ${568.75}
         ${0.91}        | ${4}      | ${2500}    | ${2500}
         ${0.91}        | ${5}      | ${2500}    | ${3125}
-        ${0.91}        | ${10}     | ${2500}    | ${6250}
-        ${0.91}        | ${15}     | ${2500}    | ${13750}
-        ${0.91}        | ${20}     | ${2500}    | ${23750}
-        ${0.91}        | ${25}     | ${2500}    | ${36250}
+        ${0.91}        | ${10}     | ${2500}    | ${8125}
+        ${0.91}        | ${15}     | ${2500}    | ${15625}
+        ${0.91}        | ${20}     | ${2500}    | ${25625}
+        ${0.91}        | ${25}     | ${2500}    | ${38125}
         ${0.91}        | ${50}     | ${5000}    | ${90000}
       `(
         "Avec une ancienneté $seniority ans, un salaire de référence $salaireRef € => une compensation de base de $expectedCompensation €",
@@ -241,32 +241,32 @@ describe("CC 275", () => {
       );
     });
 
-    describe("Calcul de l'indemnité de licenciement pour Cadres", () => {
+    describe("Pour un Cadre", () => {
       test.each`
         seniorityRight | seniority | salaireRef | expectedCompensation | age
         ${0.61}        | ${0.61}   | ${2500}    | ${0}                 | ${45}
         ${0.91}        | ${0.91}   | ${2500}    | ${568.75}            | ${45}
         ${0.91}        | ${5}      | ${2500}    | ${3125}              | ${45}
-        ${0.91}        | ${10}     | ${2500}    | ${6250}              | ${45}
-        ${0.91}        | ${15}     | ${2500}    | ${16250}             | ${45}
-        ${0.91}        | ${20}     | ${2500}    | ${28750}             | ${45}
-        ${0.91}        | ${25}     | ${2500}    | ${41250}             | ${45}
+        ${0.91}        | ${10}     | ${2500}    | ${8125}              | ${45}
+        ${0.91}        | ${15}     | ${2500}    | ${18125}             | ${45}
+        ${0.91}        | ${20}     | ${2500}    | ${30625}             | ${45}
+        ${0.91}        | ${25}     | ${2500}    | ${43125}             | ${45}
         ${0.91}        | ${50}     | ${5000}    | ${90000}             | ${45}
         ${0.61}        | ${0.61}   | ${2500}    | ${0}                 | ${50}
         ${0.91}        | ${0.91}   | ${2500}    | ${568.75}            | ${45}
         ${0.91}        | ${5}      | ${2500}    | ${3125}              | ${50}
-        ${0.91}        | ${10}     | ${2500}    | ${8750}              | ${50}
-        ${0.91}        | ${15}     | ${2500}    | ${18750}             | ${50}
-        ${0.91}        | ${20}     | ${2500}    | ${31250}             | ${50}
-        ${0.91}        | ${25}     | ${2500}    | ${43750}             | ${50}
+        ${0.91}        | ${10}     | ${2500}    | ${10625}              | ${50}
+        ${0.91}        | ${15}     | ${2500}    | ${20625}             | ${50}
+        ${0.91}        | ${20}     | ${2500}    | ${33125}             | ${50}
+        ${0.91}        | ${25}     | ${2500}    | ${45625}             | ${50}
         ${0.91}        | ${50}     | ${5000}    | ${95000}             | ${50}
         ${0.61}        | ${0.61}   | ${2500}    | ${0}                 | ${55}
         ${0.91}        | ${0.91}   | ${2500}    | ${568.75}            | ${45}
         ${0.91}        | ${5}      | ${2500}    | ${3125}              | ${55}
-        ${0.91}        | ${10}     | ${2500}    | ${11250}             | ${55}
-        ${0.91}        | ${15}     | ${2500}    | ${21250}             | ${55}
-        ${0.91}        | ${20}     | ${2500}    | ${33750}             | ${55}
-        ${0.91}        | ${25}     | ${2500}    | ${46250}             | ${55}
+        ${0.91}        | ${10}     | ${2500}    | ${13125}             | ${55}
+        ${0.91}        | ${15}     | ${2500}    | ${23125}             | ${55}
+        ${0.91}        | ${20}     | ${2500}    | ${35625}             | ${55}
+        ${0.91}        | ${25}     | ${2500}    | ${48125}             | ${55}
         ${0.91}        | ${50}     | ${5000}    | ${100000}            | ${55}
       `(
         "Avec une ancienneté $seniority ans et $age ans d'age, un salaire de référence $salaireRef € => une compensation de base de $expectedCompensation €",
