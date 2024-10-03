29ec0d61ac93d8546b8c57532a2d1f080af48f62
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2024-10-03 12:15:11 +0200

    feat(dsfr): ajout des pages d'erreurs (404 + 500) (#6146)
    * fix: pages
    
    * fix: 404
    
    * fix: tests
    
    * fix: tests
    
    * fix: tests
    
    * fix: lint
    
    * test error
    
    * fix errors
    
    * Fix spec
    
    * add button to test error page
    
    * feat: force error 500 for testing purpose
    
    * feat: revert errors
    
    ---------
    
    Co-authored-by: carolineBda <caroline.bourdeu@gmail.com>
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>

--- packages/code-du-travail-frontend/app/error.tsx
+++ packages/code-du-travail-frontend/app/error.tsx
@@ -1,5 +1,28 @@
 "use client";
 
-export default function Error() {
-  return <div>Erreur react todo</div>;
+import { UnexpectedError } from "../src/modules/error/UnexpectedError";
+import { DsfrLayout } from "../src/modules/layout";
+import { useEffect } from "react";
+import * as Sentry from "@sentry/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Erreur",
+  description: "Erreur inattendue s'est produite",
+};
+
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  useEffect(() => {
+    console.error(error);
+    Sentry.captureException(error);
+  }, [error]);
+  return (
+    <DsfrLayout>
+      <UnexpectedError />
+    </DsfrLayout>
+  );
 }
