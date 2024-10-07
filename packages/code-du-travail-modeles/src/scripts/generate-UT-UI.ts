14d0a3abddccbb060fe0281ec2acef9684413ba8
Victor <victor.zeinstra@gmail.com>
2024-10-07 12:13:03 +0200

    chore: simplify script

--- packages/code-du-travail-modeles/src/scripts/generate-UT-UI.ts
+++ packages/code-du-travail-modeles/src/scripts/generate-UT-UI.ts
@@ -1,20 +1,21 @@
 import { generateUITestFiles } from "./lib";
-import { generateHeureRechercheEmploiTree } from "./generateHeureRechercheEmploiTree";
+// import { generateHeureRechercheEmploiTree } from "./generateHeureRechercheEmploiTree";
 import { generatePreavisDemissionTree } from "./generatePreavisDemissionTree";
-import { generatePreavisLicenciementTree } from "./generatePreavisLicenciementTree";
-import { generateIndemnitePrecariteTree } from "./generateIndemnitePrecarite";
+// import { generatePreavisLicenciementTree } from "./generatePreavisLicenciementTree";
+// import { generateIndemnitePrecariteTree } from "./generateIndemnitePrecarite";
 
 async function main() {
-  if (process.argv.length < 3) {
-    throw new Error("missing argument 'path'");
-  }
-  const path = process.argv[2];
-  const hre = generateHeureRechercheEmploiTree();
-  await generateUITestFiles(
-    hre,
-    "HeuresRechercheEmploi",
-    `${path}/src/outils/HeuresRechercheEmploi/__tests__/agreements`
-  );
+  const path = "../code-du-travail-frontend/";
+  // if (process.argv.length < 3) {
+  //   throw new Error("missing argument 'path'");
+  // }
+  // const path = process.argv[2];
+  // const hre = generateHeureRechercheEmploiTree();
+  // await generateUITestFiles(
+  //   hre,
+  //   "HeuresRechercheEmploi",
+  //   `${path}/src/outils/HeuresRechercheEmploi/__tests__/agreements`
+  // );
 
   const dpd = generatePreavisDemissionTree();
   await generateUITestFiles(
@@ -26,21 +27,21 @@ async function main() {
     ]
   );
 
-  const dpl = generatePreavisLicenciementTree();
-  await generateUITestFiles(
-    dpl,
-    "DureePreavisLicenciement",
-    `${path}/src/outils/DureePreavisLicenciement/__tests__/agreements`,
-    (texts) => [texts[0] === "0" ? "Aucun préavis" : texts[0]]
-  );
+  // const dpl = generatePreavisLicenciementTree();
+  // await generateUITestFiles(
+  //   dpl,
+  //   "DureePreavisLicenciement",
+  //   `${path}/src/outils/DureePreavisLicenciement/__tests__/agreements`,
+  //   (texts) => [texts[0] === "0" ? "Aucun préavis" : texts[0]]
+  // );
 
-  const ip = generateIndemnitePrecariteTree();
-  await generateUITestFiles(
-    ip,
-    "SimulateurIndemnitePrecarite",
-    `${path}/src/outils/IndemnitePrecarite/__tests__/agreements`,
-    (texts) => [texts[0]]
-  );
+  // const ip = generateIndemnitePrecariteTree();
+  // await generateUITestFiles(
+  //   ip,
+  //   "SimulateurIndemnitePrecarite",
+  //   `${path}/src/outils/IndemnitePrecarite/__tests__/agreements`,
+  //   (texts) => [texts[0]]
+  // );
 }
 
 main();
