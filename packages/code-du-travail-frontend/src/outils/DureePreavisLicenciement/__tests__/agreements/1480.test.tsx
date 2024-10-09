65a3ace40d4756aa56c4923cfd6d81b93fc4fb25
victor <victor.zeinstra@gmail.com>
2024-10-09 11:13:27 +0200

    chore: update TU

--- packages/code-du-travail-frontend/src/outils/DureePreavisLicenciement/__tests__/agreements/1480.test.tsx
+++ packages/code-du-travail-frontend/src/outils/DureePreavisLicenciement/__tests__/agreements/1480.test.tsx
@@ -1,67 +1,92 @@
-import { DureePreavisLicenciement } from "../../index";
-import { ui } from "../ui";
-import { fireEvent, render, screen } from "@testing-library/react";
 
-jest.spyOn(Storage.prototype, "setItem");
-Storage.prototype.getItem = jest.fn(
-  () => `
+        import { DureePreavisLicenciement } from "../../index";
+        import { ui } from "../ui";
+        import { fireEvent, render, screen } from "@testing-library/react";
+        
+        jest.spyOn(Storage.prototype, "setItem");
+        Storage.prototype.getItem = jest.fn(
+          () => `
         {
           "num": 1480,
-          "shortTitle": "Transports routiers et activités auxiliaires du transport",
-          "id": "KALICONT000005635624",
-          "title": "Transports routiers et activités auxiliaires du transport",
-          "url": "https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000005635624",
-          "slug": "16-transports-routiers-et-activites-auxiliaires-du-transport"
+          "shortTitle": "shortTitle",
+          "id": "id",
+          "title": "title",
+          "url": "https://www.url.fr",
+          "slug": "1480"
         }
         `
-);
-
-describe("DureePreavisLicenciement", () => {
-  beforeEach(() => {
-    render(<DureePreavisLicenciement icon={""} title={""} displayTitle={""} />);
-    fireEvent.click(ui.introduction.startButton.get());
-
-    fireEvent.click(screen.getByTestId("seriousMisconduct-non"));
-    fireEvent.click(ui.next.get());
-
-    fireEvent.click(screen.getByTestId("disabledWorker-non"));
-    fireEvent.click(ui.next.get());
-
-    fireEvent.change(screen.getByTestId("cdt.ancienneté"), {
-      target: { value: "15| Moins de 6 mois" },
-    });
-    fireEvent.click(ui.next.get());
-
-    fireEvent.click(ui.next.get());
-  });
-
-  describe("criteria.ancienneté = 38| Moins de 2 ans", () => {
-    beforeEach(() => {
-      fireEvent.change(screen.getByTestId("criteria.ancienneté"), {
-        target: { value: "38| Moins de 2 ans" },
-      });
-      fireEvent.click(ui.next.get());
-    });
-
+        );
+    
+        describe("DureePreavisLicenciement", () => {
+          beforeEach(() => {
+            render(<DureePreavisLicenciement icon={""} title={""} displayTitle={""} />);
+                fireEvent.click(ui.introduction.startButton.get());
+                
+    
+        fireEvent.click(screen.getByTestId("seriousMisconduct-non"));
+        fireEvent.click(ui.next.get());
+      
+    
+    
+        fireEvent.click(screen.getByTestId("disabledWorker-non"));
+        fireEvent.click(ui.next.get());
+      
+    
+    
+        fireEvent.change(screen.getByTestId("cdt.ancienneté"), {
+          target: { value: "15| Moins de 6 mois" },
+        });
+        fireEvent.click(ui.next.get());
+      
+    
+    
+        fireEvent.click(ui.next.get());
+      
+    
+          });
+          
+      describe("criteria.ancienneté = 38| Moins de 2 ans", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.change(screen.getByTestId("criteria.ancienneté"), {
+          target: { value: "38| Moins de 2 ans" },
+        });
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+        
     it("should display expected answer", () => {
       expect(screen.queryAllByText(/1 mois/g)[0]).toBeInTheDocument();
-
-      expect(screen.queryAllByText(/Article 46/)[0]).toBeInTheDocument();
+          
+        expect(screen.queryAllByText(/Article 46/)[0]).toBeInTheDocument();
+          
     });
-  });
-
-  describe("criteria.ancienneté = 42| 2 ans ou plus", () => {
-    beforeEach(() => {
-      fireEvent.change(screen.getByTestId("criteria.ancienneté"), {
-        target: { value: "42| 2 ans ou plus" },
+  
       });
-      fireEvent.click(ui.next.get());
-    });
-
+    
+      describe("criteria.ancienneté = 42| 2 ans ou plus", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.change(screen.getByTestId("criteria.ancienneté"), {
+          target: { value: "42| 2 ans ou plus" },
+        });
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+        
     it("should display expected answer", () => {
       expect(screen.queryAllByText(/2 mois/g)[0]).toBeInTheDocument();
-
-      expect(screen.queryAllByText(/Article 46/)[0]).toBeInTheDocument();
+          
+        expect(screen.queryAllByText(/Article 46/)[0]).toBeInTheDocument();
+          
     });
-  });
-});
+  
+      });
+    
+          
+        });
+      
