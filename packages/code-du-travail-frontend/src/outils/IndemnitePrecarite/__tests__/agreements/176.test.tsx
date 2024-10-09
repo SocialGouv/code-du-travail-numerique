24c0ee509e3023c3ed0c03675780691ae92ccfe8
victor <victor.zeinstra@gmail.com>
2024-10-09 12:46:37 +0200

    feat: add indemnite preca UT

---
+++ packages/code-du-travail-frontend/src/outils/IndemnitePrecarite/__tests__/agreements/176.test.tsx
@@ -0,0 +1,154 @@
+
+        import { SimulateurIndemnitePrecarite } from "../../index";
+        import { ui } from "../ui";
+        import { fireEvent, render, screen } from "@testing-library/react";
+        
+        jest.spyOn(Storage.prototype, "setItem");
+        Storage.prototype.getItem = jest.fn(
+          () => `
+        {
+          "num": 176,
+          "shortTitle": "shortTitle",
+          "id": "id",
+          "title": "title",
+          "url": "https://www.url.fr",
+          "slug": "176"
+        }
+        `
+        );
+    
+        describe("SimulateurIndemnitePrecarite", () => {
+          beforeEach(() => {
+            render(<SimulateurIndemnitePrecarite icon={""} title={""} displayTitle={""} />);
+                fireEvent.click(ui.introduction.startButton.get());
+                
+    
+        fireEvent.click(ui.next.get());
+      
+    
+          });
+          
+      describe("contractType = CDD", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.click(screen.getByTestId("contractType-cdd"));
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+      describe("criteria.cddType = Autres", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.change(screen.getByTestId("criteria.cddType"), {
+          target: { value: "Autres" },
+        });
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+      describe("finContratPeriodeDessai = Non", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.click(screen.getByTestId("finContratPeriodeDessai-non"));
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+      describe("propositionCDIFindeContrat = Non", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.click(screen.getByTestId("propositionCDIFindeContrat-non"));
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+      describe("refusCDIFindeContrat = Non", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.click(screen.getByTestId("refusCDIFindeContrat-non"));
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+      describe("interruptionFauteGrave = Non", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.click(screen.getByTestId("interruptionFauteGrave-non"));
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+      describe("refusRenouvellementAuto = Non", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.click(screen.getByTestId("refusRenouvellementAuto-non"));
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+      describe("typeRemuneration = amount", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.click(screen.getByTestId("typeRemuneration-amount"));
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+      describe("currency = 3000", () => {
+        
+        beforeEach(() => {
+          
+        fireEvent.change(screen.getByTestId("currency"), {
+          target: { value: "3000" },
+        });
+        fireEvent.click(ui.next.get());
+      
+        });
+        
+        
+    it("should display expected answer", () => {
+      expect(screen.queryAllByText(/300/g)[0]).toBeInTheDocument();
+          expect(screen.queryAllByText(/La prime de précarité/g)[0]).toBeInTheDocument();
+          
+        expect(screen.queryAllByText(/Article L1243-8 du code du travail/)[0]).toBeInTheDocument();
+          expect(screen.queryAllByText(/Article L1243-9 du code du travail/)[0]).toBeInTheDocument();
+          
+    });
+  
+      });
+    
+        
+      });
+    
+        
+      });
+    
+        
+      });
+    
+        
+      });
+    
+        
+      });
+    
+        
+      });
+    
+        
+      });
+    
+        
+      });
+    
+          
+        });
+      
