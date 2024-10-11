fb4a99a8e46d462a4bd36ce0808808e1f2df7e31
victor <victor.zeinstra@gmail.com>
2024-10-11 13:49:36 +0200

    feat: implémentation simulateur brut net

---
+++ packages/code-du-travail-frontend/app/outils/simulateur-embauche/page.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+import { DsfrLayout } from "../../../src/modules/layout";
+import { HiringSimulator } from "../../../src/modules/simulateur-embauche";
+import { fetchRelatedItems } from "../../../src/modules/documents";
+import { fetchToolId } from "../../../src/modules/outils";
+
+export const metadata: Metadata = {
+  title: "Simulateur - Calculer le salaire brut/net",
+  description:
+    "Réalisez vos conversions et calculs de salaire (brut en net, net en brut, net après impôt, heures supplémentaires et coût total employeur) avec notre simulateur.",
+};
+
+export async function Tools() {
+  const _id = await fetchToolId("simulateur-embauche");
+  const relatedItems = await fetchRelatedItems({ _id }, "simulateur-embauche");
+  return (
+    <DsfrLayout>
+      <HiringSimulator relatedItems={relatedItems}></HiringSimulator>
+    </DsfrLayout>
+  );
+}
+
+export default Tools;
