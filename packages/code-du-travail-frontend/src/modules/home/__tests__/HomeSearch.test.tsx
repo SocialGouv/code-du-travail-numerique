1df9472cea0f6e7ef4a253b0d4527e56a3307a8f
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-10-16 20:23:21 +0200

    fix: tests

--- packages/code-du-travail-frontend/src/modules/home/__tests__/HomeSearch.test.tsx
+++ packages/code-du-travail-frontend/src/modules/home/__tests__/HomeSearch.test.tsx
@@ -1,10 +1,67 @@
-import { render } from "@testing-library/react";
-import React from "react";
 import { HomeSearch } from "../HomeSearch";
+import { render, waitFor } from "@testing-library/react";
+import React from "react";
+import { byTestId } from "testing-library-selector";
+import { fetchSuggestResults } from "../../layout/header/fetchSuggestResults";
+import { useLayoutTracking } from "../../layout/tracking";
+import { UserAction } from "../../../common";
+
+jest.mock("../../layout/header/fetchSuggestResults");
+jest.mock("../../layout/tracking");
 
 describe("<HomeSearch />", () => {
   it("should match snapshot", () => {
     const { container } = render(<HomeSearch onSearchSubmit={jest.fn()} />);
     expect(container).toMatchSnapshot();
+  });
+
+  it("should show suggestions and send event tracking", async () => {
+    const suggestions = [
+      "congés payés et fractionnement",
+      "congés sans solde",
+      "congés payés acquisition",
+      "congés payés",
+      "congés payés et maladie",
+    ];
+    (fetchSuggestResults as jest.Mock).mockImplementation((data: string) => {
+      if (data === "congés") {
+        return Promise.resolve(suggestions);
+      }
+      return Promise.resolve([]);
+    });
+
+    const emitSuggestionEventMock = jest.fn();
+    (useLayoutTracking as jest.Mock).mockReturnValue({
+      emitSuggestionEvent: emitSuggestionEventMock,
+    });
+
+    let onSearchSubmitHasBeenCalled = false;
+    const { getByText } = render(
+      <HomeSearch
+        onSearchSubmit={() => {
+          onSearchSubmitHasBeenCalled = true;
+        }}
+      />
+    );
+    const userAction = new UserAction();
+    userAction.setInput(byTestId("search-input").get(), "congés");
+
+    await waitFor(() => {
+      expect(getByText("congés payés et fractionnement")).toBeInTheDocument();
+    });
+    expect(onSearchSubmitHasBeenCalled).toBeFalsy();
+    const congesSansSolde = getByText("congés sans solde");
+    expect(getByText("congés sans solde")).toBeInTheDocument();
+    expect(getByText("congés payés acquisition")).toBeInTheDocument();
+    expect(getByText("congés payés")).toBeInTheDocument();
+    expect(getByText("congés payés et maladie")).toBeInTheDocument();
+
+    userAction.click(congesSansSolde);
+    expect(onSearchSubmitHasBeenCalled).toBeTruthy();
+    expect(emitSuggestionEventMock).toHaveBeenCalledWith(
+      "congés",
+      "congés sans solde",
+      suggestions
+    );
   });
 });
