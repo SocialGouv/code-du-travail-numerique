6964fffabc65d28a8ff3c2be95d9994bc66d0085
carolineBda <caroline.bourdeu@gmail.com>
2024-10-29 16:57:13 +0100

    test to upgrade timeout

--- packages/code-du-travail-frontend/cypress.config.js
+++ packages/code-du-travail-frontend/cypress.config.js
@@ -6,7 +6,7 @@ import { downloadAllUrlsToValidate } from "./cypress/support/before";
 dotenv.config();
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 20000, // 20s
+  defaultCommandTimeout: 200000, // 20s
   e2e: {
     baseUrl: process.env.TEST_BASEURL ?? "http://localhost:3000",
     specPattern:
