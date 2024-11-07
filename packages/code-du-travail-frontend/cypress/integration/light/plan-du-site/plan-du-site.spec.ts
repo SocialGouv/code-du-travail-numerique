0ca26bb6ac4ecc6f505e0b033140041ae76190c1
carolineBda <caroline.bourdeu@gmail.com>
2024-11-07 15:43:13 +0100

    try to add scroll to e2e

--- packages/code-du-travail-frontend/cypress/integration/light/plan-du-site/plan-du-site.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/light/plan-du-site/plan-du-site.spec.ts
@@ -1,6 +1,7 @@
 describe("Plan du site", () => {
   it("je vois le plan du site", () => {
     cy.visit("/");
+    cy.scrollTo('bottom')
     cy.get("a").contains("Plan du site").click();
     cy.findByRole("heading", { level: 1 }).should("have.text", "Plan du site");
     cy.contains("a", "Page d'accueil");
