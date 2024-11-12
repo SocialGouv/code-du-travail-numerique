43505f5a790c74f4ec951aa7f009b31f34ae5734
Caroline <4971715+carolineBda@users.noreply.github.com>
2024-11-12 17:08:25 +0100

    feat(DSFR): salaire brut / net (#6260)
    * Revert "fix(simulateur embauche): revert DSFR sur simulateur embauche (#6245)"
    
    This reverts commit 6a359dded859d056e405c2b5432a7eed0ea0e138.
    
    * add click in test
    
    * try to memoised
    
    * update meta data
    
    * fix snapshots
    
    * more logging
    
    * sans aysnc
    
    * fix!!

---
+++ packages/code-du-travail-frontend/app/outils/simulateur-embauche/page.tsx
@@ -0,0 +1,45 @@
+import { DsfrLayout } from "../../../src/modules/layout";
+import { fetchRelatedItems } from "../../../src/modules/documents";
+import { fetchTool } from "../../../src/modules/outils";
+import { notFound } from "next/navigation";
+import { generateDefaultMetadata } from "../../../src/modules/common/metas";
+import HiringSimulator from "../../../src/modules/outils/simulateur-embauche/HiringSimulator";
+
+export async function generateMetadata() {
+  const { metaTitle, metaDescription } = await getTool();
+
+  return generateDefaultMetadata({
+    title: metaTitle,
+    description: metaDescription,
+    path: `/outils/simulateur-embauche`,
+  });
+}
+
+async function HiringSimulatorPage() {
+  const tool = await getTool();
+  const relatedItems = await fetchRelatedItems(
+    { _id: tool._id },
+    "simulateur-embauche"
+  );
+  return (
+    <DsfrLayout>
+      <HiringSimulator
+        title={tool.displayTitle}
+        breadcrumbTitle={tool.title}
+        relatedItems={relatedItems}
+        description={tool.description}
+      />
+    </DsfrLayout>
+  );
+}
+
+const getTool = async () => {
+  const tool = await fetchTool("simulateur-embauche");
+
+  if (!tool) {
+    return notFound();
+  }
+  return tool;
+};
+
+export default HiringSimulatorPage;
---
+++ packages/code-du-travail-frontend/src/modules/outils/simulateur-embauche/HiringSimulator.tsx
@@ -0,0 +1,102 @@
+"use client";
+import { Highlight } from "@codegouvfr/react-dsfr/Highlight";
+import { fr } from "@codegouvfr/react-dsfr";
+import { createRef, memo, useEffect, useState } from "react";
+import { ContainerSimulator } from "../../layout/ContainerSimulator";
+import { RelatedItem } from "../../documents";
+import * as Sentry from "@sentry/nextjs";
+
+type Props = {
+  relatedItems: {
+    items: RelatedItem[];
+    title: string;
+  }[];
+  title: string;
+  breadcrumbTitle: string;
+  description: string;
+};
+
+const HiringSimulator = memo(function HiringSimulator({
+  relatedItems,
+  description,
+  title,
+  breadcrumbTitle,
+}: Props) {
+  const simRef = createRef<HTMLDivElement>();
+  const [state, setState] = useState({
+    error: "",
+    simulator: "loading",
+  });
+  const onError = (error) => {
+    console.log(`Erreur durant le chargement de l'iframe brut/net ${error}`);
+    setState({ error, simulator: "error" });
+    Sentry.captureMessage(`Erreur durant le chargement de l'iframe brut/net`);
+  };
+
+  const onLoad = () => {
+    setState({ simulator: "success", error: "" });
+    if (!simRef.current?.querySelector("#simulateurEmbauche")) {
+      console.log(
+        `Erreur durant le chargement de l'iframe brut/net, "empty child"`
+      );
+      setState({ error: "empty child", simulator: "error" });
+      Sentry.captureMessage(
+        `Erreur durant le chargement de l'iframe brut/net "empty child"`
+      );
+    }
+  };
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src =
+      "https://mon-entreprise.urssaf.fr/simulateur-iframe-integration.js";
+    script.id = "script-simulateur-embauche";
+    script.onload = onLoad;
+    script.onerror = onError;
+
+    if (simRef.current) {
+      simRef.current.appendChild(script);
+    }
+
+    return () => {
+      if (simRef.current) {
+        simRef.current.removeChild(script);
+      }
+    };
+  }, []);
+  const { simulator } = state;
+  return (
+    <ContainerSimulator
+      relatedItems={relatedItems}
+      title={breadcrumbTitle}
+      description={description}
+      segments={[{ label: "Simulateurs", linkProps: { href: "/outils" } }]}
+    >
+      <h1 id="simulateur-embauche">{title}</h1>
+      <Highlight size="lg" className={fr.cx("fr-mb-12v")}>
+        Pour information, l&apos;estimation du salaire net après impôt est basée
+        sur la situation d&apos;une personne célibataire sans enfants ni
+        patrimoine.
+      </Highlight>
+      {simulator === "loading" && <p>Chargement de l’outil</p>}
+      {simulator === "error" ? (
+        <p>
+          Le simulateur d’embauche n’est pas disponible actuellement.
+          <br />
+          Retrouvez les autres simulateurs autour du thème de l’entreprise, sur
+          le site:{" "}
+          <a
+            title="Voir les simulateurs"
+            href="https://mon-entreprise.urssaf.fr/"
+          >
+            https://mon-entreprise.urssaf.fr/
+          </a>
+        </p>
+      ) : (
+        <div ref={simRef} className={fr.cx("fr-col-12")} />
+      )}
+    </ContainerSimulator>
+  );
+});
+
+export default HiringSimulator;
