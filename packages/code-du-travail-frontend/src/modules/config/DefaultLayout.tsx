feacea7dc63afbaa2603f971dbdaeba4e8eba235
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-12-02 10:16:06 +0100

    fix: polyfill

--- packages/code-du-travail-frontend/src/modules/config/DefaultLayout.tsx
+++ packages/code-du-travail-frontend/src/modules/config/DefaultLayout.tsx
@@ -7,7 +7,6 @@ import { DefaultColorScheme } from "@codegouvfr/react-dsfr/next-appdir";
 import { StartDsfrSystem } from "./StartDsfrSystem";
 import { StartDsfrLight } from "./StartDsfrLight";
 import { StartDsfrDark } from "./StartDsfrDark";
-import { PolyfillComponent } from "./PolyfillComponent";
 
 type Props = {
   children: React.ReactNode;
@@ -45,7 +44,6 @@ export default function DefaultLayout({
         />
       </head>
       <body>
-        <PolyfillComponent />
         <DsfrProvider lang={lang}>{children}</DsfrProvider>
         <MatomoAnalytics />
       </body>
