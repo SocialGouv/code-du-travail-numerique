26580e0abd9544c11b21c3ff9039f6391552a0f7
Martial Maillot <martial.maillot@gmail.com>
2024-12-11 15:41:02 +0100

    fix(seo): retrait des h1 dupliqués (#6342)

---
+++ packages/code-du-travail-frontend/src/modules/besoin-plus-informations/__tests__/BesoinPlusInformations.test.tsx
@@ -0,0 +1,49 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { BesoinPlusInformations } from "..";
+import { UserAction } from "../../../common";
+import { getServiceInfo } from "../data/servicesDeRenseignement";
+
+jest.mock("../data/servicesDeRenseignement");
+
+describe("<BesoinPlusInformations />", () => {
+  beforeAll(() => {
+    (getServiceInfo as jest.Mock).mockImplementation((data: string) => {
+      if (data === "75") {
+        return {
+          name: "PARIS",
+          url: "https://idf.drieets.gouv.fr/Adresse-et-horaires-d-ouverture-de-l-unite-departementale-75",
+        };
+      }
+      return undefined;
+    });
+  });
+
+  it("doit trouver la DREETs à partir de son code postal", () => {
+    const { getByTestId, getByLabelText } = render(<BesoinPlusInformations />);
+    const userAction = new UserAction();
+    userAction.setInput(
+      getByLabelText("Saisissez le numéro de votre département"),
+      "75"
+    );
+    userAction.click(getByTestId("button-search-service"));
+
+    expect(getByTestId("result-search-service").textContent).toBe(
+      "https://idf.drieets.gouv.fr/Adresse-et-horaires-d-ouverture-de-l-unite-departementale-75"
+    );
+  });
+
+  it("doit trouver indiquer si le code postal n'existe pas", () => {
+    const { getByTestId, getByLabelText } = render(<BesoinPlusInformations />);
+    const userAction = new UserAction();
+    userAction.setInput(
+      getByLabelText("Saisissez le numéro de votre département"),
+      "999"
+    );
+    userAction.click(getByTestId("button-search-service"));
+
+    expect(getByTestId("result-search-service-failed").textContent).toBe(
+      "Aucun service de renseignement n'a été trouvé pour ce département."
+    );
+  });
+});
--- packages/code-du-travail-frontend/src/modules/layout/infos/index.tsx
+++ packages/code-du-travail-frontend/src/modules/layout/infos/index.tsx
@@ -4,7 +4,6 @@ import { fr } from "@codegouvfr/react-dsfr";
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import { css } from "@styled-system/css";
 import { createModal } from "@codegouvfr/react-dsfr/Modal";
-import { PopupContent } from "./PopupContent";
 import { useIsModalOpen } from "@codegouvfr/react-dsfr/Modal/useIsModalOpen";
 import { useNeedMoreInfoEvents } from "./tracking";
 import { useEffect } from "react";
@@ -27,7 +26,13 @@ export const NeedMoreInfo = () => {
       <div className={fr.cx("fr-container")}>
         <div className={fr.cx("fr-grid-row", "fr-grid-row--center")}>
           <div className={`${fr.cx("fr-col-md-6", "fr-py-6w")}`}>
-            <h2 className={title}>Besoin de plus d&apos;informations ?</h2>
+            <div
+              role="heading"
+              aria-level={2}
+              className={`${fr.cx("fr-h2")} ${title}`}
+            >
+              Besoin de plus d&apos;informations ?
+            </div>
             <p className={paragraph}>
               Les services du ministère du Travail en région informent,
               conseillent et orientent les salariés et les employeurs du secteur
@@ -35,16 +40,15 @@ export const NeedMoreInfo = () => {
             </p>
             <div className={buttonContainer}>
               <Button
+                linkProps={{
+                  href: "/besoin-plus-informations",
+                }}
                 iconId="fr-icon-chat-3-line"
                 iconPosition="right"
                 priority="secondary"
-                nativeButtonProps={needMoreInfoModal.buttonProps}
               >
                 Trouver les services près de chez moi
               </Button>
-              <needMoreInfoModal.Component title="Les services du ministère du Travail">
-                <PopupContent />
-              </needMoreInfoModal.Component>
             </div>
           </div>
         </div>
