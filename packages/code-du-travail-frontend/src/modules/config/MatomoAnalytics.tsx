ef845e61b792e7e5116c75b4d3a977ea79e91748
Martial Maillot <martial.maillot@gmail.com>
2024-12-16 11:46:56 +0100

    fix(matomo): envoyer les events de changement de pages

--- packages/code-du-travail-frontend/src/modules/config/MatomoAnalytics.tsx
+++ packages/code-du-travail-frontend/src/modules/config/MatomoAnalytics.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { init, push } from "@socialgouv/matomo-next";
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { Suspense, useEffect, useState } from "react";
 import { PIWIK_SITE_ID, PIWIK_URL, SITE_URL, WIDGETS_PATH } from "../../config";
 import { getSourceUrlFromPath } from "../../lib";
+import init, { push } from "./MatomoNext";
 
-export function MatomoAnalytics() {
-  const pathname = usePathname();
+function MatomoComponent() {
+  const searchParams = useSearchParams();
+  const searchParamsString = searchParams?.toString();
+  const path = usePathname();
+  const [previousPath, setPreviousPath] = useState<string | null>(null);
 
   useEffect(() => {
     init({
@@ -15,11 +18,11 @@ export function MatomoAnalytics() {
       url: PIWIK_URL,
       onInitialization: () => {
         const referrerUrl =
-          document?.referrer || getSourceUrlFromPath(SITE_URL + pathname);
+          document?.referrer || getSourceUrlFromPath(SITE_URL + path);
         if (referrerUrl) {
           push(["setReferrerUrl", referrerUrl]);
         }
-        if (pathname && pathname.match(WIDGETS_PATH)) {
+        if (path && path.match(WIDGETS_PATH)) {
           push(["setCookieSameSite", "None"]);
         }
       },
@@ -27,5 +30,47 @@ export function MatomoAnalytics() {
     });
   }, []);
 
+  useEffect(() => {
+    if (path && !isExcludedUrl(path, [WIDGETS_PATH])) {
+      let [pathname] = path.split("?");
+      pathname = pathname.replace(/#.*/, "");
+
+      if (previousPath) {
+        push(["setReferrerUrl", `${previousPath}`]);
+      }
+
+      push(["setCustomUrl", pathname]);
+      push(["deleteCustomVariables", "page"]);
+      setPreviousPath(pathname);
+
+      const query = searchParams?.get("q");
+      push(["setDocumentTitle", document.title]);
+      if (startsWith(path, "/recherche") || startsWith(path, "/search")) {
+        push(["trackSiteSearch", query ?? ""]);
+      } else {
+        push(["trackPageView"]);
+      }
+    }
+  }, [path, searchParamsString]);
   return null;
 }
+
+export const MatomoAnalytics = () => (
+  <Suspense>
+    <MatomoComponent />
+  </Suspense>
+);
+
+const startsWith = (str: string, needle: string) => {
+  return str.substring(0, needle.length) === needle;
+};
+
+const isExcludedUrl = (url: string, patterns: RegExp[]): boolean => {
+  let excluded = false;
+  patterns.forEach((pattern) => {
+    if (pattern.exec(url) !== null) {
+      excluded = true;
+    }
+  });
+  return excluded;
+};
