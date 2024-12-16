754c1999f70828b8bd239a5d72bb9cd9fc79838e
carolineBda <caroline.bourdeu@gmail.com>
2024-12-16 11:42:20 +0100

    add more test

--- packages/code-du-travail-frontend/src/modules/fiche-service-public/builder/components/__tests__/Accordion.test.tsx
+++ packages/code-du-travail-frontend/src/modules/fiche-service-public/builder/components/__tests__/Accordion.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import accordionDataMock from "./mocks/accordionData.json";
+import accordionNestedDataMock from "./mocks/accordionNestedData.json";
 import { FicheSPDataTextWithChapitre } from "../../type";
 import AccordionWrapper from "../Accordion";
 
@@ -16,6 +17,7 @@ describe("<Accordion />", () => {
 
     const h2 = getAllByRole("heading", { level: 2 });
     expect(h2[0].textContent).toEqual("Qui peut être indemnisé ?");
+    expect(h2[0].children[0].id).toEqual("qui-peut-etre-indemnise__toggle-btn");
     expect(h2[1].textContent).toEqual("Comment faire la demande ?");
 
     const h3 = getAllByRole("heading", { level: 3 });
@@ -35,11 +37,32 @@ describe("<Accordion />", () => {
     );
     expect(container).toMatchSnapshot();
 
-    const h2 = getAllByRole("heading", { level: 4 });
-    expect(h2[0].textContent).toEqual("Qui peut être indemnisé ?");
+    const h4 = getAllByRole("heading", { level: 4 });
+    expect(h4[0].textContent).toEqual("Qui peut être indemnisé ?");
 
-    const h3 = getAllByRole("heading", { level: 5 });
-    expect(h3[0].textContent).toEqual("Vous êtes une victime directe");
-    expect(h3[1].textContent).toEqual("Vous êtes un ayant-droit de victime");
+    const h5 = getAllByRole("heading", { level: 5 });
+    expect(h5[0].textContent).toEqual("Vous êtes une victime directe");
+    expect(h5[1].textContent).toEqual("Vous êtes un ayant-droit de victime");
+  });
+
+  it("handle unique ids in nested accordion", () => {
+    const { container, getAllByRole } = render(
+      <AccordionWrapper
+        data={accordionNestedDataMock as FicheSPDataTextWithChapitre}
+        headingLevel={0}
+      />
+    );
+    expect(container).toMatchSnapshot();
+
+    const h2 = getAllByRole("heading", { level: 2 });
+    expect(h2[0].textContent).toEqual("Niveau 1");
+    expect(h2[0].children[0].id).toEqual("niveau-1__toggle-btn");
+
+    const h3 = getAllByRole("heading", { level: 3 });
+    expect(h3[0].textContent).toEqual("Niveau 2");
+    expect(h3[0].children[0].id).toEqual("56uid22__toggle-btn");
+
+    expect(h3[1].textContent).toEqual("Niveau 2");
+    expect(h3[1].children[0].id).toEqual("56uid23__toggle-btn");
   });
 });
