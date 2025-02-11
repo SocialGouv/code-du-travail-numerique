fb8255efc455c62753eecea6c778374ad408f5e1
carolineBda <caroline.bourdeu@gmail.com>
2025-02-11 14:11:30 +0100

    fusion des 2 tests pour enlever la flakyness

--- packages/code-du-travail-frontend/cypress/integration/light/themes.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/light/themes.spec.ts
@@ -8,7 +8,7 @@ describe("Navigation par thème", () => {
     cy.get("p").should("contain", "Besoin de plus d’informations");
   });
 
-  it('redirige vers la page "/themes/embauche-et-contrat-de-travail" lorsque je clique sur "Embauche et contrat de travail"', () => {
+  it('redirige vers la page "/themes/embauche-et-contrat-de-travail" lorsque je clique sur "Embauche et contrat de travail" puis sur "/themes/embauche" lorsque je clique sur "Embauche"', () => {
     cy.get("a").contains("Embauche et contrat de travail").click();
     cy.urlEqual("/themes/embauche-et-contrat-de-travail");
     cy.get("h1").should("contain", "Embauche et contrat de travail");
@@ -16,11 +16,7 @@ describe("Navigation par thème", () => {
     cy.get("a").should("contain", "Contrat de travail");
     cy.get("a").should("not.contain", "Méthodes de recrutement");
     cy.get("span").should("contain", "Contenu correspondant");
-  });
 
-  it('redirige vers la page "/themes/embauche" lorsque je clique sur "Embauche"', () => {
-    cy.get("a").contains("Embauche").click();
-    cy.urlEqual("/themes/embauche-et-contrat-de-travail");
     cy.get("a").contains("Embauche").click();
     cy.urlEqual("/themes/embauche");
     cy.get("h1").should("contain", "Embauche");
