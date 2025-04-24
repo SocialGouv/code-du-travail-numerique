454002590d3ff07e8d234e3125f87d7775283ca1
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2025-04-24 21:49:40 +0200

    feat(outils): passage en dsfr de la page principale (#6539)
    * fix: test
    
    # Conflicts:
    #	packages/code-du-travail-frontend/__tests__/__snapshots__/glossaire-[slug].test.tsx.snap
    #	packages/code-du-travail-frontend/__tests__/__snapshots__/glossaire.test.tsx.snap
    
    # Conflicts:
    #	packages/code-du-travail-frontend/app/glossaire/[slug]/page.tsx
    #	packages/code-du-travail-frontend/app/glossaire/page.tsx
    #	packages/code-du-travail-frontend/src/modules/common/List.tsx
    #	packages/code-du-travail-frontend/src/modules/glossaire/GlossaryList.tsx
    #	packages/code-du-travail-frontend/src/modules/glossaire/GlossaryTermDetail.tsx
    #	packages/code-du-travail-frontend/src/modules/glossaire/GlossaryTerms.tsx
    #	packages/code-du-travail-frontend/src/modules/glossaire/__tests__/GlossaryList.test.tsx
    #	packages/code-du-travail-frontend/src/modules/glossaire/__tests__/GlossaryTermDetail.test.tsx
    #	packages/code-du-travail-frontend/src/modules/glossaire/__tests__/utils.test.ts
    #	packages/code-du-travail-frontend/src/modules/glossaire/types.ts
    #	packages/code-du-travail-frontend/src/modules/glossaire/utils.ts
    #	packages/code-du-travail-frontend/src/modules/layout/ContainerWithBreadcrumbs.tsx
    
    * fix: test
    
    * fix: retours
    
    * fix: all
    
    * fix: done
    
    * fix: types and missing links
    
    * fix: tests
    
    * fix: tests
    
    * fix: tests
    
    ---------
    
    Co-authored-by: Martial Maillot <martial.maillot@gmail.com>

--- packages/code-du-travail-frontend/app/outils/convention-collective/page.tsx
+++ packages/code-du-travail-frontend/app/outils/convention-collective/page.tsx
@@ -3,7 +3,7 @@ import { DocumentElasticResult } from "../../../src/modules/documents";
 import { fetchTool, FindAgreementLayout } from "../../../src/modules/outils";
 import { notFound } from "next/navigation";
 import { generateDefaultMetadata } from "../../../src/modules/common/metas";
-import { ElasticTool } from "../../../src/modules/outils/type";
+import { ElasticTool } from "@socialgouv/cdtn-types";
 import { AgreementSearchIntro } from "../../../src/modules/convention-collective";
 import { agreementRelatedItems } from "../../../src/modules/convention-collective/agreementRelatedItems";
 import { SITE_URL } from "../../../src/config";
