146ee275d1e8b4c696d1477fabb881eb52b853f0
maxgfr <25312957+maxgfr@users.noreply.github.com>
2025-05-02 13:51:11 +0200

    fix: done

--- packages/code-du-travail-frontend/app/droit-du-travail/page.tsx
+++ packages/code-du-travail-frontend/app/droit-du-travail/page.tsx
@@ -1,13 +1,7 @@
 import { DsfrLayout } from "../../src/modules/layout";
 import { generateDefaultMetadata } from "../../src/modules/common/metas";
-import {
-  Introduction,
-  Origins,
-  Hierarchy,
-} from "../../src/modules/droit-du-travail";
+import { DroitDuTravailWrapper } from "../../src/modules/droit-du-travail";
 import { SITE_URL } from "src/config";
-import Breadcrumb from "@codegouvfr/react-dsfr/Breadcrumb";
-import { fr } from "@codegouvfr/react-dsfr";
 
 export const metadata = generateDefaultMetadata({
   title: "Le droit du travail",
@@ -19,46 +13,7 @@ export const metadata = generateDefaultMetadata({
 export default function DroitDuTravail() {
   return (
     <DsfrLayout container="fr-container--fluid">
-      <div className={fr.cx("fr-container")}>
-        <Breadcrumb
-          currentPageLabel={"Le droit du travail"}
-          homeLinkProps={{
-            href: "/",
-          }}
-          segments={[]}
-          className={fr.cx("fr-mb-2w", "fr-mt-2w")}
-        />
-        <div
-          className={fr.cx(
-            "fr-grid-row",
-            "fr-grid-row--gutters",
-            "fr-grid-row--center",
-            "fr-mb-12w",
-            "fr-mt-4w"
-          )}
-        >
-          <div className={fr.cx("fr-col-12")}>
-            <Introduction />
-          </div>
-        </div>
-      </div>
-
-      <Origins />
-      <div className={fr.cx("fr-container")}>
-        <div
-          className={fr.cx(
-            "fr-grid-row",
-            "fr-grid-row--gutters",
-            "fr-grid-row--center",
-            "fr-mb-12w",
-            "fr-mt-4w"
-          )}
-        >
-          <div className={fr.cx("fr-col-12")}>
-            <Hierarchy />
-          </div>
-        </div>
-      </div>
+      <DroitDuTravailWrapper />
     </DsfrLayout>
   );
 }
--- packages/code-du-travail-frontend/src/modules/droit-du-travail/index.ts
+++ packages/code-du-travail-frontend/src/modules/droit-du-travail/index.ts
@@ -1,3 +1,4 @@
 export { default as Introduction } from "./Introduction";
 export { default as Origins } from "./Origins";
 export { default as Hierarchy } from "./Hierarchy";
+export { default as DroitDuTravailWrapper } from "./DroitDuTravailWrapper";
