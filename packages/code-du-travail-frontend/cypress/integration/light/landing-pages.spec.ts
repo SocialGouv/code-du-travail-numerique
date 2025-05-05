6a5dd04eeb6a83eca79b852bdf732c9cbd1358af
maxgfr <25312957+maxgfr@users.noreply.github.com>
2025-05-05 07:52:58 +0200

    fix: test

--- packages/code-du-travail-frontend/cypress/integration/light/landing-pages.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/light/landing-pages.spec.ts
@@ -34,8 +34,8 @@ describe("Landing pages", () => {
   it("je vois la page droit du travail", () => {
     cy.visit("/droit-du-travail");
 
-    cy.contains("Qu’est-ce que le droit du travail ?");
-    cy.contains("Quels sont les textes à l’origine du droit du travail ?");
+    cy.contains("Qu'est-ce que le droit du travail ?");
+    cy.contains("Quels sont les textes à l'origine du droit du travail ?");
     cy.contains("Existe-t-il une hiérarchie entre les textes ?");
   });
 
