f228b1411fd02f68cc28b6b773bba8c788e39382
maxgfr <25312957+maxgfr@users.noreply.github.com>
2025-05-13 16:34:30 +0200

    feat: page

---
+++ packages/code-du-travail-frontend/src/modules/contributions/ContributionsList.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { fr } from "@codegouvfr/react-dsfr";
+import React, { useState } from "react";
+import { ContainerList } from "../layout/ContainerList";
+import { css } from "@styled-system/css";
+import Card from "@codegouvfr/react-dsfr/Card";
+import { getRouteBySource, SOURCES } from "@socialgouv/cdtn-utils";
+import { summarize } from "../utils";
+import Select from "@codegouvfr/react-dsfr/Select";
+
+type ContributionItem = {
+  title: string;
+  description: string;
+  slug: string;
+  source: string;
+};
+
+type ContributionsData = {
+  [theme: string]: ContributionItem[];
+};
+
+type Props = {
+  contribs: ContributionsData;
+};
+
+export const ContributionsList = ({ contribs }: Props) => {
+  const [selectedTheme, setTheme] = useState<string>("");
+  const themes = Object.keys(contribs);
+  const documents =
+    selectedTheme === ""
+      ? contribs
+      : { [selectedTheme]: contribs[selectedTheme] };
+
+  return (
+    <ContainerList title="Vos fiches pratiques" segments={[]}>
+      <h1 id="contributions" className={fr.cx("fr-mt-0", "fr-mb-6w")}>
+        Vos fiches pratiques
+      </h1>
+
+      <p className={fr.cx("fr-text--md")}>
+        Obtenez une réponse personnalisée selon votre convention collective
+      </p>
+
+      <Select
+        className={`${fr.cx("fr-mt-6w")} ${list}`}
+        label="Sélectionnez un thème"
+        nativeSelectProps={{
+          onChange: (event) => setTheme(event.target.value),
+          value: selectedTheme,
+        }}
+      >
+        <option value="">Tous les thèmes</option>
+        {themes.map((theme) => (
+          <option key={theme} value={theme}>
+            {theme}
+          </option>
+        ))}
+      </Select>
+
+      {Object.keys(documents).map((theme) => {
+        return (
+          <React.Fragment key={theme}>
+            <h2 className={fr.cx("fr-mt-6w", "fr-h3")}>{theme}</h2>
+            <ul
+              className={`${fr.cx(
+                "fr-grid-row",
+                "fr-grid-row--gutters",
+                "fr-grid-row--left"
+              )}`}
+            >
+              {documents[theme].map((item) => (
+                <li
+                  key={item.slug}
+                  className={`${fr.cx(
+                    "fr-col-12",
+                    "fr-col-sm-12",
+                    "fr-col-md-6",
+                    "fr-col-lg-6"
+                  )} ${li}`}
+                >
+                  <Card
+                    border
+                    desc={summarize(item.description)}
+                    horizontal
+                    linkProps={{
+                      href: `/${getRouteBySource(SOURCES.CONTRIBUTIONS)}/${item.slug}`,
+                    }}
+                    size="medium"
+                    title={item.title}
+                    titleAs="h3"
+                    enlargeLink
+                    classes={{
+                      // Hack: start div is render and take some place
+                      start: fr.cx("fr-hidden"),
+                    }}
+                  />
+                </li>
+              ))}
+            </ul>
+          </React.Fragment>
+        );
+      })}
+    </ContainerList>
+  );
+};
+
+const li = css({
+  listStyle: "none!",
+});
+
+const list = css({
+  maxWidth: 500,
+});
---
+++ packages/code-du-travail-frontend/src/modules/contributions/__tests__/ContributionsList.test.tsx
@@ -0,0 +1,69 @@
+import { getAllByRole, getByLabelText, render } from "@testing-library/react";
+import { ContributionsList } from "../ContributionsList";
+import userEvent from "@testing-library/user-event";
+
+const contribs = {
+  "Thème A": [
+    {
+      title: "Contribution AA",
+      slug: "contribution-aa",
+      description: "Description contribution AA",
+      source: "contributions",
+    },
+    {
+      title: "Contribution AB",
+      slug: "contribution-ab",
+      description: "Description contribution AB",
+      source: "contributions",
+    },
+  ],
+  "Thème B": [
+    {
+      title: "Contribution BA",
+      slug: "contribution-ba",
+      description: "Description contribution BA",
+      source: "contributions",
+    },
+  ],
+};
+
+describe("<ContributionsList />", () => {
+  it("affiche la liste des fiches pratiques", () => {
+    const { container, getByLabelText } = render(
+      <ContributionsList contribs={contribs} />
+    );
+    const selectThemes = getByLabelText("Sélectionnez un thème");
+    expect(selectThemes).toBeInTheDocument();
+    const themeOptions = getAllByRole(selectThemes, "option");
+    expect(themeOptions).toHaveLength(3);
+    expect(themeOptions[0]).toHaveTextContent("Tous les thèmes");
+    expect(themeOptions[1]).toHaveTextContent("Thème A");
+    expect(themeOptions[2]).toHaveTextContent("Thème B");
+    expect(container).toMatchSnapshot();
+  });
+
+  it("filtre sur le thème sélectionné", () => {
+    const { getAllByRole, getByLabelText } = render(
+      <ContributionsList contribs={contribs} />
+    );
+
+    const themes = getAllByRole("heading", { level: 2 });
+    expect(themes).toHaveLength(2);
+    expect(themes[0]).toHaveTextContent("Thème A");
+    expect(themes[1]).toHaveTextContent("Thème B");
+    const documents = getAllByRole("heading", { level: 3 });
+    expect(documents).toHaveLength(3);
+    expect(documents[0]).toHaveTextContent("Contribution AA");
+    expect(documents[1]).toHaveTextContent("Contribution AB");
+    expect(documents[2]).toHaveTextContent("Contribution BA");
+
+    const selectThemes = getByLabelText("Sélectionnez un thème");
+    userEvent.selectOptions(selectThemes, "Thème B");
+    const themesFiltered = getAllByRole("heading", { level: 2 });
+    expect(themesFiltered).toHaveLength(1);
+    expect(themesFiltered[0]).toHaveTextContent("Thème B");
+    const documentsFiltered = getAllByRole("heading", { level: 3 });
+    expect(documentsFiltered).toHaveLength(1);
+    expect(documentsFiltered[0]).toHaveTextContent("Contribution BA");
+  });
+});
