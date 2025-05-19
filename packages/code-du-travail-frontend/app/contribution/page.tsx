c4c4179d590688b973b245295ac77cff92cb2f5b
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2025-05-19 14:43:07 +0200

    feat(fiches-pratiques): passage en DSFR (#6571)

---
+++ packages/code-du-travail-frontend/app/contribution/page.tsx
@@ -0,0 +1,28 @@
+import { DsfrLayout } from "../../src/modules/layout";
+import { generateDefaultMetadata } from "../../src/modules/common/metas";
+import { ContributionsList } from "../../src/modules/contributions";
+import { getGenericContributionsGroupByThemes } from "../../src/api";
+
+export const metadata = generateDefaultMetadata({
+  title: "Vos fiches pratiques",
+  description:
+    "Obtenez une réponse personnalisée selon votre convention collective",
+  path: "/contribution",
+});
+
+async function Index() {
+  const { documents, themes } = await getContributions();
+
+  return (
+    <DsfrLayout>
+      <ContributionsList contribs={documents} themes={themes} />
+    </DsfrLayout>
+  );
+}
+
+const getContributions = async () => {
+  const data = await getGenericContributionsGroupByThemes();
+  return data;
+};
+
+export default Index;
