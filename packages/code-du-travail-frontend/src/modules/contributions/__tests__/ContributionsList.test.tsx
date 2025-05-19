6ade0f14acdacaccb9ca5eaae6d4c28e161045fe
maxgfr <25312957+maxgfr@users.noreply.github.com>
2025-05-19 14:16:31 +0200

    fix: retours

--- packages/code-du-travail-frontend/src/modules/contributions/__tests__/ContributionsList.test.tsx
+++ packages/code-du-travail-frontend/src/modules/contributions/__tests__/ContributionsList.test.tsx
@@ -1,69 +1,80 @@
-import { render, getAllByRole as getByRole } from "@testing-library/react";
+import { render, within } from "@testing-library/react";
 import { ContributionsList } from "../ContributionsList";
 import userEvent from "@testing-library/user-event";
 
 const contribs = {
-  "Thème A": [
+  "Congés et repos": [
     {
-      title: "Contribution AA",
-      slug: "contribution-aa",
-      description: "Description contribution AA",
+      title: "Congés payés",
+      slug: "conges-payes",
+      description: "Tout savoir sur les congés payés",
       source: "contributions",
+      breadcrumbs: [{ label: "Congés et repos" }],
+      theme: "Congés et repos",
+      idcc: "0000",
     },
     {
-      title: "Contribution AB",
-      slug: "contribution-ab",
-      description: "Description contribution AB",
+      title: "Jours fériés",
+      slug: "jours-feries",
+      description: "Informations sur les jours fériés",
       source: "contributions",
+      breadcrumbs: [{ label: "Congés et repos" }],
+      theme: "Congés et repos",
+      idcc: "0000",
     },
   ],
-  "Thème B": [
+  "Embauche et contrat": [
     {
-      title: "Contribution BA",
-      slug: "contribution-ba",
-      description: "Description contribution BA",
+      title: "Période d'essai",
+      slug: "periode-essai",
+      description: "Tout savoir sur la période d'essai",
       source: "contributions",
+      breadcrumbs: [{ label: "Embauche et contrat" }],
+      theme: "Embauche et contrat",
+      idcc: "0000",
     },
   ],
 };
 
+const themes = Object.keys(contribs);
+
 describe("<ContributionsList />", () => {
   it("affiche la liste des fiches pratiques", () => {
     const { container, getByLabelText } = render(
-      <ContributionsList contribs={contribs} />
+      <ContributionsList contribs={contribs} themes={themes} />
     );
     const selectThemes = getByLabelText("Sélectionnez un thème");
     expect(selectThemes).toBeInTheDocument();
-    const themeOptions = getByRole(selectThemes, "option");
+    const themeOptions = within(selectThemes).getAllByRole("option");
     expect(themeOptions).toHaveLength(3);
     expect(themeOptions[0]).toHaveTextContent("Tous les thèmes");
-    expect(themeOptions[1]).toHaveTextContent("Thème A");
-    expect(themeOptions[2]).toHaveTextContent("Thème B");
+    expect(themeOptions[1]).toHaveTextContent("Congés et repos");
+    expect(themeOptions[2]).toHaveTextContent("Embauche et contrat");
     expect(container).toMatchSnapshot();
   });
 
   it("filtre sur le thème sélectionné", () => {
     const { getAllByRole, getByLabelText } = render(
-      <ContributionsList contribs={contribs} />
+      <ContributionsList contribs={contribs} themes={themes} />
     );
 
-    const themes = getAllByRole("heading", { level: 2 });
-    expect(themes).toHaveLength(2);
-    expect(themes[0]).toHaveTextContent("Thème A");
-    expect(themes[1]).toHaveTextContent("Thème B");
+    const themeHeadings = getAllByRole("heading", { level: 2 });
+    expect(themeHeadings).toHaveLength(2);
+    expect(themeHeadings[0]).toHaveTextContent("Congés et repos");
+    expect(themeHeadings[1]).toHaveTextContent("Embauche et contrat");
     const documents = getAllByRole("heading", { level: 3 });
     expect(documents).toHaveLength(3);
-    expect(documents[0]).toHaveTextContent("Contribution AA");
-    expect(documents[1]).toHaveTextContent("Contribution AB");
-    expect(documents[2]).toHaveTextContent("Contribution BA");
+    expect(documents[0]).toHaveTextContent("Congés payés");
+    expect(documents[1]).toHaveTextContent("Jours fériés");
+    expect(documents[2]).toHaveTextContent("Période d'essai");
 
     const selectThemes = getByLabelText("Sélectionnez un thème");
-    userEvent.selectOptions(selectThemes, "Thème B");
-    const themesFiltered = getAllByRole("heading", { level: 2 });
-    expect(themesFiltered).toHaveLength(1);
-    expect(themesFiltered[0]).toHaveTextContent("Thème B");
+    userEvent.selectOptions(selectThemes, "Embauche et contrat");
+    const themeHeadingsFiltered = getAllByRole("heading", { level: 2 });
+    expect(themeHeadingsFiltered).toHaveLength(1);
+    expect(themeHeadingsFiltered[0]).toHaveTextContent("Embauche et contrat");
     const documentsFiltered = getAllByRole("heading", { level: 3 });
     expect(documentsFiltered).toHaveLength(1);
-    expect(documentsFiltered[0]).toHaveTextContent("Contribution BA");
+    expect(documentsFiltered[0]).toHaveTextContent("Période d'essai");
   });
 });
