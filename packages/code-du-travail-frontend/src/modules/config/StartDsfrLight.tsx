6b4937b0af74316fbd3812e86ea440ec43a46e9a
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-06-10 10:31:41 +0200

    feat: upgrade version for react dsfr

--- packages/code-du-travail-frontend/src/modules/config/StartDsfrLight.tsx
+++ packages/code-du-travail-frontend/src/modules/config/StartDsfrLight.tsx
@@ -1,15 +1,20 @@
 "use client";
 
-import { startReactDsfr } from "@codegouvfr/react-dsfr/next-appdir";
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 import Link from "../common/Link";
 
-declare module "@codegouvfr/react-dsfr/next-appdir" {
+declare module "@codegouvfr/react-dsfr/spa" {
   interface RegisterLink {
     Link: typeof Link;
   }
 }
 
-startReactDsfr({ defaultColorScheme: "light", Link, doCheckNonce: true });
+startReactDsfr({
+  defaultColorScheme: "light",
+  Link,
+  nonce: "true",
+  trustedTypesPolicyName: "react-dsfr",
+});
 
 export function StartDsfrLight() {
   return null;
