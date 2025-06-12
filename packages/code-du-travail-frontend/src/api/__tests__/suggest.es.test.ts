3d53b70483d6528db14f01d7e50c1a57ec8aa132
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-06-12 08:13:27 +0200

    revert to dev

--- packages/code-du-travail-frontend/src/api/__tests__/suggest.es.test.ts
+++ packages/code-du-travail-frontend/src/api/__tests__/suggest.es.test.ts
@@ -21,6 +21,7 @@ describe("Suggestion", () => {
   });
   test("accentuation is ignored", async () => {
     const res = await request(server).get("/api/suggest?q=rét");
+    expect(res.body.includes("retraite")).toBeTruthy();
     expect(res.body).toMatchSnapshot();
   });
 
