ed37dd8ea57384333fb41337c514f5ba0eef67e3
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-06-20 15:11:37 +0200

    feat: implement test e2e for autocomplete with accent

--- packages/code-du-travail-frontend/cypress/integration/light/header-search.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/light/header-search.spec.ts
@@ -51,4 +51,27 @@ describe("Header Search", () => {
     // Vérifier que la page de résultats de recherche est affichée
     cy.contains("h1", "Recherche").should("exist");
   });
+  it("should show 'retraite' in autocomplete suggestions when typing 'rét'", () => {
+    // Ouvrir le modal de recherche dans l'en-tête
+    cy.get(".fr-header__tools").click();
+
+    // Attendre que le modal de recherche soit visible
+    cy.get(".fr-header__search").should("be.visible");
+
+    // Trouver le champ de recherche dans le modal
+    cy.get(".fr-header__search input").as("searchInput");
+
+    // Taper "rét" dans le champ de recherche
+    cy.get("@searchInput").type("rét");
+
+    // Attendre que les suggestions d'autocomplétion apparaissent
+    // La liste de suggestions est un élément ul
+    cy.get(".fr-header__search ul").should("be.visible");
+
+    // Vérifier que "retraite" apparaît dans les suggestions
+    // Les suggestions sont des éléments li dans la liste ul
+    cy.get(".fr-header__search ul li")
+      .contains("retraite", { matchCase: false })
+      .should("be.visible");
+  });
 });
