5cc4194511e78272da1bb0ceaa13522a0132d4f5
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-07-02 14:31:07 +0200

    feat: maj mentions legales

--- packages/code-du-travail-frontend/src/modules/mentions-legales/index.tsx
+++ packages/code-du-travail-frontend/src/modules/mentions-legales/index.tsx
@@ -8,28 +8,26 @@ export const MentionsLegales = () => (
       Mentions légales
     </h1>
 
-    <h2>Éditeur</h2>
+    <h2>Éditeur du site</h2>
     <div className={fr.cx("fr-mb-3w")}>
       <p className={fr.cx("fr-mb-0")}>
-        Le site est édité par la{" "}
-        <Link target="_blank" href="https://www.fabrique.social.gouv.fr/">
-          fabrique des Ministères sociaux
-        </Link>{" "}
+        Le site est édité par la Direction générale du Travail, au sein du
+        Ministère du Travail, de la Santé, des Solidarités et des Familles
         située&nbsp;:
       </p>
       <address>
-        Tour High Line
+        14 avenue Duquesne
         <br />
-        8 Rue François Ory
+        75007 PARIS
         <br />
-        92120 Montrouge
+        Tél.&nbsp;: 01 40 56 60 00
       </address>
     </div>
 
     <h2>Directeur de la publication</h2>
     <p>Pierre RAMAIN, Directeur Général du Travail</p>
 
-    <h2>Hébergement du site</h2>
+    <h2>Hébergement</h2>
     <div className={fr.cx("fr-mb-3w")}>
       <p>
         Ce site est hébergé par OVH SAS&nbsp;: <br />
@@ -40,26 +38,22 @@ export const MentionsLegales = () => (
       <p>
         <strong>
           Le code du logiciel est libre, et peut donc être vérifié et amélioré
-          par toutes et tous à l’adresse suivante:{" "}
-          <a href="https://github.com/SocialGouv/code-du-travail-numerique">
-            https://github.com/SocialGouv/code-du-travail-numerique
-          </a>
-          .
+          par toutes et tous à l&apos;adresse suivante.
         </strong>
       </p>
     </div>
 
     <h2>Accessibilité</h2>
     <p>
-      La conformité aux normes d’accessibilité numérique est un objectif
+      La conformité aux normes d&apos;accessibilité numérique est un objectif
       ultérieur mais nous tâchons de rendre dès la conception, ce site
       accessible à toutes et à tous.
     </p>
 
     <h3>Signaler un dysfonctionnement</h3>
     <p>
-      Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder à un
-      contenu ou une fonctionnalité du site,{" "}
+      Si vous rencontrez un défaut d&apos;accessibilité vous empêchant
+      d&apos;accéder à un contenu ou une fonctionnalité du site,{" "}
       <a
         title="Merci de nous en faire part en envoyant un mail à codedutravailnumerique@travail.gouv.fr"
         href="mailto:codedutravailnumerique@travail.gouv.fr"
@@ -68,17 +62,20 @@ export const MentionsLegales = () => (
       </a>
     </p>
     <p>
-      Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en droit
-      de faire parvenir vos doléances ou une demande de saisine au Défenseur des
-      droits.
+      Si vous n&apos;obtenez pas de réponse rapide de notre part, vous êtes en
+      droit de faire parvenir vos doléances ou une demande de saisine au
+      Défenseur des droits.
     </p>
 
     <h3>En savoir plus</h3>
     <p>
-      Pour en savoir plus sur la politique d’accessibilité numérique de
-      l’État&nbsp;:{" "}
-      <Link href="https://accessibilite.numerique.gouv.fr/" target="_blank">
-        https://accessibilite.numerique.gouv.fr
+      Pour en savoir plus sur la politique d&apos;accessibilité numérique de
+      l&apos;État&nbsp;:{" "}
+      <Link
+        href="http://references.modernisation.gouv.fr/accessibilite-numerique"
+        target="_blank"
+      >
+        http://references.modernisation.gouv.fr/accessibilite-numerique
       </Link>
     </p>
 
