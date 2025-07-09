3c1a66e6dc06b17c956a5b7a3af31817c549ad6a
Martial Maillot <martial.maillot@gmail.com>
2025-07-09 17:49:25 +0200

    fix: focus sur les formulaires de satisfaction

--- packages/code-du-travail-frontend/src/modules/outils/indemnite-depart/feedback/Questionnaire.tsx
+++ packages/code-du-travail-frontend/src/modules/outils/indemnite-depart/feedback/Questionnaire.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import { SmileyQuestionnaireItem } from "./SmileyQuestionnaireItem";
-import { useState } from "react";
+import React, { forwardRef, useState } from "react";
 import {
   EVENT_ACTION,
   EVENT_CATEGORY,
@@ -16,50 +16,63 @@ type QuestionnaireProps = {
   category: EVENT_CATEGORY;
 };
 
-export const Questionnaire = ({
-  onClick,
-  category,
-}: QuestionnaireProps): JSX.Element => {
-  const [status, setStatus] = useState<FEEDBACK_RESULT>();
-  const [displayError, setDisplayError] = useState(false);
-  const { trackFeedback } = useFeedbackEvents();
+export const Questionnaire = forwardRef<HTMLHeadingElement, QuestionnaireProps>(
+  ({ onClick, category }, ref): React.ReactElement => {
+    const [status, setStatus] = useState<FEEDBACK_RESULT>();
+    const [displayError, setDisplayError] = useState(false);
+    const { trackFeedback } = useFeedbackEvents();
 
-  return (
-    <form
-      onSubmit={(e) => {
-        if (!status) {
-          setDisplayError(true);
-          e.preventDefault();
-        } else {
-          trackFeedback(EVENT_ACTION.GLOBAL, status, category);
-          onClick();
-        }
-      }}
-    >
-      <fieldset>
-        <legend>
-          <h3 className={fr.cx("fr-text--lg", "fr-mb-2w", "fr-text--bold")}>
-            Comment s&apos;est passée cette simulation pour vous ?
-          </h3>
-        </legend>
-        <SmileyQuestionnaireItem
-          badEventValue={FEEDBACK_RESULT.NOT_GOOD}
-          averageEventValue={FEEDBACK_RESULT.AVERAGE}
-          goodEventValue={FEEDBACK_RESULT.GOOD}
-          onChange={(status: FEEDBACK_RESULT) => {
-            setStatus(status);
-            setDisplayError(false);
-          }}
-          displayError={displayError}
-        />
-      </fieldset>
-      <div
-        className={fr.cx("fr-btns-group", "fr-btns-group--inline", "fr-mt-2w")}
+    return (
+      <form
+        onSubmit={(e) => {
+          if (!status) {
+            setDisplayError(true);
+            e.preventDefault();
+          } else {
+            trackFeedback(EVENT_ACTION.GLOBAL, status, category);
+            onClick();
+          }
+        }}
       >
-        <Button priority="secondary" type="submit">
-          Envoyer
-        </Button>
-      </div>
-    </form>
-  );
-};
+        <fieldset>
+          <legend>
+            <h2
+              ref={ref}
+              tabIndex={-1}
+              className={fr.cx("fr-text--lg", "fr-mb-2w", "fr-text--bold")}
+            >
+              Comment s&apos;est passée cette simulation pour vous ?
+            </h2>
+          </legend>
+          <SmileyQuestionnaireItem
+            badEventValue={FEEDBACK_RESULT.NOT_GOOD}
+            averageEventValue={FEEDBACK_RESULT.AVERAGE}
+            goodEventValue={FEEDBACK_RESULT.GOOD}
+            onChange={(status: FEEDBACK_RESULT) => {
+              setStatus(status);
+              setDisplayError(false);
+            }}
+            displayError={displayError}
+          />
+        </fieldset>
+        <div
+          className={fr.cx(
+            "fr-btns-group",
+            "fr-btns-group--inline",
+            "fr-mt-2w"
+          )}
+        >
+          <Button
+            priority="secondary"
+            type="submit"
+            nativeButtonProps={{ "aria-disabled": displayError }}
+          >
+            Envoyer
+          </Button>
+        </div>
+      </form>
+    );
+  }
+);
+
+Questionnaire.displayName = "SatisfactionQuestionnaire";
