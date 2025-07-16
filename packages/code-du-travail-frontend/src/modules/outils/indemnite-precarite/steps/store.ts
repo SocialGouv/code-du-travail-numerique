e48c8a3fccb205d3c3cf84a34699053a0a2ac247
maxgfr <25312957+maxgfr@users.noreply.github.com>
2025-07-16 12:54:18 +0200

    fix: retour

--- packages/code-du-travail-frontend/src/modules/outils/indemnite-precarite/steps/store.ts
+++ packages/code-du-travail-frontend/src/modules/outils/indemnite-precarite/steps/store.ts
@@ -5,24 +5,13 @@ import {
   createInformationsStore,
   InformationsStoreSlice,
 } from "./Informations/store";
-<<<<<<<< HEAD:packages/code-du-travail-frontend/src/modules/outils/indemnite-precarite/steps/store.ts
 import {
   createRemunerationStore,
   RemunerationStoreSlice,
 } from "./Remuneration/store";
-|||||||| 0ed33d3ab:packages/code-du-travail-frontend/src/outils/PreavisRetraite/steps/store.ts
-import { createSeniorityStore, SeniorityStoreSlice } from "./Seniority/store";
-========
->>>>>>>> dev:packages/code-du-travail-frontend/src/modules/outils/preavis-demission/steps/store.ts
 import { createResultStore, ResultStoreSlice } from "./Result/store";
 
-<<<<<<<< HEAD:packages/code-du-travail-frontend/src/modules/outils/indemnite-precarite/steps/store.ts
 export type StoreSliceWrapperIndemnitePrecarite<
-|||||||| 0ed33d3ab:packages/code-du-travail-frontend/src/outils/PreavisRetraite/steps/store.ts
-export type StoreSliceWrapperPreavisRetraite<
-========
-export type StoreSliceWrapperPreavisDemission<
->>>>>>>> dev:packages/code-du-travail-frontend/src/modules/outils/preavis-demission/steps/store.ts
   T extends object,
   E extends object = T,
 > = (
@@ -32,12 +21,7 @@ export type StoreSliceWrapperPreavisDemission<
 
 export type MainStore = AgreementStoreSlice &
   InformationsStoreSlice &
-<<<<<<<< HEAD:packages/code-du-travail-frontend/src/modules/outils/indemnite-precarite/steps/store.ts
   RemunerationStoreSlice &
-|||||||| 0ed33d3ab:packages/code-du-travail-frontend/src/outils/PreavisRetraite/steps/store.ts
-  SeniorityStoreSlice &
-========
->>>>>>>> dev:packages/code-du-travail-frontend/src/modules/outils/preavis-demission/steps/store.ts
   ResultStoreSlice;
 
 export type StepData<T, U> = {
@@ -53,12 +37,7 @@ const createRootSlice = (
 ) => ({
   ...createAgreementStore(set, get),
   ...createInformationsStore(set, get),
-<<<<<<<< HEAD:packages/code-du-travail-frontend/src/modules/outils/indemnite-precarite/steps/store.ts
   ...createRemunerationStore(set, get),
-|||||||| 0ed33d3ab:packages/code-du-travail-frontend/src/outils/PreavisRetraite/steps/store.ts
-  ...createSeniorityStore(set, get),
-========
->>>>>>>> dev:packages/code-du-travail-frontend/src/modules/outils/preavis-demission/steps/store.ts
   ...createResultStore(set, get),
 });
 
@@ -70,39 +49,15 @@ const createStore = () =>
     ) => createRootSlice(set, get)
   );
 
-<<<<<<<< HEAD:packages/code-du-travail-frontend/src/modules/outils/indemnite-precarite/steps/store.ts
 const IndemnitePrecariteContext = createContext<StoreApi<MainStore>>(
-|||||||| 0ed33d3ab:packages/code-du-travail-frontend/src/outils/PreavisRetraite/steps/store.ts
-const PreavisRetraiteContext = createContext<StoreApi<MainStore>>(
-========
-const PreavisDemissionContext = createContext<StoreApi<MainStore>>(
->>>>>>>> dev:packages/code-du-travail-frontend/src/modules/outils/preavis-demission/steps/store.ts
   {} as StoreApi<MainStore>
 );
 
-<<<<<<<< HEAD:packages/code-du-travail-frontend/src/modules/outils/indemnite-precarite/steps/store.ts
 const { Provider } = IndemnitePrecariteContext;
-|||||||| 0ed33d3ab:packages/code-du-travail-frontend/src/outils/PreavisRetraite/steps/store.ts
-const { Provider } = PreavisRetraiteContext;
-========
-const { Provider } = PreavisDemissionContext;
->>>>>>>> dev:packages/code-du-travail-frontend/src/modules/outils/preavis-demission/steps/store.ts
 
 export {
-<<<<<<<< HEAD:packages/code-du-travail-frontend/src/modules/outils/indemnite-precarite/steps/store.ts
   Provider as IndemnitePrecariteProvider,
   createStore as createIndemnitePrecariteStore,
   IndemnitePrecariteContext,
   useStore as useIndemnitePrecariteStore,
-|||||||| 0ed33d3ab:packages/code-du-travail-frontend/src/outils/PreavisRetraite/steps/store.ts
-  Provider as PreavisRetraiteProvider,
-  createStore as createPreavisRetraiteStore,
-  PreavisRetraiteContext,
-  useStore as usePreavisRetraiteStore,
-========
-  Provider as PreavisDemissionProvider,
-  createStore as createPreavisDemissionStore,
-  PreavisDemissionContext,
-  useStore as usePreavisDemissionStore,
->>>>>>>> dev:packages/code-du-travail-frontend/src/modules/outils/preavis-demission/steps/store.ts
 };
