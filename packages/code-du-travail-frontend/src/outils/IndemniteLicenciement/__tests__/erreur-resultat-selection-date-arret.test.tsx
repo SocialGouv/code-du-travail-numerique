64d1ebb7d3190b7b3685832db5e2d782b9299958
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-08-04 15:26:23 +0200

    feat: add test case

---
+++ packages/code-du-travail-frontend/src/outils/IndemniteLicenciement/__tests__/erreur-resultat-selection-date-arret.test.tsx
@@ -0,0 +1,42 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { CalculateurIndemniteLicenciement } from "../../../../src/outils";
+import { ui } from "../../CommonIndemniteDepart/__tests__/ui";
+import { UserAction } from "../../../common";
+
+jest.mock("../../../conventions/Search/api/agreements.service");
+jest.mock("../../../conventions/Search/api/enterprises.service");
+
+describe("Page salaire: vérification l'affichage du résultat lorsqu'une date d'arrêt est sélectionnée", () => {
+  test("should show result", async () => {
+    const { getByText } = render(
+      <CalculateurIndemniteLicenciement
+        icon={""}
+        title={""}
+        displayTitle={""}
+      />
+    );
+    const userAction = new UserAction();
+    userAction.click(ui.introduction.startButton.get());
+    userAction.click(ui.contract.type.cdi.get());
+    userAction.click(ui.contract.fauteGrave.non.get());
+    userAction.click(ui.contract.inaptitude.non.get());
+    userAction.click(ui.contract.arretTravail.oui.get());
+    userAction.setInput(ui.contract.dateArretTravail.get(), "03/12/1992");
+    userAction.click(ui.next.get());
+    userAction.click(ui.agreement.noAgreement.get());
+    userAction.click(ui.next.get());
+    userAction.setInput(ui.seniority.startDate.get(), "01/12/1992");
+    userAction.setInput(ui.seniority.notificationDate.get(), "01/08/2025");
+    userAction.setInput(ui.seniority.endDate.get(), "01/09/2025");
+
+    userAction.click(ui.seniority.hasAbsence.non.get());
+    userAction.click(ui.next.get());
+    userAction.click(ui.salary.hasPartialTime.non.get());
+    userAction.click(ui.salary.hasSameSalary.oui.get());
+    userAction.setInput(ui.salary.sameSalaryValue.get(), "2668");
+    userAction.click(ui.next.get());
+    expect(ui.activeStep.query()).toHaveTextContent("Indemnité");
+    expect(ui.result.resultat.get()).toHaveTextContent("26 902,33");
+  });
+});
