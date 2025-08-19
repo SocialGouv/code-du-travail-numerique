611d9d7975ab9f16be55ec8e9aacb7ed4e15b2b8
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-08-19 16:31:32 +0200

    fix: mention legale

--- packages/code-du-travail-frontend/src/modules/mentions-legales/index.tsx
+++ packages/code-du-travail-frontend/src/modules/mentions-legales/index.tsx
@@ -50,47 +50,11 @@ export const MentionsLegales = () => (
       </p>
     </div>
 
-    <h2>Accessibilité</h2>
+    <h2>Traitement des données personnelles</h2>
     <p>
-      La conformité aux normes d&apos;accessibilité numérique est un objectif
-      ultérieur mais nous tâchons de rendre dès la conception, ce site
-      accessible à toutes et à tous.
-    </p>
-
-    <h3>Signaler un dysfonctionnement</h3>
-    <p>
-      Si vous rencontrez un défaut d&apos;accessibilité vous empêchant
-      d&apos;accéder à un contenu ou une fonctionnalité du site,{" "}
-      <a
-        title="Merci de nous en faire part en envoyant un mail à codedutravailnumerique@travail.gouv.fr"
-        href="mailto:codedutravailnumerique@travail.gouv.fr"
-      >
-        merci de nous en faire part.
-      </a>
-    </p>
-    <p>
-      Si vous n&apos;obtenez pas de réponse rapide de notre part, vous êtes en
-      droit de faire parvenir vos doléances ou une demande de saisine au
-      Défenseur des droits.
-    </p>
-
-    <h3>En savoir plus</h3>
-    <p>
-      Pour en savoir plus sur la politique d&apos;accessibilité numérique de
-      l&apos;État&nbsp;:{" "}
-      <Link
-        href="https://www.modernisation.gouv.fr/accessibilite"
-        target="_blank"
-      >
-        https://www.modernisation.gouv.fr/accessibilite
-      </Link>
-    </p>
-
-    <h2>Sécurité</h2>
-    <p>
-      Le site est protégé par un certificat électronique, matérialisé pour la
-      grande majorité des navigateurs par un cadenas. Cette protection participe
-      à la confidentialité des échanges.
+      Les informations relatives au traitement des données à caractère personnel
+      figurent dans la Politique de protection des données à caractère personnel
+      accessible à partir de la page d’accueil du site.
     </p>
   </Container>
 );
