352a741cd299c43ac1070934abfd0846804d2b07
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-09-04 17:21:33 +0200

    remove timer

--- packages/code-du-travail-frontend/src/modules/cookie-consent/ConsentManager.tsx
+++ packages/code-du-travail-frontend/src/modules/cookie-consent/ConsentManager.tsx
@@ -8,32 +8,28 @@ const CookieConsentDSFR = lazy(
     new Promise<{
       default: typeof import("./CookieConsent").CookieConsentDSFR;
     }>((resolve) =>
-      setTimeout(
-        () =>
-          import("./CookieConsent").then((module) =>
-            resolve({ default: module.CookieConsentDSFR })
-          ),
-        1000
+      import("./CookieConsent").then((module) =>
+        resolve({ default: module.CookieConsentDSFR })
       )
     )
 );
 
 export const ConsentManager = () => {
-  const [shouldRender, setShouldRender] = useState(false);
+  // const [shouldRender, setShouldRender] = useState(false);
 
   useEffect(() => {
     initConsent();
 
-    const timer = setTimeout(() => {
-      setShouldRender(true);
-    }, 1500);
+    // const timer = setTimeout(() => {
+    //   setShouldRender(true);
+    // }, 1500);
 
-    return () => clearTimeout(timer);
+    // return () => clearTimeout(timer);
   }, []);
 
-  if (!shouldRender) {
-    return null;
-  }
+  // if (!shouldRender) {
+  //   return null;
+  // }
 
   return (
     <Suspense fallback={null}>
