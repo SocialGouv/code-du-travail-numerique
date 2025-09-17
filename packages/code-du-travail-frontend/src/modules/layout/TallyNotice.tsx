6a44ebb9cbf29d930ea2c0bd0456b1f922b1602b
Victor <victor.zeinstra@gmail.com>
2025-09-17 14:42:44 +0200

    fix: review

--- packages/code-du-travail-frontend/src/modules/layout/TallyNotice.tsx
+++ packages/code-du-travail-frontend/src/modules/layout/TallyNotice.tsx
@@ -2,45 +2,52 @@
 
 import Script from "next/script";
 
-type TallyNoticeProps = {
-  onClose: () => void;
-  id: string;
-};
+type TallyNoticeProps = { onClose: () => void; id: string };
 
 export const TallyNotice = ({ onClose, id }: TallyNoticeProps) => {
   return (
     <>
       <Script id="tally-js" src="https://tally.so/widgets/embed.js" />
       <div className="fr-notice fr-notice--info">
-        <div className="fr-container">
-          <div className="fr-notice__content fr-grid-row fr-grid-row--middle">
-            <div className="fr-col-10 fr-grid-row fr-grid-row--middle">
-              <p className="fr-notice__title">
-                Donnez votre avis pour améliorer le site
-              </p>
-              <button
-                className="fr-btn fr-btn--primary fr-ml-md-2w"
-                data-tally-open={id}
-                data-tally-width="500"
-                data-tally-overlay="1"
-                data-tally-auto-resize="true"
-                aria-label="Ouvrir le formulaire d'avis"
-                aria-haspopup="dialog"
-                aria-expanded="false"
-              >
-                Répondre
-              </button>
-            </div>
+        <div
+          className="fr-container fr-notice__content fr-grid-row fr-grid-row--middle"
+          style={{ position: "relative" }}
+        >
+          <p className="fr-notice__title fr-col-12 fr-col-md-4 fr-my-1w">
+            Donnez votre avis pour améliorer le site
+          </p>
+          <button
+            className="fr-btn fr-btn--primary fr-ml-md-2w fr-col-12 fr-col-md-2 fr-text--middle"
+            data-tally-open={id}
+            data-tally-width="500"
+            data-tally-overlay="1"
+            data-tally-auto-resize="true"
+            aria-label="Ouvrir le formulaire d'avis"
+            aria-haspopup="dialog"
+            aria-expanded="false"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            Répondre
+          </button>
 
-            <button
-              className={`fr-btn--close fr-btn fr-col-md-2`}
-              title="Fermer la notice"
-              onClick={onClose}
-              aria-label="Fermer la notice"
-            >
-              ×
-            </button>
-          </div>
+          <button
+            className="fr-btn--close fr-btn fr-col-md-2"
+            title="Fermer la notice"
+            onClick={onClose}
+            aria-label="Fermer la notice"
+            style={{
+              position: "absolute",
+              top: "0.5rem",
+              right: "1rem",
+              zIndex: 10,
+            }}
+          >
+            ×
+          </button>
         </div>
       </div>
     </>
