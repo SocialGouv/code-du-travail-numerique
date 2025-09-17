0057ce041e946bbd1d6381d8625cd26640f53ba4
Victor <victor.zeinstra@gmail.com>
2025-09-17 17:16:45 +0200

    refacto

--- packages/code-du-travail-frontend/src/modules/layout/TallyNotice.tsx
+++ packages/code-du-travail-frontend/src/modules/layout/TallyNotice.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { fr } from "@codegouvfr/react-dsfr";
 import Button from "@codegouvfr/react-dsfr/Button";
 import Script from "next/script";
+import { css } from "@styled-system/css";
 
 type TallyNoticeProps = { onClose: () => void; id: string };
 
@@ -9,16 +11,36 @@ export const TallyNotice = ({ onClose, id }: TallyNoticeProps) => {
   return (
     <>
       <Script id="tally-js" src="https://tally.so/widgets/embed.js" />
-      <div className="fr-notice fr-notice--info">
+      <div className={fr.cx("fr-notice", "fr-notice--info")}>
         <div
-          className="fr-container fr-notice__content fr-grid-row fr-grid-row--middle"
-          style={{ position: "relative" }}
+          className={`${fr.cx(
+            "fr-container",
+            //@ts-ignore
+            "fr-notice__content",
+            "fr-grid-row",
+            "fr-grid-row--middle"
+          )} ${containerStyles}`}
         >
-          <p className="fr-notice__title fr-col-12 fr-col-md-4 fr-my-1w">
+          <p
+            className={fr.cx(
+              "fr-notice__title",
+              "fr-col-12",
+              "fr-col-md-4",
+              "fr-my-1w"
+            )}
+          >
             Donnez votre avis pour améliorer le site
           </p>
           <button
-            className="fr-btn fr-btn--primary fr-ml-md-2w fr-col-12 fr-col-md-2 fr-text--middle"
+            className={`${fr.cx(
+              "fr-btn",
+              //@ts-ignore
+              "fr-btn--primary",
+              "fr-ml-md-2w",
+              "fr-col-12",
+              "fr-col-md-2",
+              "fr-text--middle"
+            )} ${respondButtonStyles}`}
             data-tally-open={id}
             data-tally-width="500"
             data-tally-overlay="1"
@@ -26,11 +48,6 @@ export const TallyNotice = ({ onClose, id }: TallyNoticeProps) => {
             aria-label="Ouvrir le formulaire d'avis"
             aria-haspopup="dialog"
             aria-expanded="false"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
           >
             Répondre
           </button>
@@ -40,15 +57,25 @@ export const TallyNotice = ({ onClose, id }: TallyNoticeProps) => {
             onClick={onClose}
             priority="tertiary no outline"
             title="Fermer la notice"
-            style={{
-              position: "absolute",
-              top: "0rem",
-              right: "1rem",
-              zIndex: 10,
-            }}
+            className={closeButtonStyles}
           />
         </div>
       </div>
     </>
   );
 };
+
+const containerStyles = css({ position: "relative" });
+
+const respondButtonStyles = css({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const closeButtonStyles = css({
+  position: "absolute",
+  top: "0rem",
+  right: "1rem",
+  zIndex: 10,
+});
