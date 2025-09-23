c825f5f3f792fb0c42c596049e3433975c792db4
maxgfr <25312957+maxgfr@users.noreply.github.com>
2025-09-23 11:01:08 +0200

    fix: synchro

--- packages/code-du-travail-frontend/src/modules/common/DisplayContent.tsx
+++ packages/code-du-travail-frontend/src/modules/common/DisplayContent.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import parse, {
   DOMNode,
   domToReact,
--- packages/code-du-travail-frontend/src/modules/fiche-service-public/builder/components/Accordion.tsx
+++ packages/code-du-travail-frontend/src/modules/fiche-service-public/builder/components/Accordion.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { filterOutTitle, getTitleInChildren } from "../utils";
 import { ElementBuilder } from "./ElementBuilder";
