db0de73cac112a39a2dca7dfb60c0d565f689e47
maxgfr <25312957+maxgfr@users.noreply.github.com>
2025-10-01 20:07:36 +0200

    fix: merge

--- packages/code-du-travail-frontend/src/modules/config/MatomoAnalytics.tsx
+++ packages/code-du-travail-frontend/src/modules/config/MatomoAnalytics.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname, useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect } from "react";
 import { PIWIK_SITE_ID, PIWIK_URL, WIDGETS_PATH } from "../../config";
 import { getStoredConsent } from "../utils/consent";
 import init, { push } from "@socialgouv/matomo-next";
@@ -16,90 +16,40 @@ function MatomoComponent({
   hasHeartBeatTimerEnabled,
 }: MatomoComponentProps) {
   const searchParams = useSearchParams();
-  const searchParamsString = searchParams?.toString();
-  const path = usePathname();
-  const [previousPath, setPreviousPath] = useState<string | null>(null);
+  const pathname = usePathname();
 
   useEffect(() => {
+    const consent = getStoredConsent();
+
     init({
       siteId: PIWIK_SITE_ID,
       url: PIWIK_URL,
+      pathname,
+      excludeUrlsPatterns: [WIDGETS_PATH],
+      enableHeatmapSessionRecording:
+        consent.matomoHeatmap && hasMatomoHeatmapEnabled,
+      heatmapConfig: {
+        captureKeystrokes: false,
+        captureVisibleContentOnly: false,
+        debug: true,
+      },
+      enableHeartBeatTimer: consent.matomo && hasHeartBeatTimerEnabled,
       onInitialization: () => {
         const referrerUrl = document?.referrer || searchParams.get("src_url");
         if (referrerUrl) {
           push(["setReferrerUrl", referrerUrl]);
         }
-        if (path && path.match(WIDGETS_PATH)) {
+        if (pathname && pathname.match(WIDGETS_PATH)) {
           push(["setCookieSameSite", "None"]);
         }
       },
-      excludeUrlsPatterns: [WIDGETS_PATH],
     });
-
-    const consent = getStoredConsent();
-    console.log("Consent for Matomo Heatmap:", consent.matomoHeatmap);
-
-    if (consent.matomoHeatmap && hasMatomoHeatmapEnabled) {
-      // Charger tracker.min.js explicitement
-      const script = document.createElement("script");
-      script.src = `${PIWIK_URL}/plugins/HeatmapSessionRecording/tracker.min.js`;
-      script.async = true;
-      script.onload = () => {
-        console.log("HeatmapSessionRecording tracker.min.js loaded");
-        // Ajouter un espion pour vérifier les requêtes
-        push(["HeatmapSessionRecording::debug", "true"]);
-      };
-      script.onerror = () => {
-        console.error("Failed to load HeatmapSessionRecording tracker.min.js");
-      };
-      document.head.appendChild(script);
-
-      const handleLoad = () => {
-        console.log("Activating Matomo Heatmap in DSFR version");
-        push(["HeatmapSessionRecording.setKeystrokes", "false"]);
-        console.log("Keystrokes disabled");
-        push(["HeatmapSessionRecording.setCaptureVisibleContentOnly", "false"]);
-        console.log("Capture full page enabled");
-        push(["HeatmapSessionRecording::enable"]);
-        console.log(
-          "HeatmapSessionRecording enabled at",
-          new Date().toISOString()
-        );
-      };
-      window.addEventListener("load", handleLoad);
-      return () => {
-        window.removeEventListener("load", handleLoad);
-        document.head.removeChild(script);
-      };
-    }
-
-    if (consent.matomo && hasHeartBeatTimerEnabled) {
-      push(["enableHeartBeatTimer"]);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (path && !isExcludedUrl(path, [WIDGETS_PATH])) {
-      let [pathname] = path.split("?");
-      pathname = pathname.replace(/#.*/, "");
-
-      if (previousPath) {
-        push(["setReferrerUrl", `${previousPath}`]);
-      }
-
-      push(["setCustomUrl", pathname]);
-      push(["deleteCustomVariables", "page"]);
-      setPreviousPath(pathname);
-
-      const query = searchParams?.get("q");
-      push(["setDocumentTitle", document.title]);
-      if (startsWith(path, "/recherche") || startsWith(path, "/search")) {
-        push(["trackSiteSearch", query ?? ""]);
-      } else {
-        push(["trackPageView"]);
-      }
-    }
-  }, [path, searchParamsString]);
+  }, [
+    pathname,
+    searchParams,
+    hasMatomoHeatmapEnabled,
+    hasHeartBeatTimerEnabled,
+  ]);
 
   return null;
 }
@@ -118,17 +68,3 @@ export const MatomoAnalytics = ({
     />
   </Suspense>
 );
-
-const startsWith = (str: string, needle: string) => {
-  return str.substring(0, needle.length) === needle;
-};
-
-const isExcludedUrl = (url: string, patterns: RegExp[]): boolean => {
-  let excluded = false;
-  patterns.forEach((pattern) => {
-    if (pattern.exec(url) !== null) {
-      excluded = true;
-    }
-  });
-  return excluded;
-};
