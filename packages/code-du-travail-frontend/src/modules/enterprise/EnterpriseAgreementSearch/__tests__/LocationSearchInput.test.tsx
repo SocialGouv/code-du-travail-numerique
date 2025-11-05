f3da42cdebbb2646a1175d55f944b2ddd7ad0d45
maxgfr <25312957+maxgfr@users.noreply.github.com>
2025-11-05 16:02:16 +0100

    fix: v1

--- packages/code-du-travail-frontend/src/modules/enterprise/EnterpriseAgreementSearch/__tests__/LocationSearchInput.test.tsx
+++ packages/code-du-travail-frontend/src/modules/enterprise/EnterpriseAgreementSearch/__tests__/LocationSearchInput.test.tsx
@@ -52,12 +52,13 @@ describe("LocationSearchInput", () => {
     );
     userAction = new UserAction();
     userAction.setInput(ui.input.get(), "paris");
-    await waitFor(() => {
-      expect(ui.AutocompleteItemParis.query()).toBeInTheDocument();
-      expect(ui.input.get()).toHaveValue("paris");
-      userAction.click(ui.inputCloseBtn.get());
-      expect(ui.input.get()).toHaveValue("");
-      expect(ui.AutocompleteItemParis.query()).not.toBeInTheDocument();
-    });
+    userAction.setInput(ui.input.get(), "paris");
+
+    const parisItem = await ui.AutocompleteItemParis.find(); // remplace waitFor
+    expect(parisItem).toBeInTheDocument();
+
+    userAction.click(ui.inputCloseBtn.get());
+    expect(ui.input.get()).toHaveValue("");
+    expect(ui.AutocompleteItemParis.query()).not.toBeInTheDocument();
   });
 });
