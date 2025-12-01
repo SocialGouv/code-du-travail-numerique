757f9cdf76bd765f7cf7dfa8735d63009590222a
Martial Maillot <martial.maillot@gmail.com>
2025-12-01 10:01:16 +0100

    feat(theme): partage des icones des thèmes avec l'admin (#6953)

--- packages/code-du-travail-frontend/src/modules/themes/HomeThemeTile.tsx
+++ packages/code-du-travail-frontend/src/modules/themes/HomeThemeTile.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
 import Link from "../common/Link";
+import { css } from "@styled-system/css";
+import { fr } from "@codegouvfr/react-dsfr";
+import { ThemeIcon } from "../common";
 
 type Props = {
   title: string;
@@ -8,18 +10,25 @@ type Props = {
 };
 
 export const HomeThemeTile = (props: Props) => (
-  <div className="fr-tile fr-enlarge-link fr-tile--no-icon fr-tile--sm">
-    <p className="fr-tile__title">
+  <div
+    className={fr.cx(
+      "fr-tile",
+      "fr-enlarge-link",
+      "fr-tile--no-icon",
+      "fr-tile--sm"
+    )}
+  >
+    <p className={fr.cx("fr-tile__title")}>
       <Link href={props.link}>{props.title}</Link>
     </p>
 
-    <div className="fr-tile__img">
-      <Image
-        src={`/static/assets/icons/themes/${props.iconName}.svg`}
-        alt=""
-        width={56}
-        height={56}
-      />
+    <div className={`${fr.cx("fr-tile__img")} ${frTileImgSmall}`}>
+      <ThemeIcon name={props.iconName} />
     </div>
   </div>
 );
+
+const frTileImgSmall = css({
+  width: "56px !important",
+  height: "56px !important",
+});
