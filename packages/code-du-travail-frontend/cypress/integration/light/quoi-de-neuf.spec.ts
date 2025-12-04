d77f08cba18da451be4ce1b7cb24b99ea55462c4
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-12-04 10:58:31 +0100

    fix: e2e

--- packages/code-du-travail-frontend/cypress/integration/light/quoi-de-neuf.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/light/quoi-de-neuf.spec.ts
@@ -4,12 +4,14 @@ describe("Pages quoi de neuf ?", () => {
     cy.isIndexable();
     cy.canonicalUrlEqual("/quoi-de-neuf");
     cy.titleAndMetaDescriptionEqual(
-      "Quoi de neuf",
+      "Quoi de neuf - Code du travail numérique",
       "Consultez les dernières évolutions et mises à jour du Code du travail numérique."
     );
-    cy.findByRole("heading", { level: 1 }).should(
-      "have.text",
-      "Quoi de neuf sur le code du travail ?"
-    );
+    cy.findByRole("heading", { level: 1 })
+      .invoke("text")
+      .then((text) => {
+        const normalized = text.replace(/\u00A0/g, " ");
+        expect(normalized).to.eq("Quoi de neuf sur le code du travail ?");
+      });
   });
 });
