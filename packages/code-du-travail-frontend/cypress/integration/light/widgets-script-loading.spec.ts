45c7c297b7a2428d0c8a5edd60d8bbbcaf0c275e
maxgfr <25312957+maxgfr@users.noreply.github.com>
2026-02-04 09:58:37 +0100

    fix: widget

--- packages/code-du-travail-frontend/cypress/integration/light/widgets-script-loading.spec.ts
+++ packages/code-du-travail-frontend/cypress/integration/light/widgets-script-loading.spec.ts
@@ -21,7 +21,7 @@ describe("Widgets - Chargement via script widget-loader.js", () => {
     cy.visit(`http://localhost:${PORT}`);
 
     // Attendre que le script widget-loader.js soit chargé et que les iframes soient créées
-    cy.wait(5000);
+    cy.wait(2000);
 
     // Vérifier que les iframes ont été créées pour chaque widget
     const widgets = [
