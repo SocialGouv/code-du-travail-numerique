e1f8c1dc5628d9305afa1802e50b279b9ec94cc8
maxgfr <25312957+maxgfr@users.noreply.github.com>
2026-02-27 19:04:53 +0100

    fix: retours

--- packages/code-du-travail-frontend/cypress/support/errors.ts
+++ packages/code-du-travail-frontend/cypress/support/errors.ts
@@ -3,12 +3,12 @@ Cypress.on("uncaught:exception", (err) => {
   // Cypress and React Hydrating the document don't get along
   // for some unknown reason. Hopefully, we figure out why eventually
   // so we can remove this.
-  return false;
   if (
     /hydrat/i.test(err.message) ||
     /Wrong assertion encountered/i.test(err.message) ||
     /Minified React error #418/.test(err.message) ||
-    /Minified React error #423/.test(err.message)
+    /Minified React error #423/.test(err.message) ||
+    /ResizeObserver loop/i.test(err.message)
   ) {
     return false;
   }
