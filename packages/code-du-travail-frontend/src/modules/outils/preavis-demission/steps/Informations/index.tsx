d8dbfc4e3bcc82130bf1eacbe58847afc9521acd
Martial Maillot <martial.maillot@gmail.com>
2026-04-07 16:19:27 +0200

    feat(outils): ajout d'une illustration du bulletin de paie sur le préavis de démission (#7210)

--- packages/code-du-travail-frontend/src/modules/outils/preavis-demission/steps/Informations/index.tsx
+++ packages/code-du-travail-frontend/src/modules/outils/preavis-demission/steps/Informations/index.tsx
@@ -3,6 +3,7 @@ import { PreavisDemissionContext, usePreavisDemissionStore } from "../store";
 import { fr } from "@codegouvfr/react-dsfr";
 import { PubliQuestion } from "src/modules/outils/indemnite-depart/steps/Informations/components/PubliQuestion";
 import { AccessibleAlert } from "src/modules/outils/common/components/AccessibleAlert";
+import { BulletinDePaie } from "./components";
 
 const InformationsStepComponent = () => {
   const store = useContext(PreavisDemissionContext);
@@ -28,52 +29,61 @@ const InformationsStepComponent = () => {
   }, [agreement, generatePublicodesQuestions]);
 
   return (
-    <>
-      {informations.map((info, index) => {
-        return (
-          <PubliQuestion
-            key={info.id}
-            name={"infos-" + info.question.name}
-            rule={info.question.rule}
-            value={info.info}
-            onChange={(v: any) => {
-              onInformationsChange(
-                info.question.rule.nom,
-                v,
-                info.question.rule.cdtn?.type
-              );
-            }}
-            error={
-              errors.errorInformations[info.question.rule.nom] ?? undefined
-            }
-            autoFocus={index === 0}
+    <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
+      <div className={fr.cx("fr-col-lg-6", "fr-col-12")}>
+        {informations.map((info, index) => {
+          return (
+            <PubliQuestion
+              key={info.id}
+              name={"infos-" + info.question.name}
+              rule={info.question.rule}
+              value={info.info}
+              onChange={(v: any) => {
+                onInformationsChange(
+                  info.question.rule.nom,
+                  v,
+                  info.question.rule.cdtn?.type
+                );
+              }}
+              error={
+                errors.errorInformations[info.question.rule.nom] ?? undefined
+              }
+              autoFocus={index === 0}
+            />
+          );
+        })}
+        {informations.length === 0 && (
+          <p className={fr.cx("fr-mt-2w")}>
+            Aucune information à renseigner. Vous pouvez passer à l&apos;étape
+            suivante.
+          </p>
+        )}
+        {errors.errorPublicodes && (
+          <AccessibleAlert
+            title="Attention"
+            severity="error"
+            description={errors.errorPublicodes}
+            className={["fr-mt-2w"]}
           />
-        );
-      })}
-      {informations.length === 0 && (
-        <p className={fr.cx("fr-mt-2w")}>
-          Aucune information à renseigner. Vous pouvez passer à l&apos;étape
-          suivante.
+        )}
+        {errors.errorNote && (
+          <AccessibleAlert
+            title="À noter"
+            severity="info"
+            description={errors.errorNote}
+            data-testid="alert-note"
+            className={["fr-mt-2w"]}
+          />
+        )}
+      </div>
+      <div className={fr.cx("fr-col-lg-6", "fr-col-12")}>
+        <p className={fr.cx("fr-mb-3w", "fr-text--md")}>
+          Ces informations sont généralement disponible sur le bulletin de paie,
+          dans la partie haute précisant les informations du salarié.
         </p>
-      )}
-      {errors.errorPublicodes && (
-        <AccessibleAlert
-          title="Attention"
-          severity="error"
-          description={errors.errorPublicodes}
-          className={["fr-mt-2w"]}
-        />
-      )}
-      {errors.errorNote && (
-        <AccessibleAlert
-          title="À noter"
-          severity="info"
-          description={errors.errorNote}
-          data-testid="alert-note"
-          className={["fr-mt-2w"]}
-        />
-      )}
-    </>
+        <BulletinDePaie />
+      </div>
+    </div>
   );
 };
 
