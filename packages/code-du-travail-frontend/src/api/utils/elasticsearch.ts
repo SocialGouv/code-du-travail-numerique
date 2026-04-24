067cb32971d51199a3ac8476b3fde1f71c324d77
Martial Maillot <martial.maillot@gmail.com>
2026-04-24 16:37:32 +0200

    feat: migration vers une instance interne d'ES

--- packages/code-du-travail-frontend/src/api/utils/elasticsearch.ts
+++ packages/code-du-travail-frontend/src/api/utils/elasticsearch.ts
@@ -4,6 +4,8 @@ import { Client } from "@elastic/elasticsearch";
 const ELASTICSEARCH_URL =
   process.env.ELASTICSEARCH_URL ?? "http://localhost:9200";
 const ELASTICSEARCH_TOKEN_API = process.env.ELASTICSEARCH_TOKEN_API;
+const ELASTICSEARCH_USER = process.env.ELASTICSEARCH_USER;
+const ELASTICSEARCH_PASSWORD = process.env.ELASTICSEARCH_PASSWORD;
 const ES_INDEX_PREFIX = process.env.NEXT_PUBLIC_ES_INDEX_PREFIX ?? "cdtn";
 const CDTN_ADMIN_VERSION = process.env.CDTN_ADMIN_VERSION ?? "v2";
 const BRANCH_NAME_SLUG = process.env.NEXT_PUBLIC_BRANCH_NAME_SLUG;
@@ -11,6 +13,13 @@ const BRANCH_NAME_SLUG = process.env.NEXT_PUBLIC_BRANCH_NAME_SLUG;
 const esClientConfig: ClientOptions = {
   node: ELASTICSEARCH_URL,
 };
+
+if (ELASTICSEARCH_USER && ELASTICSEARCH_PASSWORD) {
+  esClientConfig.auth = {
+    username: ELASTICSEARCH_USER,
+    password: ELASTICSEARCH_PASSWORD,
+  };
+}
 
 if (ELASTICSEARCH_TOKEN_API) {
   esClientConfig.auth = { apiKey: ELASTICSEARCH_TOKEN_API };
